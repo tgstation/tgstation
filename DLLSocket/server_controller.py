4cfbf31ab5c392e805b2407b2d053cab536a863b
cib <cib123@googlemail.com>
2012-06-22 13:58:30 -0700

    Rewrote server_controller.py functionality

--- DLLSocket/server_controller.py
+++ DLLSocket/server_controller.py
@@ -1,14 +1,33 @@
-import SocketServer
+import subprocess
+import socket
+import urlparse
 
-class UDPHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print "{} wrote:".format(self.client_address[0])
-        print data
-        socket.sendto(data.upper(), self.client_address)
+UDP_IP="127.0.0.1"
+UDP_PORT=8019
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 8019
-    server = SocketServer.UDPServer((HOST, PORT), UDPHandler)
-    server.serve_forever()
+sock = socket.socket( socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM ) # UDP
+sock.bind( (UDP_IP,UDP_PORT) )
+
+def handle_message(data, addr):
+    params = urlparse.parse_qs(data)
+    print(data)
+    
+    try:
+        if params["type"][0] == "log" and str(params["log"][0]) and str(params["message"][0]):
+            open(params["log"][0],"a+").write(params["message"][0]+"\n")
+    except IOError:
+        pass
+    except KeyError:
+        pass
+
+while True:
+    try:
+        data, addr = sock.recvfrom( 1024 ) # buffer size is 1024 bytes
+        handle_message(data,addr)
+    except socket.timeout:
+        print("No response in 120 seconds.. Trying to reboot server.")
+        subprocess.call("./restart")
+    
+    # start expecting a message after the first timeout
+    sock.settimeout(120)
