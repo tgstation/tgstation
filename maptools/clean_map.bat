610860bfab4b00185f812d57b04fcea689f05962
cib <blub@blub-Latitude-D430.(none)>
2012-11-11 13:14:34 +0100

    A few bug fixes to the .bat files.

--- maptools/clean_map.bat
+++ maptools/clean_map.bat
@@ -1,1 +1,2 @@
-java -jar MapPatcher.jar ../maps/tgstation.2.0.9.1.dmm.backup ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm
+java -jar MapPatcher.jar -clean ../maps/tgstation.2.0.9.1.dmm.backup ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm
+del ../maps/tgstation.2.0.9.1.dmm.backup
--- maptools/prepare_map.bat
+++ maptools/prepare_map.bat
@@ -1,1 +1,2 @@
-cp ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm.backup
+cd ../maps
+copy tgstation.2.0.9.1.dmm tgstation.2.0.9.1.dmm.backup
