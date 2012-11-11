9cf5266e10ce0f396d69ea30b839703256c7feea
cib <blub@blub-Latitude-D430.(none)>
2012-11-11 13:04:29 +0100

    Moved map tools into maptools directory.
    Also added .bat files to make map cleaning easier.

---
+++ maptools/clean_map.bat
@@ -0,0 +1,1 @@
+java -jar MapPatcher.jar ../maps/tgstation.2.0.9.1.dmm.backup ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm
---
+++ maptools/prepare_map.bat
@@ -0,0 +1,1 @@
+cp ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm.backup
