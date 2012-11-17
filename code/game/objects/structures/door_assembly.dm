tgstation/tgstation

commit 0258d5be22215667090085b461175e2c681ce999

-Changed all minerals path from /obj/item/stack/sheet to /obj/item/stack/sheet/mineral. (This change excludes glass, Rglass, metal and plasteel) -Reduced the chunks of code about minerals in door_assembly.dm
Preeeetty much this commit is just to update all the files to the new path of the minerals. From here, if I can, I'll start changing one by one to remove the huge chunks of code.

git-svn-id: http://tgstation13.googlecode.com/svn/trunk@5087 316c924e-a436-60f5-8080-3fe189b3f50e

code/game/objects/structures/door_assembly.dm
@@ -290,30 +290,13 @@ obj/structure/door_assembly
 				if(!src || !WT.isOn()) return
 				user << "\blue You dissasembled the airlock assembly!"
 				new /obj/item/stack/sheet/metal(get_turf(src), 4)
-				switch(mineral)
-					if("glass")
+				if (mineral)
+					if (mineral == "glass")
 						new /obj/item/stack/sheet/rglass(get_turf(src))
-					if("gold")
-						new /obj/item/stack/sheet/gold(get_turf(src))
-						new /obj/item/stack/sheet/gold(get_turf(src))
-					if("silver")
-						new /obj/item/stack/sheet/silver(get_turf(src))
-						new /obj/item/stack/sheet/silver(get_turf(src))
-					if("diamond")
-						new /obj/item/stack/sheet/diamond(get_turf(src))
-						new /obj/item/stack/sheet/diamond(get_turf(src))
-					if("uranium")
-						new /obj/item/stack/sheet/uranium(get_turf(src))
-						new /obj/item/stack/sheet/uranium(get_turf(src))
-					if("plasma")
-						new /obj/item/stack/sheet/plasma(get_turf(src))
-						new /obj/item/stack/sheet/plasma(get_turf(src))
-					if("clown")
-						new /obj/item/stack/sheet/clown(get_turf(src))
-						new /obj/item/stack/sheet/clown(get_turf(src))
-					if("sandstone")
-						new /obj/item/stack/sheet/sandstone(get_turf(src))
-						new /obj/item/stack/sheet/sandstone(get_turf(src))
+					else
+						var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
+						new M(get_turf(src))
+						new M(get_turf(src))
 				del(src)
 		else
 			user << "\blue You need more welding fuel to dissassemble the airlock assembly."
@@ -396,95 +379,27 @@ obj/structure/door_assembly
 		var/obj/item/stack/sheet/G = W
 		if(G)
 			if(G.amount>=1)
-				switch(G.type)
-					if(/obj/item/stack/sheet/rglass)
+				if(G.type == /obj/item/stack/sheet/rglass)
+					playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
+					user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
+					if(do_after(user, 40))
+						user << "\blue You installed reinforced glass windows into the airlock assembly!"
+						G.use(1)
+						src.mineral = "glass"
+						src.name = "Near finished Window Airlock Assembly"
+						src.airlock_type = /obj/machinery/door/airlock/glass
+						src.base_icon_state = "door_as_g" //this will be applied to the icon_state with the correct state number at the proc's end.
+				else if(istype(G, /obj/item/stack/sheet/mineral))
+					if(G.amount>=2)
 						playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
 						user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
 						if(do_after(user, 40))
-							user << "\blue You installed reinforced glass windows into the airlock assembly!"
-							G.use(1)
-							src.mineral = "glass"
-							src.name = "Near finished Window Airlock Assembly"
-							src.airlock_type = /obj/machinery/door/airlock/glass
-							src.base_icon_state = "door_as_g" //this will be applied to the icon_state with the correct state number at the proc's end.
-					if(/obj/item/stack/sheet/gold)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed gold plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "gold"
-								src.name = "Near finished Gold Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/gold
-								src.base_icon_state = "door_as_gold"
-					if(/obj/item/stack/sheet/silver)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed silver plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "silver"
-								src.name = "Near finished Silver Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/silver
-								src.base_icon_state = "door_as_silver"
-					if(/obj/item/stack/sheet/diamond)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed diamond plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "diamond"
-								src.name = "Near finished Diamond Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/diamond
-								src.base_icon_state = "door_as_diamond"
-					if(/obj/item/stack/sheet/uranium)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed uranium plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "uranium"
-								src.name = "Near finished Uranium Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/uranium
-								src.base_icon_state = "door_as_uranium"
-					if(/obj/item/stack/sheet/plasma)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed plasma plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "plasma"
-								src.name = "Near finished Plasma Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/plasma
-								src.base_icon_state = "door_as_plasma"
-					if(/obj/item/stack/sheet/clown)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed bananium plating into the airlock assembly!HONK"
-								G.use(2)
-								playsound(src.loc, 'sound/items/bikehorn.ogg', 15, 1, -3)
-								src.mineral = "clown"
-								src.name = "Near finished Bananium Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/clown
-								src.base_icon_state = "door_as_clown"
-					if(/obj/item/stack/sheet/sandstone)
-						if(G.amount>=2)
-							playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
-							user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
-							if(do_after(user, 40))
-								user << "\blue You installed sandstone plating into the airlock assembly!"
-								G.use(2)
-								src.mineral = "sandstone"
-								src.name = "Near finished Sandstone Airlock Assembly"
-								src.airlock_type = /obj/machinery/door/airlock/sandstone
-								src.base_icon_state = "door_as_sandstone"
+							user << "\blue You installed [G.sheettype] plating into the airlock assembly!"
+							G.use(2)
+							src.mineral = "[G.sheettype]"
+							src.name = "Near finished [G.sheettype] Airlock Assembly"
+							src.airlock_type = text2path ("/obj/machinery/door/airlock/[G.sheettype]")
+							src.base_icon_state = "door_as_[G.sheettype]"
 
 	else if(istype(W, /obj/item/weapon/screwdriver) && state == 2 )
 		playsound(src.loc, 'sound/items/Screwdriver.ogg', 100, 1)
@@ -494,33 +409,9 @@ obj/structure/door_assembly
 			if(!src) return
 			user << "\blue You finish the airlock!"
 			var/obj/machinery/door/airlock/door
-			switch(mineral)
-				if("glass")
-					airlock_type = /obj/machinery/door/airlock/glass
-					door = new src.airlock_type( src.loc )
-				if("gold")
-					airlock_type = /obj/machinery/door/airlock/gold
-					door = new src.airlock_type( src.loc )
-				if("silver")
-					airlock_type = /obj/machinery/door/airlock/silver
-					door = new src.airlock_type( src.loc )
-				if("diamond")
-					airlock_type = /obj/machinery/door/airlock/diamond
-					door = new src.airlock_type( src.loc )
-				if("uranium")
-					airlock_type = /obj/machinery/door/airlock/uranium
-					door = new src.airlock_type( src.loc )
-				if("plasma")
-					airlock_type = /obj/machinery/door/airlock/plasma
-					door = new src.airlock_type( src.loc )
-				if("clown")
-					airlock_type = /obj/machinery/door/airlock/clown
-					door = new src.airlock_type( src.loc )
-				if("sandstone")
-					airlock_type = /obj/machinery/door/airlock/sandstone
-					door = new src.airlock_type( src.loc )
-				else
-					door = new src.airlock_type( src.loc )
+			if (mineral)
+				airlock_type = text2path("/obj/machinery/door/airlock/[mineral]")
+			door = new src.airlock_type( src.loc )
 			//door.req_access = src.req_access
 			door.electronics = src.electronics
 			door.req_access = src.electronics.conf_access

code/game/turfs/simulated/walls.dm
@@ -35,11 +35,11 @@
 		if(!devastated)
 			playsound(src.loc, 'sound/items/Welder.ogg', 100, 1)
 			new /obj/structure/girder(src)
-			var/M = text2path("/obj/item/stack/sheet/[mineral]")
+			var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
 			new M( src )
 			new M( src )
 		else
-			var/M = text2path("/obj/item/stack/sheet/[mineral]")
+			var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
 			new M( src )
 			new M( src )
 			new /obj/item/stack/sheet/metal( src )

code/defines/obj/storage.dm
@@ -520,23 +520,23 @@
 
 	//Plasma (rare ore)
 	if(pickednum >= 15)
-		new /obj/item/stack/sheet/plasma(src, rand(rare_min, rare_max))
+		new /obj/item/stack/sheet/mineral/plasma(src, rand(rare_min, rare_max))
 
 	//Silver (rare ore)
 	if(pickednum >= 20)
-		new /obj/item/stack/sheet/silver(src, rand(rare_min, rare_max))
+		new /obj/item/stack/sheet/mineral/silver(src, rand(rare_min, rare_max))
 
 	//Gold (rare ore)
 	if(pickednum >= 30)
-		new /obj/item/stack/sheet/gold(src, rand(rare_min, rare_max))
+		new /obj/item/stack/sheet/mineral/gold(src, rand(rare_min, rare_max))
 
 	//Uranium (rare ore)
 	if(pickednum >= 40)
-		new /obj/item/stack/sheet/uranium(src, rand(rare_min, rare_max))
+		new /obj/item/stack/sheet/mineral/uranium(src, rand(rare_min, rare_max))
 
 	//Diamond (rare HONK)
 	if(pickednum >= 45)
-		new /obj/item/stack/sheet/diamond(src, rand(rare_min, rare_max))
+		new /obj/item/stack/sheet/mineral/diamond(src, rand(rare_min, rare_max))
 
 	//Jetpack (You hit the jackpot!)
 	if(pickednum == 50)
@@ -554,12 +554,12 @@
 
 	/obj/item/stack/sheet/metal,
 	/obj/item/stack/sheet/glass,
-	/obj/item/stack/sheet/gold,
-	/obj/item/stack/sheet/silver,
-	/obj/item/stack/sheet/plasma,
-	/obj/item/stack/sheet/uranium,
-	/obj/item/stack/sheet/diamond,
-	/obj/item/stack/sheet/clown,
+	/obj/item/stack/sheet/mineral/gold,
+	/obj/item/stack/sheet/mineral/silver,
+	/obj/item/stack/sheet/mineral/plasma,
+	/obj/item/stack/sheet/mineral/uranium,
+	/obj/item/stack/sheet/mineral/diamond,
+	/obj/item/stack/sheet/mineral/clown,
 	/obj/item/stack/sheet/plasteel,
 	/obj/item/stack/rods
 

code/game/gamemodes/objective.dm
@@ -285,9 +285,9 @@ datum/objective/steal
 		"diamond drill" = /obj/item/weapon/pickaxe/diamonddrill,
 		"bag of holding" = /obj/item/weapon/storage/backpack/holding,
 		"hyper-capacity cell" = /obj/item/weapon/cell/hyper,
-		"10 diamonds" = /obj/item/stack/sheet/diamond,
-		"50 gold bars" = /obj/item/stack/sheet/gold,
-		"25 refined uranium bars" = /obj/item/stack/sheet/uranium,
+		"10 diamonds" = /obj/item/stack/sheet/mineral/diamond,
+		"50 gold bars" = /obj/item/stack/sheet/mineral/gold,
+		"25 refined uranium bars" = /obj/item/stack/sheet/mineral/uranium,
 	)
 
 

code/game/machinery/camera/camera_assembly.dm
@@ -10,7 +10,7 @@
 	g_amt = 300
 
 	//	Motion, EMP-Proof, X-Ray
-	var/list/obj/item/possible_upgrades = list(/obj/item/device/assembly/prox_sensor, /obj/item/stack/sheet/plasma, /obj/item/weapon/reagent_containers/food/snacks/grown/carrot)
+	var/list/obj/item/possible_upgrades = list(/obj/item/device/assembly/prox_sensor, /obj/item/stack/sheet/mineral/plasma, /obj/item/weapon/reagent_containers/food/snacks/grown/carrot)
 	var/list/upgrades = list()
 	var/state = 0
 	var/busy = 0

code/game/machinery/camera/presets.dm
@@ -53,7 +53,7 @@
 // CHECKS
 
 /obj/machinery/camera/proc/isEmpProof()
-	var/O = locate(/obj/item/stack/sheet/plasma) in assembly.upgrades
+	var/O = locate(/obj/item/stack/sheet/mineral/plasma) in assembly.upgrades
 	return O
 
 /obj/machinery/camera/proc/isXRay()
@@ -67,7 +67,7 @@
 // UPGRADE PROCS
 
 /obj/machinery/camera/proc/upgradeEmpProof()
-	assembly.upgrades.Add(new /obj/item/stack/sheet/plasma(assembly))
+	assembly.upgrades.Add(new /obj/item/stack/sheet/mineral/plasma(assembly))
 
 /obj/machinery/camera/proc/upgradeXRay()
 	assembly.upgrades.Add(new /obj/item/weapon/reagent_containers/food/snacks/grown/carrot(assembly))

code/game/mecha/equipment/tools/tools.dm
@@ -837,7 +837,7 @@
 		return
 
 	proc/init()
-		fuel = new /obj/item/stack/sheet/plasma(src)
+		fuel = new /obj/item/stack/sheet/mineral/plasma(src)
 		fuel.amount = 0
 		pr_mech_generator = new /datum/global_iterator/mecha_generator(list(src),0)
 		pr_mech_generator.set_delay(equip_cooldown)
@@ -967,7 +967,7 @@
 	reliability = 1000
 
 	init()
-		fuel = new /obj/item/stack/sheet/uranium(src)
+		fuel = new /obj/item/stack/sheet/mineral/uranium(src)
 		fuel.amount = 0
 		pr_mech_generator = new /datum/global_iterator/mecha_generator/nuclear(list(src),0)
 		pr_mech_generator.set_delay(equip_cooldown)

code/game/mecha/mech_fabricator.dm
@@ -674,17 +674,17 @@
 		if("glass")
 			type = /obj/item/stack/sheet/glass
 		if("gold")
-			type = /obj/item/stack/sheet/gold
+			type = /obj/item/stack/sheet/mineral/gold
 		if("silver")
-			type = /obj/item/stack/sheet/silver
+			type = /obj/item/stack/sheet/mineral/silver
 		if("diamond")
-			type = /obj/item/stack/sheet/diamond
+			type = /obj/item/stack/sheet/mineral/diamond
 		if("plasma")
-			type = /obj/item/stack/sheet/plasma
+			type = /obj/item/stack/sheet/mineral/plasma
 		if("uranium")
-			type = /obj/item/stack/sheet/uranium
+			type = /obj/item/stack/sheet/mineral/uranium
 		if("bananium")
-			type = /obj/item/stack/sheet/clown
+			type = /obj/item/stack/sheet/mineral/clown
 		else
 			return 0
 	var/result = 0
@@ -728,22 +728,22 @@
 				var/obj/item/stack/sheet/glass/G = new /obj/item/stack/sheet/glass(src.loc)
 				G.amount = round(src.resources["glass"] / G.perunit)
 			if(src.resources["plasma"] >= 2000)
-				var/obj/item/stack/sheet/plasma/G = new /obj/item/stack/sheet/plasma(src.loc)
+				var/obj/item/stack/sheet/mineral/plasma/G = new /obj/item/stack/sheet/mineral/plasma(src.loc)
 				G.amount = round(src.resources["plasma"] / G.perunit)
 			if(src.resources["silver"] >= 2000)
-				var/obj/item/stack/sheet/silver/G = new /obj/item/stack/sheet/silver(src.loc)
+				var/obj/item/stack/sheet/mineral/silver/G = new /obj/item/stack/sheet/mineral/silver(src.loc)
 				G.amount = round(src.resources["silver"] / G.perunit)
 			if(src.resources["gold"] >= 2000)
-				var/obj/item/stack/sheet/gold/G = new /obj/item/stack/sheet/gold(src.loc)
+				var/obj/item/stack/sheet/mineral/gold/G = new /obj/item/stack/sheet/mineral/gold(src.loc)
 				G.amount = round(src.resources["gold"] / G.perunit)
 			if(src.resources["uranium"] >= 2000)
-				var/obj/item/stack/sheet/uranium/G = new /obj/item/stack/sheet/uranium(src.loc)
+				var/obj/item/stack/sheet/mineral/uranium/G = new /obj/item/stack/sheet/mineral/uranium(src.loc)
 				G.amount = round(src.resources["uranium"] / G.perunit)
 			if(src.resources["diamond"] >= 2000)
-				var/obj/item/stack/sheet/diamond/G = new /obj/item/stack/sheet/diamond(src.loc)
+				var/obj/item/stack/sheet/mineral/diamond/G = new /obj/item/stack/sheet/mineral/diamond(src.loc)
 				G.amount = round(src.resources["diamond"] / G.perunit)
 			if(src.resources["bananium"] >= 2000)
-				var/obj/item/stack/sheet/clown/G = new /obj/item/stack/sheet/clown(src.loc)
+				var/obj/item/stack/sheet/mineral/clown/G = new /obj/item/stack/sheet/mineral/clown(src.loc)
 				G.amount = round(src.resources["bananium"] / G.perunit)
 			del(src)
 			return 1
@@ -756,21 +756,21 @@
 		return
 	var/material
 	switch(W.type)
-		if(/obj/item/stack/sheet/gold)
+		if(/obj/item/stack/sheet/mineral/gold)
 			material = "gold"
-		if(/obj/item/stack/sheet/silver)
+		if(/obj/item/stack/sheet/mineral/silver)
 			material = "silver"
-		if(/obj/item/stack/sheet/diamond)
+		if(/obj/item/stack/sheet/mineral/diamond)
 			material = "diamond"
-		if(/obj/item/stack/sheet/plasma)
+		if(/obj/item/stack/sheet/mineral/plasma)
 			material = "plasma"
 		if(/obj/item/stack/sheet/metal)
 			material = "metal"
 		if(/obj/item/stack/sheet/glass)
 			material = "glass"
-		if(/obj/item/stack/sheet/clown)
+		if(/obj/item/stack/sheet/mineral/clown)
 			material = "bananium"
-		if(/obj/item/stack/sheet/uranium)
+		if(/obj/item/stack/sheet/mineral/uranium)
 			material = "uranium"
 		else
 			return ..()

commit 5d9f6d4ccd9ee984a778359ff0a272c7a310cfe5

- Fixes the ugly runtimes caused in r5087 about dismantling walls. - Fixed the mineral of the silver walls. It was by default, metal. - Fixed a runtime about building airlocks/walls/false walls with only 2 sheets.
git-svn-id: http://tgstation13.googlecode.com/svn/trunk@5095 316c924e-a436-60f5-8080-3fe189b3f50e

code/game/objects/structures/door_assembly.dm
@@ -390,16 +390,17 @@ obj/structure/door_assembly
 						src.airlock_type = /obj/machinery/door/airlock/glass
 						src.base_icon_state = "door_as_g" //this will be applied to the icon_state with the correct state number at the proc's end.
 				else if(istype(G, /obj/item/stack/sheet/mineral))
+					var/M = G.sheettype
 					if(G.amount>=2)
 						playsound(src.loc, 'sound/items/Crowbar.ogg', 100, 1)
 						user.visible_message("[user] adds [G.name] to the airlock assembly.", "You start to install [G.name] into the airlock assembly.")
 						if(do_after(user, 40))
-							user << "\blue You installed [G.sheettype] plating into the airlock assembly!"
+							user << "\blue You installed [M] plating into the airlock assembly!"
 							G.use(2)
-							src.mineral = "[G.sheettype]"
-							src.name = "Near finished [G.sheettype] Airlock Assembly"
-							src.airlock_type = text2path ("/obj/machinery/door/airlock/[G.sheettype]")
-							src.base_icon_state = "door_as_[G.sheettype]"
+							src.mineral = "[M]"
+							src.name = "Near finished [M] Airlock Assembly"
+							src.airlock_type = text2path ("/obj/machinery/door/airlock/[M]")
+							src.base_icon_state = "door_as_[M]"
 
 	else if(istype(W, /obj/item/weapon/screwdriver) && state == 2 )
 		playsound(src.loc, 'sound/items/Screwdriver.ogg', 100, 1)

code/game/objects/structures/girders.dm
@@ -121,11 +121,12 @@
 							return
 
 			if(S.sheettype)
+				var/M = S.sheettype
 				if(!anchored)
 					if(S.amount < 2) return
 					S.use(2)
 					user << "\blue You create a false wall! Push on it to open or close the passage."
-					var/F = text2path("/obj/structure/falsewall/[S.sheettype]")
+					var/F = text2path("/obj/structure/falsewall/[M]")
 					new F (src.loc)
 					del(src)
 				else
@@ -136,7 +137,7 @@
 						S.use(2)
 						user << "\blue You added the plating!"
 						var/turf/Tsrc = get_turf(src)
-						Tsrc.ChangeTurf(text2path("/turf/simulated/wall/mineral/[S.sheettype]"))
+						Tsrc.ChangeTurf(text2path("/turf/simulated/wall/mineral/[M]"))
 						for(var/turf/simulated/wall/mineral/X in Tsrc.loc)
 							if(X)	X.add_hiddenprint(usr)
 						del(src)

code/game/turfs/simulated/walls.dm
@@ -35,14 +35,23 @@
 		if(!devastated)
 			playsound(src.loc, 'sound/items/Welder.ogg', 100, 1)
 			new /obj/structure/girder(src)
-			var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
-			new M( src )
-			new M( src )
+			if (mineral == "metal")
+				new /obj/item/stack/sheet/metal( src )
+				new /obj/item/stack/sheet/metal( src )
+			else
+				var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
+				new M( src )
+				new M( src )
 		else
-			var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
-			new M( src )
-			new M( src )
-			new /obj/item/stack/sheet/metal( src )
+			if (mineral == "metal")
+				new /obj/item/stack/sheet/metal( src )
+				new /obj/item/stack/sheet/metal( src )
+				new /obj/item/stack/sheet/metal( src )
+			else
+				var/M = text2path("/obj/item/stack/sheet/mineral/[mineral]")
+				new M( src )
+				new M( src )
+				new /obj/item/stack/sheet/metal( src )
 
 	for(var/obj/O in src.contents) //Eject contents!
 		if(istype(O,/obj/effect/decal/poster))

code/game/turfs/simulated/walls_mineral.dm
@@ -19,6 +19,7 @@
 	desc = "A wall with silver plating. Shiny!"
 	icon_state = "silver0"
 	walltype = "silver"
+	mineral = "silver"
 	//var/electro = 0.75
 	//var/shocked = null
 