3877d0c0b8cfcd21ef1909e7fa7ef676baa72c92
cib <cib123@googlemail.com>
2013-02-18 23:02:38 +0100

    Fixes issue #2363
    Maptools now points to the right map again.

--- maptools/clean_map.bat
+++ maptools/clean_map.bat
@@ -1,2 +1,1 @@
-java -jar MapPatcher.jar -clean ../maps/tgstation.2.0.9.1.dmm.backup ../maps/tgstation.2.0.9.1.dmm ../maps/tgstation.2.0.9.1.dmm
-del ../maps/tgstation.2.0.9.1.dmm.backup
+java -jar MapPatcher.jar -clean ../maps/tgstation.2.1.0.0.1.dmm.backup ../maps/tgstation.2.1.0.0.1.dmm ../maps/tgstation.2.1.0.0.1.dmm
