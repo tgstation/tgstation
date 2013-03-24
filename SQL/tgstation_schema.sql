dffba3c8271b3cbbf718b04b3e2d588ca06b9f7c
Pete Goodfellow <petethegoat@gmail.com>
2013-03-24 18:34:00 +0000

    Apologies in advanced for cramming this all into one commit, but it's all fairly interdependent.
    Removes the old tgstation database. It has been merged into the newer feedback database.
    All mentions of the old database have been replaced with the new one.
    This includes updating tgstation_schema.sql and dbconfig.txt.
    
    Removes README feedback.txt as it is now covered by the new schema.
    
    Removes the unused forum DB stuff, including forumdbconfig.txt.
    Updates the config, including both config.txt and game_options.txt. Many options have been moved into the latter.
    Updates configuration.dm to reflect these changes.
    
    Removes the config var/feature_object_spell_system as it did nothing.

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -1,100 +1,303 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+CREATE DATABASE  IF NOT EXISTS `feedback` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `feedback`;
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-CREATE SCHEMA IF NOT EXISTS `tgstation` DEFAULT CHARACTER SET latin1 ;
-USE `mydb` ;
-USE `tgstation` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Table `tgstation`.`death`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `tgstation`.`death` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `pod` TEXT NOT NULL COMMENT 'Place of death' ,
-  `coord` TEXT NOT NULL COMMENT 'X, Y, Z POD' ,
-  `tod` DATETIME NOT NULL COMMENT 'Time of death' ,
-  `job` TEXT NOT NULL ,
-  `special` TEXT NOT NULL ,
-  `name` TEXT NOT NULL ,
-  `byondkey` TEXT NOT NULL ,
-  `laname` TEXT NOT NULL COMMENT 'Last attacker name' ,
-  `lakey` TEXT NOT NULL COMMENT 'Last attacker key' ,
-  `gender` TEXT NOT NULL ,
-  `bruteloss` INT(11) NOT NULL ,
-  `brainloss` INT(11) NOT NULL ,
-  `fireloss` INT(11) NOT NULL ,
-  `oxyloss` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 3409
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `erro_admin`
+--
 
+DROP TABLE IF EXISTS `erro_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `rank` varchar(32) NOT NULL DEFAULT 'Administrator',
+  `level` int(2) NOT NULL DEFAULT '0',
+  `flags` int(16) NOT NULL DEFAULT '0',
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tgstation`.`karma`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `tgstation`.`karma` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `spendername` TEXT NOT NULL ,
-  `spenderkey` TEXT NOT NULL ,
-  `receivername` TEXT NOT NULL ,
-  `receiverkey` TEXT NOT NULL ,
-  `receiverrole` TEXT NOT NULL ,
-  `receiverspecial` TEXT NOT NULL ,
-  `isnegative` TINYINT(1) NOT NULL ,
-  `spenderip` TEXT NOT NULL ,
-  `time` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 943
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `erro_admin_log`
+--
 
+DROP TABLE IF EXISTS `erro_admin_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `adminckey` varchar(32) NOT NULL,
+  `adminip` varchar(18) NOT NULL,
+  `log` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=560 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tgstation`.`karmatotals`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `tgstation`.`karmatotals` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `byondkey` TEXT NOT NULL ,
-  `karma` INT(11) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 244
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `erro_ban`
+--
 
+DROP TABLE IF EXISTS `erro_ban`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_ban` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bantime` datetime NOT NULL,
+  `serverip` varchar(32) NOT NULL,
+  `bantype` varchar(32) NOT NULL,
+  `reason` text NOT NULL,
+  `job` varchar(32) DEFAULT NULL,
+  `duration` int(11) NOT NULL,
+  `rounds` int(11) DEFAULT NULL,
+  `expiration_time` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `ip` varchar(32) NOT NULL,
+  `a_ckey` varchar(32) NOT NULL,
+  `a_computerid` varchar(32) NOT NULL,
+  `a_ip` varchar(32) NOT NULL,
+  `who` text NOT NULL,
+  `adminwho` text NOT NULL,
+  `edits` text,
+  `unbanned` int(2) DEFAULT NULL,
+  `unbanned_datetime` datetime DEFAULT NULL,
+  `unbanned_ckey` varchar(32) DEFAULT NULL,
+  `unbanned_computerid` varchar(32) DEFAULT NULL,
+  `unbanned_ip` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7755 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tgstation`.`library`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `tgstation`.`library` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `author` TEXT NOT NULL ,
-  `title` TEXT NOT NULL ,
-  `content` TEXT NOT NULL ,
-  `category` TEXT NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 184
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `erro_connection_log`
+--
 
+DROP TABLE IF EXISTS `erro_connection_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_connection_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime DEFAULT NULL,
+  `serverip` varchar(45) DEFAULT NULL,
+  `ckey` varchar(45) DEFAULT NULL,
+  `ip` varchar(18) DEFAULT NULL,
+  `computerid` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=311192 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `tgstation`.`population`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `tgstation`.`population` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `playercount` INT(11) NULL DEFAULT NULL ,
-  `admincount` INT(11) NULL DEFAULT NULL ,
-  `time` DATETIME NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 2544
-DEFAULT CHARACTER SET = latin1;
+--
+-- Table structure for table `erro_death`
+--
 
+DROP TABLE IF EXISTS `erro_death`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_death` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `pod` text NOT NULL COMMENT 'Place of death',
+  `coord` text NOT NULL COMMENT 'X, Y, Z POD',
+  `tod` datetime NOT NULL COMMENT 'Time of death',
+  `job` text NOT NULL,
+  `special` text NOT NULL,
+  `name` text NOT NULL,
+  `byondkey` text NOT NULL,
+  `laname` text NOT NULL COMMENT 'Last attacker name',
+  `lakey` text NOT NULL COMMENT 'Last attacker key',
+  `gender` text NOT NULL,
+  `bruteloss` int(11) NOT NULL,
+  `brainloss` int(11) NOT NULL,
+  `fireloss` int(11) NOT NULL,
+  `oxyloss` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `erro_feedback`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `erro_feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `round_id` int(8) NOT NULL,
+  `var_name` varchar(32) NOT NULL,
+  `var_value` int(16) DEFAULT NULL,
+  `details` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=456134 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_legacy_population`
+--
+
+DROP TABLE IF EXISTS `erro_legacy_population`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_legacy_population` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `playercount` int(11) DEFAULT NULL,
+  `admincount` int(11) DEFAULT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_library`
+--
+
+DROP TABLE IF EXISTS `erro_library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_library` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `author` text NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `category` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_player`
+--
+
+DROP TABLE IF EXISTS `erro_player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ckey` varchar(32) NOT NULL,
+  `firstseen` datetime NOT NULL,
+  `lastseen` datetime NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `computerid` varchar(32) NOT NULL,
+  `lastadminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ckey` (`ckey`)
+) ENGINE=InnoDB AUTO_INCREMENT=8849 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_poll_option`
+--
+
+DROP TABLE IF EXISTS `erro_poll_option`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_option` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pollid` int(11) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `percentagecalc` tinyint(1) NOT NULL DEFAULT '1',
+  `minval` int(3) DEFAULT NULL,
+  `maxval` int(3) DEFAULT NULL,
+  `descmin` varchar(32) DEFAULT NULL,
+  `descmid` varchar(32) DEFAULT NULL,
+  `descmax` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=330 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_poll_question`
+--
+
+DROP TABLE IF EXISTS `erro_poll_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `polltype` varchar(16) NOT NULL DEFAULT 'OPTION',
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `adminonly` tinyint(1) DEFAULT '0',
+  `multiplechoiceoptions` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_poll_textreply`
+--
+
+DROP TABLE IF EXISTS `erro_poll_textreply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_textreply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `ip` varchar(18) NOT NULL,
+  `replytext` text NOT NULL,
+  `adminrank` varchar(32) NOT NULL DEFAULT 'Player',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_poll_vote`
+--
+
+DROP TABLE IF EXISTS `erro_poll_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_poll_vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `pollid` int(11) NOT NULL,
+  `optionid` int(11) NOT NULL,
+  `ckey` varchar(255) NOT NULL,
+  `ip` varchar(16) NOT NULL,
+  `adminrank` varchar(32) NOT NULL,
+  `rating` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15753 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `erro_privacy`
+--
+
+DROP TABLE IF EXISTS `erro_privacy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `erro_privacy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `ckey` varchar(32) NOT NULL,
+  `option` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5011 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-03-24 18:02:35
--- code/global.dm
+++ code/global.dm
@@ -191,20 +191,6 @@ var/sqlfdbkpass = ""
 
 var/sqllogging = 0 // Should we log deaths, population stats, etc?
 
-
-
-	// Forum MySQL configuration (for use with forum account/key authentication)
-	// These are all default values that will load should the forumdbconfig.txt
-	// file fail to read for whatever reason.
-
-var/forumsqladdress = "localhost"
-var/forumsqlport = "3306"
-var/forumsqldb = "tgstation"
-var/forumsqllogin = "root"
-var/forumsqlpass = ""
-var/forum_activated_group = "2"
-var/forum_authenticated_group = "10"
-
 	// For FTP requests. (i.e. downloading runtime logs.)
 	// However it'd be ok to use for accessing attack logs and such too, which are even laggier.
 var/fileaccess_timer = 0
@@ -212,4 +198,3 @@ var/fileaccess_timer = 0
 //Database connections
 //A connection is established on world creation. Ideally, the connection dies when the server restarts (After feedback logging.).
 var/DBConnection/dbcon = new()	//Feedback database (New database)
-var/DBConnection/dbcon_old = new()	//Tgstation database (Old database) - See the files in the SQL folder for information what goes where.
