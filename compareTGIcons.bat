91c4d44e6978c570d2e9f5934cbf09f4ceddff96
Rob Nelson <nexisentertainment@gmail.com>
2013-11-12 11:29:16 -0800

    Introducing OpenBYOND, an opensource BYOND toolkit.  Replaces and fixes clusterfuck of python scripts.

--- compareTGIcons.bat
+++ compareTGIcons.bat
@@ -1,2 +1,2 @@
-python tools/DMITool/DMI.py compare-all ../tgstation/icons/ icons/ compare_report_tg.txt
+python tools/OpenBYOND/src/DMITool.py compare-all ../tgstation/icons/ icons/ compare_report_tg.txt
 pause
