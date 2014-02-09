de71749ccb047f6769708aa4458344f2a49aeeb7
Rob Nelson <nexis@7chan.org>
2014-02-09 00:01:12 -0800

    Fuck it.

--- tools/jenkins-build.sh
+++ tools/jenkins-build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash -ex
-# Blatantly stolen from /tg/'s .travis.yml script.
-# Minus all the fun BYOND stuff, of course.
-chmod +x dm.sh
-./dm.sh baystation12.dme -Mtgstation.2.1.0.0.1
+DreamMaker baystation12.dme
