6afffacfaff72b8c7d84cdb545455a6b29490a26
ESwordTheCat <ESwordTheCat@plea.se>
2014-05-17 13:58:44 -0800

    Fix our maptools.

--- maptools/1prepare_map.bat
+++ maptools/1prepare_map.bat
@@ -1,6 +1,8 @@
-set MAPFILE=tgstation2.dmm
+set MAPFILE_TG=tgstation.2.1.0.0.1.dmm
+set MAPFILE_VG=vgstation.dmm
 
-cd ../../maps
-copy %MAPFILE% %MAPFILE%.backup
+cd ../maps
+copy %MAPFILE_TG% %MAPFILE_TG%.backup
+copy %MAPFILE_VG% %MAPFILE_VG%.backup
 
 pause
