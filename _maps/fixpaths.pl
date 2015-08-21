0651055cadce7a392855ad17b3889b3cd43847f5
Zaers <zaers@8chan.co>
2015-08-21 03:10:13 -0300

    Fix map paths

--- _maps/fixpaths.pl
+++ _maps/fixpaths.pl
@@ -3,7 +3,5 @@
 #
 use warnings;
 use strict;
-s@/obj/item/organ/heart@/obj/item/organ/internal/heart@g;
-s@/obj/item/organ/appendix@/obj/item/organ/internal/appendix@g;
-s@/obj/item/body_egg@/obj/item/organ/internal/body_egg@g;
+s@/obj/item/organ/brain@/obj/item/organ/internal/brain@g;
 
