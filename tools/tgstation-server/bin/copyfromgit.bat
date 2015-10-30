1fe8d273151c34a8cdf97c4e10ed11a24889a920
Kyle Spier-Swenson <kyleshome@gmail.com>
2015-10-29 19:24:56 -0700

    Makes the updater symlink awaymission config.
    (So it can persist between updates)

--- tools/tgstation-server/bin/copyfromgit.bat
+++ tools/tgstation-server/bin/copyfromgit.bat
@@ -5,6 +5,7 @@ rmdir /q gamecode\%AB%\config >nul 2>nul
 rmdir /q gamecode\%AB%\cfg >nul 2>nul
 del /q gamecode\%AB%\nudge.py >nul 2>nul
 del /q gamecode\%AB%\CORE_DATA.py >nul 2>nul
+del /q gamecode\%AB%\_maps\RandomZLevels\fileList.txt >nul 2>nul
 
 del /S /F /Q gamecode\%AB% >nul 2>nul
 
@@ -13,10 +14,14 @@ xcopy gitrepo gamecode\%AB% /Y /X /K /R /H /I /C /V /E /Q /EXCLUDE:copyexclude.t
 mkdir gamecode\%AB%\.git\logs
 copy gitrepo\.git\logs\HEAD gamecode\%AB%\.git\logs\HEAD /D /V /Y >nul
 
-mklink gamecode\%AB%\nudge.py ..\..\bot\nudge.py >nul
-mklink gamecode\%AB%\CORE_DATA.py ..\..\bot\CORE_DATA.py >nul
 rmdir /q gamecode\%AB%\data >nul 2>nul
 rmdir /s /q gamecode\%AB%\data >nul 2>nul
+
 mklink /d gamecode\%AB%\data ..\..\gamedata\data >nul
 mklink /d gamecode\%AB%\config ..\..\gamedata\config >nul
 mklink /d gamecode\%AB%\cfg ..\..\gamedata\cfg >nul
+mklink /d gamecode\%AB%\cfg ..\..\gamedata\cfg >nul
+
+mklink gamecode\%AB%\nudge.py ..\..\bot\nudge.py >nul
+mklink gamecode\%AB%\CORE_DATA.py ..\..\bot\CORE_DATA.py >nul
+mklink gamecode\%AB%\_maps\RandomZLevels\fileList.txt ..\..\..\..\Gamedata\config\awaymissionconfig.txt >nul
