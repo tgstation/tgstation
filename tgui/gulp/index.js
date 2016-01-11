0f395332fc388b5cfce466bedc57f0917e1a4da8
Bjorn Neergaard <bjorn@neersighted.com>
2016-01-11 14:56:08 -0600

    Add missing tasks

--- tgui/gulp/index.js
+++ tgui/gulp/index.js
@@ -5,5 +5,8 @@ import { js, watch_js } from './tasks/js'
 import { reload, watch_reload } from './tasks/reload'
 import { size } from './tasks/size'
 
+gulp.task(reload)
+gulp.task(size)
+
 gulp.task('default', gulp.series(gulp.parallel(css, js), size))
 gulp.task('watch', gulp.parallel(watch_css, watch_js, watch_reload))
