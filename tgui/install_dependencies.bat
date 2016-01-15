4faebf5bc53af61f1588f33eaae06cb97b39c89f
Bjorn Neergaard <bjorn@neersighted.com>
2016-01-15 12:24:08 -0600

    Fix message in install script

--- tgui/install_dependencies.bat
+++ tgui/install_dependencies.bat
@@ -1,5 +1,5 @@
 @echo off
-echo node.js > 5.3.0 must be installed for this script to work.
+echo node.js 5.3.0 or newer must be installed for this script to work.
 echo If this script fails, try closing editors and running it again first.
 echo Any warnings about optional dependencies can be safely ignored.
 pause
