1fc1c6c5aba216554bd0ed3066570dd7176c3a1d
Jack Edge <yellowbounder@gmail.com>
2016-05-13 14:07:20 +0100

    Further modifications to my incredible running script

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import subprocess
 import os
 import sys
+import argparse
 from subprocess import PIPE, STDOUT
 
 null = open("/dev/null", "wb")
@@ -20,18 +21,44 @@ def play(soundfile):
     assert p.wait() == 0
     return p
 
-p = subprocess.Popen("(cd tgui; /bin/bash ./build.sh)", shell=True)
-wait(p)
-play("sound/misc/compiler-stage1.ogg")
+def stage1():
+    p = subprocess.Popen("(cd tgui; /bin/bash ./build.sh)", shell=True)
+    wait(p)
+    play("sound/misc/compiler-stage1.ogg")
 
-p = subprocess.Popen("DreamMaker tgstation.dme", shell=True)
-wait(p)
-play("sound/misc/compiler-stage2.ogg")
+def stage2():
+    p = subprocess.Popen("DreamMaker tgstation.dme", shell=True)
+    wait(p)
 
-p = subprocess.Popen(
-    "DreamDaemon tgstation.dmb 25001 -trace -trusted 2>&1 | tee server.log~",
-    shell=True, stdout=PIPE, stderr=STDOUT)
-while p.returncode is None:
-    stdout, stderr = p.communicate()
-    if "Initializations complete." in stdout:
-        play("sound/misc/server-ready.ogg")
+def stage3():
+    play("sound/misc/compiler-stage2.ogg")
+    logfile = open('server.log~','w')
+    p = subprocess.Popen(
+        "DreamDaemon tgstation.dmb 25001 -trace -trusted",
+        shell=True, stdout=PIPE, stderr=STDOUT)
+    while p.returncode is None:
+        try:
+            stdout = p.stdout.readline()
+            t = "Initializations complete."
+            if t in stdout:
+                play("sound/misc/server-ready.ogg")
+            sys.stdout.write(stdout)
+            sys.stdout.flush()
+        finally:
+            logfile.flush()
+            os.fsync(logfile.fileno())
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s','---stage',default=1,type=int)
+    args = parser.parse_args()
+    stage = args.stage
+    assert stage in (1,2,3)
+    if stage == 1:
+        stage1()
+        stage = 2
+    if stage == 2:
+        stage2()
+        stage = 3
+    if stage == 3:
+        stage3()
