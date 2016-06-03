d69932070dd574a1c2b931e6ca7447df17bef169
oranges <email@oranges.net.nz>
2016-06-03 08:23:03 +0000

    test

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -7,5 +7,7 @@ shopt -s globstar
 if [ "$BUILD_TOOLS" = false ]; then
     (! grep 'step_[xy]' _maps/**/*.dmm)
     source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
-    tools/travis/dm.sh -M${DM_MAPFILE} tgstation.dme
+    output=$(tools/travis/dm.sh -M${DM_MAPFILE} tgstation.dme)
+    #exit with value of last command
+    exit $?
 fi;
