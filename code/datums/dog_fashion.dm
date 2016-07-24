tgstation/tgstation

commit d2fd3f36918b9abfa833b75fda8af3c124c85336

Fixes ties on humans and corgis not applying color and alpha to the overlays

code/datums/dog_fashion.dm
@@ -10,6 +10,8 @@
 	// sprite that the associated item uses
 	var/icon_file
 	var/icon_state
+	var/alpha
+	var/color
 
 /datum/dog_fashion/New(mob/M)
 	name = replacetext(name, "REAL_NAME", M.real_name)
@@ -31,7 +33,10 @@
 
 /datum/dog_fashion/proc/get_image(var/dir)
 	if(icon_file && icon_state)
-		return image(icon_file, icon_state = icon_state, dir = dir)
+		var/image/corgI = image(icon_file, icon_state = icon_state, dir = dir)
+		corgI.alpha = alpha
+		corgI.color = color
+		return corgI
 
 
 /datum/dog_fashion/head
@@ -39,8 +44,6 @@
 
 /datum/dog_fashion/back
 	icon_file = 'icons/mob/corgi_back.dmi'
-
-/datum/dog_fashion/head
 
 /datum/dog_fashion/head/helmet
 	name = "Sergeant REAL_NAME"

code/modules/mob/living/simple_animal/friendly/dog.dm
@@ -400,8 +400,9 @@
 		var/image/head_icon
 		var/datum/dog_fashion.DF = new inventory_head.dog_fashion(src)
 
-		if(!DF.icon_state)
-			DF.icon_state = inventory_head.icon_state
+		DF.icon_state = inventory_head.icon_state
+		DF.alpha = inventory_head.alpha
+		DF.color = inventory_head.color
 
 		if(health <= 0)
 			head_icon = DF.get_image(dir = EAST)
@@ -416,8 +417,9 @@
 		var/image/back_icon
 		var/datum/dog_fashion.DF = new inventory_back.dog_fashion(src)
 
-		if(!DF.icon_state)
-			DF.icon_state = inventory_back.icon_state
+		DF.icon_state = inventory_back.icon_state
+		DF.alpha = inventory_back.alpha
+		DF.color = inventory_back.color
 
 		if(health <= 0)
 			back_icon = DF.get_image(dir = EAST)

code/modules/clothing/clothing.dm
@@ -387,7 +387,10 @@ BLIND     // can't see anything
 			var/tie_color = hastie.item_color
 			if(!tie_color)
 				tie_color = hastie.icon_state
-			. += image("icon"='icons/mob/ties.dmi', "icon_state"="[tie_color]")
+			var/image/tI = image("icon"='icons/mob/ties.dmi', "icon_state"="[tie_color]")
+			tI.alpha = hastie.alpha
+			tI.color = hastie.color
+			. += tI
 
 
 /obj/item/clothing/under/New()

commit 2595ce2d322cd74653eca2be43f79ad63104e61a

Renames /datum/dog_fashion vars to reduce confusion Also fixes issue introduced in d2fd3f36918b9abfa833b75fda8af3c124c85336 where custom DF icon_states (now obj_icon_state) would not carry over to the corgi

code/datums/dog_fashion.dm
@@ -9,9 +9,9 @@
 	// This isn't applied to the dog, but stores the icon_state of the
 	// sprite that the associated item uses
 	var/icon_file
-	var/icon_state
-	var/alpha
-	var/color
+	var/obj_icon_state
+	var/obj_alpha
+	var/obj_color
 
 /datum/dog_fashion/New(mob/M)
 	name = replacetext(name, "REAL_NAME", M.real_name)
@@ -32,10 +32,10 @@
 		D.speak_emote = speak_emote
 
 /datum/dog_fashion/proc/get_image(var/dir)
-	if(icon_file && icon_state)
-		var/image/corgI = image(icon_file, icon_state = icon_state, dir = dir)
-		corgI.alpha = alpha
-		corgI.color = color
+	if(icon_file && obj_icon_state)
+		var/image/corgI = image(icon_file, icon_state = obj_icon_state, dir = dir)
+		corgI.alpha = obj_alpha
+		corgI.color = obj_color
 		return corgI
 
 
@@ -132,7 +132,7 @@
 	emote_see = list("stumbles around.", "shivers.")
 	emote_hear = list("howls!","groans.")
 	desc = "Spooky!"
-	icon_state = "sheet"
+	obj_icon_state = "sheet"
 
 /datum/dog_fashion/head/santa
 	name = "Santa's Corgi Helper"

code/modules/mob/living/simple_animal/friendly/dog.dm
@@ -400,9 +400,12 @@
 		var/image/head_icon
 		var/datum/dog_fashion.DF = new inventory_head.dog_fashion(src)
 
-		DF.icon_state = inventory_head.icon_state
-		DF.alpha = inventory_head.alpha
-		DF.color = inventory_head.color
+		if(!DF.obj_icon_state)
+			DF.obj_icon_state = inventory_head.icon_state
+		if(!DF.obj_alpha)
+			DF.obj_alpha = inventory_head.alpha
+		if(!DF.obj_color)
+			DF.obj_color = inventory_head.color
 
 		if(health <= 0)
 			head_icon = DF.get_image(dir = EAST)
@@ -417,9 +420,9 @@
 		var/image/back_icon
 		var/datum/dog_fashion.DF = new inventory_back.dog_fashion(src)
 
-		DF.icon_state = inventory_back.icon_state
-		DF.alpha = inventory_back.alpha
-		DF.color = inventory_back.color
+		DF.obj_icon_state = inventory_back.icon_state
+		DF.obj_alpha = inventory_back.alpha
+		DF.obj_color = inventory_back.color
 
 		if(health <= 0)
 			back_icon = DF.get_image(dir = EAST)