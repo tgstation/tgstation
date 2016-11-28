801c6e06b57197a67faf99168649c7d74b406269
Mervill <mervills.email@gmail.com>
2016-11-27 22:45:49 -0800

    Update all of tgui's dependencies except ractive

--- tgui/src/tgui.js
+++ tgui/src/tgui.js
@@ -48,6 +48,6 @@ loadCSS('https://cdn.jsdelivr.net/fontawesome/4.5.0/css/font-awesome.min.css')
 // Handle font loads.
 import FontFaceObserver from 'fontfaceobserver'
 const fontawesome = new FontFaceObserver('FontAwesome')
-fontawesome.check('\uf240')
+fontawesome.load('\uf240')
   .then(() => document.body.classList.add('icons'))
   .catch(() => document.body.classList.add('no-icons'))
