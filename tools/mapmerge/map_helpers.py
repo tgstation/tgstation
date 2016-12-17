b676cf4074d0eeb2cb1efd3c62fd4e78120cff02
xxalpha <anon976952@gmail.com>
2016-12-17 22:59:19 +0000

    Trimmed down map_merge function to be simpler and straightforward, no more recycling keys; New function get_key, looks up a dict key by abusing python; New function key_int_value, converts a base 52 key to a base 10 integer value; Map helper functions are no longer dependent on global values; Reorganized map_helpers.py;

--- tools/mapmerge/map_helpers.py
+++ tools/mapmerge/map_helpers.py
@@ -11,146 +11,109 @@ except:
 
 import collections
 
-error = {0:"OK", 1:"WARNING: Key lengths are different, all the lines change."}
-
-maxx = 0
-maxy = 0
-key_length = 1
-
-def reset_globals():
-    global key_length
-    global maxx
-    global maxy
-    key_length = 1
-    maxx = 0
-    maxy = 0
+error = {0:"OK", 1:"WARNING: Detected key length difference, not merging.", 2:"WARNING: Detected map size difference, not merging."}
 
 def merge_map(newfile, backupfile, tgm):
-    reset_globals()
+    key_length = 1
+    maxx = 1
+    maxy = 1
 
-    shitmap = parse_map(newfile)
-    originalmap = parse_map(backupfile)
+    new_map = parse_map(newfile)
+    old_map = parse_map(backupfile)
 
-    global key_length
-    if shitmap["key_length"] != originalmap["key_length"]:
+    if new_map["key_length"] != old_map["key_length"]:
         if tgm:
-            write_dictionary_tgm(newfile, shitmap["dictionary"])
-            write_grid_coord_small(newfile, shitmap["grid"])
+            write_dictionary_tgm(newfile, new_map["dictionary"])
+            write_grid_coord_small(newfile, new_map["grid"])
         return 1
     else:
-        key_length = originalmap["key_length"]
+        key_length = old_map["key_length"]
 
-    shitDict = shitmap["dictionary"] #key to tile data dictionary
-    shitGrid = shitmap["grid"] #x,y coords to tiles (keys) dictionary (the map's layout)
-    
-    originalDict = sort_dictionary(originalmap["dictionary"])
-    originalGrid = originalmap["grid"]
-
-    mergeGrid = dict() #final map layout
-    known_keys = dict() #mapping known keys to original keys
-    tempGrid = dict() #saving tiles with newly generated keys for later processing
-    temp_keys = dict() #mapping known keys to newly generated keys
-    unused_keys = list(originalDict.keys()) #list with all existing keys that aren't being used
-    tempDict = collections.OrderedDict() #mapping new keys to new data
-    originalDict_size = len(originalDict)
-
-    for y in range(1,maxy+1):
-        for x in range(1,maxx+1):
-            shitKey = shitGrid[x,y]
-
-            #if this key was seen before, add it to the pile immediately
-            if shitKey in known_keys:
-                mergeGrid[x,y] = known_keys[shitKey]
-                continue
-            #if this key was seen before, add it to the pile immediately
-            if shitKey in temp_keys:
-                tempGrid[x,y] = temp_keys[shitKey]
-                continue
-
-            shitData = shitDict[shitKey]
-            originalKey = originalGrid[x,y]
-            originalData = originalDict[originalKey]
-
-            #if new tile data at x,y is the same as original tile data at x,y, add to the pile
-            if shitData == originalData:
-                mergeGrid[x,y] = originalKey
-                known_keys[shitKey] = originalKey
-                unused_keys.remove(originalKey)
-            else:
-                #search for the new tile data in the original dictionary, if a key is found add it to the pile, else generate a new key
-                newKey = search_key(originalDict, shitData)
-                if newKey != None:
-                    try:
-                        unused_keys.remove(newKey)
-                    except ValueError: #caused by a duplicate entry
-                        print("NOTICE: Correcting duplicate dictionary entry. ({})".format(shitKey))
-                    mergeGrid[x,y] = newKey
-                    known_keys[shitKey] = newKey    
-                #if data at original x,y no longer exists we reuse the key immediately
-                elif search_key(shitDict, originalData) == None:
-                    mergeGrid[x,y] = originalKey
-                    originalDict[originalKey] = shitData
-                    unused_keys.remove(originalKey)
-                    known_keys[shitKey] = originalKey
-                else:
-                    if len(tempDict) == 0:
-                        newKey = generate_new_key(originalDict)
-                    else:
-                        newKey = generate_new_key(tempDict)
-                    tempGrid[x,y] = newKey
-                    temp_keys[shitKey] = newKey
-                    tempDict[newKey] = shitData
-
-    sort = 0
-    #find gaps in the dictionary keys sequence and add the missing keys to be recycled
-    dict_list = list(originalDict.keys())
-    for index in range(0, len(dict_list)):
-        if index + 1 == len(dict_list):
-            break
-
-        key = dict_list[index]
-        next_key = dict_list[index+1]
-
-        difference = key_difference(key, next_key)
-        if difference > 1:
-            i = 1
-            nextnew = key
-            while i < difference:
-                nextnew = get_next_key(nextnew)
-                unused_keys.append(nextnew)
-                i += 1
-            sort = 1
-
-    #Recycle outdated keys with any new tile data, starting from the bottom of the dictionary
-    i = 0
-    for key, value in reversed(tempDict.items()):
-        recycled_key = key
-        if len(unused_keys) > 0:
-            recycled_key = unused_keys.pop()
-
-        for coord, gridkey in tempGrid.items():
-            if gridkey == None:
-                continue
-            if gridkey == key:
-                mergeGrid[coord] = recycled_key
-                tempGrid[coord] = None
-
-        originalDict[recycled_key] = value
-
-    #if gaps in the key sequence were found, sort the dictionary for cleanliness
-    if sort == 1:
-        originalDict = sort_dictionary(originalDict)
-
-    if tgm:
-        write_dictionary_tgm(newfile, originalDict)
-        write_grid_coord_small(newfile, mergeGrid)
+    if new_map["maxx"] != old_map["maxx"] or new_map["maxy"] != old_map["maxy"]:
+        if tgm:
+            write_dictionary_tgm(newfile, new_map["dictionary"])
+            write_grid_coord_small(newfile, new_map["grid"])
+        return 2
     else:
-        write_dictionary(newfile, originalDict)
-        write_grid(newfile, mergeGrid)
+        maxx = old_map["maxx"]
+        maxy = old_map["maxy"]
+
+    new_dict = new_map["dictionary"]
+    new_grid = new_map["grid"]
+    old_dict = sort_dictionary(old_map["dictionary"]) #impose order; old_dict is used in the end as the merged dictionary
+    old_grid = old_map["grid"]
+
+    merged_grid = dict()
+    known_keys = dict()
+    unused_keys = list(old_dict.keys())
+
+    #both new and old dictionary in lists, for faster key lookup by tile tuple
+    old_dict_keys = list(old_dict.keys())
+    old_dict_values = list(old_dict.values())
+    new_dict_keys = list(new_dict.keys())
+    new_dict_values = list(new_dict.values())
+
+    #step one: parse the new version, compare it to the old version, merge both
+    for y in range(1, maxy+1):
+        for x in range(1, maxx+1):
+
+            new_key = new_grid[x,y]
+            #if this key has been processed before, it can immediately be merged
+            if new_key in known_keys:
+                merged_grid[x,y] = known_keys[new_key]
+                continue
+
+            old_key = old_grid[x,y]
+            old_tile = old_dict[old_key]
+            new_tile = new_dict[new_key]
+            
+            if new_tile == old_tile: #this tile is the exact same as before, so the old key is used
+                merged_grid[x,y] = old_key
+                known_keys[new_key] = old_key
+                unused_keys.remove(old_key)
+                continue
+
+            #the tile is different here, but if it exists in the old dictionary, its old key can be used
+            newold_key = get_key(old_dict_keys, old_dict_values, new_tile) 
+            if newold_key != None:
+                merged_grid[x,y] = newold_key
+                known_keys[new_key] = newold_key
+                try:
+                    unused_keys.remove(newold_key)
+                except ValueError:
+                    print("NOTICE: Correcting duplicate dictionary entry. ({})".format(new_key))
+
+            #the tile is brand new and it needs a new key, but if the old key isn't being used any longer it can be used instead
+            elif get_key(new_dict_keys, new_dict_values, old_tile) == None:
+                merged_grid[x,y] = old_key
+                old_dict[old_key] = new_tile
+                known_keys[new_key] = old_key
+                unused_keys.remove(old_key)
+
+            #all other options ruled out, a brand new key is generated for the brand new tile
+            else: 
+                fresh_key = generate_new_key(old_dict)
+                old_dict[fresh_key] = new_tile
+                merged_grid[x,y] = fresh_key
+
+    #step two: clean the dictionary if it has too many unused keys
+    print(len(unused_keys))
+    if True or len(unused_keys) > min(100, (len(old_dict) * 0.5)):
+        print("NOTICE: Trimming the dictionary.")
+        old_dict = trim_dictionary(old_dict)
+
+    #step three: write the map to file
+    if tgm:
+        write_dictionary_tgm(newfile, old_dict)
+        write_grid_coord_small(newfile, merged_grid, maxx, maxy)
+    else:
+        write_dictionary(newfile, old_dict)
+        write_grid(newfile, merged_grid, maxx, maxy)
     return 0
 
-#write dictionary in tgm format
-def write_dictionary_tgm(filename, dictionary): 
+#######################
+#write to file helpers#
+def write_dictionary_tgm(filename, dictionary): #write dictionary in tgm format
     with open(filename, "w") as output:
         output.write("//MAP CONVERTED BY dmm2tgm.py THIS HEADER COMMENT PREVENTS RECONVERSION, DO NOT REMOVE \n")
         for key, list_ in dictionary.items():
@@ -194,8 +157,8 @@ def write_dictionary_tgm(filename, dictionary):
                         
             output.write(")\n")
 
-#thanks to YotaXP for finding out about this one
-def write_grid_coord_small(filename, grid):
+
+def write_grid_coord_small(filename, grid, maxx, maxy): #thanks to YotaXP for finding out about this one
     with open(filename, "a") as output:
         output.write("\n")
 
@@ -205,54 +168,62 @@ def write_grid_coord_small(filename, grid):
                 output.write("{}\n".format(grid[x,y]))
             output.write("{}\n\"}}\n".format(grid[x,maxy]))
 
+
+def write_dictionary(filename, dictionary): #writes a tile dictionary the same way Dreammaker does
+    with open(filename, "w") as output:
+        for key, value in dictionary.items():
+            output.write("\"{}\" = ({})\n".format(key, ",".join(value)))
+
+
+def write_grid(filename, grid, maxx, maxy): #writes a map grid the same way Dreammaker does
+    with open(filename, "a") as output:
+        output.write("\n")
+        output.write("(1,1,1) = {\"\n")
+
+        for y in range(1, maxy+1):
+            for x in range(1, maxx+1):
+                try:
+                    output.write(grid[x,y])
+                except KeyError:
+                    print("Key error: ({},{})".format(x,y))
+            output.write("\n")
+        output.write("\"}")
+        output.write("\n")
+
+####################
+#dictionary helpers#
 def search_key(dictionary, data):
     for key, value in dictionary.items():
         if value == data:
             return key
     return None
 
-def generate_new_key(dictionary):
-    last_key = next(reversed(dictionary))
-    return get_next_key(last_key)
+def get_key(keys, values, data):
+    try:
+        return keys[values.index(data)]
+    except:
+        return None
 
-def get_next_key(key):
-    if key == "":
-        return "".join("a" for _ in range(key_length))
-
-    length = len(key)
-    new_key = ""
-    carry = 1
-    for char in key[::-1]:
-        if carry <= 0:
-            new_key = new_key + char
-            continue
-        if char == 'Z':
-            new_key = new_key + 'a'
-            carry += 1
-            length -= 1
-            if length <= 0:
-                return "OVERFLOW"
-        elif char == 'z':
-            new_key = new_key + 'A'
-        else:
-            new_key = new_key + chr(ord(char) + 1)
-        if carry > 0:
-            carry -= 1
-    return new_key[::-1]
+def trim_dictionary(dictionary): #rewrites dictionary into an ordered dictionary with no unused keys
+    trimmed_dict = collections.OrderedDict()
+    key_length = len(list(dictionary.keys())[0])
+    key = ""
+    for tile in dictionary.values():
+        key = get_next_key(key, key_length)
+        trimmed_dict[key] = tile
+    return trimmed_dict
 
 def sort_dictionary(dictionary):
     sorted_dict = collections.OrderedDict()
-    next_key = get_next_key("")
-    while len(sorted_dict) < len(dictionary):
-        try:
-            sorted_dict[next_key] = dictionary[next_key]
-        except KeyError:
-            pass
-        next_key = get_next_key(next_key)
+    keys = list(dictionary.keys())
+    keys.sort(key=key_int_value)
+    for key in keys:
+        sorted_dict[key] = dictionary[key]
     return sorted_dict
 
-#still does not support more than one z level per file, but should parse any format
-def parse_map(map_file):
+############
+#map parser#
+def parse_map(map_file): #still does not support more than one z level per file, but should parse any format
     with open(map_file, "r") as map_input:
         characters = map_input.read()
 
@@ -278,10 +249,11 @@ def parse_map(map_file):
         curr_num = ""
         reading_coord = "x"
 
+        key_length = 0
         
-        global maxx
-        global maxy
-        key_length_local = 0
+        maxx = 0
+        maxy = 0
+        
         curr_x = 0
         curr_y = 0
         curr_z = 1
@@ -366,7 +338,7 @@ def parse_map(map_file):
                 if in_key_block:
                     if char == "\"":
                         in_key_block = False
-                        key_length_local = len(curr_key)
+                        key_length = len(curr_key)
                     else:
                         curr_key = curr_key + char
                     continue    
@@ -442,7 +414,7 @@ def parse_map(map_file):
 
                     
                     curr_key = curr_key + char
-                    if len(curr_key) == key_length_local:
+                    if len(curr_key) == key_length:
                         iter_x += 1
                         if iter_x > 1:
                             curr_x += 1
@@ -467,60 +439,49 @@ def parse_map(map_file):
         data = dict()
         data["dictionary"] = dictionary
         data["grid"] = grid
-        data["key_length"] = key_length_local
+        data["key_length"] = key_length
+        data["maxx"] = maxx
+        data["maxy"] = maxy
         return data
 
-#subtract keyB from keyA
-def key_difference(keyA, keyB):
-    if len(keyA) != len(keyB):
-        return "you fucked up"
+#############
+#key helpers#
+def generate_new_key(dictionary):
+    last_key = next(reversed(dictionary))
+    return get_next_key(last_key, len(last_key))
 
-    Ayek = keyA[::-1]
-    Byek = keyB[::-1]
+def get_next_key(key, key_length):
+    if key == "":
+        return "".join("a" for _ in range(key_length))
 
-    result = 0
-    for i in range(0, len(keyA)):
-        base = 52**i
-        A = 26 if Ayek[i].isupper() else 0
-        B = 26 if Byek[i].isupper() else 0
-        result += ( (ord(Byek[i].lower()) + B) - (ord(Ayek[i].lower()) + A) ) * base
-    return result
+    length = len(key)
+    new_key = ""
+    carry = 1
+    for char in key[::-1]:
+        if carry <= 0:
+            new_key = new_key + char
+            continue
+        if char == 'Z':
+            new_key = new_key + 'a'
+            carry += 1
+            length -= 1
+            if length <= 0:
+                return "OVERFLOW"
+        elif char == 'z':
+            new_key = new_key + 'A'
+        else:
+            new_key = new_key + chr(ord(char) + 1)
+        if carry > 0:
+            carry -= 1
+    return new_key[::-1]
 
-def string_to_num(s):
-    try:
-        return int(s)
-    except ValueError:
-        return -1
-
-#writes a tile data dictionary the same way Dreammaker does
-def write_dictionary(filename, dictionary):
-    with open(filename, "w") as output:
-        for key, value in dictionary.items():
-            output.write("\"{}\" = ({})\n".format(key, ",".join(value)))
-
-#writes a map grid the same way Dreammaker does
-def write_grid(filename, grid):
-    with open(filename, "a") as output:
-        output.write("\n")
-        output.write("(1,1,1) = {\"\n")
-
-        for y in range(1, maxy+1):
-            for x in range(1, maxx+1):
-                try:
-                    output.write(grid[x,y])
-                except KeyError:
-                    print("Key error: ({},{})".format(x,y))
-            output.write("\n")
-        output.write("\"}")
-        output.write("\n")
-
-#inflated map grid; unused
-def write_grid_coord(filename, grid):
-    with open(filename, "a") as output:
-        output.write("\n")
-        for y in range(1, maxy+1):
-            for x in range(1, maxx+1):
-                output.write("({},{},1) = {{\"{}\"}}\n".format(x, y, grid[x,y]))
+def key_int_value(key):
+    value = 0
+    b = 0
+    for digit in reversed(key):
+        value += base52.index(digit) * (52 ** b)
+        b += 1
+    return value
 
 def key_compare(keyA, keyB): #thanks byond for not respecting ascii
     pos = 0
@@ -548,3 +509,34 @@ def key_compare(keyA, keyB): #thanks byond for not respecting ascii
                     return 1
                 break
     return 0
+
+
+def key_difference(keyA, keyB): #subtract keyB from keyA
+    if len(keyA) != len(keyB):
+        return "you fucked up"
+
+    Ayek = keyA[::-1]
+    Byek = keyB[::-1]
+
+    result = 0
+    for i in range(0, len(keyA)):
+        base = 52**i
+        A = 26 if Ayek[i].isupper() else 0
+        B = 26 if Byek[i].isupper() else 0
+        result += ( (ord(Byek[i].lower()) + B) - (ord(Ayek[i].lower()) + A) ) * base
+    return result
+
+#############
+#other stuff#
+
+#Base 52 a-z A-Z dictionary (it's a python list) for fast conversion
+base52 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+             'p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
+             'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+             'T','U','V','W','X','Y','Z']
+
+def string_to_num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return -1
