b66f544c8491ad977b07e5788635cb73695a2a5b
Cyberboss <Cyberboss@users.noreply.github.com>
2017-05-01 11:56:50 -0400

    Makes the global var checker check the goon tree

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -13,9 +13,9 @@ if [ "$BUILD_TOOLS" = false ]; then
     	echo "base /turf path use detected in maps, please replace with proper paths."
     	exit 1
 	fi;
-	if grep '^/*var/' code/**/*.dm; then
+	if grep '^/*var/' code/**/*.dm goon/**/*.dm; then
 		echo "Unmanaged global var use detected in code, please use the helpers."
-		grep '^var/' code/*.dm | echo
+		grep '^/*var/' code/**/*.dm goon/**/*.dm | echo
 		exit 1
 	fi;
     source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
