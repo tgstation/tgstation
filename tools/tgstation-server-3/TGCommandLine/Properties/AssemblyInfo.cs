5c4893521ea69a5f42e8403147515879d3f26969
Cyberboss <Cyberboss@users.noreply.github.com>
2017-05-24 11:58:51 -0400

    Version bump [TGSDeploy]

--- tools/tgstation-server-3/TGCommandLine/Properties/AssemblyInfo.cs
+++ tools/tgstation-server-3/TGCommandLine/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.74.0")]
-[assembly: AssemblyFileVersion("3.0.74.0")]
+[assembly: AssemblyVersion("3.0.75.0")]
+[assembly: AssemblyFileVersion("3.0.75.0")]
