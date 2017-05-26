7f09db7672b39c786d00899649ff30f52c61b909
Jordie0608 <jordie0608@gmail.com>
2017-05-26 20:12:32 +1000

    adds round id to ban table

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -74,6 +74,7 @@ CREATE TABLE `ban` (
   `bantime` datetime NOT NULL,
   `server_ip` int(10) unsigned NOT NULL,
   `server_port` smallint(5) unsigned NOT NULL,
+  `round_id` int(11) NOT NULL,
   `bantype` enum('PERMABAN','TEMPBAN','JOB_PERMABAN','JOB_TEMPBAN','ADMIN_PERMABAN','ADMIN_TEMPBAN') NOT NULL,
   `reason` varchar(2048) NOT NULL,
   `job` varchar(32) DEFAULT NULL,
