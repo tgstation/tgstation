e09fb26609441eba9d2223a728d288fb7c3290c4
Tad Hardesty <tad@platymuus.com>
2017-09-30 04:37:39 -0700

    Improve Python detection and prepare hooks for merge drivers

--- tools/hooks/install.sh
+++ tools/hooks/install.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname "$0")"
 for f in *.hook; do
-	echo Installing ${f%.hook}
+	echo Installing hook: ${f%.hook}
 	cp $f ../../.git/hooks/${f%.hook}
 done
-echo Done
+for f in *.merge; do
+	echo Installing merge driver: ${f%.merge}
+	git config --replace-all merge.${f%.merge}.driver "tools/hooks/$f %P %O %A %B %L"
+done
+echo "Done"
