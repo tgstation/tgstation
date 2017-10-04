d41ca348efdb8c4ba292aa0e34291506b9446be6
Jordan Brown <Cyberboss@users.noreply.github.com>
2017-10-04 09:29:49 -0400

    Fixes maintainer detection for review auto-tagging

--- tools/WebhookProcessor/github_webhook_processor.php
+++ tools/WebhookProcessor/github_webhook_processor.php
@@ -99,11 +99,8 @@ switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 	case 'pull_request_review':
 		if($payload['action'] == 'submitted'){
 			$lower_state = strtolower($payload['review']['state']);
-			if(($lower_state == 'approved' || $lower_state == 'changes_requested') && is_maintainer($payload, $payload['review']['user']['login'])){
-				$lower_association = strtolower($payload['review']['author_association']);
-				if($lower_association == 'member' || $lower_association == 'contributor' || $lower_association == 'owner')
-					remove_ready_for_review($payload);
-			}
+			if(($lower_state == 'approved' || $lower_state == 'changes_requested') && is_maintainer($payload, $payload['review']['user']['login']))
+				remove_ready_for_review($payload);
 		}
 		break;
 	default:
@@ -287,8 +284,7 @@ function check_ready_for_review($payload, $labels = null){
 	$dismissed_an_approved_review = false;
 
 	foreach($reviews as $R){
-		$lower_association = strtolower($R['author_association']);
-		if($lower_association == 'member' || $lower_association == 'contributor' || $lower_association == 'owner'){
+		if(is_maintainer($R['author']['login'])){
 			$lower_state = strtolower($R['state']);
 			if($lower_state == 'changes_requested')
 				$reviews_ids_with_changes_requested[] = $R['id'];
@@ -297,7 +293,6 @@ function check_ready_for_review($payload, $labels = null){
 				$dismissed_an_approved_review = true;
 			}
 		}
-	}
 
 	if(!$dismissed_an_approved_review && count($reviews_ids_with_changes_requested) == 0){
 		if($has_label_already)
