1af9f01bd46799ae6d78b8247a5aef2863328495
Jordan Brown <Cyberboss@users.noreply.github.com>
2017-10-12 23:54:17 -0400

    Negatives only

--- tools/WebhookProcessor/github_webhook_processor.php
+++ tools/WebhookProcessor/github_webhook_processor.php
@@ -509,7 +509,8 @@ function update_pr_balance($payload) {
 	$balances = pr_balances();
 	$friendliness = get_pr_code_friendliness($payload, $balances[$author]);
 	adjust_pr_balance($author, $friendliness, $balances);
-	adjust_pr_balance($merger, $author != $merger ? 1 : -100, $balances);
+	if($author == $merger)
+		adjust_pr_balance($merger,  -100, $balances);
 }
 	
 function adjust_pr_balance($user, $amount, $balances = null) {
