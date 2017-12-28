tgstation/tgstation

commit 395ce00bb161c0cff528db50fba4bd378da3e1cd

Remove the handholding, so I can merge the pr by proxy
So the admin team won't let me be
Or let me be me, so let me see
They try to shut me down on OOC
But it feels so empty without me

code/modules/mob/mob.dm
@@ -362,7 +362,7 @@
 
 		add_logs(src, M, "grabbed", addition="passive grab")
 		if(!supress_message)
-			visible_message("<span class='warning'>[src] has grabbed [M][(zone_selected == "l_arm" || zone_selected == "r_arm")? " by their hands":" passively"]!</span>")
+			visible_message("<span class='warning'>[src] has grabbed [M] passively!</span>")
 		if(!iscarbon(src))
 			M.LAssailant = null
 		else

commit 6fcaa2d68373449cf7b85c7e699ce03137fdce16

Corrects the bug introduced in 395ce00bb161c0cff528db50fba4bd378da3e1cd.

code/modules/mob/mob.dm
@@ -356,7 +356,7 @@
 
 		add_logs(src, M, "grabbed", addition="passive grab")
 		if(!supress_message)
-			visible_message("<span class='warning'>[src] has grabbed [M] passively!</span>")
+			visible_message("<span class='warning'>[src] has grabbed [M][(zone_selected == "l_arm" || zone_selected == "r_arm")? " by their hands":" passively"]!</span>")
 		if(!iscarbon(src))
 			M.LAssailant = null
 		else