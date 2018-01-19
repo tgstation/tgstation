25ebe94e512b09352381d3381af00f6aa4275d6d
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-01-19 14:41:50 -0500

    Disable ruins for test run mode

--- tools/travis/build_byond.sh
+++ tools/travis/build_byond.sh
@@ -43,6 +43,10 @@ if [ "$BUILD_TOOLS" = false ]; then
 
 	#config folder should not be mandatory
 	rm -rf config
+	
+	#disable ruins because can't test em all
+	mkdir config
+	echo e- "LAVALAND_BUDGET 0\nSPACE_BUDGET 0" > config/config.txt
 
     source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
 	if [ "$BUILD_TESTING" = true ]; then
