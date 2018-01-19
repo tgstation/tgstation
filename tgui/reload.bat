fe5eddeac11611f3bc44ded1c3304b92a394f007
AnturK <AnturK@users.noreply.github.com>
2018-01-19 09:25:23 +0100

    Fixes reload bat

--- tgui/reload.bat
+++ tgui/reload.bat
@@ -6,3 +6,5 @@ for /f "tokens=3*" %%p in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVe
 )
 REM Copy assets to the BYOND cache
 cmd /c copy assets\* "%DocumentsFolder%\BYOND\cache" /y
+REM Copy to tmp subdirectories
+FOR /D %%G in ("%DocumentsFolder%\BYOND\cache\tmp*") DO (cmd /c copy assets\* %%G /y)
