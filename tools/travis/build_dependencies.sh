1efa602edb8aa1a756b13ae7bbf75086cca6452b
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-06-09 13:59:47 -0400

    Oh that's why she did it here

--- tools/travis/build_dependencies.sh
+++ tools/travis/build_dependencies.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ $BUILD_TOOLS = true ]; then
+if [ $BUILD_TOOLS = false ] && [ $BUILD_TESTING = true ]; then
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-host i686-unknown-linux-gnu
     source ~/.profile
 
