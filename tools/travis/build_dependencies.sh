f7ef4ae6f83893f05fb234a8e408434b39a96f6c
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-06-20 13:04:48 -0400

    Absolute madness

--- tools/travis/build_dependencies.sh
+++ tools/travis/build_dependencies.sh
@@ -35,6 +35,7 @@ if [ $BUILD_TOOLS = false ] && [ $BUILD_TESTING = false ]; then
         dpkg -x libmariadb2_2.0.0-1_i386.deb /tmp/extract
         rm libmariadb2_2.0.0-1_i386.deb
         mv /tmp/extract/usr/lib/i386-linux-gnu/libmariadb.so.2 $HOME/MariaDB/
+        mv /tmp/extract/usr/include $HOME/MariaDB/
         ln -s $HOME/MariaDB/libmariadb.so.2 $HOME/MariaDB/libmariadb.so
         rm -rf /tmp/extract
     fi
@@ -42,7 +43,7 @@ if [ $BUILD_TOOLS = false ] && [ $BUILD_TESTING = false ]; then
     cd artifacts
     export CXX=g++-7
     ls /usr/include
-    cmake .. -DMARIA_INCLUDE_DIR=/usr/include
+    cmake .. -DMARIA_INCLUDE_DIR=$HOME/MariaDB/include
     make
     mv src/BSQL/libBSQL.so ../../
     ldd ../../libBSQL.so
