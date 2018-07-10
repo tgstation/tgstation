bfc155548cac548d874413081d2c80c9ae866da7
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-07-09 23:21:43 -0400

    Fixfixfix

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -1,5 +1,11 @@
-wget http://www.byond.com/download/build/512/512.1427_byond.zip -O byond.zip
-[System.IO.Compression.ZipFile]::ExtractToDirectory("byond.zip", "C:/")
+
+if(!(Test-Path -Path "C:/byond")){
+    wget http://www.byond.com/download/build/512/512.1427_byond.zip -O C:\byond.zip
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("C:/byond.zip", "C:/")
+    rm C:\byond.zip
+}
+
+cd $env:APPVEYOR_BUILD_FOLDER
 
 &"C:/byond/bin/dm.exe" -max_errors 0 tgstation.dme
 exit $LASTEXITCODE
