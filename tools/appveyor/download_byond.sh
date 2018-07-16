aaa0e24d115babc539099285423f0a8b982519c9
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-07-16 12:24:21 -0400

    Curl it then

--- tools/appveyor/download_byond.sh
+++ tools/appveyor/download_byond.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source dependencies.sh
-wget "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.$BYOND_MINOR}_byond.zip" -O byond.zip
+curl "http://www.byond.com/download/build/$BYOND_MAJOR/$BYOND_MAJOR.$BYOND_MINOR}_byond.zip" -O byond.zip
