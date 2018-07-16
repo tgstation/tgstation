2684d23b8a33f03333edb062ce67ca2e8415f5ff
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-07-16 12:29:25 -0400

    Fix everything

--- tools/travis/before_build_byond.sh
+++ tools/travis/before_build_byond.sh
@@ -20,11 +20,12 @@ then
   echo "Using cached directory."
 else
   echo "Setting up BYOND."
+  rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
-  rm -rf "$HOME/BYOND/*"
   cd "$HOME/BYOND"
   curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
+  rm byond.zip
   cd byond
   make here
   echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
