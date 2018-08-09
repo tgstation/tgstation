59f15aebd11bc86982a9fa97d55f056708259037
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-08-09 17:05:14 -0400

    Adds TGS4 post compile scripts

---
+++ tools/tgs4_scripts/PostCompile.ps1
@@ -0,0 +1,16 @@
+param(
+    $game_path
+)
+
+cd $game_path
+
+mkdir build
+
+#.github is a little special cause of the prefix
+mv .github build/.github
+
+mv * build   #thank god it's that easy 
+
+&"build/tools/deploy.sh" $game_path $game_path/build
+
+Remove-Item build -Recurse
---
+++ tools/tgs4_scripts/PostCompile.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Basically run deploy.sh, but first
+
+cd $1
+
+mkdir build
+
+shopt -s extglob dotglob
+mv !(build) build
+shopt -u dotglob
+
+build/tools/deploy.sh $1 $1/build
+
+#rm -rf build
