d739ed0a83e7a7e4f2ed683e03e68a472fd59b6b
Kyle Spier-Swenson <kyleshome@gmail.com>
2019-01-26 19:57:05 -0800

    Adds discord webhook support to the github webhook processor.

--- tools/WebhookProcessor/github_webhook_processor.php
+++ tools/WebhookProcessor/github_webhook_processor.php
@@ -14,6 +14,9 @@
  * @author  Miloslav Hula (https://github.com/milo)
  */
 
+define('S_LINK_EMBED', 1<<0);
+define('S_MENTIONS', 1<<1);
+define('S_MARKDOWN', 1<<2);
 
 //CONFIGS ARE IN SECRET.PHP, THESE ARE JUST DEFAULTS!
 
@@ -32,6 +35,7 @@ $validation = "org";
 $validation_count = 1;
 $tracked_branch = 'master';
 $require_changelogs = false;
+$discordWebHooks = array();
 
 require_once 'secret.php';
 
@@ -109,24 +113,36 @@ switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 		print_r($payload); # For debug only. Can be found in GitHub hook log.
 		die();
 }
-
-function apisend($url, $method = 'GET', $content = NULL) {
-	global $apiKey;
+function apisend($url, $method = 'GET', $content = null, $authorization = null) {
 	if (is_array($content))
 		$content = json_encode($content);
 	
+	$headers = array();
+	$headers[] = 'Content-type: application/json';
+	if ($authorization)
+		$headers[] = 'Authorization: ' . $authorization;
+	
 	$scontext = array('http' => array(
-		'method'	=> $method,
-		'header'	=>
-			"Content-type: application/json\r\n".
-			'Authorization: token ' . $apiKey,
+		'method'		=> $method,
+		'header'		=> implode('\r\n', $headers),
 		'ignore_errors' => true,
 		'user_agent' 	=> 'tgstation13.org-Github-Automation-Tools'
 	));
+	
 	if ($content)
 		$scontext['http']['content'] = $content;
 	
 	return file_get_contents($url, false, stream_context_create($scontext));
+	
+}
+function github_apisend($url, $method = 'GET', $content = NULL) {
+	global $apiKey;
+	apisend($url, $method, $content, 'token ' . $apiKey);
+}
+function discord_webhook_send($webhook, $content) {
+	echo "$webhook\r\n";
+	echo json_encode($content);
+	apisend($webhook, 'POST', $content);
 }
 function validate_user($payload) {
 	global $validation, $validation_count;
@@ -148,7 +164,7 @@ function validate_user($payload) {
 	$querystring = '';
 	foreach($query as $key => $value)
 		$querystring .= ($querystring == '' ? '' : '+') . urlencode($key) . ':' . urlencode($value);
-	$res = apisend('https://api.github.com/search/issues?q='.$querystring);
+	$res = github_apisend('https://api.github.com/search/issues?q='.$querystring);
 	$res = json_decode($res, TRUE);
 	return $res['total_count'] >= (int)$validation_count;
 	
@@ -156,7 +172,7 @@ function validate_user($payload) {
 
 function get_labels($payload){
 	$url = $payload['pull_request']['issue_url'] . '/labels';
-	$existing_labels = json_decode(apisend($url), true);
+	$existing_labels = json_decode(github_apisend($url), true);
 	$existing = array();
 	foreach($existing_labels as $label)
 		$existing[] = $label['name'];
@@ -188,18 +204,18 @@ function set_labels($payload, $labels, $remove) {
 		$final[] = $t;
 
 	$url = $payload['pull_request']['issue_url'] . '/labels';
-	echo apisend($url, 'PUT', $final);
+	echo github_apisend($url, 'PUT', $final);
 }
 
 //rip bs-12
 function tag_pr($payload, $opened) {
 	//get the mergeable state
 	$url = $payload['pull_request']['url'];
-	$payload['pull_request'] = json_decode(apisend($url), TRUE);
+	$payload['pull_request'] = json_decode(github_apisend($url), TRUE);
 	if($payload['pull_request']['mergeable'] == null) {
 		//STILL not ready. Give it a bit, then try one more time
 		sleep(10);
-		$payload['pull_request'] = json_decode(apisend($url), TRUE);
+		$payload['pull_request'] = json_decode(github_apisend($url), TRUE);
 	}
 
 	$tags = array();
@@ -249,16 +265,16 @@ function remove_ready_for_review($payload, $labels = null){
 	if($index !== FALSE)
 		unset($labels[$index]);
 	$url = $payload['pull_request']['issue_url'] . '/labels';
-	apisend($url, 'PUT', $labels);
+	github_apisend($url, 'PUT', $labels);
 }
 
 function dismiss_review($payload, $id, $reason){
 	$content = array('message' => $reason);
-	apisend($payload['pull_request']['url'] . '/reviews/' . $id . '/dismissals', 'PUT', $content);
+	github_apisend($payload['pull_request']['url'] . '/reviews/' . $id . '/dismissals', 'PUT', $content);
 }
 
 function get_reviews($payload){
-	return json_decode(apisend($payload['pull_request']['url'] . '/reviews'), true);
+	return json_decode(github_apisend($payload['pull_request']['url'] . '/reviews'), true);
 }
 
 function check_ready_for_review($payload, $labels = null, $remove = array()){
@@ -308,7 +324,7 @@ function check_ready_for_review($payload, $labels = null, $remove = array()){
 	if(count($reviews_ids_with_changes_requested) > 0){
 		//now get the review comments for the offending reviews
 
-		$review_comments = json_decode(apisend($payload['pull_request']['review_comments_url']), true);
+		$review_comments = json_decode(github_apisend($payload['pull_request']['review_comments_url']), true);
 
 		foreach($review_comments as $C){
 			//make sure they are part of an offending review
@@ -350,7 +366,7 @@ function check_dismiss_changelog_review($payload){
 			if($R['body'] == $review_message && strtolower($R['state']) == 'changes_requested')
 				return;
 		//otherwise make it ourself
-		apisend($payload['pull_request']['url'] . '/reviews', 'POST', array('body' => $review_message, 'event' => 'REQUEST_CHANGES'));
+		github_apisend($payload['pull_request']['url'] . '/reviews', 'POST', array('body' => $review_message, 'event' => 'REQUEST_CHANGES'));
 	}
 	else
 		//kill previous reviews
@@ -402,7 +418,7 @@ function handle_pr($payload) {
 		default:
 			return;
 	} 
-	
+	discord_announce($action, $payload);
 	if (strpos(strtolower($payload['pull_request']['title']), '[s]') !== false) {
 		echo "PR Announcement Halted; Secret tag detected.\n";
 		return;
@@ -411,20 +427,79 @@ function handle_pr($payload) {
 		echo "PR Announcement Halted; User not validated.\n";
 		return;
 	}
-		
+	game_announce($action, $payload);
+	
+}
+function game_announce($action, $payload) {
 	$msg = '['.$payload['pull_request']['base']['repo']['full_name'].'] Pull Request '.$action.' by '.htmlSpecialChars($payload['sender']['login']).': <a href="'.$payload['pull_request']['html_url'].'">'.htmlSpecialChars('#'.$payload['pull_request']['number'].' '.$payload['pull_request']['user']['login'].' - '.$payload['pull_request']['title']).'</a>';
 	sendtoallservers('?announce='.urlencode($msg), $payload);
+}
+function discord_announce($action, $payload) {
+	global $discordWebHooks;
+	$color;
+	switch ($action) {
+		case 'reopened':
+		case 'opened':
+			$color = 0x2cbe4e;
+			break;
+		case 'closed':
+			$color = 0xcb2431;
+			break;
+		case 'merged':
+			$color = 0x6f42c1;
+			break;
+		default:
+			return;
+	}
+	$content = array(
+		'username' => 'GitHub',
+		'avatar_url' => $payload['pull_request']['base']['user']['avatar_url'],
+		'content' => 'Pull Request #'.$payload['pull_request']['number'].' *'.$action.'* by '.discord_sanitize($payload['sender']['login'])."\n".discord_sanitize($payload['pull_request']['user']['login']).' - __**'.discord_sanitize($payload['pull_request']['title']).'**__'."\n".'<'.$payload['pull_request']['html_url'].'>',
+		'embeds' => array(
+			array(
+				'title' => '__**'.discord_sanitize($payload['pull_request']['title'], S_MARKDOWN).'**__',
+				'description' => str_replace(array("\r\n", "\n"), array(' ', ' '), substr($payload['pull_request']['body'], 0, 320)),
+				'url' => $payload['pull_request']['html_url'],
+				'color' => $color,
+				'author' => array(
+					'name' => discord_sanitize($payload['pull_request']['user']['login'], S_MARKDOWN),
+					'url' => $payload['pull_request']['user']['html_url'],
+					'icon_url' => $payload['pull_request']['user']['avatar_url']
+				),
+				'footer' => array(
+					'text' => '#'.$payload['pull_request']['number'].' '.discord_sanitize($payload['pull_request']['base']['repo']['full_name'], S_MARKDOWN).' '.discord_sanitize($payload['pull_request']['head']['ref'], S_MARKDOWN).' -> '.discord_sanitize($payload['pull_request']['base']['ref'], S_MARKDOWN),
+					'icon_url' => $payload['pull_request']['base']['user']['avatar_url']
+				)
+			)
+		)
+	);
+	foreach ($discordWebHooks as $discordWebHook)
+		discord_webhook_send($discordWebHook, $content);
+	
+}
+
+function discord_sanitize($text, $flags = S_MENTIONS|S_LINK_EMBED|S_MARKDOWN) { 
+	if ($flags & S_MARKDOWN)
+		$text = str_ireplace(array('\\', '*', '_', '~', '`'), (array('\\\\', '\\*', '\\_', '\\~', '\\`')), $text);
+	
+	if ($flags & S_MENTIONS)
+		$text = str_ireplace(array('@everyone', '@here', '<@'), array('`@everyone`', '`@here`', '@<'), $text);
+
+	if ($flags & S_LINK_EMBED)
+		$text = preg_replace("/((https?|ftp|byond)\:\/\/)([a-z0-9-.]*)\.([a-z]{2,3})(\:[0-9]{2,5})?(\/(?:[a-z0-9+\$_-]\.?)+)*\/?(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?(#[a-z_.-][a-z0-9+\$_.-]*)?/mi", '<$0>', $text);
+	
+	return $text;
 }
 
 //creates a comment on the payload issue
 function create_comment($payload, $comment){
-	apisend($payload['pull_request']['comments_url'], 'POST', json_encode(array('body' => $comment)));
+	github_apisend($payload['pull_request']['comments_url'], 'POST', json_encode(array('body' => $comment)));
 }
 
 //returns the payload issue's labels as a flat array
 function get_pr_labels_array($payload){
 	$url = $payload['pull_request']['issue_url'] . '/labels';
-	$issue = json_decode(apisend($url), true);
+	$issue = json_decode(github_apisend($url), true);
 	$result = array();
 	foreach($issue as $l)
 		$result[] = $l['name'];
@@ -492,13 +567,13 @@ function is_maintainer($payload, $author){
 	$repo_is_org = $payload['pull_request']['base']['repo']['owner']['type'] == 'Organization';
 	if($maintainer_team_id == null || !$repo_is_org) {
 		$collaburl = str_replace('{/collaborator}', '/' . $author, $payload['pull_request']['base']['repo']['collaborators_url']) . '/permission';
-		$perms = json_decode(apisend($collaburl), true);
+		$perms = json_decode(github_apisend($collaburl), true);
 		$permlevel = $perms['permission'];
 		return $permlevel == 'admin' || $permlevel == 'write';
 	}
 	else {
 		$check_url = 'https://api.github.com/teams/' . $maintainer_team_id . '/memberships/' . $author;
-		$result = json_decode(apisend($check_url), true);
+		$result = json_decode(github_apisend($check_url), true);
 		return isset($result['state']) && $result['state'] == 'active';
 	}
 }
@@ -773,7 +848,7 @@ function checkchangelog($payload, $compile = true) {
 	);
 
 	$filename = '/html/changelogs/AutoChangeLog-pr-'.$payload['pull_request']['number'].'.yml';
-	echo apisend($payload['pull_request']['base']['repo']['url'].'/contents'.$filename, 'PUT', $content);
+	echo github_apisend($payload['pull_request']['base']['repo']['url'].'/contents'.$filename, 'PUT', $content);
 }
 
 function sendtoallservers($str, $payload = null) {
