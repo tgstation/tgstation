b9115a64cf50b0970a4588fa352cb82912f32549
actioninja <actioninja@gmail.com>
2019-03-21 08:21:30 -0700

    no default darkmode

--- code/modules/goonchat/browserassets/js/browserOutput.js
+++ code/modules/goonchat/browserassets/js/browserOutput.js
@@ -35,7 +35,7 @@ var opts = {
 	'wasd': false, //Is the user in wasd mode?
 	'priorChatHeight': 0, //Thing for height-resizing detection
 	'restarting': false, //Is the round restarting?
-	'darkmode':true, //Are we using darkmode? If not WHY ARE YOU LIVING IN 2009???
+	'darkmode':false, //Are we using darkmode? If not WHY ARE YOU LIVING IN 2009???
 
 	//Options menu
 	'selectedSubLoop': null, //Contains the interval loop for closing the selected sub menu
