930e9dc50154a239e78a788088df1512b4b6ef35
Rob Bailey <actioninja@gmail.com>
2019-03-21 12:56:17 -0700

    [READY] Darkmode fixes (#43252)
    * better darkmode v1
    
    * ever better colors
    
    * slightly improved tip color + fixed scrolly bug
    
    * updates skin.dmf as well as some small color tweaks
    
    * slightly tweaked more readable info and admin shit
    
    * span_class fix
    
    * merges ooc fixes
    
    * whoops
    
    * MC and ticket screen fix
    
    * no default darkmode
    
    * skin fixes
    
    * more obnoxious adminpm
    
    * more obnoxious adminshit
    
    * skin revert to ensure proper light theme.
    
    * prevents weirdness with default whiteness

--- code/modules/goonchat/browserassets/js/browserOutput.js
+++ code/modules/goonchat/browserassets/js/browserOutput.js
@@ -35,7 +35,7 @@ var opts = {
 	'wasd': false, //Is the user in wasd mode?
 	'priorChatHeight': 0, //Thing for height-resizing detection
 	'restarting': false, //Is the round restarting?
-	'darkmode':true, //Are we using darkmode? If not WHY ARE YOU LIVING IN 2009???
+	'darkmode':false, //Are we using darkmode? If not WHY ARE YOU LIVING IN 2009???
 
 	//Options menu
 	'selectedSubLoop': null, //Contains the interval loop for closing the selected sub menu
@@ -1010,7 +1010,7 @@ $(function() {
 
 		$.ajax({
 			type: 'GET',
-			url: 'browserOutput.css',
+			url: 'browserOutput_white.css',
 			success: function(styleData) {
 				var blob = new Blob(['<head><title>Chat Log</title><style>', styleData, '</style></head><body>', $messages.html(), '</body>']);
 
