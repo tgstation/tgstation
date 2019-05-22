eb8c61a47a58ed37025099533a0b5523e6c23abc
vuonojenmustaturska <naksu@youzen.ext.b2.fi>
2019-05-22 13:24:26 +0300

    can we remove nvm install?
    it should come on the image

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -4,7 +4,7 @@ set -e
 source dependencies.sh
 
 if [ "$BUILD_TOOLS" = true ]; then
-      rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $NODE_VERSION
+      nvm install $NODE_VERSION
       pip3 install --user PyYaml
       pip3 install --user beautifulsoup4
 fi;
