d4386c2fea1d6b338db9b0ab86966815fbc107fd
vuonojenmustaturska <naksu@youzen.ext.b2.fi>
2019-05-22 13:29:12 +0300

    Update install_build_tools.sh

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -4,6 +4,7 @@ set -e
 source dependencies.sh
 
 if [ "$BUILD_TOOLS" = true ]; then
+      source ~/.nvm/nvm.sh
       nvm install $NODE_VERSION
       pip3 install --user PyYaml
       pip3 install --user beautifulsoup4
