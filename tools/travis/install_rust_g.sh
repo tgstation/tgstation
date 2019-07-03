997a84410fedd4deb28f9de471111e5cbe8c0a2f
Tad Hardesty <tad@platymuus.com>
2019-07-03 13:02:03 -0700

    Create .byond/bin before saving there

--- tools/travis/install_rust_g.sh
+++ tools/travis/install_rust_g.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 source dependencies.sh
 
+mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/rust_g "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/rust_g
