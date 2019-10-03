931802298b5fb2446f6fa2f597358a300360a2bf
Style Mistake <stylemistake@gmail.com>
2019-10-03 06:12:17 +0300

    Support title-less Sections

--- tgui-next/packages/tgui/components/Section.js
+++ tgui-next/packages/tgui/components/Section.js
@@ -2,20 +2,23 @@ import { classes } from 'react-tools';
 
 export const Section = props => {
   const { title, level = 1, buttons, children } = props;
+  const hasTitle = !!(title || buttons);
   return (
     <div
       className={classes([
         'Section',
         'Section--level--' + level,
       ])}>
-      <div className="Section__title">
-        <span className="Section__titleText">
-          {title}
-        </span>
-        <div className="Section__buttons">
-          {buttons}
+      {hasTitle && (
+        <div className="Section__title">
+          <span className="Section__titleText">
+            {title}
+          </span>
+          <div className="Section__buttons">
+            {buttons}
+          </div>
         </div>
-      </div>
+      )}
       <div className="Section__content">
         {children}
       </div>
