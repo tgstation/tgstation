6cc1ed03dd2d1320b897084c763bb92f6b336138
spookydonut <github@spooksoftware.com>
2019-10-24 18:38:59 +0800

    f

--- tgui-next/packages/tgui/interfaces/PortableGenerator.js
+++ tgui-next/packages/tgui/interfaces/PortableGenerator.js
@@ -1,68 +1,99 @@
 
 import { Fragment } from 'inferno';
 import { act } from '../byond';
-import { Button, Section, Table, NoticeBox, LabeledList } from '../components';
+import { Box, Button, Section, NoticeBox, LabeledList, ProgressBar } from '../components';
 
 export const PortableGenerator = props => {
   const { state } = props;
   const { config, data } = state;
   const { ref } = config;
+  let stackPercentState;
+  if (data.stack_percent > 50) {
+    stackPercentState = "good";
+  }
+  else if (data.stack_percent > 15) {
+    stackPercentState = "average";
+  }
+  else {
+    stackPercentState = "bad";
+  }
+  let sheetsAmount;
+  if (data.sheets > 5) {
+    sheetsAmount = "good";
+  }
+  else if (data.sheets > 0) {
+    sheetsAmount = "average";
+  }
+  else {
+    sheetsAmount = "bad";
+  }
+
   return (
     <Fragment>
       {!data.anchored && (
         <NoticeBox>Generator not anchored.</NoticeBox>
       )}
-      <Section
-        title="Status">
+      <Section title="Status">
         <LabeledList>
           <LabeledList.Item label="Power switch">
             <Button
               icon={data.active ? "power-off" : "cross"}
               onClick={() => act(ref, 'toggle_power')}
               disabled={!data.ready_to_boot}>
-
+              {data.active ? "On" : "Off"}
             </Button>
+          </LabeledList.Item>
+          <LabeledList.Item label={data.sheet_name + " sheets"}>
+            <Box inline color={stackPercentState}>{data.sheets}</Box>
+            {(data.sheets >= 1) && (
+              <Button
+                icon="eject"
+                disabled={data.active}
+                onClick={() => act(ref, 'eject')}>
+                Eject
+              </Button>
+            )}
+          </LabeledList.Item>
+          <LabeledList.Item label="Current sheet level">
+            <ProgressBar
+              value={data.stack_percent}
+              color={sheetsAmount} />
+          </LabeledList.Item>
+          <LabeledList.Item label="Heat level">
+            {data.current_heat < 100 ? (
+              <Box inline color="good">Nominal</Box>
+            ) : (
+              data.current_heat < 200 ? (
+                <Box inline color="average">Caution</Box>
+              ) : (
+                <Box inline color="bad">DANGER</Box>
+              )
+            )}
           </LabeledList.Item>
         </LabeledList>
       </Section>
-        buttons={!!data.isdryer && (
-          <Button
-            icon={data.drying ? "stop" : "tint"}
-            onClick={() => act(ref, 'Dry')}>
-            {data.drying ? "Stop drying" : "Dry"}
-          </Button>
-        )} />
-      <Section>
-        {data.contents.length === 0 ? (
-          <NoticeBox>Unfortunately, this {data.name} is empty.</NoticeBox>
-        ) : (
-          <Table>
-            <Table.Row>
-              <Table.Cell>Item</Table.Cell>
-              <Table.Cell>Quantity</Table.Cell>
-              <Table.Cell>{data.verb ? data.verb : "Dispense"}</Table.Cell>
-            </Table.Row>
-            {map((value, key) => {
-              return (
-                <Table.Row key={key}>
-                  <Table.Cell>{value.name}</Table.Cell>
-                  <Table.Cell>{value.amount}</Table.Cell>
-                  <Table.Cell>
-                    <Button
-                      disabled={value.amount < 1}
-                      onClick={() => act(ref, 'Release', {name: value.name, amount: 1})}>
-                      One
-                    </Button>
-                    <Button
-                      disabled={value.amount <= 1}
-                      onClick={() => act(ref, 'Release', {name: value.name})}>
-                      Many
-                    </Button>
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })(data.contents)}
-          </Table>
-        )}
+      <Section title="Output">
+        <LabeledList>
+          <LabeledList.Item label="Current output">
+            {data.power_output}
+          </LabeledList.Item>
+          <LabeledList.Item label="Adjust output">
+            <Button
+              icon="minus"
+              onClick={() => act(ref, 'lower_power')}>
+              {data.power_generated}
+            </Button>
+            <Button
+              icon="plus"
+              onClick={() => act(ref, 'higher_power')}>
+              {data.power_generated}
+            </Button>
+          </LabeledList.Item>
+          <LabeledList.Item label="Power available">
+            <Box inline color={data.connected ? "" : "Bad"}>
+              {data.connected ? data.power_available : "Unconnected"}
+            </Box>
+          </LabeledList.Item>
+        </LabeledList>
       </Section>
     </Fragment>); };
