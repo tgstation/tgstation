4477fb387b819ad96aa0411b4499c27b70136f05
Rob Bailey <actioninja@gmail.com>
2019-11-02 16:52:17 -0700

    Language and Crafting tgui-next (#47474)
    * Crafting + Language Menu
    
    crafting menu work start
    
    personal crafting menu
    
    rebuild
    
    early return cleanup
    
    remove a debug print
    
    minor refactors
    
    rebuil
    
    * Crafting + Language Menu
    
    crafting menu work start
    
    personal crafting menu
    
    rebuild
    
    early return cleanup
    
    remove a debug print
    
    minor refactors
    
    rebuil
    
    * Unfuck crew console
    
    * import cleanup
    
    * one more

--- tgui-next/packages/tgui/routes.js
+++ tgui-next/packages/tgui/routes.js
@@ -14,18 +14,21 @@ import { BrigTimer } from './interfaces/BrigTimer';
 import { Canister } from './interfaces/Canister';
 import { Cargo, CargoExpress } from './interfaces/Cargo';
 import { CellularEmporium } from './interfaces/CellularEmporium';
-import { ChemAcclimator } from './interfaces/ChemAcclimator';
 import { CentcomPodLauncher } from './interfaces/CentcomPodLauncher';
+import { ChemAcclimator } from './interfaces/ChemAcclimator';
 import { ChemDispenser } from './interfaces/ChemDispenser';
 import { ChemHeater } from './interfaces/ChemHeater';
 import { ChemMaster } from './interfaces/ChemMaster';
 import { CodexGigas } from './interfaces/CodexGigas';
 import { Crayon } from './interfaces/Crayon';
+import { CrewConsole } from './interfaces/CrewConsole';
 import { Cryo } from './interfaces/Cryo';
 import { DisposalUnit } from './interfaces/DisposalUnit';
 import { KitchenSink } from './interfaces/KitchenSink';
+import { LanguageMenu } from './interfaces/LanguageMenu';
 import { Mint } from './interfaces/Mint';
 import { OperatingComputer } from './interfaces/OperatingComputer';
+import { PersonalCrafting } from './interfaces/PersonalCrafting';
 import { PortableGenerator } from './interfaces/PortableGenerator';
 import { ShuttleManipulator } from './interfaces/ShuttleManipulator';
 import { SmartVend } from './interfaces/SmartVend';
@@ -142,9 +145,17 @@ const ROUTES = {
     component: () => DisposalUnit,
     scrollable: false,
   },
+  language_menu: {
+    component: () => LanguageMenu,
+    scrollable: true,
+  },
   mint: {
     component: () => Mint,
     scrollable: false,
+  },
+  personal_crafting: {
+    component: () => PersonalCrafting,
+    scrollable: true,
   },
   portable_generator: {
     component: () => PortableGenerator,
