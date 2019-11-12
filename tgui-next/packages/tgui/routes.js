828d6c7547fcecc371a8f88385eeabd21a612e3c
actioninja <actioninja@gmail.com>
2019-11-12 00:27:46 -0800

    The Bomb
    aaa
    
    nevermind not refactoring backend
    
    I'M NUCLEAAAAR
    
    I'M WIIIIILD
    
    more work
    
    adasd
    
    hopefully final version
    
    fixed compile error
    
    small cleanup

--- tgui-next/packages/tgui/routes.js
+++ tgui-next/packages/tgui/routes.js
@@ -44,6 +44,7 @@ import { NtosMain } from './interfaces/NtosMain';
 import { NtosNetDownloader } from './interfaces/NtosNetDownloader';
 import { NtosSupermatterMonitor } from './interfaces/NtosSupermatterMonitor';
 import { NtosWrapper } from './interfaces/NtosWrapper';
+import { NuclearBomb } from './interfaces/NuclearBomb';
 import { OperatingComputer } from './interfaces/OperatingComputer';
 import { OreRedemptionMachine } from './interfaces/OreRedemptionMachine';
 import { PersonalCrafting } from './interfaces/PersonalCrafting';
@@ -263,6 +264,11 @@ const ROUTES = {
     wrapper: () => NtosWrapper,
     scrollable: true,
     theme: 'ntos',
+  },
+  nuclear_bomb: {
+    component: () => NuclearBomb,
+    scrollable: false,
+    theme: 'retro',
   },
   ore_redemption_machine: {
     component: () => OreRedemptionMachine,
