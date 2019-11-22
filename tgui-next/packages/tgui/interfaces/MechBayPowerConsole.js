ebeaaf04013f004d0b7b26b5f0d72560b79de91c
TheVekter <spacialvekter@gmail.com>
2019-11-22 01:56:48 -0600

    Re-refactoring code

--- tgui-next/packages/tgui/interfaces/MechBayPowerConsole.js
+++ tgui-next/packages/tgui/interfaces/MechBayPowerConsole.js
@@ -6,7 +6,6 @@ export const MechBayPowerConsole = props => {
   const { state } = props;
   const { data, config } = state;
   const { ref } = config;
-
   return (
     <Section
       title="Mech status"
@@ -18,36 +17,43 @@ export const MechBayPowerConsole = props => {
           onClick={() => act(ref, "reconnect")} />)}>
       <LabeledList>
         <LabeledList.Item label="Integrity">
-        {data.recharge_port ? (
-          data.recharge_port.mech ? (
-          <ProgressBar
-            ranges={{
-              green: [0.7, 1],
-              yellow: [0.3, 0.7],
-              red: [0, 0.3]}}
-            value={data.recharge_port.mech.health}
-            minValue={0}
-            maxValue={data.recharge_port.mech.maxhealth} />
+          {data.recharge_port ? (
+            data.recharge_port.mech ? (
+              <ProgressBar
+                ranges={{
+                  green: [0.7, 1],
+                  yellow: [0.3, 0.7],
+                  red: [0, 0.3]}}
+                value={data.recharge_port.mech.health}
+                minValue={0}
+                maxValue={data.recharge_port.mech.maxhealth} />
+            ) : (
+              <NoticeBox>No mech detected.</NoticeBox>)
           ) : (
-            <NoticeBox>No mech detected.</NoticeBox>)
-          ) : (
-          <NoticeBox>No charger detected.</NoticeBox>)}
+            <NoticeBox>No power port detected. Please re-sync.</NoticeBox>)}
         </LabeledList.Item>
         <LabeledList.Item label="Power">
-          {data.recharge_port.mech.cell ? (
-            <ProgressBar
-              ranges={{
-                green: [0.7, 1],
-                yellow: [0.3, 0.7],
-                red: [0, 0.3]}}
-              value={data.recharge_port.mech.cell.charge}
-              minValue={0}
-              maxValue={data.recharge_port.mech.cell.charge}>
-              <AnimatedNumber value={data.recharge_port.mech.cell.charge} />
-                 / {data.recharge_port.mech.cell.maxcharge}
-            </ProgressBar>
+          {data.recharge_port ? (
+            data.recharge_port.mech ? (
+              data.recharge_port.mech.cell ? (
+                <ProgressBar
+                  ranges={{
+                    green: [0.7, 1],
+                    yellow: [0.3, 0.7],
+                    red: [0, 0.3]}}
+                  value={data.recharge_port.mech.cell.charge}
+                  minValue={0}
+                  maxValue={data.recharge_port.mech.cell.charge}>
+                  <AnimatedNumber value={data.recharge_port.mech.cell.charge} />
+                  /{data.recharge_port.mech.cell.maxcharge}
+                </ProgressBar>
+              ) : (
+                <NoticeBox>No cell is installed.</NoticeBox>)
+            ) : (
+              <NoticeBox>No mech detected.</NoticeBox>)
           ) : (
-            <NoticeBox>No cell is installed.</NoticeBox>)}
+            <NoticeBox>No power port detected. Please re-sync.</NoticeBox>
+          )}
         </LabeledList.Item>
       </LabeledList>
     </Section>
