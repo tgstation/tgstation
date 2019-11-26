e87a6fb82576a1f28cc348e9981537ccc1012570
nightred <nightred@gmail.com>
2019-11-25 18:00:00 -0600

    tgui_next the ore box
    placed or places ??
    
    tweaked layout

--- tgui-next/packages/tgui/routes.js
+++ tgui-next/packages/tgui/routes.js
@@ -48,6 +48,7 @@ import { NtosWrapper } from './interfaces/NtosWrapper';
 import { NuclearBomb } from './interfaces/NuclearBomb';
 import { OperatingComputer } from './interfaces/OperatingComputer';
 import { OreRedemptionMachine } from './interfaces/OreRedemptionMachine';
+import { OreBox } from './interfaces/OreBox';
 import { PersonalCrafting } from './interfaces/PersonalCrafting';
 import { PortableGenerator } from './interfaces/PortableGenerator';
 import { PowerMonitor } from './interfaces/PowerMonitor';
@@ -284,6 +285,10 @@ const ROUTES = {
   },
   ore_redemption_machine: {
     component: () => OreRedemptionMachine,
+    scrollable: true,
+  },
+  ore_box: {
+    component: () => OreBox,
     scrollable: true,
   },
   operating_computer: {
