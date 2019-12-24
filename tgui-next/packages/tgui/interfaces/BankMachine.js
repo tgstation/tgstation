fa5d1224cb83296f23add81c42d0b865d25458f1
actioninja <actioninja@gmail.com>
2019-12-24 05:45:32 -0800

    bank machine Ꞥ

--- tgui-next/packages/tgui/interfaces/BankMachine.js
+++ tgui-next/packages/tgui/interfaces/BankMachine.js
@@ -21,7 +21,7 @@ export const BankMachine = props => {
                 selected={siphoning}
                 onClick={() => act(siphoning ? 'halt' : 'siphon')} />
             )}>
-            {'$' + current_balance}
+            {current_balance + 'Ꞥ'}
           </LabeledList.Item>
         </LabeledList>
       </Section>
