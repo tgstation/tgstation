22ccb3b3367c8544865099d1c20e54a2071ad4fa
SteelSlayer <SteelSlayer@users.noreply.github.com>
2020-01-07 11:51:09 -0600

    Coverted UI over to use <Table> for aesthetic reasons

--- tgui-next/packages/tgui/interfaces/Signaler.js
+++ tgui-next/packages/tgui/interfaces/Signaler.js
@@ -1,4 +1,4 @@
-import { NumberInput, Button, Section, LabeledList } from '../components';
+import { Table, NumberInput, Button, Section } from '../components';
 import { useBackend } from '../backend';
 import { toFixed } from 'common/math';
 
@@ -13,54 +13,64 @@ export const Signaler = props => {
 
   return (
     <Section>
-      <LabeledList>
-        <LabeledList.Item label="Frequency">
-          <NumberInput
-            animate
-            unit="kHz"
-            step={0.2}
-            stepPixelSize={6}
-            minValue={minFrequency / 10}
-            maxValue={maxFrequency / 10}
-            value={frequency / 10}
-            format={value => toFixed(value, 1)}
-            width={13}
-            onDrag={(e, value) => act('freq', {
-              freq: value,
-            })} />
-          <Button
-            ml={0.5}
-            icon="sync"
-            content="Reset"
-            onClick={() => act('reset', {
-              reset: "freq",
-            })} />
-        </LabeledList.Item>
-        <LabeledList.Item label="Code">
-          <NumberInput
-            animate
-            step={1}
-            stepPixelSize={6}
-            minValue={1}
-            maxValue={100}
-            value={code}
-            width={13}
-            onDrag={(e, value) => act('code', {
-              code: value,
-            })} />
-          <Button
-            ml={0.5}
-            icon="sync"
-            content="Reset"
-            onClick={() => act('reset', {
-              reset: "code",
-            })} />
-        </LabeledList.Item>
-      </LabeledList>
+      <Table>
+        <Table.Row>
+          <Table.Cell color="label">
+            Frequency:
+          </Table.Cell>
+          <Table.Cell collapsing>
+            <NumberInput
+              animate
+              unit="kHz"
+              step={0.2}
+              stepPixelSize={6}
+              minValue={minFrequency / 10}
+              maxValue={maxFrequency / 10}
+              value={frequency / 10}
+              format={value => toFixed(value, 1)}
+              width={13}
+              onDrag={(e, value) => act('freq', {
+                freq: value,
+              })} />
+            <Button
+              ml={0.5}
+              icon="sync"
+              content="Reset"
+              onClick={() => act('reset', {
+                reset: "freq",
+              })} />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row lineHeight={5}>
+          <Table.Cell color="label">
+            Code:
+          </Table.Cell>
+          <Table.Cell collapsing>
+            <NumberInput
+              animate
+              step={1}
+              stepPixelSize={6}
+              minValue={1}
+              maxValue={100}
+              value={code}
+              width={13}
+              onDrag={(e, value) => act('code', {
+                code: value,
+              })} />
+            <Button
+              ml={0.5}
+              icon="sync"
+              content="Reset"
+              onClick={() => act('reset', {
+                reset: "code",
+              })} />
+          </Table.Cell>
+        </Table.Row>
+      </Table>
       <Button
-        mt={1}
+        mt={0.2}
         mb={-0.2}
-        width={37.4}
+        fluid
         icon="arrow-up"
         content="Send Signal"
         textAlign="center"
