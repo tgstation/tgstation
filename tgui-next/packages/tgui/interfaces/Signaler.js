724967d2a0fc71e337b94a8b4344a5e1645028a9
SteelSlayer <SteelSlayer@users.noreply.github.com>
2020-01-07 21:54:59 -0600

    Changes to using tgui Grid's because Im an indecisive bastard, final commit, hopefully

--- tgui-next/packages/tgui/interfaces/Signaler.js
+++ tgui-next/packages/tgui/interfaces/Signaler.js
@@ -1,4 +1,4 @@
-import { Table, NumberInput, Button, Section } from '../components';
+import { Grid, NumberInput, Button, Section } from '../components';
 import { useBackend } from '../backend';
 import { toFixed } from 'common/math';
 
@@ -13,68 +13,73 @@ export const Signaler = props => {
 
   return (
     <Section>
-      <Table>
-        <Table.Row>
-          <Table.Cell color="label">
-            Frequency:
-          </Table.Cell>
-          <Table.Cell collapsing>
-            <NumberInput
-              animate
-              unit="kHz"
-              step={0.2}
-              stepPixelSize={6}
-              minValue={minFrequency / 10}
-              maxValue={maxFrequency / 10}
-              value={frequency / 10}
-              format={value => toFixed(value, 1)}
-              width={13}
-              onDrag={(e, value) => act('freq', {
-                freq: value,
-              })} />
-            <Button
-              ml={0.5}
-              icon="sync"
-              content="Reset"
-              onClick={() => act('reset', {
-                reset: "freq",
-              })} />
-          </Table.Cell>
-        </Table.Row>
-        <Table.Row lineHeight={5}>
-          <Table.Cell color="label">
-            Code:
-          </Table.Cell>
-          <Table.Cell collapsing>
-            <NumberInput
-              animate
-              step={1}
-              stepPixelSize={6}
-              minValue={1}
-              maxValue={100}
-              value={code}
-              width={13}
-              onDrag={(e, value) => act('code', {
-                code: value,
-              })} />
-            <Button
-              ml={0.5}
-              icon="sync"
-              content="Reset"
-              onClick={() => act('reset', {
-                reset: "code",
-              })} />
-          </Table.Cell>
-        </Table.Row>
-      </Table>
-      <Button
-        mt={0.2}
-        mb={-0.2}
-        fluid
-        icon="arrow-up"
-        content="Send Signal"
-        textAlign="center"
-        onClick={() => act('signal')} />
+      <Grid>
+        <Grid.Column size={1.4}>
+          Frequency:
+        </Grid.Column>
+        <Grid.Column>
+          <NumberInput
+            animate
+            unit="kHz"
+            step={0.2}
+            stepPixelSize={6}
+            minValue={minFrequency / 10}
+            maxValue={maxFrequency / 10}
+            value={frequency / 10}
+            format={value => toFixed(value, 1)}
+            width={13}
+            onDrag={(e, value) => act('freq', {
+              freq: value,
+            })} />
+        </Grid.Column>
+        <Grid.Column>
+          <Button
+            ml={1.3}
+            icon="sync"
+            content="Reset"
+            onClick={() => act('reset', {
+              reset: "freq",
+            })} />
+        </Grid.Column>
+      </Grid>
+      <Grid mt={0.6}>
+        <Grid.Column size={1.4}>
+          Code:
+        </Grid.Column>
+        <Grid.Column>
+          <NumberInput
+            animate
+            step={1}
+            stepPixelSize={6}
+            minValue={1}
+            maxValue={100}
+            value={code}
+            width={13}
+            onDrag={(e, value) => act('code', {
+              code: value,
+            })} />
+        </Grid.Column>
+        <Grid.Column>
+          <Button
+            ml={1.3}
+            icon="sync"
+            content="Reset"
+            onClick={() => act('reset', {
+              reset: "code",
+            })} />
+        </Grid.Column>
+      </Grid>
+      <Grid mt={0.8}>
+        <Grid.Column>
+          <Button
+            mb={-0.1}
+            fluid
+            icon="arrow-up"
+            content="Send Signal"
+            textAlign="center"
+            onClick={() => act('signal')} />
+        </Grid.Column>
+      </Grid>
     </Section>
   );
 };
