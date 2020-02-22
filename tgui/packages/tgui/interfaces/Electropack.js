4a778725dfc488ef4255b72725e9df840f2b2cac
Arkatos <arkatos1234@gmail.com>
2020-02-22 15:53:38 +0100

    Electropack UI

---
+++ tgui/packages/tgui/interfaces/Electropack.js
@@ -0,0 +1,66 @@
+import { Grid, NumberInput, Button, Section, LabeledList } from '../components';
+import { useBackend } from '../backend';
+import { toFixed } from 'common/math';
+
+export const Electropack = props => {
+  const { act, data } = useBackend(props);
+  const {
+    power,
+    code,
+    frequency,
+    minFrequency,
+    maxFrequency,
+  } = data;
+
+  return (
+    <Section>
+      <LabeledList>
+        <LabeledList.Item label="Power">
+          <Button
+            icon={power ? 'power-off' : 'times'}
+            content={power ? 'On' : 'Off'}
+            selected={power}
+            onClick={() => act('power')} />
+        </LabeledList.Item>
+      </LabeledList>
+      <Grid>
+        <Grid.Column size={1.4} color="label">
+          Frequency:
+        </Grid.Column>
+        <Grid.Column>
+          <NumberInput
+            animate
+            unit="kHz"
+            step={0.2}
+            stepPixelSize={6}
+            minValue={minFrequency / 10}
+            maxValue={maxFrequency / 10}
+            value={frequency / 10}
+            format={value => toFixed(value, 1)}
+            width={13}
+            onDrag={(e, value) => act('freq', {
+              freq: value,
+            })} />
+        </Grid.Column>
+      </Grid>
+      <Grid mt={0.6}>
+        <Grid.Column size={1.4} color="label">
+          Code:
+        </Grid.Column>
+        <Grid.Column>
+          <NumberInput
+            animate
+            step={1}
+            stepPixelSize={6}
+            minValue={1}
+            maxValue={100}
+            value={code}
+            width={13}
+            onDrag={(e, value) => act('code', {
+              code: value,
+            })} />
+        </Grid.Column>
+      </Grid>
+    </Section>
+  );
+};
