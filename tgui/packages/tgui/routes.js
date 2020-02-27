59379b371f0bccdc8e8248fdf46188f793945176
Arkatos1 <43862960+Arkatos1@users.noreply.github.com>
2020-02-27 10:19:26 +0100

    tgui: Malfunction Module Picker (#49532)
    * Module Picker UI
    
    * Bugfixes, tweaks, malf theme
    
    * Malfunction theme removed, generalized Uplink interface and tweaks
    
    * Revert "Malfunction theme removed, generalized Uplink interface and tweaks"
    
    This reverts commit e30dd183567325dd7a339ec3eb9d237a98b5fe3e.
    
    * Rename and cleanup
    
    * Cleanup

--- tgui/packages/tgui/routes.js
+++ tgui/packages/tgui/routes.js
@@ -58,6 +58,7 @@ import { MechBayPowerConsole } from './interfaces/MechBayPowerConsole';
 import { MedicalKiosk } from './interfaces/MedicalKiosk';
 import { MiningVendor } from './interfaces/MiningVendor';
 import { Mint } from './interfaces/Mint';
+import { MalfunctionModulePicker } from './interfaces/MalfunctionModulePicker';
 import { Mule } from './interfaces/Mule';
 import { NaniteChamberControl } from './interfaces/NaniteChamberControl';
 import { NaniteCloudControl } from './interfaces/NaniteCloudControl';
@@ -372,6 +373,11 @@ const ROUTES = {
   mint: {
     component: () => Mint,
     scrollable: false,
+  },
+  malfunction_module_picker: {
+    component: () => MalfunctionModulePicker,
+    scrollable: true,
+    theme: 'malfunction',
   },
   mulebot: {
     component: () => Mule,
