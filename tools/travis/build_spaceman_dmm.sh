3899672e00729e6fee7d9a9b3bd4905c8a21b264
spookydonut <github@spooksoftware.com>
2020-03-05 00:47:24 +0800

    ooops cache conflict

--- tools/travis/build_spaceman_dmm.sh
+++ tools/travis/build_spaceman_dmm.sh
@@ -5,10 +5,13 @@ source dependencies.sh
 
 cd $HOME/SpacemanDMM
 
-git init
-git remote add origin https://github.com/SpaceManiac/SpacemanDMM.git
-git fetch origin --depth=1 $SPACEMAN_DMM_COMMIT_HASH
-git reset --hard FETCH_HEAD
+if [ ! -d .git ]
+then
+	git init
+	git remote add origin https://github.com/SpaceManiac/SpacemanDMM.git
+	git fetch origin --depth=1 $SPACEMAN_DMM_COMMIT_HASH
+	git reset --hard FETCH_HEAD
+fi
 
 cargo build --release --bin $1
 cp target/release/$1 ~
