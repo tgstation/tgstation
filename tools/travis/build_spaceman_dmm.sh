e26d9d770038b3984b15ce05127c4602fab2c65d
spookydonut <github@spooksoftware.com>
2020-03-05 02:35:29 +0800

    oops

--- tools/travis/build_spaceman_dmm.sh
+++ tools/travis/build_spaceman_dmm.sh
@@ -9,9 +9,10 @@ if [ ! -d .git ]
 then
 	git init
 	git remote add origin https://github.com/SpaceManiac/SpacemanDMM.git
-	git fetch origin --depth=1 $SPACEMAN_DMM_COMMIT_HASH
-	git reset --hard FETCH_HEAD
 fi
+
+git fetch origin --depth=1 $SPACEMAN_DMM_COMMIT_HASH
+git reset --hard FETCH_HEAD
 
 cargo build --release --bin $1
 cp target/release/$1 ~
