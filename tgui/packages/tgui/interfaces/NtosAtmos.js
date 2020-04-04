0f116c9dcb6792856fff9010196e8207c464e404
Aleksej Komarov <stylemistake@gmail.com>
2020-04-05 00:54:33 +0300

    Copy progress bar code from NtosSupermatterMonitor

--- tgui/packages/tgui/interfaces/NtosAtmos.js
+++ tgui/packages/tgui/interfaces/NtosAtmos.js
@@ -1,31 +1,51 @@
+import { filter, sortBy } from 'common/collections';
+import { flow } from 'common/fp';
+import { toFixed } from 'common/math';
+import { Fragment } from 'inferno';
 import { useBackend } from '../backend';
-import { Box, Button, LabeledList, Section } from '../components';
+import { LabeledList, ProgressBar, Section } from '../components';
+import { getGasColor, getGasLabel } from '../constants';
 
 export const NtosAtmos = props => {
   const { act, data } = useBackend(props);
-  const air = data.AirData || [];
+  const {
+    AirTemp,
+    AirPressure,
+  } = data;
+  const gases = flow([
+    filter(gas => gas.percentage >= 0.01),
+    sortBy(gas => -gas.percentage),
+  ])(data.AirData || []);
+  const gasMaxPercentage = Math.max(1, ...gases.map(gas => gas.percentage));
   return (
-    <Section
-      title="Atmospheric Readings"
-      textAlign="center">
-      <Box>
-        Temperature: {data.AirTemp} Celcius
-      </Box>
-      <Box>
-        Pressure: {data.AirPressure} kPa
-      </Box>
-      <Box my={1} />
-      <LabeledList>
-        {air.map(gas => (
-          <LabeledList.Item
-            key={gas.name}
-            label={gas.name}
-            textAlign="center">
-            {gas.percentage} %
-            <Box my={1} />
+    <Fragment>
+      <Section>
+        <LabeledList>
+          <LabeledList.Item label="Temperature">
+            {AirTemp}°C
           </LabeledList.Item>
-        ))}
-      </LabeledList>
-    </Section>
+          <LabeledList.Item label="Pressure">
+            {AirPressure} kPa
+          </LabeledList.Item>
+        </LabeledList>
+      </Section>
+      <Section>
+        <LabeledList>
+          {gases.map(gas => (
+            <LabeledList.Item
+              key={gas.name}
+              label={getGasLabel(gas.name)}>
+              <ProgressBar
+                color={getGasColor(gas.name)}
+                value={gas.percentage}
+                minValue={0}
+                maxValue={gasMaxPercentage}>
+                {toFixed(gas.percentage, 2) + '%'}
+              </ProgressBar>
+            </LabeledList.Item>
+          ))}
+        </LabeledList>
+      </Section>
+    </Fragment>
   );
 };
