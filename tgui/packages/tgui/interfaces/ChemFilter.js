2cff520a28b678deeffa5bdcb22d27b8694e3d5b
Aleksej Komarov <stylemistake@gmail.com>
2020-04-20 17:46:14 +0300

    Half em units for margin/padding, full em unit for everything else.

--- tgui/packages/tgui/interfaces/ChemFilter.js
+++ tgui/packages/tgui/interfaces/ChemFilter.js
@@ -10,7 +10,7 @@ export const ChemFilterPane = (props, context) => {
   return (
     <Section
       title={title}
-      minHeight={40}
+      minHeight="120px"
       buttons={(
         <Fragment>
           <Input
