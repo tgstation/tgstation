84ef1e82253e45b6d69247229b3b430a18b5d266
ArcaneMusic <hero12290@aol.com>
2020-05-21 05:19:04 -0400

    The left list doesn't work, stop for now.

--- code/game/machinery/request_kiosk.dm
+++ code/game/machinery/request_kiosk.dm
@@ -12,11 +12,24 @@
 	light_color = LIGHT_COLOR_GREEN
 	var/static/list/request_list
 	var/request_amount = 0
+	var/datum/bank_account/current_user
 
 /obj/machinery/request_kiosk/Initialize()
 	. = ..()
 	if(!request_list)
 		request_list = list()
+		request_list += list("owner" = "Pat Sajack", "value" = 100, "description" = "Ayo get me some fuckin coffee ya dig?", "applicants" = list())
+
+/obj/machinery/request_kiosk/attackby(obj/item/I, mob/living/user, params)
+	. = ..()
+	if(istype(I,/obj/item/card/id))
+		var/obj/item/card/id/current_card = I
+		if(current_card.registered_account)
+			current_user = current_card.registered_account
+			return TRUE
+		else
+			to_chat(user, "There's no account assigned with this ID.")
+			return TRUE
 
 /obj/machinery/request_kiosk/ui_interact(mob/user, ui_key, datum/tgui/ui, force_open, datum/tgui/master_ui, datum/ui_state/state)
 	ui = SStgui.try_update_ui(user, src, ui_key, ui, force_open)
@@ -24,16 +37,28 @@
 		ui = new(user, src, ui_key, "RequestKiosk", name, ui_x, ui_y, master_ui, state)
 		ui.open()
 
+/obj/machinery/request_kiosk/ui_data(mob/user)
+	var/list/data = list()
+	if(current_user)
+		data["AccountName"] = current_user.account_holder
+	data["Requests"] = request_list
+	return data
+
 /obj/machinery/request_kiosk/ui_act(action, list/params)
 	if(..())
 		return
 	switch(action)
 		if("ApplyRequest")
 			say("Applied for a request.")
-		if("CreateRequest")
-			say("Request Created")
+		if("CreateBounty")
+			say("Dispensing Card.")
+			new /obj/item/bounty_card(loc)
 		if("PayApplicant")
 			say("Paid out [request_amount] credits to someone.")
+		if("Clear")
+			if(current_user)
+				current_user = null
+				say("Account Reset.")
 		if("DeleteRequest")
 			say("Deleted Current Request.")
 		if("amount")
@@ -41,7 +66,30 @@
 			if(input)
 				request_amount = input
 
+/obj/item/bounty_card
+	name = "bounty card"
+	desc = "Can be filled out, then inserted into a request console to create a bounty."
+	icon = 'icons/obj/bureaucracy.dmi'
+	icon_state = "bountycard"
+	w_class = WEIGHT_CLASS_TINY
+	var/bounty_desc
+	var/bounty_price
 
-/obj/machinery/request_kiosk/ui_data(mob/user)
-	var/list/data = list()
-	return data
+/obj/item/bounty_card/attack_self(mob/user)
+	var/description = input(user, "Please enter your request description.", "Request description") as message|null
+	if(!description || !(user.canUseTopic(src, BE_CLOSE, FALSE, NO_TK)))
+		return
+	description = sanitize(copytext_char(description, 1, MAX_MESSAGE_LEN))
+	var/new_cost = input(user, "Set the cost for this request.","Bounty cost") as num|null
+	if(!new_cost || !(user.canUseTopic(src, BE_CLOSE, FALSE, NO_TK)))
+		return
+	new_cost = clamp(round(new_cost, 1), 10, 1000)
+	bounty_desc = description
+	bounty_price = new_cost
+	icon_state = "bountycardfull"
+
+/obj/item/bounty_card/examine(mob/user)
+	. = ..()
+	if(bounty_desc && bounty_price)
+		. += (bounty_desc)
+		. += ("The Price on this bounty is set for [bounty_price] credits.")
--- tgui/packages/tgui/interfaces/RequestKiosk.js
+++ tgui/packages/tgui/interfaces/RequestKiosk.js
@@ -3,63 +3,74 @@ import { flow } from 'common/fp';
 import { classes } from 'common/react';
 import { Fragment } from 'inferno';
 import { useBackend, useLocalState } from '../backend';
-import { Button, Flex, Input, LabeledList, NumberInput, Section } from '../components';
+import { Box, Button, Flex, Input, LabeledList, NumberInput, Section } from '../components';
 import { refocusLayout, Window } from '../layouts';
 
 export const RequestKiosk = (props, context) => {
   const { act, data } = useBackend(context);
-  // Extract `health` and `color` variables from the `data` object.
   const {
-    health,
-    color,
+	  Requests,
   } = data;
   return (
     <Window resizable>
       <div className="RequestKiosk__right">
-		<Window.Content scrollable>
-		<Flex mb={1}>
-          <Flex.Item mr={1}>
-          <Section>
-			a list of tasks here
-		  </Section>
-		  </Flex.Item>
-		  <Flex.Item grow={1} basis={0}>
-		  <Section
-		  title="Create Request">
-			Some buttons that would let you create a new request would be here.
-			<Input
-			fluid
-			mb={1}
-			placeholder="At some point this could be a description"
-			onInput={(e, value) => setSearchText(value)} />
-			Bounty Amount 
-			<NumberInput
-                      value={0}
-                      unit="Credits"
-                      minValue={0}
-                      maxValue={1000}
-                      step={10}
-                      stepPixelSize={2}
-					  />
-		</Section>
-		<LabeledList>
-			<LabeledList.Item
-			label="name">
-				Jimmy Twoshoes 
-				<Button
-				  content="Pay me"/>
-			</LabeledList.Item>
-			<LabeledList.Item
-			label="name">
-				Johnny Dicklips 
-				<Button
-				  content="Pay me"/>
-			</LabeledList.Item>
-		</LabeledList>
-		</Flex.Item>
-		</Flex>
+        <Window.Content scrollable>
+          <Flex mb={1}>
+            <Flex.Item mr={1}>
+              <Section>
+                Broken List
+				{Requests?.map(task => (
+				  <div
+					key={task.owner}
+					title={task.owner}
+					className={classes([
+					  'Button',
+					  'Button--fluid',
+					  'Button--color--transparent',
+					  'Button--ellipsis',
+					
+					])}
+					onClick={() => {
+					  refocusLayout();
+						}}>
+						Tasky
+						</div>
+				))}
+              </Section>
+            </Flex.Item>
+            <Flex.Item grow={1} basis={0}>
+			<Section>
+              <LabeledList
+                title="Create Requests">
+				<LabeledList.Item
+				label = "Current Account">
+                {data.AccountName ? data.AccountName: 'N/A'}
+				</LabeledList.Item>
+				<LabeledList.Item
+				label = "Current Bounty">
+				</LabeledList.Item>
+              </LabeledList>
+			  </Section>
+			  
+			  
+              <LabeledList>
+                <LabeledList.Item
+                  label="name">
+                  Jimmy Twoshoes 
+                  <Button
+                    content="Pay me" />
+                </LabeledList.Item>
+                <LabeledList.Item
+                  label="name">
+                  Johnny Smollips 
+                  <Button
+                    content="Pay me" />
+                </LabeledList.Item>
+              </LabeledList>
+            </Flex.Item>
+          </Flex>
         </Window.Content>
-	  </div>
+      </div>
     </Window>
   );
 };
