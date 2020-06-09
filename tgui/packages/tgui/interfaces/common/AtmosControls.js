5acce6603cb78d64cfc552cd882046dc939f20ac
Paul Bruner <warlockd@gmail.com>
2020-06-09 00:01:31 -0500

    Updated tgui bundle and eslent junk

--- tgui/packages/tgui/interfaces/common/AtmosControls.js
+++ tgui/packages/tgui/interfaces/common/AtmosControls.js
@@ -38,7 +38,7 @@ export const Vent = (props, context) => {
         <LabeledList.Item label="Mode">
           <Button
             icon="sign-in-alt"
-            content={direction ? 'Pressurizing' : 'Scrubbing' }
+            content={direction ? 'Pressurizing' : 'Scrubbing'}
             color={!direction && 'danger'}
             onClick={() => act('direction', {
               id_tag,
