965ea8b3811b52873a2b0df92d09bd75e00e8a19
Jared-Fogle <35135081+Jared-Fogle@users.noreply.github.com>
2020-06-12 14:01:28 -0700

    Update as per stylemistake's feedback

--- tgui/packages/tgui/interfaces/Orbit.js
+++ tgui/packages/tgui/interfaces/Orbit.js
@@ -1,32 +1,28 @@
-import { Button, Input, Section } from '../components';
+import { createSearch } from 'common/string';
+import { Box, Button, Input, Section } from '../components';
 import { Window } from '../layouts';
 import { useBackend, useLocalState } from '../backend';
 
 const PATTERN_DESCRIPTOR = / \[(?:ghost|dead)\]$/;
 const PATTERN_NUMBER = / \(([0-9]+)\)$/;
 
-const searchFor = bySearch => thing => {
-  return thing.name.toLowerCase().startsWith(bySearch.toLowerCase());
-};
+const searchFor = searchText => createSearch(searchText, thing => thing.name);
 
-const sortByName = (a, b) => {
-  const [aName, bName] = [a.name, b.name];
+const compareNumberedText = (a, b) => {
+  const aName = a.name;
+  const bName = b.name;
 
   // Check if aName and bName are the same except for a number at the end
   // e.g. Medibot (2) and Medibot (3)
-  const [aNumberMatch, bNumberMatch] = [
-    aName.match(PATTERN_NUMBER),
-    bName.match(PATTERN_NUMBER),
-  ];
+  const aNumberMatch = aName.match(PATTERN_NUMBER);
+  const bNumberMatch = bName.match(PATTERN_NUMBER);
 
   if (aNumberMatch
     && bNumberMatch
     && aName.replace(PATTERN_NUMBER, "") === bName.replace(PATTERN_NUMBER, "")
   ) {
-    const [aNumber, bNumber] = [
-      parseInt(aNumberMatch[1], 10),
-      parseInt(bNumberMatch[1], 10),
-    ];
+    const aNumber = parseInt(aNumberMatch[1], 10);
+    const bNumber = parseInt(bNumberMatch[1], 10);
 
     return aNumber - bNumber;
   }
@@ -34,46 +30,50 @@ const sortByName = (a, b) => {
   return aName.localeCompare(bName);
 };
 
-const BasicSection = props => {
-  const { orbit, search, source, title } = props;
+const BasicSection = (props, context) => {
+  const { act } = useBackend(context);
+  const { searchText, source, title } = props;
+  const things = source.filter(searchFor(searchText));
+  things.sort(compareNumberedText);
 
   return source.length > 0 && (
     <Section title={title}>
-      { source.filter(searchFor(search)).sort(sortByName).map(thing => (<Button
-        key={thing.name}
-        content={thing.name.replace(PATTERN_DESCRIPTOR, "")}
-        onClick={() => orbit(thing.name)}
-      />)) }
+      {
+        things.map(thing => (<Button
+          key={thing.name}
+          content={thing.name.replace(PATTERN_DESCRIPTOR, "")}
+          onClick={() => act("orbit", { name: thing.name })}
+        />))
+      }
     </Section>
   );
 };
 
-const OrbitedButton = props => {
-  const { color, orbit, thing } = props;
+const OrbitedButton = (props, context) => {
+  const { act } = useBackend(context);
+  const { color, thing } = props;
 
   return (<Button
     color={color}
     content={(
       <span>
-        {thing.name}{" "}
+        {thing.name}
         {
-          thing.orbiters
-            ? (
-              <span>
-                ({thing.orbiters}{" "}
-                <img
-                  src="ghost.png"
-                  style={{
-                    opacity: "0.7",
-                  }}
-                />)
-              </span>
-            )
-            : undefined
+          thing.orbiters && (
+            <span>
+              <Box inline mr={1} />
+              ({thing.orbiters}{" "}
+              <Box
+                as="img"
+                src="ghost.png"
+                opacity={0.7}
+              />)
+            </span>
+          )
         }
       </span>
     )}
-    onClick={() => orbit(thing.name)}
+    onClick={() => act("orbit", { name: thing.name })}
   />);
 };
 
@@ -88,7 +88,7 @@ export const Orbit = (props, context) => {
     npcs,
   } = data;
 
-  const [search, setSearch] = useLocalState(context, "search", "");
+  const [searchText, setSearchText] = useLocalState(context, "searchText", "");
 
   const collatedAntagonists = {};
   for (const antagonist of antagonists) {
@@ -99,7 +99,10 @@ export const Orbit = (props, context) => {
     collatedAntagonists[antagonist.antag].push(antagonist);
   }
 
-  const orbit = name => act("orbit", { name });
+  const sortedAntagonists = Object.entries(collatedAntagonists);
+  sortedAntagonists.sort((a, b) => {
+    return a[0].localeCompare(b[0]);
+  });
 
   return (
     <Window>
@@ -107,28 +110,25 @@ export const Orbit = (props, context) => {
         <Section title={
           <Input
             fluid
-            value={search}
-            onInput={(_, value) => setSearch(value)}
+            value={searchText}
+            onInput={(_, value) => setSearchText(value)}
           />
         } />
 
         { antagonists.length > 0 && (
           <Section title="Ghost-Visible Antagonists">
-            { Object.entries(collatedAntagonists).sort((a, b) => {
-              return a.localeCompare(b);
-            }).map(([name, antags]) => {
+            { sortedAntagonists.map(([name, antags]) => {
               return (
                 <Section key={name} title={name} level={2}>
                   {
                     antags
-                      .filter(searchFor(search))
-                      .sort(sortByName)
+                      .filter(searchFor(searchText))
+                      .sort(compareNumberedText)
                       .map(antag => (
                         <OrbitedButton
                           key={antag.name}
                           color="bad"
                           thing={antag}
-                          orbit={orbit}
                         />
                       ))
                   }
@@ -139,41 +139,40 @@ export const Orbit = (props, context) => {
         ) }
 
         <Section title="Alive">
-          { alive.filter(searchFor(search)).sort(sortByName).map(thing => (
-            <OrbitedButton
-              key={thing.name}
-              color="good"
-              thing={thing}
-              orbit={orbit}
-            />)) }
+          { alive
+            .filter(searchFor(searchText))
+            .sort(compareNumberedText)
+            .map(thing => (
+              <OrbitedButton
+                key={thing.name}
+                color="good"
+                thing={thing}
+              />)
+            )}
         </Section>
 
         <BasicSection
           title="Ghosts"
           source={ghosts}
-          orbit={orbit}
-          search={search}
+          searchText={searchText}
         />
 
         <BasicSection
           title="Dead"
           source={dead}
-          orbit={orbit}
-          search={search}
+          searchText={searchText}
         />
 
         <BasicSection
           title="NPCs"
           source={npcs}
-          orbit={orbit}
-          search={search}
+          searchText={searchText}
         />
 
         <BasicSection
           title="Misc"
           source={misc}
-          orbit={orbit}
-          search={search}
+          searchText={searchText}
         />
       </Window.Content>
     </Window>
