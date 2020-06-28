d7c6b72d28f140d0b44474591d683371e55031f2
Timberpoes <silent_insomnia_pp@hotmail.co.uk>
2020-06-28 21:51:20 +0100

    Spinning arrow, refactor code to components

--- tgui/packages/tgui/interfaces/BluespaceLocator.js
+++ tgui/packages/tgui/interfaces/BluespaceLocator.js
@@ -1,41 +1,84 @@
 import { Fragment } from 'inferno';
 import { useBackend } from '../backend';
-import { Section, Divider, Collapsible, Tabs } from '../components';
+import { Section, Divider, Collapsible, Icon } from '../components';
 import { Window } from '../layouts';
 
+const directionToIcon = {
+  "north": 0,
+  "northeast": 45,
+  "east": 90,
+  "southeast": 135,
+  "south": 180,
+  "southwest": 225,
+  "west": 270,
+  "northwest": 315,
+};
+
 export const BluespaceLocator = (props, context) => {
-  const { data } = useBackend(context);
-  const {
-    telebeacons, 
-    trackimplants,
-  } = data;
   return (
     <Window resizable>
       <Window.Content scrollable>
-       {/*              FUCK THIS SO MUCH
-        <Tabs>
-            <Tabs.tab selected={tabIndex === 1} onClick={() => setTabIndex(1)}>Teleporter Beacons</Tabs.tab>
-            <Tabs.tab selected={tabIndex === 2} onClick={() => setTabIndex(2)}>Tracking Implants</Tabs.tab>
-        </Tabs>
-        */}
-        <Section title="Teleporter Beacons">
-            {telebeacons.map(tpbeacon => (
-                <Collapsible key={tpbeacon.name} title={tpbeacon.name}>
-                    Direction : {tpbeacon.direction}, {tpbeacon.distance}
-                </Collapsible>
-                )
-            )}
-        </Section>
-        <Divider></Divider>
-        <Section title="Tracking implants">
-            {trackimplants.map(implant => (
-                <Collapsible key={implant.name} title={implant.name}>
-                    Direction : {implant.direction}, {implant.distance}
-                </Collapsible>
-                )
-            )}
-        </Section>
+        <TeleporterBeacons />
+        <Divider />
+        <TrackingImplants />
       </Window.Content>
     </Window>
+  );
+};
+
+const TeleporterBeacons = (props, context) => {
+  const { data } = useBackend(context);
+
+  const { telebeacons } = data;
+
+  return (
+    <Section title="Teleporter Beacons">
+      {telebeacons.map(beacon => (
+        <SignalLocator
+          key={beacon.name}
+          name={beacon.name}
+          distance={beacon.distance}
+          direction={directionToIcon[beacon.direction]} />
+      )
+      )}
+    </Section>
+  );
+};
+
+const TrackingImplants = (props, context) => {
+  const { data } = useBackend(context);
+
+  const { trackimplants } = data;
+
+  return (
+    <Section title="Tracking Implants">
+      {trackimplants.map(implant => (
+        <SignalLocator
+          key={implant.name}
+          name={implant.name}
+          distance={implant.distance}
+          direction={directionToIcon[implant.direction]} />
+      )
+      )}
+    </Section>
+  );
+};
+
+const SignalLocator = (props, context) => {
+  const {
+    name,
+    direction,
+    distance,
+  } = props;
+  return (
+    <Collapsible
+      key={name}
+      title={name}>
+      Direction :
+      <Icon
+        name="arrow-up"
+        rotation={direction} />
+      , {distance}
+    </Collapsible>
   );
 };
