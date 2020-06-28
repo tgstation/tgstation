2d54543cea22e35d5887e7f15e7c58e0582ca1ce
Kathy <kathyryals@outlook.com>
2020-06-28 17:11:29 +0200

    Initial TGUI UI commit.

--- tgui/packages/tgui/interfaces/BluespaceLocator.js
+++ tgui/packages/tgui/interfaces/BluespaceLocator.js
@@ -1,5 +1,6 @@
+import { Fragment } from 'inferno';
 import { useBackend } from '../backend';
-import { Section } from '../components';
+import { Section, Divider, Collapsible, Tabs } from '../components';
 import { Window } from '../layouts';
 
 export const BluespaceLocator = (props, context) => {
@@ -11,8 +12,28 @@ export const BluespaceLocator = (props, context) => {
   return (
     <Window resizable>
       <Window.Content scrollable>
-        <Section title="Hello, world!">
-          We can put words and stuff and things here!
+       {/*              FUCK THIS SO MUCH
+        <Tabs>
+            <Tabs.tab selected={tabIndex === 1} onClick={() => setTabIndex(1)}>Teleporter Beacons</Tabs.tab>
+            <Tabs.tab selected={tabIndex === 2} onClick={() => setTabIndex(2)}>Tracking Implants</Tabs.tab>
+        </Tabs>
+        */}
+        <Section title="Teleporter Beacons">
+            {telebeacons.map(tpbeacon => (
+                <Collapsible key={tpbeacon.name} title={tpbeacon.name}>
+                    Direction : {tpbeacon.direction}, {tpbeacon.distance}
+                </Collapsible>
+                )
+            )}
+        </Section>
+        <Divider></Divider>
+        <Section title="Tracking implants">
+            {trackimplants.map(implant => (
+                <Collapsible key={implant.name} title={implant.name}>
+                    Direction : {implant.direction}, {implant.distance}
+                </Collapsible>
+                )
+            )}
         </Section>
       </Window.Content>
     </Window>
