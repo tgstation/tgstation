51facbf93e7bfbcafc96756f863306170f0906aa
ArcaneMusic <hero12290@aol.com>
2020-06-30 03:19:23 -0400

    Style's changes

--- tgui/packages/tgui/interfaces/RequestKiosk.js
+++ tgui/packages/tgui/interfaces/RequestKiosk.js
@@ -18,97 +18,95 @@ export const RequestKiosk = (props, context) => {
   const backcolor = 'rgba(0, 0, 69, 0.5)';
   return (
     <Window resizable>
-      <div className="RequestKiosk__right">
-        <Window.Content scrollable>
-          <Flex mb={1}>
-            <Flex.Item grow={1} basis={0}>
-              <Section
-                buttons={(
-                  <Flex>
-                    <Flex.Item mt={2}>
-                      <Button
-                        icon="power-off"
-                        content="Log out"
-                        onClick={() => act('Clear')} />
-                      <Button
-                        icon="print"
-                        content="Print Bounty Slip"
-                        onClick={() => act('CreateBounty')} />
-                    </Flex.Item>
-                  </Flex>
-                )}>
-                <LabeledList
-                  title="Create Requests">
-                  <LabeledList.Item
-                    label="Current Account">
-                    {AccountName ? AccountName: 'N/A'}
-                  </LabeledList.Item>
-                </LabeledList>
+      <Window.Content scrollable>
+        <Flex mb={1}>
+          <Flex.Item grow={1} basis={0}>
+            <Section
+              buttons={(
+                <Flex>
+                  <Flex.Item mt={2}>
+                    <Button
+                      icon="power-off"
+                      content="Log out"
+                      onClick={() => act('Clear')} />
+                    <Button
+                      icon="print"
+                      content="Print Bounty Slip"
+                      onClick={() => act('CreateBounty')} />
+                  </Flex.Item>
+                </Flex>
+              )}>
+              <LabeledList
+                title="Create Requests">
+                <LabeledList.Item
+                  label="Current Account">
+                  {AccountName ? AccountName: 'N/A'}
+                </LabeledList.Item>
+              </LabeledList>
+            </Section>
+            {Requests?.map(request => (
+              <Section key={request.name}>
+                <Flex spacing={1} align="baseline">
+                  <Flex.Item bold width="310px">
+                    {request.owner}
+                  </Flex.Item>
+                  <Flex.Item>
+                    {formatMoney(request.value) + ' cr'}
+                  </Flex.Item>
+                  <Flex.Item>
+                    <Button
+                      icon="pen-fancy"
+                      content="Apply"
+                      onClick={() => act('Apply', {
+                        request: request.acc_number,
+                      })} />
+                    <Button
+                      icon="trash-alt"
+                      content="Delete"
+                      color="red"
+                      onClick={() => act('DeleteRequest', {
+                        request: request.acc_number,
+                      })} />
+                  </Flex.Item>
+                </Flex>
+                <Section align="center">
+                  <i>&quot;{request.description}&quot;</i>
+                </Section>
+                <Section
+                  title="Request Applicants">
+                  {Applicants?.map(applicant => (
+                    applicant.request_id === request.acc_number && (
+                      <Flex>
+                        <Flex.Item
+                          grow={1}
+                          p={0.5}
+                          backgroundColor={backcolor}
+                          width="510px"
+                          style={{
+                            border: `2px solid ${color}`,
+                          }}>
+                          {applicant.name}
+                        </Flex.Item>
+                        <Flex.Item
+                          align="end">
+                          <Button
+                            fluid
+                            icon="cash-register"
+                            onClick={() => act('PayApplicant', {
+                              applicant: applicant.requestee_id,
+                              request: request.acc_number,
+                            })} />
+                        </Flex.Item>
+                      </Flex>
+                    )
+                  ))}
+                </Section>
               </Section>
-              {Requests?.map(request => (
-                <Section key={request.name}>
-                  <Flex spacing={1} align="baseline">
-                    <Flex.Item bold width="310px">
-                      {request.owner}
-                    </Flex.Item>
-                    <Flex.Item>
-                      {formatMoney(request.value) + ' cr'}
-                    </Flex.Item>
-                    <Flex.Item>
-                      <Button
-                        icon="pen-fancy"
-                        content="Apply"
-                        onClick={() => act('Apply', {
-                          request: request.acc_number,
-                        })} />
-                      <Button
-                        icon="trash-alt"
-                        content="Delete"
-                        color="red"
-                        onClick={() => act('DeleteRequest', {
-                          request: request.acc_number,
-                        })} />
-                    </Flex.Item>
-                  </Flex>
-                  <Section align="center">
-                    <i>&quot;{request.description}&quot;</i>
-                  </Section>
-                  <Section
-                    title="Request Applicants">
-                    {Applicants?.map(applicant => (
-                      applicant.request_id === request.acc_number && (
-                        <Flex>
-                          <Flex.Item
-                            grow={1}
-                            p={0.5}
-                            backgroundColor={backcolor}
-                            width="510px"
-                            style={{
-                              border: `2px solid ${color}`,
-                            }}>
-                            {applicant.name}
-                          </Flex.Item>
-                          <Flex.Item 
-                            align="end">
-                            <Button
-                              fluid
-                              icon="cash-register"
-                              onClick={() => act('PayApplicant', {
-                                applicant: applicant.requestee_id,
-                                request: request.acc_number,
-                              })} />
-                          </Flex.Item>
-                        </Flex>
-                      )
-                    ))}
-                  </Section>
-                </Section>  
-              ))}
-              
-            </Flex.Item>
-          </Flex>
-        </Window.Content>
-      </div>
+            ))}
+
+          </Flex.Item>
+        </Flex>
+      </Window.Content>
     </Window>
   );
 };
