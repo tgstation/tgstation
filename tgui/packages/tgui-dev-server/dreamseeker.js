df6f847ddc89fb987363504e9b781648fde2a34d
Aleksej Komarov <stylemistake@gmail.com>
2020-08-20 02:44:23 +0300

    Increase buffer size and reduce output of netstat

--- tgui/packages/tgui-dev-server/dreamseeker.js
+++ tgui/packages/tgui-dev-server/dreamseeker.js
@@ -52,13 +52,15 @@ DreamSeeker.getInstancesByPids = async pids => {
   }
   if (pidsToResolve.length > 0) {
     try {
-      const command = 'netstat -a -n -o';
-      const { stdout } = await promisify(exec)(command);
+      const command = 'netstat -ano | findstr LISTENING';
+      const { stdout } = await promisify(exec)(command, {
+        // Max buffer of 1MB (default is 200KB)
+        maxBuffer: 1024 * 1024,
+      });
       // Line format:
       // proto addr mask mode pid
       const entries = stdout
         .split('\r\n')
-        .filter(line => line.includes('LISTENING'))
         .map(line => {
           const words = line.match(/\S+/g);
           return {
@@ -77,7 +79,12 @@ DreamSeeker.getInstancesByPids = async pids => {
       }
     }
     catch (err) {
-      logger.error(err);
+      if (err.code === 'ERR_CHILD_PROCESS_STDIO_MAXBUFFER') {
+        logger.error(err.message, err.code);
+      }
+      else {
+        logger.error(err);
+      }
       return [];
     }
   }
