d190449a76c1b5f3037d6a213428301af6867c3e
ThePotato <10260415+ThePotato97@users.noreply.github.com>
2020-10-06 10:37:28 +0100

    adds icon stacking to tgui

--- tgui/packages/tgui/components/Icon.js
+++ tgui/packages/tgui/components/Icon.js
@@ -34,3 +34,48 @@ export const Icon = props => {
 };
 
 Icon.defaultHooks = pureComponentHooks;
+
+export const IconStack = props => {
+  const { nameTop, nameBottom, size, spin, className, style = {}, rotation, sizeBottom='1x', sizeTop='1x', ...rest } = props;
+  if (size) {
+    style['font-size'] = (size * 100) + '%';
+  }
+  if (typeof rotation === 'number') {
+    style['transform'] = `rotate(${rotation}deg)`;
+  }
+  const faRegularBottom = FA_OUTLINE_REGEX.test(nameTop);
+  const faRegularTop = FA_OUTLINE_REGEX.test(nameBottom);
+  const faNameBottom = nameTop.replace(FA_OUTLINE_REGEX, '');
+  const faNameTop = nameBottom.replace(FA_OUTLINE_REGEX, '');
+  return (
+    <Box
+      as="span"
+      class="fa-stack">
+      <Box
+        as="i"
+        className={classes([
+          className,
+          faRegularBottom ? 'far' : 'fas',
+          'fa-' + faNameBottom,
+          spin && 'fa-spin',
+          'fa-stack-' + sizeBottom,
+        ])}
+        style={style}
+        {...rest} />
+      <Box
+        as="i"
+        className={classes([
+          className,
+          faRegularTop ? 'far' : 'fas',
+          'fa-' + faNameTop,
+          spin && 'fa-spin',
+          'fa-stack-' + sizeTop,
+        ])}
+        style={style}
+        {...rest} />
+    </Box>
+  );
+};
+
+
+Icon.Stack = IconStack;
