e07876a8378a4c0359d701225cb4d3531dd47434
Jared-Fogle <35135081+Jared-Fogle@users.noreply.github.com>
2020-10-29 02:57:49 -0700

    New view tracked playtime menu

---
+++ tgui/packages/tgui/interfaces/TrackedPlaytime.js
@@ -0,0 +1,111 @@
+import { sortBy } from "common/collections";
+import { useBackend } from "../backend";
+import { Box, Flex, ProgressBar, Section, Table } from "../components";
+import { Window } from "../layouts";
+
+const JOB_REPORT_MENU_FAIL_REASON_TRACKING_DISABLED = 1;
+const JOB_REPORT_MENU_FAIL_REASON_NO_RECORDS = 2;
+
+const sortByPlaytime = sortBy(([_, playtime]) => -playtime);
+
+const PlaytimeSection = props => {
+  const { playtimes, total, totalName } = props;
+  const sortedPlaytimes = sortByPlaytime(Object.entries(playtimes));
+  const mostPlayed = sortedPlaytimes[0][1];
+
+  return (
+    <Table>
+      <Table.Row mb={1}>
+        <Table.Cell collapsing p={0.65} header style={{
+          "vertical-align": "middle",
+        }}>
+          <Box align="right">{totalName}</Box>
+        </Table.Cell>
+
+        <Table.Cell>
+          <ProgressBar value={1}>
+            {total.toLocaleString()}h
+          </ProgressBar>
+        </Table.Cell>
+      </Table.Row>
+
+      {sortedPlaytimes.map(([jobName, playtime]) => {
+        const ratio = playtime / mostPlayed;
+
+        return (
+          <Table.Row key={jobName}>
+            <Table.Cell collapsing p={0.5} style={{
+              "vertical-align": "middle",
+            }}>
+              <Box align="right">{jobName}</Box>
+            </Table.Cell>
+
+            <Table.Cell>
+              <ProgressBar
+                maxValue={mostPlayed}
+                value={playtime}>
+                <Flex>
+                  <Flex.Item width={`${ratio * 100}%`} />
+
+                  <Flex.Item>
+                    {(playtime / 60).toLocaleString(undefined, {
+                      "minimumFractionDigits": 1,
+                      "maximumFractionDigits": 1,
+                    })}h
+                  </Flex.Item>
+                </Flex>
+              </ProgressBar>
+            </Table.Cell>
+          </Table.Row>
+        );
+      })}
+    </Table>
+  );
+};
+
+export const TrackedPlaytime = (props, context) => {
+  const { data } = useBackend(context);
+  const {
+    failReason,
+    jobPlaytimes,
+    specialPlaytimes,
+
+    livingTime,
+    ghostTime,
+  } = data;
+
+  return (
+    <Window
+      title="Tracked Playtime"
+      width={550}
+      height={650}
+      resizable>
+      <Window.Content scrollable>
+        {failReason && (
+          failReason === JOB_REPORT_MENU_FAIL_REASON_TRACKING_DISABLED
+            && <Box>This server has disabled tracking.</Box>
+          || failReason === JOB_REPORT_MENU_FAIL_REASON_NO_RECORDS
+            && <Box>You have no records.</Box>
+        ) || (
+          <Box>
+            <Section title="Jobs">
+              <PlaytimeSection
+                total={livingTime}
+                totalName="Living"
+                playtimes={jobPlaytimes}
+              />
+            </Section>
+
+            <Section title="Special">
+              <PlaytimeSection
+                total={ghostTime}
+                totalName="Ghost"
+                playtimes={specialPlaytimes}
+              />
+            </Section>
+          </Box>
+        )}
+      </Window.Content>
+    </Window>
+  );
+};
