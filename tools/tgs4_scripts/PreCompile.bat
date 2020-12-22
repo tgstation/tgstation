2caf86965f047f22917c22f2b5a4ac233f7202bf
Tad Hardesty <tad@platymuus.com>
2020-12-21 17:29:00 -0800

    Infer source tree directories in TGS3 PreCompile

--- tools/tgs4_scripts/PreCompile.bat
+++ tools/tgs4_scripts/PreCompile.bat
@@ -1,5 +1,14 @@
 @echo off
 set TG_BOOTSTRAP_CACHE=%cd%
-cd %1
+IF NOT "%1" == "" (
+	rem TGS4: we are passed the game directory on the command line
+	cd %1
+) ELSE IF EXIST "..\Game\A\tgstation.dmb" (
+	rem TGS3: Game/A/tgstation.dmb exists, so build in Game/B
+	cd ..\Game\B
+) ELSE (
+	rem TGS3: Otherwise build in Game/A
+	cd ..\Game\A
+)
 set TG_BUILD_TGS_MODE=1
 tools\build\build
