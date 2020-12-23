4f65f5a8a7feeaac63102f2bf41278e930708e13
Tad Hardesty <tad@platymuus.com>
2020-12-22 16:34:12 -0800

    Prefer Game/B rather than Game/A
    Co-authored-by: Jordan Brown <Cyberboss@users.noreply.github.com>

--- tools/tgs4_scripts/PreCompile.bat
+++ tools/tgs4_scripts/PreCompile.bat
@@ -4,12 +4,12 @@ set TG_BOOTSTRAP_CACHE=%cd%
 IF NOT "%1" == "" (
 	rem TGS4: we are passed the game directory on the command line
 	cd %1
-) ELSE IF EXIST "..\Game\A\tgstation.dmb" (
-	rem TGS3: Game/A/tgstation.dmb exists, so build in Game/B
-	cd ..\Game\B
-) ELSE (
-	rem TGS3: Otherwise build in Game/A
+) ELSE IF EXIST "..\Game\B\tgstation.dmb" (
+	rem TGS3: Game/B/tgstation.dmb exists, so build in Game/A
 	cd ..\Game\A
+) ELSE (
+	rem TGS3: Otherwise build in Game/B
+	cd ..\Game\B
 )
 set TG_BUILD_TGS_MODE=1
 tools\build\build
