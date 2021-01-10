0089653f3f59fa65d5624027be9be9dcd069ad87
Aleksej Komarov <stylemistake@gmail.com>
2021-01-10 04:56:28 +0200

    tgui: Stories

---
+++ tgui/packages/tgui/stories/Stack.stories.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @copyright 2021 Aleksej Komarov
+ * @license MIT
+ */
+
+import { Section } from '../components';
+
+export const meta = {
+  title: 'Stack',
+  render: () => <Story />,
+};
+
+const Story = (props, context) => {
+  return (
+    <Section>
+      asdfasdf
+    </Section>
+  );
+};
