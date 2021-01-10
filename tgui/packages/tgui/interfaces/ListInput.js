37b075a8e816403e5b2c6fcc8fffdb5abf863c9e
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2021-01-10 15:22:54 +0000

    Added arrow-key scrolling

--- tgui/packages/tgui/interfaces/ListInput.js
+++ tgui/packages/tgui/interfaces/ListInput.js
@@ -9,6 +9,9 @@ import { useBackend, useLocalState } from '../backend';
 import { Box, Button, Flex, Section, Input } from '../components';
 import { Window } from '../layouts';
 
+let ARROW_KEY_UP = 38;
+let ARROW_KEY_DOWN = 40;
+
 let lastScrollTime = 0;
 
 export const ListInput = (props, context) => {
@@ -30,7 +33,7 @@ export const ListInput = (props, context) => {
   const [lastCharCode, setLastCharCode] = useLocalState(context, "last_char_code", null);
 
   // Selected Button
-  const [selectedButton, setSelectedButton] = useLocalState(context, "selected_button", null);
+  const [selectedButton, setSelectedButton] = useLocalState(context, "selected_button", buttons[0]);
   return (
     <Window
       title={title}
@@ -53,9 +56,32 @@ export const ListInput = (props, context) => {
               title={message}
               tabIndex={0}
               onKeyDown={e => {
+                e.preventDefault();
+                if (lastScrollTime > performance.now()) {
+                  return;
+                }
+                lastScrollTime = performance.now() + 125;
+
+                if (e.keyCode === ARROW_KEY_UP || e.keyCode === ARROW_KEY_DOWN)
+                {
+                  let direction = 1;
+                  if (e.keyCode === ARROW_KEY_UP) direction = -1;
+
+                  let index = 0;
+                  for (index; index < buttons.length; index++) {
+                    if (buttons[index] === selectedButton) break;
+                  }
+                  index += direction;
+                  if (index < 0) index = buttons.length-1;
+                  else if (index >= buttons.length) index = 0;
+                  setSelectedButton(buttons[index]);
+                  setLastCharCode(null);
+                  document.getElementById(buttons[index]).focus();
+                  return;
+                }
+
                 const charCode = String.fromCharCode(e.keyCode).toLowerCase();
-                if (!charCode || lastScrollTime > performance.now()) return;
-                lastScrollTime = performance.now() + 150;
+                if (!charCode) return;
 
                 let foundValue;
                 if (charCode === lastCharCode && searchArray.length > 0) {
@@ -106,7 +132,7 @@ export const ListInput = (props, context) => {
               <Flex
                 wrap="wrap"
               >
-                {displayedArray.map(button => (
+                {displayedArray.map((button, index) => (
                   <Flex.Item key={button} basis="100%">
                     <Button
                       color="transparent"
