e1a90b98258580b32e7b010fc6f649303f147a82
Tad Hardesty <tad@platymuus.com>
2021-01-11 17:01:31 -0800

    Remove tools/ci/install_build_tools.sh

--- tools/ci/install_build_tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-base_dir="$(dirname "${0}")"
-
-source dependencies.sh
-source "${base_dir}/install_node.sh"
-pip3 install --user PyYaml
-pip3 install --user beautifulsoup4
