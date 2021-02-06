18ad634b5c00aeeb81eeb91609a579124c4eb92a
Fermi <33956696+Thalpy@users.noreply.github.com>
2021-02-06 01:17:11 +0000

    Redid thermometer ui

--- tgui/packages/tgui/interfaces/Thermometer.js
+++ tgui/packages/tgui/interfaces/Thermometer.js
@@ -1,46 +1,44 @@
 import { useBackend } from '../backend';
-import { Box, Button, Icon, LabeledList, ProgressBar, Section } from '../components';
+import { Stack } from '../components';
 import { Window } from '../layouts';
+import { ThermometerIcon } from './common/ThermometerIcon';
+import { Component } from 'inferno'
 
-export const Thermometer = (props, context) => {
-  const { act, data } = useBackend(context);
-  const style = {};
-  style['transform'] = `rotate(${270}deg)`;
-  return (
-    <Window
-      width={90}
-      height={480}
-      key="Thermometer"
-      resizable>
-      <Window.Content>
-        <Section>
-          <Box height={40}>
-            <Icon
-              mt={60}
-              size={5}
-              top={30}
-              name="circle"
-              color="#bd2020" />
-            <ProgressBar 
-              left={-12.4} 
-              top={-20.5} 
-              height={2} 
-              width={30} 
-              maxValue={1000} 
-              color="red" 
-              style={style} 
-              value={data.Temperature}>
-              {null}
-            </ProgressBar> 
-          </Box>
-          <Box 
-            mt={-16.5} 
-            ml={2.5} 
-            top={-70.5}>
-            {data.Temperature}K
-          </Box>
-        </Section>
-      </Window.Content>
-    </Window>
-  );
-};
+export class Thermometer extends Component {
+  componentDidMount() {
+    Byond.winset(window.__windowId__, {
+      'transparent-color': '#242322',
+    });
+  }
+  
+  componentWillUnmount() {
+      Byond.winset(window.__windowId__, {
+        'transparent-color': null,
+      });
+    }
+  
+  render() {
+    const { act, data } = useBackend(this.context);
+    return (
+      <Window
+        width={70}
+        height={430}
+        key="Thermometer">
+          <Stack
+            fill
+            align="center"
+            justify="space-around"
+            backgroundColor="#242322"
+            style={{
+              'background-image': "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACAQMAAABIeJ9nAAAABlBMVEVya3UjIyN3S/1dAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAMSURBVAjXY2hgcAAAAcQAwUlFKkkAAAAASUVORK5CYII=')"
+            }} >
+            <Stack.Item ml={1}>
+              <ThermometerIcon
+                temperature={data.Temperature}
+                maxTemperature={1000} />
+            </Stack.Item>
+          </Stack>
+      </Window>
+    );
+  };
+}; 
