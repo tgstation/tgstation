d72d551f32ce77b9d063539383fb32accc240023
Floyd <Floydje123@hotmail.com>
2021-02-16 19:35:01 +0100

    some more

--- tgui/packages/tgui/interfaces/TachyonArray.js
+++ tgui/packages/tgui/interfaces/TachyonArray.js
@@ -13,7 +13,7 @@ export const TachyonArray = (props, context) => {
       width={550}
       height={250}
       resizable>
-      <Window.Content>
+      <Window.Content scrollable>
         <Flex direction="column" height="100%">
           <Flex.Item grow={1} className="TachyonArray__Content">
             {!records.length ? (
