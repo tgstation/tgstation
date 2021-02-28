905b61acf6cb60c5ad9f42d9acff6b67de2b3ddc
mozi_h <die-123-321@web.de>
2021-02-28 19:14:50 +0100

    TGUI for clipboards and better functionality

---
+++ tgui/packages/tgui/interfaces/Clipboard.js
@@ -0,0 +1,122 @@
+import { useBackend } from "../backend";
+import {
+  Box,
+  Button,
+  Divider,
+  LabeledList,
+  Flex,
+  Section,
+} from "../components";
+import { Window } from "../layouts";
+
+export const Clipboard = (props, context) => {
+  const { act, data } = useBackend(context);
+  var { pen, top_paper, top_paper_ref, paper, paper_ref } = data;
+  return (
+    <Window resizable title="Clipboard" theme="nt" width={400} height={500}>
+      <Window.Content backgroundColor="#704D25" scrollable>
+        <Section>
+          {pen ? (
+            <LabeledList>
+              <LabeledList.Item
+                label="Pen"
+                buttons={
+                  <Button icon="eject" onClick={() => act("remove_pen")} />
+                }
+              >
+                {pen}
+              </LabeledList.Item>
+            </LabeledList>
+          ) : (
+            <Box color="grey" align="center">
+              No pen attached!
+            </Box>
+          )}
+        </Section>
+        <Divider />
+        {top_paper ? (
+          <>
+            <Flex
+              color="black"
+              backgroundColor="white"
+              style={{ padding: "2px 2px 0 2px" }}
+            >
+              <Flex.Item align="center" grow={1}>
+                <Box align="center">{top_paper}</Box>
+              </Flex.Item>
+              <Flex.Item>
+                <Button
+                  icon={pen ? "pen" : "eye"}
+                  onClick={() => act("edit_paper", { ref: top_paper_ref })}
+                />
+                <Button
+                  icon="tag"
+                  onClick={() => act("rename_paper", { ref: top_paper_ref })}
+                />
+                <Button
+                  icon="eject"
+                  onClick={() => act("remove_paper", { ref: top_paper_ref })}
+                />
+              </Flex.Item>
+            </Flex>
+            {paper.length > 0 && (
+              <>
+                <Divider />
+                {paper.map((paper_item, index) => (
+                  <>
+                    <Flex
+                      color="black"
+                      backgroundColor="white"
+                      style={{ padding: "2px 2px 0 2px" }}
+                    >
+                      <Flex.Item>
+                        <Button
+                          icon="chevron-up"
+                          color="transparent"
+                          iconColor="black"
+                          onClick={() =>
+                            act("move_top_paper", { ref: paper_ref[index] })
+                          }
+                        />
+                      </Flex.Item>
+                      <Flex.Item align="center" grow={1}>
+                        <Box align="center">{paper_item}</Box>
+                      </Flex.Item>
+                      <Flex.Item>
+                        <Button
+                          icon={pen ? "pen" : "eye"}
+                          onClick={() =>
+                            act("edit_paper", { ref: paper_ref[index] })
+                          }
+                        />
+                        <Button
+                          icon="tag"
+                          onClick={() =>
+                            act("rename_paper", { ref: paper_ref[index] })
+                          }
+                        />
+                        <Button
+                          icon="eject"
+                          onClick={() =>
+                            act("remove_paper", { ref: paper_ref[index] })
+                          }
+                        />
+                      </Flex.Item>
+                    </Flex>
+                    <Box style={{ height: "0.25em" }}></Box>
+                  </>
+                ))}
+              </>
+            )}
+          </>
+        ) : (
+          <Section>
+            <Box color="grey" align="center">
+              No paper attached!
+            </Box>
+          </Section>
+        )}
+      </Window.Content>
+    </Window>
+  );
+};
