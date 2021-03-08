4b3679f8c2fa8459efe1cc08e2dea00fdd501eba
zxaber <37497534+zxaber@users.noreply.github.com>
2021-03-07 18:01:13 -0800

    Fine, I'll use the autofix tool, sheesh

--- tgui/packages/tgui/interfaces/NtosCyborgRemoteMonitor.js
+++ tgui/packages/tgui/interfaces/NtosCyborgRemoteMonitor.js
@@ -14,7 +14,7 @@ export const NtosCyborgRemoteMonitor = (props, context) => {
   );
 };
 
-export const ProgressSwitch = (param) => {
+export const ProgressSwitch = param => {
   switch (param) {
     case -1:
       return '_';
@@ -28,7 +28,7 @@ export const ProgressSwitch = (param) => {
       return 'Downloading';
     case 100:
       return 'Formatting';
-  };
+  }
 };
 
 export const NtosCyborgRemoteMonitorContent = (props, context) => {
@@ -54,139 +54,138 @@ export const NtosCyborgRemoteMonitorContent = (props, context) => {
       direction={"column"}
       vertical
       fill>
-    <Stack.Item
-      position="relative"
-      mb={1}>
-      <Tabs>
-        <Tabs.Tab
-          icon="robot"
-          lineHeight="23px"
-          selected={tab_main === 1}
-          onClick={() => setTab_main(1)}>
-          Cyborgs
-        </Tabs.Tab>
-        <Tabs.Tab
-          icon="clipboard"
-          lineHeight="23px"
-          selected={tab_main === 2}
-          onClick={() => setTab_main(2)}>
-          Stored Log File
-        </Tabs.Tab>
-      </Tabs>
-    </Stack.Item>
-    {tab_main === 1 && (
-      <>
-        {!card && (
-          <NoticeBox>
-            Certain features require an ID card login.
-          </NoticeBox>
-        )}
-        <Stack.Item
-          grow={1}>
-          <Section
-            fill
-            scrollable>
-            {cyborgs.map(cyborg => {
-              return (
-                <Section
-                  key={cyborg.ref}
-                  title={cyborg.name}
-                  buttons={(
-                    <Button
-                      icon="terminal"
-                      content="Send Message"
-                      color="blue"
-                      disabled={!card}
-                      onClick={() => act('messagebot', {
-                        ref: cyborg.ref,
-                      })} />
-                  )}>
-                  <LabeledList>
-                    <LabeledList.Item label="Status">
-                      <Box color={cyborg.status
-                        ? 'bad'
-                        : cyborg.locked_down
-                          ? 'average'
-                          : 'good'}>
-                        {cyborg.status
-                          ? "Not Responding"
+      <Stack.Item
+        position="relative"
+        mb={1}>
+        <Tabs>
+          <Tabs.Tab
+            icon="robot"
+            lineHeight="23px"
+            selected={tab_main === 1}
+            onClick={() => setTab_main(1)}>
+            Cyborgs
+          </Tabs.Tab>
+          <Tabs.Tab
+            icon="clipboard"
+            lineHeight="23px"
+            selected={tab_main === 2}
+            onClick={() => setTab_main(2)}>
+            Stored Log File
+          </Tabs.Tab>
+        </Tabs>
+      </Stack.Item>
+      {tab_main === 1 && (
+        <>
+          {!card && (
+            <NoticeBox>
+              Certain features require an ID card login.
+            </NoticeBox>
+          )}
+          <Stack.Item
+            grow={1}>
+            <Section
+              fill
+              scrollable>
+              {cyborgs.map(cyborg => {
+                return (
+                  <Section
+                    key={cyborg.ref}
+                    title={cyborg.name}
+                    buttons={(
+                      <Button
+                        icon="terminal"
+                        content="Send Message"
+                        color="blue"
+                        disabled={!card}
+                        onClick={() => act('messagebot', {
+                          ref: cyborg.ref,
+                        })} />
+                    )}>
+                    <LabeledList>
+                      <LabeledList.Item label="Status">
+                        <Box color={cyborg.status
+                          ? 'bad'
                           : cyborg.locked_down
-                            ? "Locked Down"
-                            : cyborg.shell_discon
-                              ? "Nominal/Disconnected"
-                              : "Nominal"}
-                      </Box>
-                    </LabeledList.Item>
-                    <LabeledList.Item label="Condition">
-                      <Box color={cyborg.integ <= 25
-                        ? 'bad'
-                        : cyborg.integ <= 75
-                          ? 'average'
-                          : 'good'}>
-                        {cyborg.integ == 0
-                          ? "Hard Fault"
-                          : cyborg.integ <= 25
-                          ? "Functionality Disrupted"
+                            ? 'average'
+                            : 'good'}>
+                          {cyborg.status
+                            ? "Not Responding"
+                            : cyborg.locked_down
+                              ? "Locked Down"
+                              : cyborg.shell_discon
+                                ? "Nominal/Disconnected"
+                                : "Nominal"}
+                        </Box>
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Condition">
+                        <Box color={cyborg.integ <= 25
+                          ? 'bad'
                           : cyborg.integ <= 75
-                          ? "Functionality Impaired"
-                          : "Operational"
-                        }
-                      </Box>
-                    </LabeledList.Item>
-                    <LabeledList.Item label="Charge">
-                      <Box color={cyborg.charge <= 30
-                        ? 'bad'
-                        : cyborg.charge <= 70
-                          ? 'average'
-                          : 'good'}>
-                        {typeof cyborg.charge === 'number'
-                          ? cyborg.charge + "%"
-                          : "Not Found"}
-                      </Box>
-                    </LabeledList.Item>
-                    <LabeledList.Item label="Model">
-                      {cyborg.module}
-                    </LabeledList.Item>
-                    <LabeledList.Item label="Upgrades">
-                      {cyborg.upgrades}
-                    </LabeledList.Item>
-                  </LabeledList>
-                </Section>
-              );
-            })}
-          </Section>
-        </Stack.Item>
-      </>
-    )}
-    {tab_main === 2 && (
-      <Fragment>
-        <Stack.Item>
-          <Section>
-            Scan a cyborg to download stored logs.
-            <ProgressBar
-              value={DL_progress/100}>
-              {ProgressSwitch(DL_progress)}
-            </ProgressBar>
-          </Section>
-        </Stack.Item>
-        <Stack.Item
-          grow={1}>
-          <Section
-            fill
-            scrollable
-            backgroundColor="black">
-            {storedlog.map(log => (
-              <Box
-                mb={1}
-                key={log}
-                color="green">
-                {log}
-              </Box>
-            ))}
-          </Section>
-        </Stack.Item>
-      </Fragment>
-    )}
+                            ? 'average'
+                            : 'good'}>
+                          {cyborg.integ === 0
+                            ? "Hard Fault"
+                            : cyborg.integ <= 25
+                              ? "Functionality Disrupted"
+                              : cyborg.integ <= 75
+                                ? "Functionality Impaired"
+                                : "Operational"}
+                        </Box>
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Charge">
+                        <Box color={cyborg.charge <= 30
+                          ? 'bad'
+                          : cyborg.charge <= 70
+                            ? 'average'
+                            : 'good'}>
+                          {typeof cyborg.charge === 'number'
+                            ? cyborg.charge + "%"
+                            : "Not Found"}
+                        </Box>
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Model">
+                        {cyborg.module}
+                      </LabeledList.Item>
+                      <LabeledList.Item label="Upgrades">
+                        {cyborg.upgrades}
+                      </LabeledList.Item>
+                    </LabeledList>
+                  </Section>
+                );
+              })}
+            </Section>
+          </Stack.Item>
+        </>
+      )}
+      {tab_main === 2 && (
+        <Fragment>
+          <Stack.Item>
+            <Section>
+              Scan a cyborg to download stored logs.
+              <ProgressBar
+                value={DL_progress/100}>
+                {ProgressSwitch(DL_progress)}
+              </ProgressBar>
+            </Section>
+          </Stack.Item>
+          <Stack.Item
+            grow={1}>
+            <Section
+              fill
+              scrollable
+              backgroundColor="black">
+              {storedlog.map(log => (
+                <Box
+                  mb={1}
+                  key={log}
+                  color="green">
+                  {log}
+                </Box>
+              ))}
+            </Section>
+          </Stack.Item>
+        </Fragment>
+      )}
     </Stack>
   );
 };
