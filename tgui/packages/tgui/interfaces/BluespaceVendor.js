eddf05edd81f9b1c29c11d4574bebd524dd7bbc7
tralezab <spamqetuo2@gmail.com>
2021-03-09 23:33:36 -0800

    BIGGERING AND FIGURING MORE BIGGERING

--- tgui/packages/tgui/interfaces/BluespaceVendor.js
+++ tgui/packages/tgui/interfaces/BluespaceVendor.js
@@ -1,6 +1,7 @@
 import { filter, sortBy } from 'common/collections';
 import { flow } from 'common/fp';
 import { toFixed } from 'common/math';
+import { multiline } from 'common/string';
 import { useBackend } from '../backend';
 import { Button, Divider, LabeledList, NumberInput, ProgressBar, Section, Stack, Box } from '../components';
 import { getGasColor, getGasLabel } from '../constants';
@@ -29,45 +30,70 @@ export const BluespaceVendor = (props, context) => {
         <Stack vertical fill>
           <Stack.Item>
             <Section
-            title="Controls">
-              <NumberInput
-                animated
-                value={gas_transfer_rate}
-                width="63px"
-                unit="% of tank filled"
-                minValue={0}
-                maxValue={100}
-                onDrag={(e, value) => act('pumping_rate', {
-                  rate: value,
-                })} />
-              <Button
-                ml={1}
-                icon="plus"
-                content="Prepare Tank"
-                disabled={data.pumping || data.inserted_tank || !data.tank_amount}
-                onClick={() => act('tank_prepare')} />
-              <Button
-                ml={1}
-                icon="minus"
-                content="Remove Tank"
-                disabled={data.pumping || !data.inserted_tank}
-                onClick={() => act('tank_expel'
-                )} />
-              <Box>
-                {
-                  <ProgressBar
-                    value={data.tank_full / 1010}
-                    ranges={{
-                      good: [0.67, 1],
-                      average: [0.34, 0.66],
-                      bad: [0, 0.33],
-                    }} />
-                }
-              </Box>
+              title="Controls"
+              buttons={(
+                <>
+                  <Button
+                    ml={1}
+                    icon="plus"
+                    content="Prepare Tank"
+                    disabled={
+                      data.pumping || data.inserted_tank || !data.tank_amount
+                    }
+                    onClick={() => act('tank_prepare')} />
+                  <Button
+                    ml={1}
+                    icon="minus"
+                    content="Remove Tank"
+                    disabled={data.pumping || !data.inserted_tank}
+                    onClick={() => act('tank_expel')} />
+                </>
+              )}>
+              <Stack>
+                <Stack.Item>
+                  <NumberInput
+                    animated
+                    value={gas_transfer_rate}
+                    width="63px"
+                    unit="% tank filling goal"
+                    minValue={0}
+                    maxValue={100}
+                    onDrag={(e, value) => act('pumping_rate', {
+                      rate: value,
+                    })} />
+                </Stack.Item>
+                <Stack.Item grow>
+                  {
+                    <ProgressBar
+                      value={data.tank_full / 1010}
+                      ranges={{
+                        good: [0.67, 1],
+                        average: [0.34, 0.66],
+                        bad: [0, 0.33],
+                      }} />
+                  }
+                </Stack.Item>
+              </Stack>
+
+
             </Section>
           </Stack.Item>
           <Stack.Item grow>
-            <Section scrollable fill title="Bluespace Network Gases">
+            <Section
+              scrollable
+              fill
+              title="Bluespace Network Gases"
+              buttons={(
+                <Button
+                  color="transparent"
+                  icon="info"
+                  tooltipPosition="bottom-left"
+                  tooltip={multiline`
+                  Quick guide for machine use: prepare a tank to create a
+                  new one in the machine, pick how much you want it filled,
+                  and finally press start on the gas of your choice!
+                `} />
+              )}>
               <LabeledList>
                 {bluespace_network_gases.map(gas => (
                   <>
@@ -77,30 +103,34 @@ export const BluespaceVendor = (props, context) => {
                       </Stack.Item>
                       <Stack.Item grow mt={1}>
                         <ProgressBar
-                        color={getGasColor(gas.name)}
-                        value={gas.amount}
-                        minValue={0}
-                        maxValue={gasMax}>
+                          color={getGasColor(gas.name)}
+                          value={gas.amount}
+                          minValue={0}
+                          maxValue={gasMax}>
                           {toFixed(gas.amount, 2) + ' moles'}
                         </ProgressBar>
                       </Stack.Item>
                       <Stack.Item ml={-0.1} mr={1} mt={1}>
                         {!data.pumping && data.selected_gas !== gas.id && (
                           <Button
-                          ml={1}
-                          icon="play"
-                          disabled={data.pumping || !data.inserted_tank}
-                          onClick={() => act('start_pumping', {
-                            gas_id: gas.id,
-                          })} />
+                            ml={1}
+                            icon="play"
+                            tooltipPosition="left"
+                            tooltip={"Start adding " + gas.name + "."}
+                            disabled={data.pumping || !data.inserted_tank}
+                            onClick={() => act('start_pumping', {
+                              gas_id: gas.id,
+                            })} />
                         ) || (
                           <Button
-                          ml={1}
-                          icon="minus"
-                          content="Stop Pumping"
-                          onClick={() => act('stop_pumping', {
-                            gas_id: gas.id,
-                          })} />
+                            ml={1}
+                            disabled={data.selected_gas !== gas.id}
+                            icon="minus"
+                            tooltipPosition="left"
+                            tooltip={"Stop adding " + gas.name + "."}
+                            onClick={() => act('stop_pumping', {
+                              gas_id: gas.id,
+                            })} />
                         )}
                       </Stack.Item>
                     </Stack>
