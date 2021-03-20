207b073465dc5511525cca7243d43bbd8d292bf3
Time-Green <timkoster1@hotmail.com>
2021-03-20 15:05:01 +0100

    fixes indentation?

--- tgui/packages/tgui/interfaces/ChemReactionChamber.js
+++ tgui/packages/tgui/interfaces/ChemReactionChamber.js
@@ -158,38 +158,38 @@ export const ChemReactionChamber = (props, context) => {
             </tr>
             <tr className="LabledList__row">
               <td
-                  colSpan="2"
-                  className="LabeledList__cell">
-                  <Input
-                    fluid
-                    value=""
-                    placeholder="Reagent Name"
-                    onInput={(e, value) => setReagentName(value)} />
-                </td>
-                <td
-                  className={classes([
-                    "LabeledList__buttons",
-                    "LabeledList__cell",
-                  ])}>
-                  <NumberInput
-                    value={reagentQuantity}
-                    minValue={1}
-                    maxValue={100}
-                    step={1}
-                    stepPixelSize={3}
-                    width="39px"
-                    onDrag={(e, value) => setReagentQuantity(value)} />
-                  <Box inline mr={1} />
-                  <Button
-                    icon="plus"
-                    onClick={() => act('add', {
-                      chem: reagentName,
-                      amount: reagentQuantity,
-                    })} />
-                </td>
-              </tr>
-              {map((amount, reagent) => (
-              <LabeledList.Item
+                colSpan="2"
+                className="LabeledList__cell">
+                <Input
+                  fluid
+                  value=""
+                  placeholder="Reagent Name"
+                  onInput={(e, value) => setReagentName(value)} />
+              </td>
+              <td
+                className={classes([
+                  "LabeledList__buttons",
+                  "LabeledList__cell",
+                ])}>
+                <NumberInput
+                  value={reagentQuantity}
+                  minValue={1}
+                  maxValue={100}
+                  step={1}
+                  stepPixelSize={3}
+                  width="39px"
+                  onDrag={(e, value) => setReagentQuantity(value)} />
+                <Box inline mr={1} />
+                <Button
+                  icon="plus"
+                  onClick={() => act('add', {
+                    chem: reagentName,
+                    amount: reagentQuantity,
+                  })} />
+              </td>
+            </tr>
+            {map((amount, reagent) => (
+            <LabeledList.Item
                 key={reagent}
                 label={reagent}
                 buttons={(
