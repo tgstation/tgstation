fc476888a5d0809510b3d52fb416876b764377be
Trigg <sharkboysarrador@gmail.com>
2021-03-25 15:41:31 -0300

    more d y n a m i c

--- tgui/packages/tgui/interfaces/BigPain.js
+++ tgui/packages/tgui/interfaces/BigPain.js
@@ -4,32 +4,6 @@ import { Window } from '../layouts';
 
 export const BigPain = (props, context) => {
   const { act, data } = useBackend(context);
-  const { OutfitSlots } = data;
-  const {
-    head,
-    glasses,
-    ears,
-
-    neck,
-    mask,
-
-    uniform,
-    suit,
-    gloves,
-
-    suit_store,
-    belt,
-    id,
-
-    l_hand,
-    back,
-    r_hand,
-
-    l_pocket,
-    shoes,
-    r_pocket,
-
-  } = OutfitSlots;
 
   return (
     <Window
@@ -39,33 +13,33 @@ export const BigPain = (props, context) => {
 
         <Section fill>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Headgear" currItem={head} />
-            <OutfitSlot slotName="Glasses" currItem={glasses} />
-            <OutfitSlot slotName="Ears" currItem={ears} />
+            <OutfitSlot name="Headgear" slot="head" />
+            <OutfitSlot name="Glasses" slot="glasses" />
+            <OutfitSlot name="Ears" slot="ears" />
           </Stack>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Neck" currItem={neck} />
-            <OutfitSlot slotName="Mask" currItem={mask} />
+            <OutfitSlot name="Neck" slot="neck" />
+            <OutfitSlot name="Mask" slot="mask" />
           </Stack>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Uniform" currItem={uniform} />
-            <OutfitSlot slotName="Suit" currItem={suit} />
-            <OutfitSlot slotName="Gloves" currItem={gloves} />
+            <OutfitSlot name="Uniform" slot="uniform" />
+            <OutfitSlot name="Suit" slot="suit" />
+            <OutfitSlot name="Gloves" slot="gloves" />
           </Stack>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Suit Storage" currItem={suit_store} />
-            <OutfitSlot slotName="Belt" currItem={belt} />
-            <OutfitSlot slotName="ID" currItem={id} />
+            <OutfitSlot name="Suit Storage" slot="suit_store" />
+            <OutfitSlot name="Belt" slot="belt" />
+            <OutfitSlot name="ID" slot="id" />
           </Stack>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Left Hand" currItem={l_hand} />
-            <OutfitSlot slotName="Back" currItem={back} />
-            <OutfitSlot slotName="Right Hand" currItem={r_hand} />
+            <OutfitSlot name="Left Hand" slot="l_hand" />
+            <OutfitSlot name="Back" slot="back" />
+            <OutfitSlot name="Right Hand" slot="r_hand" />
           </Stack>
           <Stack mb={2} justify="center">
-            <OutfitSlot slotName="Left Pocket" currItem={l_pocket} />
-            <OutfitSlot slotName="Shoes" icon="socks" currItem={shoes} />
-            <OutfitSlot slotName="Right Pocket" currItem={r_pocket} />
+            <OutfitSlot name="Left Pocket" slot="l_pocket" />
+            <OutfitSlot name="Shoes" icon="socks" slot="shoes" />
+            <OutfitSlot name="Right Pocket" slot="r_pocket" />
           </Stack>
         </Section>
 
@@ -76,11 +50,12 @@ export const BigPain = (props, context) => {
 
 const OutfitSlot = (props, context) => {
   const { act, data } = useBackend(context);
-  const { slotName, currItem, icon } = props;
+  const { name, slot, icon } = props;
+  const currItem = data.OutfitSlots[slot];
   return (
     <Stack.Item grow={1} basis={0} textAlign="center">
       <Button icon={icon} fluid height={2}>
-        <b>{slotName}</b>
+        <b>{name}</b>
       </Button>
       <Box
         color="label"
