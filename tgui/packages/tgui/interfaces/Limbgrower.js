6f3f35613e2d4869273e48035e6c40d3eb04c55d
Aleksej Komarov <stylemistake@gmail.com>
2021-03-26 01:48:21 +0200

    Update Limbgrower.js

--- tgui/packages/tgui/interfaces/Limbgrower.js
+++ tgui/packages/tgui/interfaces/Limbgrower.js
@@ -11,15 +11,17 @@ export const Limbgrower = (props, context) => {
     categories = [],
     busy,
   } = data;
-  const [tab, setTab] = useSharedState(context, 'category', categories[0]?.name);
-  const designList = categories.find(category =>
-    category.name === tab)?.designs;
+  const [tab, setTab] = useSharedState(
+    context, 'category', categories[0]?.name);
+  const designList = categories
+    .find(category => category.name === tab)
+    ?.designs;
 
   return (
     <Window
+      title="Limb Grower"
       width={400}
-      height={550}
-      title="Limb Grower">
+      height={550}>
       {!!busy && (
         <Dimmer fontSize="32px">
           <Icon name="cog" spin={1} />
@@ -28,71 +30,63 @@ export const Limbgrower = (props, context) => {
       )}
       <Window.Content scrollable>
         <Section title="Reagents">
-          <Flex direction="column">
-            <Flex.Item mb={1}>
-              {total_reagents} / {max_reagents} reagent capacity used.
-            </Flex.Item>
-            <Flex.Item>
-              <LabeledList>
-                { reagents.map(reagent => (
-                  <LabeledList.Item
-                    key={reagent.reagent_name}
-                    label={reagent.reagent_name}
-                    buttons={(
-                      <Button.Confirm
-                        textAlign="center"
-                        width="120px"
-                        content="Remove Reagent"
-                        color="bad"
-                        onClick={() => act('empty_reagent', { reagent_type: reagent.reagent_type })} />)} >
-                    {reagent.reagent_amount}u
-                  </LabeledList.Item>
-                ))}
-              </LabeledList>
-            </Flex.Item>
-          </Flex>
+          <Box mb={1}>
+            {total_reagents} / {max_reagents} reagent capacity used.
+          </Box>
+          <LabeledList>
+            {reagents.map(reagent => (
+              <LabeledList.Item
+                key={reagent.reagent_name}
+                label={reagent.reagent_name}
+                buttons={(
+                  <Button.Confirm
+                    textAlign="center"
+                    width="120px"
+                    content="Remove Reagent"
+                    color="bad"
+                    onClick={() => act('empty_reagent', {
+                      reagent_type: reagent.reagent_type,
+                    })} />
+                )}>
+                {reagent.reagent_amount}u
+              </LabeledList.Item>
+            ))}
+          </LabeledList>
         </Section>
-        <Divider />
         <Section title="Designs">
-          <Flex direction="column">
-            <Flex.Item>
-              <Tabs>
-                {categories.map(category => (
-                  <Tabs.Tab
-                    fluid
-                    key={category.name}
-                    selected={tab === category.name}
-                    onClick={() => setTab(category.name)}>
-                    {category.name}
-                  </Tabs.Tab>
+          <Tabs>
+            {categories.map(category => (
+              <Tabs.Tab
+                fluid
+                key={category.name}
+                selected={tab === category.name}
+                onClick={() => setTab(category.name)}>
+                {category.name}
+              </Tabs.Tab>
+            ))}
+          </Tabs>
+          <LabeledList>
+            {designList.map(design => (
+              <LabeledList.Item
+                key={design.name}
+                label={design.name}
+                buttons={(
+                  <Button
+                    content="Make"
+                    color="good"
+                    onClick={() => act('make_limb', {
+                      design_id: design.id,
+                      active_tab: design.parent_category,
+                    })} />
+                )}>
+                {design.needed_reagents.map(reagent => (
+                  <Box key={reagent.name}>
+                    {reagent.name}: {reagent.amount}u
+                  </Box>
                 ))}
-              </Tabs>
-            </Flex.Item>
-            <Flex.Item>
-              <LabeledList>
-                {designList.map(design => (
-                  <LabeledList.Item
-                    key={design.name}
-                    label={design.name}
-                    buttons={(
-                      <Button
-                        content="Make"
-                        color="good"
-                        onClick={() => act('make_limb', {
-                          design_id: design.id,
-                          active_tab: design.parent_category,
-                        })} />
-                    )}>
-                    {design.needed_reagents.map(reagent => (
-                      <Box key={reagent.name}>
-                        {reagent.name}: {reagent.amount}u
-                      </Box>
-                    ))}
-                  </LabeledList.Item>
-                ))}
-              </LabeledList>
-            </Flex.Item>
-          </Flex>
+              </LabeledList.Item>
+            ))}
+          </LabeledList>
         </Section>
       </Window.Content>
     </Window>
