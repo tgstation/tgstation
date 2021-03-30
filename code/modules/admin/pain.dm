4f106a8dff01f993e826f7c1497923be738254b8
Trigg <sharkboysarrador@gmail.com>
2021-03-30 17:12:59 -0300

    wack

--- code/modules/admin/pain.dm
+++ code/modules/admin/pain.dm
@@ -8,16 +8,33 @@
 	ui.ui_interact(usr)
 
 /datum/outfit_manager
-	var/client/user
+	var/client/holder
 
+	var/dummy_key
+	var/static/list/allitems
 	var/datum/outfit/drip = /datum/outfit/job/miner/equipped/hardsuit
 
-/datum/outfit_manager/New(_user)
-	user = CLIENT_FROM_VAR(_user)
+/datum/outfit_manager/New(user)
+	holder = CLIENT_FROM_VAR(user)
 	drip = new drip
 
 /datum/outfit_manager/ui_state(mob/user)
 	return GLOB.admin_state
+
+/datum/outfit_manager/ui_close(mob/user)
+	clear_human_dummy(dummy_key)
+	qdel(src)
+
+/datum/outfit_manager/proc/init_dummy()
+	dummy_key = "outfit_manager_[holder]"
+	var/mob/living/carbon/human/dummy/dummy = generate_or_wait_for_human_dummy(dummy_key)
+	var/mob/living/carbon/carbon_target = holder.mob
+	if(istype(carbon_target))
+		carbon_target.dna.transfer_identity(dummy)
+		dummy.updateappearance()
+
+	unset_busy_human_dummy(dummy_key)
+	return
 
 /datum/outfit_manager/ui_interact(mob/user, datum/tgui/ui)
 	ui = SStgui.try_update_ui(user, src, ui)
@@ -47,46 +64,89 @@
 /datum/outfit_manager/ui_data(mob/user)
 	var/list/data = list()
 
-	data["OutfitSlots"] = serialize_outfit()
+	data["outfit"] = serialize_outfit()
+
+	var/datum/preferences/prefs = holder.prefs
+	var/datum/outfit/temp_drip = drip //some pre_equip actions reset certain slots i.e /datum/outfit/job/pre_equip()
+	var/icon/dummysprite = get_flat_human_icon(null, prefs = prefs, dummy_key = dummy_key, showDirs = list(SOUTH), outfit_override = temp_drip)
+	data["dummy64"] = icon2base64(dummysprite)
+
 	return data
 
 /datum/outfit_manager/ui_act(action, list/params, datum/tgui/ui, datum/ui_state/state)
 	if(..())
 		return
+	. = TRUE
 	switch(action)
 		if("click")
-			edit_item(params["slot"])
+			choose_item(params["slot"])
+		if("save")
+			GLOB.custom_outfits |= (drip)
 
-/datum/outfit_manager/proc/edit_item(slot)
-	var/list/options
+/datum/outfit_manager/proc/edit_item(slot, obj/item/choice)
+	if(ispath(choice))
+		drip.vars[slot] = choice
+
+//this proc will try to give a good selection of items that the user can choose from
+//it does *not* give a selection of all items that can fit in a slot because lag;
+//most notably the hand and pocket slots because they accept pretty much anything
+//also stuff that fits in the belt and back slots are scattered pretty much all over the place
+/datum/outfit_manager/proc/choose_item(slot)
+	var/list/options = list()
+	if(!allitems)
+		allitems = typesof(/obj/item)
+
 	switch(slot)
 		if("head")
 			options = typesof(/obj/item/clothing/head)
 		if("glasses")
 			options = typesof(/obj/item/clothing/glasses)
-		//ears
-		//neck
+		if("ears")
+			options = typesof(/obj/item/radio/headset)
+
+		if("neck")
+			options = typesof(/obj/item/clothing/neck)
 		if("mask")
 			options = typesof(/obj/item/clothing/mask)
+
 		if("uniform")
 			options = typesof(/obj/item/clothing/under)
 		if("suit")
 			options = typesof(/obj/item/clothing/suit)
 		if("gloves")
 			options = typesof(/obj/item/clothing/gloves)
-		//suit storage
-		//belt
+
+		if("suit_store")
+			var/obj/item/clothing/suit/suit = drip.suit
+			if(suit)
+				suit = new suit //initial() doesn't like lists
+				options = suit.allowed
+		if("belt")
+			options = typesof(/obj/item/storage/belt)
 		if("id")
 			options = typesof(/obj/item/card/id)
-		//lhand
-		//back
-		//rhand
-		//lpocket
+
+		if("l_hand")
+			choose_any_item(slot)
+		if("back")
+			options = typesof(/obj/item/storage/backpack)
+		if("r_hand")
+			choose_any_item(slot)
+
+		if("l_pocket")
+			choose_any_item(slot)
 		if("shoes")
 			options = typesof(/obj/item/clothing/shoes)
 		if("r_pocket")
-			options = typesof(/obj/item)
-
+			choose_any_item(slot)
 
 	if(length(options))
-		tgui_input_list(user, "Choose an item", "Outfit-O-Tron 9000", options)
+		edit_item(slot, tgui_input_list(holder, "Choose an item", "Outfit-O-Tron 9000", options))
+
+
+
+/datum/outfit_manager/proc/choose_any_item(slot)
+	if(!allitems)
+		allitems = typesof(/obj/item)
+
+	edit_item(slot, tgui_input_list(holder, "Choose an item", "Outfit-O-Tron 9000", allitems))
--- tgui/packages/tgui/interfaces/BigPain.js
+++ tgui/packages/tgui/interfaces/BigPain.js
@@ -8,40 +8,75 @@ export const BigPain = (props, context) => {
   return (
     <Window
       width={380}
-      height={570}>
+      height={625}>
       <Window.Content>
 
-        <Section fill textAlign="center">
-          <Stack mb={2}>
-            <OutfitSlot name="Headgear" slot="head" />
-            <OutfitSlot name="Glasses" slot="glasses" />
-            <OutfitSlot name="Ears" slot="ears" />
-          </Stack>
-          <Stack mb={2}>
-            <OutfitSlot name="Neck" slot="neck" />
-            <OutfitSlot name="Mask" slot="mask" />
-          </Stack>
-          <Stack mb={2}>
-            <OutfitSlot name="Uniform" slot="uniform" />
-            <OutfitSlot name="Suit" slot="suit" />
-            <OutfitSlot name="Gloves" slot="gloves" />
-          </Stack>
-          <Stack mb={2}>
-            <OutfitSlot name="Suit Storage" slot="suit_store" />
-            <OutfitSlot name="Back" slot="back" />
-            <OutfitSlot name="ID" slot="id" />
-          </Stack>
-          <Stack mb={2}>
-            <OutfitSlot name="Belt" slot="belt" />
-            <OutfitSlot name="Left Hand" slot="l_hand" />
-            <OutfitSlot name="Right Hand" slot="r_hand" />
-          </Stack>
-          <Stack>
-            <OutfitSlot name="Shoes" icon="socks" slot="shoes" />
-            <OutfitSlot name="Left Pocket" slot="l_pocket" />
-            <OutfitSlot name="Right Pocket" slot="r_pocket" />
-          </Stack>
-        </Section>
+
+        <Stack fill vertical>
+
+          <Stack.Item grow shrink={1}>
+
+            <Section fill textAlign="center">
+              <Box
+                opacity={0.5}
+                py={3}
+                position="absolute"
+                as="img"
+                src={`data:image/jpeg;base64,${data.dummy64}`}
+                height="100%"
+                width="100%"
+                style={{
+                  '-ms-interpolation-mode': 'nearest-neighbor',
+                }} />
+
+              <Stack mb={2}>
+                <OutfitSlot name="Headgear" slot="head" />
+                <OutfitSlot name="Glasses" slot="glasses" />
+                <OutfitSlot name="Ears" slot="ears" />
+              </Stack>
+              <Stack mb={2}>
+                <OutfitSlot name="Neck" slot="neck" />
+                <OutfitSlot name="Mask" slot="mask" />
+              </Stack>
+              <Stack mb={2}>
+                <OutfitSlot name="Uniform" slot="uniform" />
+                <OutfitSlot name="Suit" slot="suit" />
+                <OutfitSlot name="Gloves" slot="gloves" />
+              </Stack>
+              <Stack mb={2}>
+                <OutfitSlot name="Suit Storage" slot="suit_store" />
+                <OutfitSlot name="Back" slot="back" />
+                <OutfitSlot name="ID" slot="id" />
+              </Stack>
+              <Stack mb={2}>
+                <OutfitSlot name="Belt" slot="belt" />
+                <OutfitSlot name="Left Hand" slot="l_hand" />
+                <OutfitSlot name="Right Hand" slot="r_hand" />
+              </Stack>
+              <Stack mb={2}>
+                <OutfitSlot name="Shoes" icon="socks" slot="shoes" />
+                <OutfitSlot name="Left Pocket" slot="l_pocket" />
+                <OutfitSlot name="Right Pocket" slot="r_pocket" />
+              </Stack>
+            </Section>
+          </Stack.Item>
+
+          <Stack.Item align="end">
+            <Section>
+              <Button
+                selected
+                lineHeight={2}
+                content="Save Outfit"
+              />
+              <Button
+                selected
+                lineHeight={2}
+                content="Save Outfit"
+              />
+            </Section>
+          </Stack.Item>
+        </Stack>
+
 
       </Window.Content>
     </Window>
@@ -51,7 +86,7 @@ export const BigPain = (props, context) => {
 const OutfitSlot = (props, context) => {
   const { act, data } = useBackend(context);
   const { name, icon, slot } = props;
-  const currItem = data.OutfitSlots[slot];
+  const currItem = data.outfit[slot];
   return (
     <Stack.Item grow={1} basis={0}>
       <Button fluid height={2}
