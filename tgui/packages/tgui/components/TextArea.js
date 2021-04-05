5048eb9f1c6a2cc9aea1ba7f56115bac99202c21
Souchu Bastien <bsouchu@gmail.com>
2021-04-05 22:29:26 +0200

    Change to TextArea.js

--- tgui/packages/tgui/components/TextArea.js
+++ tgui/packages/tgui/components/TextArea.js
@@ -1,36 +1,62 @@
 /**
  * @file
  * @copyright 2020 Aleksej Komarov
- * @author Warlockd
+ * @author Original Aleksej Komarov
+ * @author Changes Warlockd (https://github.com/warlockd)
  * @license MIT
  */
 
-import { classes } from 'common/react';
+
+import { classes, canRender } from 'common/react';
 import { Component, createRef } from 'inferno';
 import { Box } from './Box';
-import { toInputValue } from './Input';
-import { KEY_ESCAPE } from 'common/keycodes';
+
+
+const toInputValue = value => {
+  if (!canRender(value)) {
+    return '';
+  }
+  return value;
+};
 
 export class TextArea extends Component {
   constructor(props, context) {
     super(props, context);
     this.textareaRef = createRef();
     this.fillerRef = createRef();
+    const {
+      autoresize = false,
+      dontUseTabForIndent = false,
+      value,
+    } = props;
     this.state = {
+      value: value,
       editing: false,
     };
-    const {
-      dontUseTabForIndent = false,
-    } = props;
+
+    // found this hack that expands the text area without
+    // having to hard set rows all the time
+    // there has GOT to be a better way though
+    this.autoresize = () => {
+      if (!autoresize) {
+        return;
+      }
+      if (this.fillerRef && this.textareaRef) {
+      //  this.fillerRef.current.innerHTML =
+      //  this.textareaRef.current.value.replace(/\n/g, '<br/>');
+      }
+    };
     this.handleOnInput = e => {
       const { editing } = this.state;
       const { onInput } = this.props;
       if (!editing) {
         this.setEditing(true);
       }
+      this.setValue(e.target.value);
       if (onInput) {
         onInput(e, e.target.value);
       }
+      this.autoresize();
     };
     this.handleOnChange = e => {
       const { editing } = this.state;
@@ -41,6 +67,7 @@ export class TextArea extends Component {
       if (onChange) {
         onChange(e, e.target.value);
       }
+      this.autoresize();
     };
     this.handleKeyPress = e => {
       const { editing } = this.state;
@@ -51,16 +78,11 @@ export class TextArea extends Component {
       if (onKeyPress) {
         onKeyPress(e, e.target.value);
       }
+      this.autoresize();
     };
     this.handleKeyDown = e => {
       const { editing } = this.state;
       const { onKeyDown } = this.props;
-      if (e.keyCode === KEY_ESCAPE) {
-        this.setEditing(false);
-        e.target.value = toInputValue(this.props.value);
-        e.target.blur();
-        return;
-      }
       if (!editing) {
         this.setEditing(true);
       }
@@ -68,17 +90,18 @@ export class TextArea extends Component {
         const keyCode = e.keyCode || e.which;
         if (keyCode === 9) {
           e.preventDefault();
-          const { value, selectionStart, selectionEnd } = e.target;
-          e.target.value = (
-            value.substring(0, selectionStart) + "\t"
-              + value.substring(selectionEnd)
-          );
-          e.target.selectionEnd = selectionStart + 1;
+          const s = e.target.selectionStart;
+          e.target.value
+          = e.target.value.substring(0, e.target.selectionStart)
+            + "\t"
+            + e.target.value.substring(e.target.selectionEnd);
+          e.target.selectionEnd = s +1;
         }
       }
       if (onKeyDown) {
         onKeyDown(e, e.target.value);
       }
+      this.autoresize();
     };
     this.handleFocus = e => {
       const { editing } = this.state;
@@ -102,7 +125,8 @@ export class TextArea extends Component {
     const nextValue = this.props.value;
     const input = this.textareaRef.current;
     if (input) {
-      input.value = toInputValue(nextValue);
+      this.setValue(nextValue);
+      this.autoresize();
     }
   }
 
@@ -112,7 +136,8 @@ export class TextArea extends Component {
     const nextValue = this.props.value;
     const input = this.textareaRef.current;
     if (input && !editing && prevValue !== nextValue) {
-      input.value = toInputValue(nextValue);
+      this.setValue(nextValue);
+      this.autoresize();
     }
   }
 
@@ -120,11 +145,16 @@ export class TextArea extends Component {
     this.setState({ editing });
   }
 
+  setValue(value) {
+    this.setState({ value });
+  }
+
   getValue() {
-    return this.textareaRef.current && this.textareaRef.current.value;
+    return toInputValue(this.state.value);
   }
 
   render() {
+    const { props } = this;
     // Input only props
     const {
       onChange,
@@ -135,7 +165,6 @@ export class TextArea extends Component {
       onBlur,
       onEnter,
       value,
-      maxLength,
       placeholder,
       ...boxProps
     } = this.props;
@@ -154,6 +183,7 @@ export class TextArea extends Component {
         ])}
         {...rest}>
         <textarea
+          value={this.getValue()}
           ref={this.textareaRef}
           className="TextArea__textarea"
           placeholder={placeholder}
@@ -162,8 +192,7 @@ export class TextArea extends Component {
           onKeyPress={this.handleKeyPress}
           onInput={this.handleOnInput}
           onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-          maxLength={maxLength} />
+          onBlur={this.handleBlur} />
       </Box>
     );
   }
