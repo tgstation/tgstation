d62c08ef227fedf8a93c13b738e883d5b3f43a08
MrMelbert <kmelbert4@gmail.com>
2021-04-05 18:32:16 -0500

    suggestions

--- tgui/packages/tgui/interfaces/CommandReport.js
+++ tgui/packages/tgui/interfaces/CommandReport.js
@@ -1,5 +1,5 @@
 import { useBackend } from '../backend';
-import { Button, Dropdown, Flex, Input, Section, TextArea } from '../components';
+import { Button, Dropdown, Input, Section, Stack, TextArea } from '../components';
 import { Window } from '../layouts';
 
 export const CommandReport = (props, context) => {
@@ -19,58 +19,71 @@ export const CommandReport = (props, context) => {
       width={325}
       height={500}>
       <Window.Content>
-        <Section title="Set Central Command name:" textAlign="center">
-          <Dropdown
-            width="100%"
-            selected={command_name}
-            options={command_name_presets}
-            onSelected={value => act('update_command_name', {
-              updated_name: value,
-            })} />
-          { !!custom_name && (
-            <Input
-              width="100%"
-              mt={1}
-              value={command_name}
-              placeholder={command_name}
-              onChange={(e, value) => act("update_command_name", {
-                updated_name: value,
-              })} />)}
-        </Section>
-        <Section title="Set announcement sound:" textAlign="center">
-          <Dropdown
-            width="100%"
-            displayText={played_sound}
-            options={announcer_sounds}
-            onSelected={value => act('set_report_sound', {
-              picked_sound: value,
-            })} />
-        </Section>
-        <Section title="Set report text:" textAlign="center">
-          <TextArea
-            width="100%"
-            height="200px"
-            mb={1}
-            value={command_report_content}
-            onChange={(e, value) => act("update_report_contents", {
-              updated_contents: value,
-            })} />
-          <Flex grow={1}>
-            <Button.Checkbox
-              width="50%"
-              height="20px"
-              checked={announce_contents}
-              content="Announce Contents"
-              onClick={() => act("toggle_announce")} />
-            <Button.Confirm
-              width="50%"
-              height="20px"
-              color="good"
-              content="Submit Report"
-              textAlign="center"
-              onClick={() => act("submit_report")} />
-          </Flex>
-        </Section>
+        <Stack vertical={1}>
+          <Stack.Item>
+            <Section title="Set Central Command name:" textAlign="center">
+              <Dropdown
+                width="100%"
+                selected={command_name}
+                options={command_name_presets}
+                onSelected={value => act('update_command_name', {
+                  updated_name: value,
+                })} />
+              { !!custom_name && (
+                <Input
+                  width="100%"
+                  mt={1}
+                  value={command_name}
+                  placeholder={command_name}
+                  onChange={(e, value) => act("update_command_name", {
+                    updated_name: value,
+                  })} />)}
+            </Section>
+          </Stack.Item>
+          <Stack.Item>
+            <Section title="Set announcement sound:" textAlign="center">
+              <Dropdown
+                width="100%"
+                displayText={played_sound}
+                options={announcer_sounds}
+                onSelected={value => act('set_report_sound', {
+                  picked_sound: value,
+                })} />
+            </Section>
+          </Stack.Item>
+          <Stack.Item grow={1}>
+            <Section title="Set report text:" textAlign="center">
+              <TextArea
+                width="100%"
+                height="200px"
+                value={command_report_content}
+                onChange={(e, value) => act("update_report_contents", {
+                  updated_contents: value,
+                })} />
+              <Stack>
+                <Stack.Item>
+                  <Button.Checkbox
+                    fluid
+                    height="20px"
+                    checked={announce_contents}
+                    onClick={() => act("toggle_announce")}>
+                    Announce Contents
+                  </Button.Checkbox>
+                </Stack.Item>
+                <Stack.Item grow={1}>
+                  <Button.Confirm
+                    fluid
+                    icon="check"
+                    height="20px"
+                    color="good"
+                    textAlign="center"
+                    content="Submit Report"
+                    onClick={() => act("submit_report")} />
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
