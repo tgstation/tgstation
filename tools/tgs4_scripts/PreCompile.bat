4369ed3561da42bd073db0aaa48f018d93134139
Jordan Brown <Cyberboss@users.noreply.github.com>
2021-04-05 21:46:58 -0400

    Undo spacification

--- tools/tgs4_scripts/PreCompile.bat
+++ tools/tgs4_scripts/PreCompile.bat
@@ -2,14 +2,14 @@
 cd /D "%~dp0"
 set TG_BOOTSTRAP_CACHE=%cd%
 IF NOT %1 == "" (
-    rem TGS4: we are passed the game directory on the command line
-    cd %1
+	rem TGS4: we are passed the game directory on the command line
+	cd %1
 ) ELSE IF EXIST "..\Game\B\tgstation.dmb" (
-    rem TGS3: Game/B/tgstation.dmb exists, so build in Game/A
-    cd ..\Game\A
+	rem TGS3: Game/B/tgstation.dmb exists, so build in Game/A
+	cd ..\Game\A
 ) ELSE (
-    rem TGS3: Otherwise build in Game/B
-    cd ..\Game\B
+	rem TGS3: Otherwise build in Game/B
+	cd ..\Game\B
 )
 set CBT_BUILD_MODE=TGS
 tools\build\build
