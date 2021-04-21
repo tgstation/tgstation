dd0f646147b440026dd840eb31415eae59e61af9
celotajstg <celotajstg@users.noreply.github.com>
2021-04-21 21:36:28 +0300

    Remove references to tgs

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-md5sum -c - <<< "e70dd40dccdf2d694aea3f56a7a0c4f1 *html/changelogs/example.yml"
+md5sum -c - <<< "4d783933c74290b3a219068790b2046f *html/changelogs/example.yml"
 python3 tools/ss13_genchangelog.py html/changelog.json html/changelogs
