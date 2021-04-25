3ce78ef358005f7701b31124b4c6af61300343b6
tralezab <spamqetuo2@gmail.com>
2021-04-25 02:51:08 -0700

    Update CentcomPodLauncher.js

--- tgui/packages/tgui/interfaces/CentcomPodLauncher.js
+++ tgui/packages/tgui/interfaces/CentcomPodLauncher.js
@@ -793,6 +793,7 @@ class PresetsPage extends Component {
     for (let i = 0; i < presets.length; i++) {
       if (presets[i].id === deleteID) {
         presets.splice(i, 1);
+        i--;
       }
     }
     storage.set("podlauncher_presetlist", presets);
