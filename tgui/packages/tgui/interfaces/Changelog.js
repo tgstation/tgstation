32494507e0cc47edcc8f6fa42ab07380ba1723dd
celotajstg <celotajstg@users.noreply.github.com>
2021-04-27 21:16:20 +0300

    Update license text in changelog

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -225,12 +225,53 @@ export class Changelog extends Component {
           <a href="http://forums.somethingawful.com/">SomethingAwful Goons</a>
           {' only.'}
         </p>
+        <h3>Traditional Games Space Station 13 License</h3>
         <p>
-          {'Traditional Games Space Station 13 is licensed under a '}
-          <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">
-            GNU Affero General Public 3.0 License
+          {'All code after '}
+          <a href={'https://github.com/tgstation/tgstation/commit/'
+            + '333c566b88108de218d882840e61928a9b759d8f'}>
+            commit 333c566b88108de218d882840e61928a9b759d8f on 2014/31/12
+            at 4:38 PM PST
           </a>
-          .
+          {' is licensed under '}
+          <a href="https://www.gnu.org/licenses/agpl-3.0.html">
+            GNU AGPL v3
+          </a>
+          {'. All code before that commit is licensed under '}
+          <a href="https://www.gnu.org/licenses/gpl-3.0.html">
+            GNU GPL v3
+          </a>
+          {', including tools unless their readme specifies otherwise. See '}
+          <a href="https://github.com/tgstation/tgstation/blob/master/LICENSE">
+            LICENSE
+          </a>
+          {' and '}
+          <a
+            href="https://github.com/tgstation/tgstation/blob/master/GPLv3.txt">
+            GPLv3.txt
+          </a>
+          {' for more details.'}
+        </p>
+        <p>
+          The TGS DMAPI API is licensed as a subproject under the MIT license.
+          {' See the footer of '}
+          <a href={'https://github.com/tgstation/tgstation/blob/master'
+            + '/code/__DEFINES/tgs.dm'}>
+            code/__DEFINES/tgs.dm
+          </a>
+          {' and '}
+          <a href={'https://github.com/tgstation/tgstation/blob/master'
+            + '/code/modules/tgs/LICENSE'}>
+            code/modules/tgs/LICENSE
+          </a>
+          {' for the MIT license.'}
+        </p>
+        <p>
+          {'All assets including icons and sound are under a '}
+          <a href="https://creativecommons.org/licenses/by-sa/3.0/">
+            Creative Commons 3.0 BY-SA license
+          </a>
+          {' unless otherwise indicated.'}
         </p>
       </Section>
     );
