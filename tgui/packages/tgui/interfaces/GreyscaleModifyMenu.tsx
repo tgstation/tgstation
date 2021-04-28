ea62e5b72f3ff5d51af2e3b3944bd4312489116c
Emmett Gaines <ninjanomnom@gmail.com>
2021-04-28 19:16:39 -0400

    Adds support for dirs and config changing in GAGS debug menu

--- tgui/packages/tgui/interfaces/GreyscaleModifyMenu.tsx
+++ tgui/packages/tgui/interfaces/GreyscaleModifyMenu.tsx
@@ -18,8 +18,41 @@ type SpriteEntry = {
 }
 
 type GreyscaleMenuData = {
+  greyscale_config: string;
   colors: Array<ColorEntry>;
   sprites: SpriteData;
+  sprites_dir: string;
+}
+
+enum Directions {
+  North = "north",
+  NorthEast = "northeast",
+  East = "east",
+  SouthEast = "southeast",
+  South = "south",
+  SouthWest = "southwest",
+  West = "west",
+  NorthWest = "northwest"
+}
+
+const ConfigDisplay = (props, context) => {
+  const { act, data } = useBackend<GreyscaleMenuData>(context);
+  return (
+    <Section title="Config">
+      <LabeledList>
+        <LabeledList.Item label="Config Type">
+          <Button
+            icon="cogs"
+            onClick={() => act("select_config")}
+          />
+          <Input
+            value={data.greyscale_config}
+            onChange={(_, value) => act("load_config_from_string", { config_string: value })}
+          />
+        </LabeledList.Item>
+      </LabeledList>
+    </Section>
+  )
 }
 
 const ColorDisplay = (props, context) => {
@@ -46,7 +79,7 @@ const ColorDisplay = (props, context) => {
             />
             {" "}
             <Button
-              content={<Icon name="palette" />}
+              icon="palette"
               onClick={() => act("pick_color", { color_index: item.index })}
             />
             <Input
@@ -60,10 +93,100 @@ const ColorDisplay = (props, context) => {
   );
 };
 
+const PreviewCompassSelect = (props, context) => {
+  const { act, data } = useBackend<GreyscaleMenuData>(context);
+  return (
+    <Section>
+      <Table width="51%" mx="25%">
+        <Table.Row key="top" height="33%">
+          <Table.Cell width="33%">
+            <Button
+              content="NW"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.NorthWest })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+          <Table.Cell width="33%">
+            <Button
+              content="N"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.North })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+          <Table.Cell width="33%">
+            <Button
+              content="NE"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.NorthEast })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row key="middle" height="33%">
+          <Table.Cell width="33%">
+            <Button
+              content="W"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.West })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+          <Table.Cell width="33%"><Box textAlign="center" ><Icon name="arrows-alt" size={1.5} /></Box></Table.Cell>
+          <Table.Cell width="33%">
+            <Button
+              content="E"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.East })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row key="bottom" height="33%">
+          <Table.Cell width="33%">
+            <Button
+              content="SW"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.SouthWest })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+          <Table.Cell width="33%">
+            <Button
+              content="S"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.South })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+          <Table.Cell width="33%">
+            <Button
+              content="SE"
+              textAlign="center"
+              onClick={() => act("change_dir", { new_sprite_dir: Directions.SouthEast })}
+              fluid
+              m={1}
+            />
+          </Table.Cell>
+        </Table.Row>
+      </Table>
+    </Section>
+  );
+};
+
 const PreviewDisplay = (props, context) => {
   const { data } = useBackend<GreyscaleMenuData>(context);
   return (
-    <Section title="Preview">
+    <Section title={`Preview (${data.sprites_dir})`}>
+      <PreviewCompassSelect />
       <Table>
         <Table.Row header>
           <Table.Cell textAlign="center">Step Layer</Table.Cell>
@@ -101,6 +224,7 @@ export const GreyscaleModifyMenu = (props, context) => {
       width={325}
       height={800}>
       <Window.Content scrollable>
+        <ConfigDisplay />
         <ColorDisplay />
         <Button
           content="Refresh Icon File"
