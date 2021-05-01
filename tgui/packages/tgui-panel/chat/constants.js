0373b15dc4a964b7e7c3ddc0d9723756c6d4f61d
Seris02 <littlenebulae421@gmail.com>
2021-05-01 11:32:36 +0800

    holoparasite, round will start in x and keybinding conflict to respective filters

--- tgui/packages/tgui-panel/chat/constants.js
+++ tgui/packages/tgui-panel/chat/constants.js
@@ -64,7 +64,7 @@ export const MESSAGE_TYPES = [
     type: MESSAGE_TYPE_INFO,
     name: 'Info',
     description: 'Non-urgent messages from the game and items',
-    selector: '.notice:not(.pm), .adminnotice, .info, .sinister, .cult, .infoplain, .announce, .hear, .smallnotice, .motd',
+    selector: '.notice:not(.pm), .adminnotice, .info, .sinister, .cult, .infoplain, .announce, .hear, .smallnotice, .motd, .holoparasite',
   },
   {
     type: MESSAGE_TYPE_WARNING,
