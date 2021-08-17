fe21e5bde903afb767a8ed40dd9839aad9d8172f
Beatrice <83368538+SpaceDragon00@users.noreply.github.com>
2021-08-16 21:42:55 -0400

    Update html/admin/banpanel.js
    - Removes one space for consistency
    
    Co-authored-by: John Willard <53777086+JohnFulpWillard@users.noreply.github.com>

--- html/admin/banpanel.js
+++ html/admin/banpanel.js
@@ -11,7 +11,7 @@ function toggle_other_checkboxes(source, copycats_str, our_index_str) {
 
 function header_click_all_checkboxes(source) {
 	var checkboxes = document.getElementsByClassName(source.name);
-	for (var i = 0, n = checkboxes.length; i < n; i++) {
+	for(var i = 0, n = checkboxes.length; i < n; i++) {
 		if(checkboxes[i].checked != source.checked && checkboxes[i] != source){
 			checkboxes[i].click();
 		}
