829dc51fb0352adb6f0ccaec5d205e7f4e88c8ee
Jared-Fogle <35135081+Mothblocks@users.noreply.github.com>
2021-08-25 19:28:28 -0700

    Add category

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/scaling_method.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/scaling_method.tsx
@@ -2,6 +2,7 @@ import { createDropdownInput, Feature } from "../base";
 
 export const scaling_method: Feature<string> = {
   name: "Scaling method",
+  category: "UI",
   component: createDropdownInput({
     blur: "Bilinear",
     distort: "Nearest Neighbor",
