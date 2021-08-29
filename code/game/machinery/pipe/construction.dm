tgstation/tgstation

commit 3e8407c4716bd8abcc79326f464384a5156eddfa

Smart Pipes(reborn) (#58038)
How these new pipes work.
-Smart pipes autoconnect to nearby smart pipes
-They are now color coded, so they only connect to the same colored pipe, the GREY pipe is the wildcard and can connect to every other color, so be aware of this
-ALL components spawned by the RPD can be colored (from pumps to connectors, from pipes to manifolds), if you leave them GREY they can connect to every other color. Color adapters can be colored, but they'll still connect two pipes with different colors. BUILDABLE machines are GREY (thermomachines, cryo, HFR) so be aware of this
-Trying to go across another smart pipe will now build a bridge pipe automatically already colored of the color you choose, so you don't have to place it yourself anymore (is still available in the RPD tho)
-ALL binary components, layer manifolds, color adapters and bridge pipe can be put ONTOP of a smart pipe, but not on another of these. Smart pipes can't be placed on top of these pipes, so you have to build them first.
-Lcrossings can't be made anymore (sorry y'all i tryed, if someone have a way of doing them ping me on discord)
-REMEMBER you still have 5 layers to go, these rules apply to the same layer pipes, so if you do a crossing on different layers you won't see a bridge pipe appear.

code/game/machinery/pipe/construction.dm
@@ -22,6 +22,7 @@ Buildable meters
 	var/RPD_type
 	/// Whether it can be painted
 	var/paintable = FALSE
+	var/pipe_color
 
 /obj/item/pipe/directional
 	RPD_type = PIPE_UNARY
@@ -41,11 +42,12 @@ Buildable meters
 	//Flipping handled manually due to custom handling for trinary pipes
 	AddComponent(/datum/component/simple_rotation, ROTATION_ALTCLICK | ROTATION_CLOCKWISE)
 
-/obj/item/pipe/Initialize(mapload, _pipe_type, _dir, obj/machinery/atmospherics/make_from)
+/obj/item/pipe/Initialize(mapload, _pipe_type, _dir, obj/machinery/atmospherics/make_from, device_color)
 	if(make_from)
 		make_from_existing(make_from)
 	else
 		pipe_type = _pipe_type
+		pipe_color = device_color
 		setDir(_dir)
 
 	update()
@@ -59,6 +61,7 @@ Buildable meters
 	add_atom_colour(make_from.color, FIXED_COLOUR_PRIORITY)
 	pipe_type = make_from.type
 	paintable = make_from.paintable
+	pipe_color = make_from.pipe_color
 
 /obj/item/pipe/trinary/flippable/make_from_existing(obj/machinery/atmospherics/components/trinary/make_from)
 	..()
@@ -128,7 +131,17 @@ Buildable meters
 /obj/item/pipe/attack_self(mob/user)
 	setDir(turn(dir,-90))
 
-/obj/item/pipe/wrench_act(mob/living/user, obj/item/wrench/W)
+///Check if the pipe on the turf and our to be placed binary pipe are perpendicular to each other
+/obj/item/pipe/proc/check_ninety_degree_dir(obj/machinery/atmospherics/machine)
+	if(ISDIAGONALDIR(machine.dir))
+		return FALSE
+	if(EWCOMPONENT(machine.dir) && EWCOMPONENT(dir))
+		return FALSE
+	if(NSCOMPONENT(machine.dir) && NSCOMPONENT(dir))
+		return FALSE
+	return TRUE
+
+/obj/item/pipe/wrench_act(mob/living/user, obj/item/wrench/wrench)
 	. = ..()
 	if(!isturf(loc))
 		return TRUE
@@ -137,23 +150,43 @@ Buildable meters
 
 	var/obj/machinery/atmospherics/fakeA = pipe_type
 	var/flags = initial(fakeA.pipe_flags)
-	for(var/obj/machinery/atmospherics/M in loc)
-		if((M.pipe_flags & flags & PIPING_ONE_PER_TURF)) //Only one dense/requires density object per tile, eg connectors/cryo/heater/coolers.
+	var/pipe_count = 0
+	for(var/obj/machinery/atmospherics/machine in loc)
+		if(machine.piping_layer != piping_layer)
+			continue
+		pipe_count += 1
+	for(var/obj/machinery/atmospherics/machine in loc)
+		if((machine.pipe_flags & flags & PIPING_ONE_PER_TURF)) //Only one dense/requires density object per tile, eg connectors/cryo/heater/coolers.
 			to_chat(user, "<span class='warning'>Something is hogging the tile!</span>")
 			return TRUE
-		if((M.piping_layer != piping_layer) && !((M.pipe_flags | flags) & PIPING_ALL_LAYER)) //don't continue if either pipe goes across all layers
+
+		if(pipe_count == 1 && istype(machine, /obj/machinery/atmospherics/pipe/smart) && ispath(pipe_type, /obj/machinery/atmospherics/pipe/smart) && lowertext(machine.pipe_color) != lowertext(pipe_color) && machine.connection_num < 3)
+			var/direction = machine.dir
+			if((direction & EAST|WEST || direction & SOUTH|NORTH) && !ISDIAGONALDIR(direction))
+				pipe_type = /obj/machinery/atmospherics/pipe/bridge_pipe
+				if(EWCOMPONENT(direction))
+					dir = NORTH
+				if(NSCOMPONENT(direction))
+					dir = EAST
+				continue
+
+		if(flags & PIPING_BRIDGE && !(machine.pipe_flags & PIPING_BRIDGE) && check_ninety_degree_dir(machine)) //continue if we are placing a bridge pipe over a normal pipe only (prevent duplicates)
 			continue
-		if(M.GetInitDirections() & SSair.get_init_dirs(pipe_type, fixed_dir())) // matches at least one direction on either type of pipe
+
+		if((machine.piping_layer != piping_layer) && !((machine.pipe_flags | flags) & PIPING_ALL_LAYER)) //don't continue if either pipe goes across all layers
+			continue
+
+		if(machine.GetInitDirections() & SSair.get_init_dirs(pipe_type, fixed_dir())) // matches at least one direction on either type of pipe
 			to_chat(user, "<span class='warning'>There is already a pipe at that location!</span>")
 			return TRUE
 	// no conflicts found
 
-	var/obj/machinery/atmospherics/A = new pipe_type(loc)
-	build_pipe(A)
-	A.on_construction(color, piping_layer)
-	transfer_fingerprints_to(A)
+	var/obj/machinery/atmospherics/built_machine = new pipe_type(loc)
+	build_pipe(built_machine)
+	built_machine.on_construction(pipe_color, piping_layer)
+	transfer_fingerprints_to(built_machine)
 
-	W.play_tool_sound(src)
+	wrench.play_tool_sound(src)
 	user.visible_message( \
 		"[user] fastens \the [src].", \
 		"<span class='notice'>You fasten \the [src].</span>", \

code/modules/atmospherics/machinery/atmosmachinery.dm
@@ -25,7 +25,7 @@
 	///Bitflag of the initialized directions (NORTH | SOUTH | EAST | WEST)
 	var/initialize_directions = 0
 	///The color of the pipe
-	var/pipe_color
+	var/pipe_color = COLOR_VERY_LIGHT_GRAY
 	///What layer the pipe is in (from 1 to 5, default 3)
 	var/piping_layer = PIPING_LAYER_DEFAULT
 	///The flags of the pipe/component (PIPING_ALL_LAYER | PIPING_ONE_PER_TURF | PIPING_DEFAULT_LAYER_ONLY | PIPING_CARDINAL_AUTONORMALIZE)
@@ -54,13 +54,16 @@
 	var/on = FALSE
 
 	///Whether it can be painted
-	var/paintable = FALSE
+	var/paintable = TRUE
 
 	///Is the thing being rebuilt by SSair or not. Prevents list bloat
 	var/rebuilding = FALSE
 
-	///The bitflag that's being checked on ventcrawling. Default is to allow ventcrawling and seeing pipes. 
+	///The bitflag that's being checked on ventcrawling. Default is to allow ventcrawling and seeing pipes.
 	var/vent_movement = VENTCRAWL_ALLOWED | VENTCRAWL_CAN_SEE
+
+	///Store the smart pipes connections, used for pipe construction
+	var/connection_num = 0
 
 /obj/machinery/atmospherics/examine(mob/user)
 	. = ..()
@@ -200,9 +203,10 @@
  */
 /obj/machinery/atmospherics/proc/findConnecting(direction, prompted_layer)
 	for(var/obj/machinery/atmospherics/target in get_step(src, direction))
-		if(target.initialize_directions & get_dir(target,src))
-			if(connection_check(target, prompted_layer))
-				return target
+		if(!(target.initialize_directions & get_dir(target,src)))
+			continue
+		if(connection_check(target, prompted_layer))
+			return target
 
 /**
  * Check the connection between two nodes
@@ -219,7 +223,7 @@
 	return FALSE
 
 /**
- * check if the piping layer are the same on both sides
+ * check if the piping layer and color are the same on both sides (grey can connect to all colors)
  * returns TRUE or FALSE if the connection is possible or not
  * Arguments:
  * * obj/machinery/atmospherics/target - the machinery we want to connect to
@@ -228,7 +232,31 @@
 /obj/machinery/atmospherics/proc/isConnectable(obj/machinery/atmospherics/target, given_layer)
 	if(isnull(given_layer))
 		given_layer = piping_layer
-	if((target.piping_layer == given_layer) || (target.pipe_flags & PIPING_ALL_LAYER))
+	if(check_connectable_layer(target, given_layer) && target.loc != loc && check_connectable_color(target))
+		return TRUE
+	return FALSE
+
+/**
+ * check if the piping layer are the same on both sides or one of them has the PIPING_ALL_LAYER flag
+ * returns TRUE if one of the parameters is TRUE
+ * called by isConnectable()
+ * Arguments:
+ * * obj/machinery/atmospherics/target - the machinery we want to connect to
+ * * given_layer - the piping_layer we are connecting to
+ */
+/obj/machinery/atmospherics/proc/check_connectable_layer(obj/machinery/atmospherics/target, given_layer)
+	if(target.piping_layer == given_layer || (target.pipe_flags | pipe_flags) & PIPING_ALL_LAYER)
+		return TRUE
+	return FALSE
+
+/**
+ * check if the color are the same on both sides or if one of the pipes are grey or have the PIPING_ALL_COLORS flag
+ * returns TRUE if one of the parameters is TRUE
+ * Arguments:
+ * * obj/machinery/atmospherics/target - the machinery we want to connect to
+ */
+/obj/machinery/atmospherics/proc/check_connectable_color(obj/machinery/atmospherics/target)
+	if(lowertext(target.pipe_color) == lowertext(pipe_color) || ((target.pipe_flags | pipe_flags) & PIPING_ALL_COLORS) || lowertext(target.pipe_color) == lowertext(COLOR_VERY_LIGHT_GRAY) || lowertext(pipe_color == COLOR_VERY_LIGHT_GRAY))
 		return TRUE
 	return FALSE
 
@@ -370,7 +398,7 @@
 /obj/machinery/atmospherics/deconstruct(disassembled = TRUE)
 	if(!(flags_1 & NODECONSTRUCT_1))
 		if(can_unwrench)
-			var/obj/item/pipe/stored = new construction_type(loc, null, dir, src)
+			var/obj/item/pipe/stored = new construction_type(loc, null, dir, src, pipe_color)
 			stored.setPipingLayer(piping_layer)
 			if(!disassembled)
 				stored.obj_integrity = stored.max_integrity * 0.5
@@ -406,6 +434,14 @@
 		PIPING_LAYER_SHIFT(pipe_overlay, piping_layer)
 		if(trinary == TRUE && (piping_layer == 1 || piping_layer == 5))
 			PIPING_FORWARD_SHIFT(pipe_overlay, piping_layer, 2)
+
+///Similar to getpipeimage(); will create an image from the set_icon and set_state; mostly used to create overlays for connections.
+/obj/machinery/atmospherics/proc/pipe_overlay(set_icon, set_state, direction, color = COLOR_VERY_LIGHT_GRAY, piping_layer = 3, set_layer = PIPE_VISIBLE_LEVEL)
+	var/image/pipe_overlay
+	pipe_overlay = image(icon = set_icon, icon_state = set_state, layer = set_layer, dir = direction)
+	pipe_overlay.color = color
+	PIPING_LAYER_SHIFT(pipe_overlay, piping_layer)
+	return pipe_overlay
 
 /obj/machinery/atmospherics/on_construction(obj_color, set_layer)
 	if(can_unwrench)
@@ -487,7 +523,7 @@
  * Update the layer in which the pipe/device is in, that way pipes have consistent layer depending on piping_layer
  */
 /obj/machinery/atmospherics/proc/update_layer()
-	layer = initial(layer) + (piping_layer - PIPING_LAYER_DEFAULT) * PIPING_LAYER_LCHANGE
+	layer = initial(layer) + (piping_layer - PIPING_LAYER_DEFAULT) * PIPING_LAYER_LCHANGE + (GLOB.pipe_colors_ordered[pipe_color] * 0.01)
 
 /**
  * Called by the RPD.dm pre_attack(), overriden by pipes.dm

code/modules/atmospherics/machinery/pipes/smart.dm
@@ -1,0 +1,114 @@
+GLOBAL_LIST_INIT(atmos_components, typecacheof(list(/obj/machinery/atmospherics)))
+
+/obj/machinery/atmospherics/pipe/smart
+	icon = 'icons/obj/atmospherics/pipes/simple.dmi'
+	icon_state = "pipe11-3"
+
+	name = "pipe"
+	desc = "A one meter section of regular pipe."
+
+	vis_flags = VIS_INHERIT_ICON | VIS_INHERIT_ICON_STATE | VIS_INHERIT_DIR | VIS_INHERIT_ID
+
+	device_type = QUATERNARY
+	construction_type = /obj/item/pipe/quaternary
+	pipe_state = "manifold4w"
+	connection_num = 0
+	var/list/connections
+	var/static/list/mutable_appearance/center_cache = list()
+	var/mutable_appearance/pipe_appearance
+
+/* We use New() instead of Initialize() because these values are used in update_icon()
+ * in the mapping subsystem init before Initialize() is called in the atoms subsystem init.
+ */
+/obj/machinery/atmospherics/pipe/smart/Initialize()
+	icon_state = ""
+	. = ..()
+
+/obj/machinery/atmospherics/pipe/smart/SetInitDirections()
+	initialize_directions = ALL_CARDINALS
+
+/obj/machinery/atmospherics/pipe/smart/proc/check_connections()
+	var/mutable_appearance/center
+	connection_num = 0
+	connections = NONE
+	for(var/direction in GLOB.cardinals)
+		var/turf/turf = get_step(src, direction)
+		if(!turf)
+			continue
+		for(var/obj/machinery/atmospherics/machine in turf)
+			if(connection_check(machine, piping_layer))
+				connections |= direction
+				connection_num++
+				break
+
+	switch(connection_num)
+		if(0)
+			center = mutable_appearance('icons/obj/atmospherics/pipes/manifold.dmi', "manifold4w_center")
+			dir = SOUTH
+		if(1)
+			center = mutable_appearance('icons/obj/atmospherics/pipes/simple.dmi', "pipe00-3")
+			dir = connections
+		if(2)
+			center = mutable_appearance('icons/obj/atmospherics/pipes/simple.dmi', "pipe00-3")
+			dir = check_binary_direction(connections)
+		if(3)
+			center = mutable_appearance('icons/obj/atmospherics/pipes/manifold.dmi', "manifold_center")
+			dir = check_manifold_direction(connections)
+
+		if(4)
+			center = mutable_appearance('icons/obj/atmospherics/pipes/manifold.dmi', "manifold4w_center")
+			dir = NORTH
+	return center
+
+/obj/machinery/atmospherics/pipe/smart/proc/check_binary_direction(direction)
+	switch(direction)
+		if(EAST|WEST)
+			return EAST
+		if(SOUTH|NORTH)
+			return SOUTH
+		else
+			return direction
+
+/obj/machinery/atmospherics/pipe/smart/proc/check_manifold_direction(direction)
+	switch(direction)
+		if(NORTH|SOUTH|EAST)
+			return WEST
+		if(NORTH|SOUTH|WEST)
+			return EAST
+		if(NORTH|WEST|EAST)
+			return SOUTH
+		if(SOUTH|WEST|EAST)
+			return NORTH
+		else
+			return null
+
+/obj/machinery/atmospherics/pipe/smart/update_overlays()
+	. = ..()
+	var/mutable_appearance/center = center_cache["[piping_layer]"]
+	center = check_connections()
+	PIPING_LAYER_DOUBLE_SHIFT(center, piping_layer)
+	center_cache["[piping_layer]"] = center
+	pipe_appearance = center
+	. += center
+
+	update_layer()
+
+	//Add non-broken pieces
+	for(var/i in 1 to device_type)
+		if(!nodes[i])
+			continue
+		. += pipe_overlay('icons/obj/atmospherics/pipes/manifold.dmi', "pipe-[piping_layer]", get_dir(src, nodes[i]), set_layer = (layer + 0.01))
+
+
+//mapping helpers
+/obj/machinery/atmospherics/pipe/smart/simple
+	icon = 'icons/obj/atmospherics/pipes/simple.dmi'
+	icon_state = "pipe11-3"
+
+/obj/machinery/atmospherics/pipe/smart/manifold
+	icon = 'icons/obj/atmospherics/pipes/manifold.dmi'
+	icon_state = "manifold-3"
+
+/obj/machinery/atmospherics/pipe/smart/manifold4w
+	icon = 'icons/obj/atmospherics/pipes/manifold.dmi'
+	icon_state = "manifold4w-3"

commit 59dd69bd532ac59592690cab8f6245c1dc2b800c

Smart pipes now actually go over smart pipes
This was ostensibly the main feature of smart pipes, so should be
fairly important

Parentheses are technically not required around the |, but I think
it's important to emphasise the importance of parentheses around
bitwise operators as this was apparently broken since 3e8407c4716b

Also purge var/connections and var/connection_num, since it's
literally only used for a `machine.connection_num < 3` check
which is completely redundant, and we already build a bitfield
tracking active directions.

code/game/machinery/pipe/construction.dm
@@ -167,9 +167,9 @@ Buildable meters
 			to_chat(user, span_warning("Something is hogging the tile!"))
 			return TRUE
 
-		if(pipe_count == 1 && istype(machine, /obj/machinery/atmospherics/pipe/smart) && ispath(pipe_type, /obj/machinery/atmospherics/pipe/smart) && lowertext(machine.pipe_color) != lowertext(pipe_color) && machine.connection_num < 3)
+		if(pipe_count == 1 && istype(machine, /obj/machinery/atmospherics/pipe/smart) && ispath(pipe_type, /obj/machinery/atmospherics/pipe/smart) && lowertext(machine.pipe_color) != lowertext(pipe_color))
 			var/direction = machine.dir
-			if((direction & EAST|WEST || direction & SOUTH|NORTH) && !ISDIAGONALDIR(direction))
+			if(((direction & (EAST|WEST)) || (direction & (SOUTH|NORTH))) && !ISDIAGONALDIR(direction))
 				pipe_type = /obj/machinery/atmospherics/pipe/bridge_pipe
 				if(EWCOMPONENT(direction))
 					dir = NORTH

code/modules/atmospherics/machinery/atmosmachinery.dm
@@ -58,9 +58,6 @@
 	///The bitflag that's being checked on ventcrawling. Default is to allow ventcrawling and seeing pipes.
 	var/vent_movement = VENTCRAWL_ALLOWED | VENTCRAWL_CAN_SEE
 
-	///Store the smart pipes connections, used for pipe construction
-	var/connection_num = 0
-
 /obj/machinery/atmospherics/LateInitialize()
 	. = ..()
 	update_name()

code/modules/atmospherics/machinery/pipes/smart.dm
@@ -10,32 +10,21 @@ GLOBAL_LIST_INIT(atmos_components, typecacheof(list(/obj/machinery/atmospherics)
 	device_type = QUATERNARY
 	construction_type = /obj/item/pipe/quaternary
 	pipe_state = "manifold4w"
-	connection_num = 0
-	var/connections
 
 /obj/machinery/atmospherics/pipe/smart/update_pipe_icon()
 	icon = 'icons/obj/atmospherics/pipes/pipes_bitmask.dmi'
 	var/bitfield = NONE
 	var/bits = 0
-	connections = NONE
-	connection_num = 0
+
 	for(var/i in 1 to device_type)
 		if(!nodes[i])
 			continue
 		var/obj/machinery/atmospherics/node = nodes[i]
 		var/connected_dir = get_dir(src, node)
-		connections |= connected_dir
-		connection_num += 1
 		bits++
-		switch(connected_dir)
-			if(NORTH)
-				bitfield |= NORTH_FULLPIPE
-			if(SOUTH)
-				bitfield |= SOUTH_FULLPIPE
-			if(EAST)
-				bitfield |= EAST_FULLPIPE
-			if(WEST)
-				bitfield |= WEST_FULLPIPE
+		// Note X_FULLPIPE == X as described in defines/atmospherics.dm
+		bitfield |= connected_dir
+	dir = check_binary_direction(bitfield)
 	//If we dont have enough bits to make a proper sprite, add some shortpipe bits
 	if(bits < 2)
 		var/list/bits_to_add = list()
@@ -61,17 +50,6 @@ GLOBAL_LIST_INIT(atmos_components, typecacheof(list(/obj/machinery/atmospherics)
 					bitfield |= WEST_SHORTPIPE
 
 	icon_state = "[bitfield]_[piping_layer]"
-	switch(connection_num)
-		if(0)
-			dir = SOUTH
-		if(1)
-			dir = connections
-		if(2)
-			dir = check_binary_direction(connections)
-		if(3)
-			dir = connections
-		if(4)
-			dir = NORTH | SOUTH | EAST | WEST
 
 /obj/machinery/atmospherics/pipe/smart/SetInitDirections(init_dir)
 	if(init_dir)