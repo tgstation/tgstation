36ef10f25edfe3f7582fac4603326b1dca8cb145
Jared-Fogle <35135081+Mothblocks@users.noreply.github.com>
2021-09-12 13:44:29 -0700

    Moth loar

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/moth.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/moth.ts
@@ -28,6 +28,11 @@ const Moth: Species = {
         masks will work.",
     }],
   },
+  lore: [
+    "Their homeworld lost to the ages, the moths live aboard the Grand Nomad Fleet. Made up of what could be found, bartered, repaired, or stolen the armada is a colossal patchwork built on a history of politely flagging travelers down and taking their things. Occasionally a moth will decide to leave the fleet, usually to strike out for fortunes to send back home.",
+    "Nomadic life produces a tight-knit culture, with moths valuing their friends, family, and vessels highly. Moths are gregarious by nature and do best in communal spaces. This has served them well on the galactic stage, maintaining a friendly and personable reputation even in the face of hostile encounters. It seems that the galaxy has come to accept these former pirates.",
+    "Surprisingly, living together in a giant fleet hasn't flattened variance in dialect and culture. These differences are welcomed and encouraged within the fleet for the variety that they bring.",
+  ],
 };
 
 export default Moth;
