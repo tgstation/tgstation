d4f5319aad8a90a854b341adf1218d6cfbb064f9
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2021-10-01 17:10:04 -0400

    Update tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/nightmare.ts
    Co-authored-by: Ghom <42542238+Ghommie@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/nightmare.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/nightmare.ts
@@ -7,7 +7,7 @@ const Nightmare: Antagonist = {
   description: [
     multiline`
       Use your light eater to break sources of light to survive and thrive.
-      Jaunt through the darkness and seek your pray with night vision.
+      Jaunt through the darkness and seek your prey with night vision.
     `,
   ],
   category: Category.Midround,
