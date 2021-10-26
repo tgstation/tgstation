b9c3a63c9bc8869a9002723e69ac52737862cbf9
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2021-10-26 10:17:41 -0700

    ESLint

--- tgui/packages/tgui/interfaces/SpawnersMenu.tsx
+++ tgui/packages/tgui/interfaces/SpawnersMenu.tsx
@@ -38,16 +38,14 @@ export const SpawnersMenu = (props, context) => {
                         onClick={() =>
                           act('jump', {
                             name: spawner.name,
-                          })
-                        }
+                          })}
                       />
                       <Button
                         content="Spawn"
                         onClick={() =>
                           act('spawn', {
                             name: spawner.name,
-                          })
-                        }
+                          })}
                       />
                     </Stack.Item>
                   </Stack>
