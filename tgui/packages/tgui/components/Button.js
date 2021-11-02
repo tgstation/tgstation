deae472e483fbf60712f6da04639370dd81fd509
Vincentius Vincentius <vincentius_vin@yahoo.com>
2021-11-02 12:08:53 +0700

    Tabs to spaces

--- tgui/packages/tgui/components/Button.js
+++ tgui/packages/tgui/components/Button.js
@@ -35,7 +35,7 @@ export const Button = props => {
     children,
     onclick,
     onClick,
-	verticalAlign,
+    verticalAlign,
     ...rest
   } = props;
   const hasContent = !!(content || children);
