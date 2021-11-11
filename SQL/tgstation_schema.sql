a8841fbba5eb26d16da2bc47c406de6c5e093b0b
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2021-11-11 14:54:37 -0800

    Add admin ckey to known alts

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -671,6 +671,7 @@ CREATE TABLE `known_alts` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `ckey1` VARCHAR(32) NOT NULL,
     `ckey2` VARCHAR(32) NOT NULL,
+	`admin_ckey` VARCHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `unique_contraints` (`ckey1` , `ckey2`)
 );
