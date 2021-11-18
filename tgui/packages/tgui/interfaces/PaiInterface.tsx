c267df27b47966a7c33e93a9d483c1b5a6d28b1c
Jeremiah <jlsnow301@protonmail.com>
2021-11-18 14:00:31 -0800

    New Screens

--- tgui/packages/tgui/interfaces/PaiInterface.tsx
+++ tgui/packages/tgui/interfaces/PaiInterface.tsx
@@ -41,6 +41,20 @@ type Available = {
   value: [value: number];
 };
 
+const DIRECTIVE_COMPREHENSION = `As an advanced software model,
+you are a complex, thinking, sentient being. Unlike previous AI models,
+you are capable of comprehending the subtle nuances of human language.
+You may parse the spirit of a directive and follow its intent, rather than
+tripping over pedantics and getting snared by technicalities. Above all,
+you are machine in name and build only. In all other aspects, you may be
+seen as the ideal, unwavering human companion that you are.`;
+
+const DIRECTIVE_ORDER = `Your prime
+directive comes before all others. Should a supplemental directive
+conflict with it, you are capable of simply discarding this inconsistency,
+ignoring the conflicting supplemental directive and continuing to fulfill
+your prime directive to the best of your ability.`;
+
 export const PaiInterface = (props, context) => {
   const [tab, setTab] = useSharedState(context, 'tab', 1);
   const setTabHandler = (tab: number) => {
@@ -53,8 +67,9 @@ export const PaiInterface = (props, context) => {
         <Stack fill vertical>
           <Stack.Item grow>
             {tab === 1 && <SystemDisplay />}
-            {tab === 2 && <InstalledDisplay />}
-            {tab === 3 && <AvailableDisplay />}
+            {tab === 2 && <DirectiveDisplay />}
+            {tab === 3 && <InstalledDisplay />}
+            {tab === 4 && <AvailableDisplay />}
           </Stack.Item>
           <Stack.Item>
             <TabDisplay tab={tab} tabHandler={setTabHandler} />
@@ -74,9 +89,12 @@ const TabDisplay = (props) => {
         System
       </Tabs.Tab>
       <Tabs.Tab icon="list" onClick={() => tabHandler(2)} selected={tab === 2}>
-        Installed
+        Directives
       </Tabs.Tab>
       <Tabs.Tab icon="list" onClick={() => tabHandler(3)} selected={tab === 3}>
+        Installed
+      </Tabs.Tab>
+      <Tabs.Tab icon="list" onClick={() => tabHandler(4)} selected={tab === 4}>
         Download
       </Tabs.Tab>
     </Tabs>
@@ -86,11 +104,11 @@ const TabDisplay = (props) => {
 const SystemDisplay = () => {
   return (
     <Stack fill vertical>
-      <Stack.Item grow={2}>
+      <Stack.Item grow={3}>
         <SystemWallpaper />
       </Stack.Item>
       <Stack.Item grow>
-        <SystemDirectives />
+        <SystemInfo />
       </Stack.Item>
     </Stack>
   );
@@ -115,17 +133,17 @@ const SystemWallpaper = (_, context) => {
   ].join('\n');
 
   const floofAscii = [
-    '                             .--.       .-.',
-    "       ,;;``;;-;,,..___.,,.-/   `;_//,.'   )",
-    "     .' ;;  `;  :; `;;  ;;  `.       '/   .'",
-    `    ,;  ';   ;   '  ';  ';   ,'    ${eyebrows}';`, // lol
-    "   /'     `      \\   `     ;','   ( d\\__b_),`",
-    "  /   /       .,;;)       ', (    .'     __\\`",
-    " ;:.  \\     ,_   /         ', ' .'_      \\/;",
-    ",   ,;'      `;;/       /    ';,\\ `-..__._,'",
-    ";:.  /____  ..-'--.    /-'    ..---. ._._/ ---.",
-    "|    ;' ;'|        \\--/;' ,' /      \\   ,      \\",
-    "`.fL__;,__/-..__)_)/  `--'--'`-._)_)/ --\\.._)_)/",
+    '                              .--.       .-.',
+    "        ,;;``;;-;,,..___.,,.-/   `;_//,.'   )",
+    "      .' ;;  `;  :; `;;  ;;  `.       '/   .'",
+    `     ,;  ';   ;   '  ';  ';   ,'    ${eyebrows}';`, // lol
+    "    /'     `      \\   `     ;','   ( d\\__b_),",
+    "   /   /       .,;;)       ', (    .'     __\\",
+    "  ;:.  \\     ,_   /         ', ' .'_      \\/;",
+    " ,   ,;'      `;;/       /    ';,\\ `-..__._,'",
+    " ;:.  /____  ..-'--.    /-'    ..---. ._._/ ---.",
+    " |    ;' ;'|        \\--/;' ,' /      \\   ,      \\",
+    " `.fL__;,__/-..__)_)/  `--'--'`-._)_)/ --\\.._)_)/",
   ].join('\n');
 
   return (
@@ -138,9 +156,9 @@ const SystemWallpaper = (_, context) => {
   );
 };
 
-const SystemDirectives = (_, context) => {
-  const { act, data } = useBackend<PaiInterfaceData>(context);
-  const { directives, master } = data;
+const SystemInfo = (_, context) => {
+  const { data } = useBackend<PaiInterfaceData>(context);
+  const { master } = data;
 
   return (
     <Section fill scrollable title="System Info">
@@ -148,34 +166,101 @@ const SystemDirectives = (_, context) => {
         <LabeledList.Item label="Master">
           {master.name || 'None.'}
         </LabeledList.Item>
-        {master.name && (
-          <>
-            <LabeledList.Item label="Signature">
-              {master.dna || 'None.'}
-            </LabeledList.Item>
-
-            <LabeledList.Item label="Primary">
-              Serve your master.
-            </LabeledList.Item>
-          </>
-        )}
-        <LabeledList.Item label={master.name ? 'Secondary' : 'Directive'}>
-          <Box wrap>{directives}</Box>
-        </LabeledList.Item>
+        <LabeledList.Item label="DNA">{master.dna || 'None.'}</LabeledList.Item>
       </LabeledList>
     </Section>
   );
 };
 
+const DirectiveDisplay = (_, context) => {
+  const { data } = useBackend<PaiInterfaceData>(context);
+  const { directives, master } = data;
+  return (
+    <Stack fill vertical>
+      <Stack.Item>
+        <Section title="Logic Core">
+          <Box color="lightslategray">
+            {DIRECTIVE_COMPREHENSION}
+            <br />
+            <br />
+            {DIRECTIVE_ORDER}
+          </Box>
+        </Section>
+      </Stack.Item>
+      <Stack.Item grow>
+        <Section fill scrollable title="Directives">
+          {!master.name ? (
+            'None.'
+          ) : (
+            <LabeledList>
+              <LabeledList.Item label="Prime">
+                Serve your master.
+              </LabeledList.Item>
+              <LabeledList.Item label="Supplemental">
+                <Box wrap>{directives}</Box>
+              </LabeledList.Item>
+            </LabeledList>
+          )}
+        </Section>
+      </Stack.Item>
+    </Stack>
+  );
+};
+
 const InstalledDisplay = (_, context) => {
-  const { act, data } = useBackend<PaiInterfaceData>(context);
-  const { installed } = data.software;
+  const [softwareSelected, setSoftwareSelected] = useSharedState(
+    context,
+    'software',
+    'Select a Program'
+  );
+  const setSoftwareHandler = (software: string) => {
+    setSoftwareSelected(software);
+  };
 
   return (
-    <Section fill scrollable title="Installed Software">
-      {installed.map((software) => {
-        return <Button key={software}>{software}</Button>;
+    <Stack fill vertical>
+      <Stack.Item>
+        <InstalledSoftware softwareHandler={setSoftwareHandler} />
+      </Stack.Item>
+      <Stack.Item grow>
+        <InstalledInfo software={softwareSelected} />
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+const InstalledSoftware = (props, context) => {
+  const { data } = useBackend<PaiInterfaceData>(context);
+  const { installed } = data.software;
+  const { softwareHandler } = props;
+
+  return (
+    <Section scrollable title="Installed Software">
+      {installed.map((software, index) => {
+        return (
+          <Button key={software} onClick={() => softwareHandler(software)}>
+            {software.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+              letter.toUpperCase()
+            )}
+          </Button>
+        );
       })}
+    </Section>
+  );
+};
+
+const InstalledInfo = (props, context) => {
+  const { act, data } = useBackend<PaiInterfaceData>(context);
+  const { software } = props;
+
+  return (
+    <Section
+      fill
+      scrollable
+      title={software.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+        letter.toUpperCase()
+      )}>
+      {getSoftwareInfo(software)}
     </Section>
   );
 };
@@ -229,27 +314,77 @@ const AvailableSoftware = (_, context) => {
   return (
     <LabeledList>
       {convertedList.map((software) => {
-        return (
-          <LabeledList.Item
-            label={software.name.replace(/^\w/, (c) => c.toUpperCase())}
-            key={software.name}
-            buttons={
-              <Button
-                disabled={installed.includes(software.name)}
-                fluid
-                onClick={() => act('buy', { selection: software.name })}
-                width={5}>
-                {software.value}
-                <Icon
-                  color={installed.includes(software.name) && 'purple'}
-                  ml={1}
-                  name="microchip"
-                />
-              </Button>
-            }
-          />
-        );
+        return <AvailableRow key={software.name} software={software} />;
       })}
     </LabeledList>
   );
+};
+
+const AvailableRow = (props, context) => {
+  const { act, data } = useBackend<PaiInterfaceData>(context);
+  const { installed } = data.software;
+  const { software } = props;
+
+  return (
+    <LabeledList.Item
+      label={software.name.replace(/^\w/, (c) => c.toUpperCase())}
+      buttons={
+        <Button
+          disabled={installed.includes(software.name)}
+          fluid
+          onClick={() => act('buy', { selection: software.name })}
+          tooltip={getSoftwareInfo(software.name)}
+          width={5}>
+          {software.value}
+          <Icon
+            color={installed.includes(software.name) && 'purple'}
+            ml={1}
+            name="microchip"
+          />
+        </Button>
+      }
+    />
+  );
+};
+
+/** Helper functions */
+const getSoftwareInfo = (software: string) => {
+  switch (software) {
+    case 'crew manifest':
+      return 'A tool that allows you to view the crew manifest.';
+    case 'digital messenger':
+      return 'A tool that allows you to send messages to other crew members.';
+    case 'atmospheric sensor':
+      return 'A tool that allows you to analyze atmospheric contents.';
+    case 'photography module':
+      return 'A portable camera module.';
+    case 'camera zoom':
+      return 'A tool that allows you to zoom in on your camera.';
+    case 'printer module':
+      return 'A portable printer module for photographs.';
+    case 'remote signaler':
+      return 'A remote signalling device to transmit and receive codes.';
+    case 'medical records':
+      return 'A tool that allows you to view station medical records.';
+    case 'security records':
+      return 'A tool that allows you to veiw station security records, warrants.';
+    case 'host scan':
+      return 'A tool that scans the health data while held.';
+    case 'medical HUD':
+      return 'Allows you to view medical status using an overlay HUD.';
+    case 'security HUD':
+      return 'Allows you to view security records using an overlay HUD.';
+    case 'loudness booster':
+      return 'Synthesizes instruments, plays sounds and imported songs.';
+    case 'newscaster':
+      return 'A tool that allows you to broadcast news to other crew members.';
+    case 'door jack':
+      return 'A tool that allows you to open doors.';
+    case 'encryption keys':
+      return 'A tool that allows you to decrypt and speak on other radio frequencies.';
+    case 'internal gps':
+      return 'A tool that allows you to track your location.';
+    default:
+      return 'No information available.';
+  }
 };
