2ee8453857deceb729f8637c0bb036a7fa2946d7
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2021-11-30 15:43:35 -0500

    What the FUCK man I lost a 100% merge rate for NOTHING

--- tgui/packages/tgui/interfaces/PreferencesMenu/jobs/jobs/lawyer.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/jobs/jobs/lawyer.ts
@@ -1,11 +1,11 @@
 import { Job } from "../base";
-import { Security } from "../departments";
+import { Service } from "../departments";
 
 const Lawyer: Job = {
   name: "Lawyer",
   description: "Advocate for prisoners, create law-binding contracts, \
     ensure Security is following protocol and Space Law.",
-  department: Security,
+  department: Service,
 };
 
 export default Lawyer;
--- tgui/packages/tgui/interfaces/PreferencesMenu/jobs/jobs/psychologist.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/jobs/jobs/psychologist.ts
@@ -1,11 +1,11 @@
 import { Job } from "../base";
-import { Medical } from "../departments";
+import { Service } from "../departments";
 
 const Psychologist: Job = {
   name: "Psychologist",
   description: "Advocate sanity, self-esteem, and teamwork in a station \
     staffed with headcases.",
-  department: Medical,
+  department: Service,
 };
 
 export default Psychologist;
