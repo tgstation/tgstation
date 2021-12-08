8970e778699a515021c754c13b41bc25e953acad
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2021-12-08 22:19:46 +0000

    Fixes

--- tgui/packages/tgui/interfaces/Uplink/ObjectiveMenu.tsx
+++ tgui/packages/tgui/interfaces/Uplink/ObjectiveMenu.tsx
@@ -200,6 +200,7 @@ export class ObjectiveMenu
                           }}
                           width="100%"
                           height="100%"
+                          minHeight="100px"
                         />
                       )}
                     </Flex.Item>
