tgstation/tgstation

commit 2834383245d2129a106acef3afd17b81e1e64777

Makes all default job titles, defines. (#63357)
Jesus christ we were just waiting for someone to misspell something eh?

code/__DEFINES/jobs.dm
@@ -13,6 +13,78 @@
 
 #define JOB_DISPLAY_ORDER_DEFAULT 0
 
+//No department
+#define JOB_ASSISTANT "Assistant"
+#define JOB_PRISONER "Prisoner"
+//Command
+#define JOB_CAPTAIN "Captain"
+#define JOB_HEAD_OF_PERSONNEL "Head of Personnel"
+#define JOB_HEAD_OF_SECURITY "Head of Security"
+#define JOB_RESEARCH_DIRECTOR "Research Director"
+#define JOB_CHIEF_ENGINEER "Chief Engineer"
+#define JOB_CHIEF_MEDICAL_OFFICER "Chief Medical Officer"
+//Silicon
+#define JOB_AI "AI"
+#define JOB_CYBORG "Cyborg"
+//Security
+#define JOB_WARDEN "Warden"
+#define JOB_DETECTIVE "Detective"
+#define JOB_SECURITY_OFFICER "Security Officer"
+#define JOB_SECURITY_OFFICER_MEDICAL "Security Officer (Medical)"
+#define JOB_SECURITY_OFFICER_ENGINEERING "Security Officer (Engineering)"
+#define JOB_SECURITY_OFFICER_SCIENCE "Security Officer (Science)"
+#define JOB_SECURITY_OFFICER_SUPPLY "Security Officer (Cargo)"
+//Engineering
+#define JOB_STATION_ENGINEER "Station Engineer"
+#define JOB_ATMOSPHERIC_TECHNICIAN "Atmospheric Technician"
+//Medical
+#define JOB_MEDICAL_DOCTOR "Medical Doctor"
+#define JOB_PARAMEDIC "Paramedic"
+#define JOB_CHEMIST "Chemist"
+#define JOB_VIROLOGIST "Virologist"
+//Science
+#define JOB_SCIENTIST "Scientist"
+#define JOB_ROBOTICIST "Roboticist"
+#define JOB_GENETICIST "Geneticist"
+//Supply
+#define JOB_QUARTERMASTER "Quartermaster"
+#define JOB_CARGO_TECHNICIAN "Cargo Technician"
+#define JOB_SHAFT_MINER "Shaft Miner"
+//Service
+#define JOB_BARTENDER "Bartender"
+#define JOB_BOTANIST "Botanist"
+#define JOB_COOK "Cook"
+#define JOB_JANITOR "Janitor"
+#define JOB_CLOWN "Clown"
+#define JOB_MIME "Mime"
+#define JOB_CURATOR "Curator"
+#define JOB_LAWYER "Lawyer"
+#define JOB_CHAPLAIN "Chaplain"
+#define JOB_PSYCHOLOGIST "Psychologist"
+//ERTs
+#define JOB_ERT_COMMANDER "Emergency Response Team Commander"
+#define JOB_ERT_OFFICER "Security Response Officer"
+#define JOB_ERT_ENGINEER "Engineering Response Officer"
+#define JOB_ERT_MEDICAL_DOCTOR "Medical Response Officer"
+#define JOB_ERT_CLOWN "Entertainment Response Officer"
+#define JOB_ERT_CHAPLAIN "Religious Response Officer"
+#define JOB_ERT_JANITOR "Janitorial Response Officer"
+#define JOB_ERT_DEATHSQUAD "Death Commando"
+//CentCom
+#define JOB_CENTCOM "Central Command"
+#define JOB_CENTCOM_OFFICIAL "CentCom Official"
+#define JOB_CENTCOM_ADMIRAL "Admiral"
+#define JOB_CENTCOM_COMMANDER "CentCom Commander"
+#define JOB_CENTCOM_VIP "VIP Guest"
+#define JOB_CENTCOM_BARTENDER "CentCom Bartender"
+#define JOB_CENTCOM_CUSTODIAN "Custodian"
+#define JOB_CENTCOM_THUNDERDOME_OVERSEER "Thunderdome Overseer"
+#define JOB_CENTCOM_MEDICAL_DOCTOR "Medical Officer"
+#define JOB_CENTCOM_RESEARCH_OFFICER "Research Officer"
+#define JOB_CENTCOM_SPECIAL_OFFICER "Special Ops Officer"
+#define JOB_CENTCOM_PRIVATE_SECURITY "Private Security Force"
+
+
 #define JOB_DISPLAY_ORDER_ASSISTANT 1
 #define JOB_DISPLAY_ORDER_CAPTAIN 2
 #define JOB_DISPLAY_ORDER_HEAD_OF_PERSONNEL 3
@@ -51,6 +123,7 @@
 
 
 #define DEPARTMENT_UNASSIGNED "No department assigned"
+
 #define DEPARTMENT_BITFLAG_SECURITY (1<<0)
 #define DEPARTMENT_SECURITY "Security"
 #define DEPARTMENT_BITFLAG_COMMAND (1<<1)

code/modules/admin/player_panel.dm
@@ -85,7 +85,7 @@
 					body += "<a href='?_src_=vars;[HrefToken()];Vars="+ref+"'>VV</a> - "
 					body += "<a href='?_src_=vars;[HrefToken()];skill="+ref+"'>SP</a> - "
 					body += "<a href='?_src_=holder;[HrefToken()];traitor="+ref+"'>TP</a> - "
-					if (job == "Cyborg")
+					if (job == JOB_CYBORG)
 						body += "<a href='?_src_=holder;[HrefToken()];borgpanel="+ref+"'>BP</a> - "
 					body += "<a href='?priv_msg="+ckey+"'>PM</a> - "
 					body += "<a href='?_src_=holder;[HrefToken()];subtlemessage="+ref+"'>SM</a> - "

commit 726b55e066f0909b6c85340a88e02f8c677c520e

Fixes bug in playerpanel javascript relating to cyborgs
Introduced by #63357, this caused the player panel mouseover behaviour
to stop working and malfunction.

I also added a big warning to the job defines so that people don't think
it's easy to just wander in and rename all the jobs for fun

code/__DEFINES/jobs.dm
@@ -13,6 +13,19 @@
 
 #define JOB_DISPLAY_ORDER_DEFAULT 0
 
+
+/**
+ * =======================
+ * WARNING WARNING WARNING
+ * WARNING WARNING WARNING
+ * WARNING WARNING WARNING
+ * =======================
+ * These names are used as keys in many locations in the database
+ * you cannot change them trivially without breaking job bans and
+ * role time tracking, if you do this and get it wrong you will die
+ * and it will hurt the entire time
+ */
+
 //No department
 #define JOB_ASSISTANT "Assistant"
 #define JOB_PRISONER "Prisoner"

code/modules/admin/player_panel.dm
@@ -85,7 +85,7 @@
 					body += "<a href='?_src_=vars;[HrefToken()];Vars="+ref+"'>VV</a> - "
 					body += "<a href='?_src_=vars;[HrefToken()];skill="+ref+"'>SP</a> - "
 					body += "<a href='?_src_=holder;[HrefToken()];traitor="+ref+"'>TP</a> - "
-					if (job == JOB_CYBORG)
+					if (job == "Cyborg")
 						body += "<a href='?_src_=holder;[HrefToken()];borgpanel="+ref+"'>BP</a> - "
 					body += "<a href='?priv_msg="+ckey+"'>PM</a> - "
 					body += "<a href='?_src_=holder;[HrefToken()];subtlemessage="+ref+"'>SM</a> - "