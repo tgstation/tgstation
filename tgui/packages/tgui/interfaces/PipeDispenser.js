3a7be77499dc78a55eb4469f7e072de29a5c198e
neopythagorean <flavivs.clavdivs.jvlianvs@gmail.com>
2022-01-07 07:55:50 -0500

    better use of existing components
    modifies RPD to be more modular, making the reuse of shared components
    easier.

--- tgui/packages/tgui/interfaces/PipeDispenser.js
+++ tgui/packages/tgui/interfaces/PipeDispenser.js
@@ -1,73 +1,7 @@
-import { multiline } from 'common/string';
 import { useBackend, useLocalState } from '../backend';
-import { Box, Button, ColorBox, LabeledList, Section, Stack, Tabs } from '../components';
+import { Button, LabeledList, Section, Stack, Tabs } from '../components';
 import { Window } from '../layouts';
-
-const ICON_BY_CATEGORY_NAME = {
-  'Atmospherics': 'wrench',
-  'Disposals': 'trash-alt',
-  'Transit Tubes': 'bus',
-  'Pipes': 'grip-lines',
-  'Disposal Pipes': 'grip-lines',
-  'Devices': 'microchip',
-  'Heat Exchange': 'thermometer-half',
-  'Station Equipment': 'microchip',
-};
-
-const ColorSection = (props, context) => {
-  const { act, data } = useBackend(context);
-  const {
-    selected_color,
-  } = data;
-  return (
-    <Section>
-      <LabeledList>
-        <LabeledList.Item
-          label="Color">
-          <Box
-            inline
-            width="64px"
-            color={data.paint_colors[selected_color]}>
-            {selected_color}
-          </Box>
-          {Object.keys(data.paint_colors)
-            .map(colorName => (
-              <ColorBox
-                key={colorName}
-                ml={1}
-                color={data.paint_colors[colorName]}
-                onClick={() => act('color', {
-                  paint_color: colorName,
-                })} />
-            ))}
-        </LabeledList.Item>
-      </LabeledList>
-    </Section>
-  );
-};
-
-const LayerSection = (props, context) => {
-  const { act, data } = useBackend(context);
-  const {
-    piping_layer,
-  } = data;
-  return (
-    <Section fill width={7.5}>
-      <Stack vertical mb={1}>
-        {[1, 2, 3, 4, 5].map(layer => (
-          <Stack.Item my={0} key={layer}>
-            <Button.Checkbox
-              checked={layer === piping_layer}
-              content={'Layer ' + layer}
-              onClick={() => act('piping_layer', {
-                piping_layer: layer,
-              })} />
-          </Stack.Item>
-        ))}
-      </Stack>
-    </Section>
-  );
-};
+import { ICON_BY_CATEGORY_NAME, ColorItem, LayerSelect, SmartPipeBlockSection } from './RapidPipeDispenser';
 
 const PipeTypeSection = (props, context) => {
   const { act, data } = useBackend(context);
@@ -112,70 +46,6 @@ const PipeTypeSection = (props, context) => {
   );
 };
 
-const SmartPipeBlockSection = (props, context) => {
-  const { act, data } = useBackend(context);
-  const init_directions = data.init_directions || [];
-  return (
-    <Section height={7.5}>
-      <Stack fill vertical textAlign="center">
-        <Stack.Item basis={1.5}>
-          <Stack>
-            <Stack.Item>
-              <Button
-                color="transparent"
-                icon="info"
-                tooltipPosition="right"
-                tooltip={multiline`
-                This is a panel for blocking certain connection
-                directions for the smart pipes.
-                The button in the center resets to
-                default (all directions can connect)`} />
-            </Stack.Item>
-            <Stack.Item>
-              <Button icon="arrow-up"
-                disabled={!!data.smart_pipe}
-                selected={init_directions["north"]}
-                onClick={() => act('init_dir_setting', {
-                  dir_flag: "north",
-                })} />
-            </Stack.Item>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item basis={1.5}>
-          <Stack fill>
-            <Stack.Item>
-              <Button icon="arrow-left"
-                selected={init_directions["west"]}
-                onClick={() => act('init_dir_setting', {
-                  dir_flag: "west",
-                })} />
-            </Stack.Item>
-            <Stack.Item grow>
-              <Button icon="circle"
-                onClick={() => act('init_reset', {
-                })} />
-            </Stack.Item>
-            <Stack.Item>
-              <Button icon="arrow-right"
-                selected={init_directions["east"]}
-                onClick={() => act('init_dir_setting', {
-                  dir_flag: "east",
-                })} />
-            </Stack.Item>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item grow>
-          <Button icon="arrow-down"
-            selected={init_directions["south"]}
-            onClick={() => act('init_dir_setting', {
-              dir_flag: "south",
-            })} />
-        </Stack.Item>
-      </Stack>
-    </Section>
-  );
-};
-
 export const PipeDispenser = (props, context) => {
   const { act, data } = useBackend(context);
   const {
@@ -189,7 +59,11 @@ export const PipeDispenser = (props, context) => {
         <Stack fill vertical>
           {rootCategoryIndex === 0 && (
             <Stack.Item>
-              <ColorSection />
+              <Section>
+                <LabeledList>
+                  <ColorItem />
+                </LabeledList>
+              </Section>
             </Stack.Item>
           )}
           <Stack.Item grow>
@@ -201,7 +75,9 @@ export const PipeDispenser = (props, context) => {
                       <SmartPipeBlockSection />
                     </Stack.Item>
                     <Stack.Item grow>
-                      <LayerSection />
+                      <Section fill width={7.5}>
+                        <LayerSelect />
+                      </Section>
                     </Stack.Item>
                   </Stack>
                 </Stack.Item>
