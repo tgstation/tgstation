e154cea2ce9304b9125e7aa0336b5686249a3014
tastyfish <crazychris32@gmail.com>
2022-01-23 04:34:26 -0500

    Make desttagger alphabetical order the only order, remove option to change

--- tgui/packages/tgui/interfaces/DestinationTagger.tsx
+++ tgui/packages/tgui/interfaces/DestinationTagger.tsx
@@ -1,19 +1,10 @@
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
-import { Stack, Section, Button, Divider } from '../components';
-
-/**
- * The sorting mode for the destination list. See sortDestinations()
- */
-enum SortMode {
-  SM_DEFAULT = "STANDARD", // By code
-  SM_ALPHABETICAL = "ALPHABETICAL", // By name
-}
+import { Stack, Section, Button } from '../components';
 
 type DestinationTaggerData = {
   locations: string[];
   currentTag: number;
-  sortListMode: SortMode;
 };
 
 /**
@@ -24,27 +15,27 @@ type DestinationInfo = {
   sorting_id: number;
 };
 
-const sortDestinations
-  = (locations: string[], mode: SortMode): DestinationInfo[] => {
-    const clean_destinations = locations.map((name, index) => ({
+/**
+ * Sort destinations in alphabetical order,
+ * and wrap them in a way that preserves what ID to return.
+ * @param locations The raw, official list of destination tags.
+ * @returns The alphetically sorted list of destinations.
+ */
+const sortDestinations = (locations: string[]): DestinationInfo[] => {
+  return locations
+    .map((name, index) => ({
       name: name.toUpperCase(),
       sorting_id: index + 1,
-    }));
-
-    switch (mode) {
-      case SortMode.SM_ALPHABETICAL:
-        return clean_destinations.sort((a, b) => a.name.localeCompare(b.name));
-      default:
-        return clean_destinations;
-    }
-  };
+    }))
+    .sort((a, b) => a.name.localeCompare(b.name));
+};
 
 export const DestinationTagger = (props, context) => {
   const { act, data } = useBackend<DestinationTaggerData>(context);
-  const { locations, currentTag, sortListMode } = data;
+  const { locations, currentTag } = data;
 
   return (
-    <Window theme="retro" title="TagMaster 2.4" width={420} height={530}>
+    <Window theme="retro" title="TagMaster 2.4" width={420} height={500}>
       <Window.Content>
         <Stack fill vertical>
           <Stack.Item grow>
@@ -56,34 +47,18 @@ export const DestinationTagger = (props, context) => {
                   ? 'Please Select A Location'
                   : `Current Destination: ${locations[currentTag - 1]}`
               }>
-              {Object.values(SortMode).map((modeName) => {
+              {sortDestinations(locations).map((location) => {
                 return (
                   <Button.Checkbox
-                    checked={sortListMode === modeName}
+                    checked={currentTag === location.sorting_id}
                     height={2}
-                    key={modeName}
-                    onClick={() => act('sort_list', { mode: modeName })}
-                    width={15} >
-                    {modeName}
+                    key={location.sorting_id}
+                    onClick={() => act('change', { index: location.sorting_id })}
+                    width={15}>
+                    {location.name}
                   </Button.Checkbox>
                 );
               })}
-
-              <Divider />
-
-              {sortDestinations(locations, sortListMode)
-                .map((location) => {
-                  return (
-                    <Button.Checkbox
-                      checked={currentTag === location.sorting_id}
-                      height={2}
-                      key={location.sorting_id}
-                      onClick={() => act('change', { index: location.sorting_id })}
-                      width={15}>
-                      {location.name}
-                    </Button.Checkbox>
-                  );
-                })}
             </Section>
           </Stack.Item>
         </Stack>
