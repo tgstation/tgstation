83ce04a2985731d07f317a1893223fe05ae1ebea
Jeremiah <jlsnow301@protonmail.com>
2022-02-06 14:02:57 -0800

    linter

--- tgui/packages/tgui/interfaces/AlertModal.tsx
+++ tgui/packages/tgui/interfaces/AlertModal.tsx
@@ -1,7 +1,7 @@
 import { Loader } from './common/Loader';
 import { Preferences } from './common/InputButtons';
 import { useBackend, useLocalState } from '../backend';
-import { KEY_ESCAPE, KEY_ENTER, KEY_LEFT, KEY_RIGHT, KEY_SPACE, KEY_TAB } from '../../common/keycodes';
+import { KEY_ENTER, KEY_ESCAPE, KEY_LEFT, KEY_RIGHT, KEY_SPACE, KEY_TAB } from '../../common/keycodes';
 import { Autofocus, Box, Button, Flex, NoticeBox, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
@@ -19,16 +19,24 @@ const KEY_INCREMENT = 1;
 
 export const AlertModal = (_, context) => {
   const { act, data } = useBackend<AlertModalData>(context);
-  const { autofocus, buttons = [], message, preferences, timeout, title } = data;
+  const {
+    autofocus,
+    buttons = [],
+    message,
+    preferences,
+    timeout,
+    title,
+  } = data;
   const { large_buttons } = preferences;
   const [selected, setSelected] = useLocalState<number>(context, 'selected', 0);
   // Dynamically sets window height
   let windowHeight = 100;
   if (message && buttons) {
-    windowHeight = 100
-    + Math.ceil(message.length / 3)
-    + (message.length && large_buttons ? 5 : 0)
-    + (buttons.length > 2 ? buttons.length * 25 : 0);
+    windowHeight
+      = 100
+      + Math.ceil(message.length / 3)
+      + (message.length && large_buttons ? 5 : 0)
+      + (buttons.length > 2 ? buttons.length * 25 : 0);
   }
   const onKey = (direction: number) => {
     if (selected === 0 && direction === KEY_DECREMENT) {
@@ -67,7 +75,10 @@ export const AlertModal = (_, context) => {
           <Stack fill vertical>
             <Stack.Item grow m={1}>
               {message ? (
-                <Box color="label">{message}</Box>) : (<NoticeBox>Please reload to continue.</NoticeBox>)}
+                <Box color="label">{message}</Box>
+              ) : (
+                <NoticeBox>Please reload to continue.</NoticeBox>
+              )}
             </Stack.Item>
             <Stack.Item>
               {!!autofocus && <Autofocus />}
