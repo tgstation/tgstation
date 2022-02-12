b206de6fa1af1f690c72cc20cb0dd6f8565ea770
Zxaber <37497534+zxaber@users.noreply.github.com>
2022-02-12 03:44:00 -0800

    Your designation is 2873

--- tgui/packages/tgui/interfaces/RoboticsControlConsole.js
+++ tgui/packages/tgui/interfaces/RoboticsControlConsole.js
@@ -7,6 +7,7 @@ export const RoboticsControlConsole = (props, context) => {
   const [tab, setTab] = useSharedState(context, 'tab', 1);
   const {
     can_hack,
+    can_deto,
     cyborgs = [],
     drones = [],
   } = data;
@@ -32,7 +33,7 @@ export const RoboticsControlConsole = (props, context) => {
           </Tabs.Tab>
         </Tabs>
         {tab === 1 && (
-          <Cyborgs cyborgs={cyborgs} can_hack={can_hack} />
+          <Cyborgs cyborgs={cyborgs} can_hack={can_hack} can_deto={can_deto} />
         )}
         {tab === 2 && (
           <Drones drones={drones} />
@@ -43,7 +44,7 @@ export const RoboticsControlConsole = (props, context) => {
 };
 
 const Cyborgs = (props, context) => {
-  const { cyborgs, can_hack } = props;
+  const { cyborgs, can_hack, can_deto } = props;
   const { act, data } = useBackend(context);
   if (!cyborgs.length) {
     return (
@@ -75,6 +76,15 @@ const Cyborgs = (props, context) => {
               onClick={() => act('stopbot', {
                 ref: cyborg.ref,
               })} />
+            {!!can_deto && (
+              <Button.Confirm
+                icon="bomb"
+                content="Detonate"
+                color="bad"
+                onClick={() => act('killbot', {
+                  ref: cyborg.ref,
+                })} />
+            )}
           </>
         )}>
         <LabeledList>
