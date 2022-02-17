9ad5277eeee0bf56722b5980db006035b70e830a
ArcaneMusic <hero12290@aol.com>
2022-02-17 16:37:36 -0500

    first implementation of tabs and the bounty board data.

--- code/game/machinery/better_newscaster.dm
+++ code/game/machinery/better_newscaster.dm
@@ -5,6 +5,16 @@
 	icon_state = "scanner_wand"
 	///What newscaster channel is currently being viewed by the player?
 	var/datum/newscaster/feed_channel/current_channel
+	///The message that's currently being written for a feed story.
+	var/feed_channel_message
+	///Reference to the currently logged in user.
+	var/datum/bank_account/current_user
+	///The station request datum being affected by UI actions.
+	var/datum/station_request/active_request
+	///Value of the currently bounty input
+	var/bounty_value = 1
+	///Text of the currently written bounty
+	var/bounty_text = ""
 
 /obj/item/newser/ui_interact(mob/user, datum/tgui/ui)
 	. = ..()
@@ -15,31 +25,40 @@
 
 /obj/item/newser/ui_data(mob/user)
 	. = ..()
+	//**************************
+	//		Newscaster Data
+	//**************************
 	var/list/data = list()
 	var/list/channel_list = list()
 	var/list/message_list = list()
 
 	//Code displaying name and Job Information, taken from the player mob's ID card if one exists.
-	var/obj/item/card/id/C
+	var/obj/item/card/id/Card
 	if(isliving(user))
 		var/mob/living/L = user
-		C = L.get_idcard(TRUE)
-	if(C?.registered_account)
+		Card = L.get_idcard(TRUE)
+	if(Card?.registered_account)
+		current_user = Card.registered_account
 		data["user"] = list()
-		data["user"]["name"] = C.registered_account.account_holder
-		data["user"]["cash"] = C.registered_account.account_balance
-		if(C.registered_account.account_job)
-			data["user"]["job"] = C.registered_account.account_job.title
-			data["user"]["department"] = C.registered_account.account_job.paycheck_department
+		data["user"]["name"] = Card.registered_account.account_holder
+		data["user"]["cash"] = Card.registered_account.account_balance
+		if(Card.registered_account.account_job)
+			data["user"]["job"] = Card.registered_account.account_job.title
+			data["user"]["department"] = Card.registered_account.account_job.paycheck_department
 		else
 			data["user"]["job"] = "No Job"
 			data["user"]["department"] = "No Department"
+
+	data["security_mode"] = FALSE
+	if(Card && (ACCESS_ARMORY in Card?.GetAccess()))
+		data["security_mode"] = TRUE
 
 	//Code breaking down the channels that have been made on-station thus far. ha
 	//Then, breaks down the messages that have been made on those channels.
 	for(var/datum/newscaster/feed_channel/channel in GLOB.news_network.network_channels)
 		channel_list += list(list(
 			"name" = channel.channel_name,
+			"author" = channel.author,
 			"censored" = channel.censored,
 			"locked" = channel.locked,
 			"ID" = channel.channel_ID,
@@ -57,10 +76,31 @@
 	data["channelName"] = current_channel?.channel_name
 	data["channelAuthor"] = current_channel?.author
 	data["channelDesc"] = current_channel?.channel_desc
+	data["channelBlocked"] = current_channel?.locked || current_channel?.censored
 
 	//We send all the information about all channels and all messages in existance.
 	data["channel"] = channel_list
 	data["messages"] = message_list
+
+	//**************************
+	//	  Bounty Board Data
+	//**************************
+	var/list/formatted_requests = list()
+	var/list/formatted_applicants = list()
+	for(var/i in GLOB.request_list)
+		if(!i)
+			continue
+		var/datum/station_request/request = i
+		formatted_requests += list(list("owner" = request.owner, "value" = request.value, "description" = request.description, "acc_number" = request.req_number))
+		if(request.applicants)
+			for(var/datum/bank_account/j in request.applicants)
+				formatted_applicants += list(list("name" = j.account_holder, "request_id" = request.owner_account.account_id, "requestee_id" = j.account_id))
+	if(Card?.registered_account) //work out current user.
+		data["accountName"] = Card.registered_account.account_holder
+	data["requests"] = formatted_requests
+	data["applicants"] = formatted_applicants
+	data["bountyValue"] = bounty_value
+	data["bountyText"] = bounty_text
 
 	return data
 
@@ -77,4 +117,28 @@
 				if(proto_chan == potential_channel.channel_ID)
 					current_channel = potential_channel
 			say("[current_channel.channel_name] has been set yaknow what i'm saying")
+			. = TRUE
+
+		if("createStory")
+			var/proto_chan = (params["current"])
+			for(var/datum/newscaster/feed_channel/potential_channel in GLOB.news_network.network_channels)
+				if(proto_chan == potential_channel.channel_ID)
+					current_channel = potential_channel
+			var/temp_message = tgui_input_text(usr, "Write your Feed story", "Network Channel Handler", feed_channel_message, multiline = TRUE)
+			if(temp_message)
+				feed_channel_message = temp_message
+			GLOB.news_network.SubmitArticle("<font face=\"[PEN_FONT]\">[parsemarkdown(feed_channel_message, usr)]</font>", current_user.account_holder, current_channel.channel_name, null, 0, FALSE)
+			SSblackbox.record_feedback("amount", "newscaster_stories", 1)
+			feed_channel_message = ""
+
+		if("storyCensor")
+			if (!params["secure"])
+				return
+			say("This is where I'd censor the story if I could.")
+			. = TRUE
+
+		if("authorCensor")
+			if (!params["secure"])
+				return
+			say("This is where I'd censor the author if I could.")
 			. = TRUE
--- tgui/packages/tgui/interfaces/Newscaster.js
+++ tgui/packages/tgui/interfaces/Newscaster.js
@@ -5,12 +5,58 @@
  */
 
 import { useBackend } from '../backend';
-import { BlockQuote, Box, Button, Flex, Icon, LabeledList, NoticeBox, Section, Stack, Tabs } from '../components';
+import { RequestKioskContent } from './RequestKiosk';
+import { BlockQuote, Box, Button, Divider, Flex, Icon, LabeledList, NoticeBox, Section, Stack, Tabs } from '../components';
 import { Window } from '../layouts';
 import marked from 'marked';
 import { sanitizeText } from "../sanitize";
 
 export const Newscaster = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    screenmode,
+  } = data;
+  return (
+    <Window
+      width={575}
+      height={420}>
+      <Window.Content scrollable>
+        <Tabs fluid textAlign="center">
+          <Tabs.Tab
+            color="Green"
+            selected={screenmode == 1}
+            onClick={() => setTab_main(1)}>
+            Newscaster
+          </Tabs.Tab>
+          <Tabs.Tab
+            color="Blue"
+            selected={screenmode == 2}
+            onClick={() => setTab_main(2)}>
+            Bounty Board
+          </Tabs.Tab>
+          <Tabs.Tab
+            color="Purple"
+            selected={screenmode == 3}
+            onClick={() => setTab_main(3)}>
+            Station Mandates
+          </Tabs.Tab>
+        </Tabs>
+        if (screenmode == 1) {
+          <NewscasterContent />
+        } else if (screenmode == 2) {
+          <RequestKioskContent />
+        } else {
+          <Box>
+            Not done yet :)
+          </Box>
+        }
+
+      </Window.Content>
+    </Window>
+  );
+};
+
+export const NewscasterContent = (props, context) => {
   const { act, data } = useBackend(context);
   const {
     index,
@@ -19,29 +65,26 @@ export const Newscaster = (props, context) => {
     icon,
     viewing_channel,
     current_channel = [],
+    security_mode,
   } = data;
   return (
-    <Window
-      width={575}
-      height={420}>
-      <Window.Content scrollable>
-        <Flex mb={1}>
-          <Flex.Item mr={1}>
-            <NewscasterChannelSelector />
-          </Flex.Item>
-          <Flex.Item grow={1} basis={0}>
-            <NewscasterInfobox />
-            <NewscasterChannelBox
-              channelName={current_channel.name}
-              channelOwner={current_channel.owner}
-              channelDesc={current_channel.desc} />
-          </Flex.Item>
-        </Flex>
-        <Section p={1}>
-          <NewscasterChannelMessages />
-        </Section>
-      </Window.Content>
-    </Window>
+    <>
+      <Flex mb={1}>
+        <Flex.Item mr={1}>
+          <NewscasterChannelSelector />
+        </Flex.Item>
+        <Flex.Item grow={1} basis={0}>
+          <NewscasterInfobox />
+          <NewscasterChannelBox
+            channelName={current_channel.name}
+            channelOwner={current_channel.owner}
+            channelDesc={current_channel.desc} />
+        </Flex.Item>
+      </Flex>
+      <Section p={1}>
+        <NewscasterChannelMessages />
+      </Section>
+    </>
   );
 };
 
@@ -81,7 +124,9 @@ const NewscasterChannelBox = (props, context) => {
   const {
     channelName,
     channelDesc,
-    author,
+    channelBlocked,
+    channelAuthor,
+    viewing_channel,
   } = data;
   return (
     <Section title={channelName} >
@@ -90,14 +135,15 @@ const NewscasterChannelBox = (props, context) => {
       </BlockQuote>
       <LabeledList mt={1} mb={1}>
         <LabeledList.Item label="Owner">
-          {author}
+          {channelAuthor}
         </LabeledList.Item>
       </LabeledList>
       <Box>
         <Button
           icon="print"
           content="Submit Story"
-          onClick={() => act('createStory')}
+          disabled={channelBlocked}
+          onClick={() => act('createStory', { current: viewing_channel })}
           mt={1} />
       </Box>
     </Section>
@@ -137,14 +183,14 @@ const NewscasterChannelSelector = (props, context) => {
 
 const processedText = value => {
   const textHtml = {
-    __html: sanitizeText(value),
+    __html: sanitizeText(marked(value, {
+      breaks: true,
+      smartypants: true,
+      smartLists: true,
+      baseUrl: 'thisshouldbreakhttp',
+    })),
   };
-  return marked(textHtml, {
-    breaks: true,
-    smartypants: true,
-    smartLists: true,
-    baseUrl: 'thisshouldbreakhttp',
-  });
+  return textHtml;
 };
 
 
@@ -154,27 +200,45 @@ const NewscasterChannelMessages = (props, context) => {
   const {
     messages = [],
     viewing_channel,
+    security_mode,
   } = data;
   return (
-    <Section>
+    <Section scrollable>
       {messages?.map(message => {
         if (message.channel_num !== viewing_channel) {
           return;
         }
         return (
-          <BlockQuote
-            // textColor="white"
-            key={message.body}>
-            <Section>
-              {processedText(message.body)}
-            </Section>
-            <Box
-              pl={2.5}
-              textColor="grey"
-              italic>
-              by: {message.auth} at {message.time}
-            </Box>
-          </BlockQuote>
+          <Section
+            key={message.body}
+            title={(
+              <i>
+                By: {message.auth} at {message.time}
+              </i>
+            )}
+            buttons={(
+              <>
+                <Button
+                  icon={"comment-slash"}
+                  tooltip={"Censor Story"}
+                  disabled={!security_mode}
+                  onClick={() => act('storyCensor', { secure: security_mode })} />
+                <Button
+                  icon={'user-slash'}
+                  tooltip={"Censor Author"}
+                  disabled={!security_mode}
+                  onClick={() => act('authorCensor', { secure: security_mode })} />
+              </>
+            )} >
+            <BlockQuote
+              textColor="white">
+              <Section
+                color="label"
+                dangerouslySetInnerHTML={processedText(message.body)}
+                pl={1} />
+            </BlockQuote>
+            <Divider />
+          </Section>
         ); }
       )}
     </Section>
