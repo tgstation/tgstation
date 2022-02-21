4f6de72c0bd18ab9cf09ff63207f81c60904ee02
Vincentius Vincentius <vincentius_vin@yahoo.com>
2022-02-21 10:38:24 +0700

    poggers??

--- tgui/packages/tgui/components/Button.js
+++ tgui/packages/tgui/components/Button.js
@@ -35,6 +35,7 @@ export const Button = props => {
     children,
     onclick,
     onClick,
+    verticalAlign,
     ...rest
   } = props;
   const hasContent = !!(content || children);
@@ -67,6 +68,7 @@ export const Button = props => {
         circular && 'Button--circular',
         compact && 'Button--compact',
         iconPosition && 'Button--iconPosition--' + iconPosition,
+        verticalAlign && 'Button--verticalAlign--' + verticalAlign,
         (color && typeof color === 'string')
           ? 'Button--color--' + color
           : 'Button--color--default',
@@ -94,22 +96,26 @@ export const Button = props => {
         }
       }}
       {...computeBoxProps(rest)}>
-      {(icon && iconPosition !== 'right') && (
-        <Icon
-          name={icon}
-          color={iconColor}
-          rotation={iconRotation}
-          spin={iconSpin} />
-      )}
-      {content}
-      {children}
-      {(icon && iconPosition === 'right') && (
-        <Icon
-          name={icon}
-          color={iconColor}
-          rotation={iconRotation}
-          spin={iconSpin} />
-      )}
+      <div className={fluid && 'Button__content--fluid'}>
+        {icon && iconPosition !== 'right' && (
+          <Icon
+            name={icon}
+            color={iconColor}
+            rotation={iconRotation}
+            spin={iconSpin}
+          />
+        )}
+        {content}
+        {children}
+        {icon && iconPosition === 'right' && (
+          <Icon
+            name={icon}
+            color={iconColor}
+            rotation={iconRotation}
+            spin={iconSpin}
+          />
+        )}
+      </div>
     </div>
   );
 
