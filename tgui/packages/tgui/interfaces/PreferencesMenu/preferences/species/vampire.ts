8459ffe26cba4aaa89e6c6cfca326f3a48fba590
Gupta <89876614+BallastMonster@users.noreply.github.com>
2022-02-22 06:42:55 -0600

    Spellcheck Vampires can become bats
    removes misleading text.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/vampire.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/vampire.ts
@@ -15,15 +15,7 @@ const Vampire: Species = {
       description: "Minor undead enjoy some of the perks of being dead, like \
         not needing to breathe or eat, but do not get many of the \
         environmental immunities involved with being fully undead.",
-    }, {
-      icon: "recycle",
-      name: "Bat Form",
-      description: "Vampires can become bats. Bats are very weak, but \
-        are great for escaping bad situations. They can also travel through \
-        vents, giving Vampires a lot of access. Just remember that access \
-        doesn't equal permission, and people may be unhappy with you showing \
-        up uninvited!",
-    }],
+    }
     neutral: [],
     bad: [{
       icon: "tint",
