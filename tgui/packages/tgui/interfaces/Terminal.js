a05e1d0ef51eb5210adc38d22221eb195e7e416e
Unit0016 <50649185+unit0016@users.noreply.github.com>
2022-03-07 18:01:07 -0500

    Stretch this out to multiple lines to see if I can find the exact problem

--- tgui/packages/tgui/interfaces/Terminal.js
+++ tgui/packages/tgui/interfaces/Terminal.js
@@ -1,19 +1,42 @@
-import { useBackend } from '../backend';
-import { NoticeBox } from '../components';
-import { Window } from '../layouts';
-import { sanitizeText } from '../sanitize';
+import {
+  useBackend
+} from '../backend';
+import {
+  NoticeBox
+} from '../components';
+import {
+  Window
+} from '../layouts';
+import {
+  sanitizeText
+} from '../sanitize';
 
 export const Terminal = (props, context) => {
-  const { act, data } = useBackend(context);
-  return (
-   <Window theme={data.tguitheme} title="Terminal" width={600} height={600}>
-    <Window.Content scrollable>
-     <NoticeBox textAlign="left">
-      {data.uppertext}
-     </NoticeBox>
-      {sanitizeText(data.text)}
-    </Window.Content>
-   </Window>
+  const {
+      act,
+      data
+  } = useBackend(context);
+  return ( <
+      Window theme = {
+          data.tguitheme
+      }
+      title = "Terminal"
+      width = {
+          600
+      }
+      height = {
+          600
+      } >
+      <
+      Window.Content scrollable >
+      <
+      NoticeBox textAlign = "left" > {
+          data.uppertext
+      } <
+      /NoticeBox> {
+          sanitizeText(data.text)
+      } <
+      /Window.Content> <
+      /Window>
   );
 };
-
