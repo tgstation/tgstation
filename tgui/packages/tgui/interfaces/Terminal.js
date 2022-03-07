8594aeddc6be4a5335b20de2ec80b30adb02fabc
Unit0016 <50649185+unit0016@users.noreply.github.com>
2022-03-07 18:09:04 -0500

    Update Terminal.js

--- tgui/packages/tgui/interfaces/Terminal.js
+++ tgui/packages/tgui/interfaces/Terminal.js
@@ -6,14 +6,14 @@ import { sanitizeText } from '../sanitize';
 export const Terminal = (props, context) => {
   const { act, data } = useBackend(context);
   return (
-   <Window theme={data.tguitheme} title="Terminal" width={600} height={600}>
-    <Window.Content scrollable>
-     <NoticeBox textAlign="left">
-      {data.uppertext}
-     </NoticeBox>
-      {sanitizeText(data.text)}
-    </Window.Content>
-   </Window>
+    <Window theme={data.tguitheme} title="Terminal" width={600} height={600}>
+      <Window.Content scrollable>
+        <NoticeBox textAlign="left">
+          {data.uppertext}
+        </NoticeBox>
+        {sanitizeText(data.text)}
+      </Window.Content>
+    </Window>
   );
 };
 
