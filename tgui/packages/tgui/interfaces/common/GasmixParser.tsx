9104fd65c8e4d60d1410f0099c38f65df5ca9760
Vincentius Vincentius <vincentius_vin@yahoo.com>
2022-03-25 08:50:24 +0700

    adds a p tag to make it block level

--- tgui/packages/tgui/interfaces/common/GasmixParser.tsx
+++ tgui/packages/tgui/interfaces/common/GasmixParser.tsx
@@ -110,7 +110,7 @@ export const GasmixParser = (props: GasmixParserProps, context) => {
                     onClick={() => reactionOnClick(reaction[0])}
                   />
                 </Box>
-              ) : (reaction[1])
+              ) : (<p>reaction[1]</p>)
             )
             : 'No reactions detected'}
         </LabeledList.Item>
