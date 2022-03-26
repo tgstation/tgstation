tgstation/tgstation

commit afc1e44ee2922a316feb958249f7806568953bbe

Tool act superpack 2 (#64428)
About The Pull Request

Continuation of #64375, extracting tool behavior from attackby() and moving it into discrete _act procs. This is about as many files as I had in the last version, as I still want this to be reviewable.

As before, I've tested everything in game and it works as it previously did.
Why It's Good For The Game

The more code moved out of attackby, the more modular things become.
Changelog

cl
refactor: Moves more tool behavior out of attackby().
/cl

code/game/machinery/doors/door.dm
@@ -245,38 +245,43 @@
 /obj/machinery/door/proc/try_to_crowbar_secondary(obj/item/acting_object, mob/user)
 	return
 
+/obj/machinery/door/welder_act(mob/living/user, obj/item/tool)
+	try_to_weld(tool, user)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/machinery/door/crowbar_act(mob/living/user, obj/item/tool)
+	if(user.combat_mode)
+		return
+
+	var/forced_open = FALSE
+	if(istype(tool, /obj/item/crowbar))
+		var/obj/item/crowbar/crowbar = tool
+		forced_open = crowbar.force_opens
+	try_to_crowbar(tool, user, forced_open)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
 /obj/machinery/door/attackby(obj/item/I, mob/living/user, params)
-	if(!user.combat_mode && (I.tool_behaviour == TOOL_CROWBAR || istype(I, /obj/item/fireaxe)))
-		var/forced_open = FALSE
-		if(istype(I, /obj/item/crowbar))
-			var/obj/item/crowbar/C = I
-			forced_open = C.force_opens
-		try_to_crowbar(I, user, forced_open)
+	if(!user.combat_mode && istype(I, /obj/item/fireaxe))
+		try_to_crowbar(I, user, FALSE)
 		return TRUE
-	else if(I.tool_behaviour == TOOL_WELDER)
-		try_to_weld(I, user, params)
-		return TRUE
-	else if((!(I.item_flags & NOBLUDGEON) && !user.combat_mode) && try_to_activate_door(user))
+	else if(I.item_flags & NOBLUDGEON || user.combat_mode)
+		return FALSE
+
+	else if(try_to_activate_door(user))
 		return TRUE
 	return ..()
 
-/obj/machinery/door/attackby_secondary(obj/item/weapon, mob/user, params)
-	if (weapon.tool_behaviour == TOOL_WELDER)
-		try_to_weld_secondary(weapon, user)
+/obj/machinery/door/welder_act_secondary(mob/living/user, obj/item/tool)
+	try_to_weld_secondary(tool, user)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
-		return SECONDARY_ATTACK_CANCEL_ATTACK_CHAIN
-
-	if (weapon.tool_behaviour == TOOL_CROWBAR)
-		var/forced_open = FALSE
-		if(istype(weapon, /obj/item/crowbar))
-			var/obj/item/crowbar/crowbar = weapon
-			forced_open = crowbar.force_opens
-		try_to_crowbar_secondary(weapon, user, forced_open)
-
-		return SECONDARY_ATTACK_CANCEL_ATTACK_CHAIN
-
-	..()
-	return SECONDARY_ATTACK_CONTINUE_CHAIN
+/obj/machinery/door/crowbar_act_secondary(mob/living/user, obj/item/tool)
+	var/forced_open = FALSE
+	if(istype(tool, /obj/item/crowbar))
+		var/obj/item/crowbar/crowbar = tool
+		forced_open = crowbar.force_opens
+	try_to_crowbar_secondary(tool, user, forced_open)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
 /obj/machinery/door/take_damage(damage_amount, damage_type = BRUTE, damage_flag = 0, sound_effect = 1, attack_dir)
 	. = ..()

code/game/machinery/doors/firedoor.dm
@@ -331,32 +331,33 @@
 			span_warning("You bash [src]!"))
 		playsound(src, bash_sound, 100, TRUE)
 
-/obj/machinery/door/firedoor/attackby(obj/item/C, mob/user, params)
+/obj/machinery/door/firedoor/wrench_act(mob/living/user, obj/item/tool)
 	add_fingerprint(user)
-	if(operating)
-		return
-	if(welded)
-		if(C.tool_behaviour == TOOL_WRENCH)
-			if(boltslocked)
-				to_chat(user, span_notice("There are screws locking the bolts in place!"))
-				return
-			C.play_tool_sound(src)
-			user.visible_message(span_notice("[user] starts undoing [src]'s bolts..."), \
-				span_notice("You start unfastening [src]'s floor bolts..."))
-			if(!C.use_tool(src, user, DEFAULT_STEP_TIME))
-				return
-			playsound(get_turf(src), 'sound/items/deconstruct.ogg', 50, TRUE)
-			user.visible_message(span_notice("[user] unfastens [src]'s bolts."), \
-				span_notice("You undo [src]'s floor bolts."))
-			deconstruct(TRUE)
-			return
-		if(C.tool_behaviour == TOOL_SCREWDRIVER)
-			user.visible_message(span_notice("[user] [boltslocked ? "unlocks" : "locks"] [src]'s bolts."), \
+	if(operating || !welded)
+		return FALSE
+
+	if(boltslocked)
+		to_chat(user, span_notice("There are screws locking the bolts in place!"))
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	tool.play_tool_sound(src)
+	user.visible_message(span_notice("[user] starts undoing [src]'s bolts..."), \
+		span_notice("You start unfastening [src]'s floor bolts..."))
+	if(!tool.use_tool(src, user, DEFAULT_STEP_TIME))
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	playsound(get_turf(src), 'sound/items/deconstruct.ogg', 50, TRUE)
+	user.visible_message(span_notice("[user] unfastens [src]'s bolts."), \
+		span_notice("You undo [src]'s floor bolts."))
+	deconstruct(TRUE)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/machinery/door/firedoor/screwdriver_act(mob/living/user, obj/item/tool)
+	if(operating || !welded)
+		return FALSE
+	user.visible_message(span_notice("[user] [boltslocked ? "unlocks" : "locks"] [src]'s bolts."), \
 				span_notice("You [boltslocked ? "unlock" : "lock"] [src]'s floor bolts."))
-			C.play_tool_sound(src)
-			boltslocked = !boltslocked
-			return
-	return ..()
+	tool.play_tool_sound(src)
+	boltslocked = !boltslocked
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
 /obj/machinery/door/firedoor/try_to_activate_door(mob/user, access_bypass = FALSE)
 	return

code/game/objects/items/tools/weldingtool.dm
@@ -98,11 +98,12 @@
 	user.visible_message(span_suicide("[user] welds [user.p_their()] every orifice closed! It looks like [user.p_theyre()] trying to commit suicide!"))
 	return (FIRELOSS)
 
+/obj/item/weldingtool/screwdriver_act(mob/living/user, obj/item/tool)
+	flamethrower_screwdriver(tool, user)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
 /obj/item/weldingtool/attackby(obj/item/tool, mob/user, params)
-	if(tool.tool_behaviour == TOOL_SCREWDRIVER)
-		flamethrower_screwdriver(tool, user)
-	else if(istype(tool, /obj/item/stack/rods))
+	if(istype(tool, /obj/item/stack/rods))
 		flamethrower_rods(tool, user)
 	else
 		. = ..()

code/game/objects/items/wall_mounted.dm
@@ -58,24 +58,26 @@
 /obj/item/wallframe/proc/after_attach(obj/O)
 	transfer_fingerprints_to(O)
 
-/obj/item/wallframe/attackby(obj/item/W, mob/user, params)
-	..()
-	if(W.tool_behaviour == TOOL_SCREWDRIVER)
-		// For camera-building borgs
-		var/turf/T = get_step(get_turf(user), user.dir)
-		if(iswallturf(T))
-			T.attackby(src, user, params)
+/obj/item/wallframe/screwdriver_act(mob/living/user, obj/item/tool)
+	// For camera-building borgs
+	var/turf/T = get_step(get_turf(user), user.dir)
+	if(iswallturf(T))
+		T.attackby(user, tool)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
+/obj/item/wallframe/wrench_act(mob/living/user, obj/item/tool)
 	var/metal_amt = round(custom_materials[GET_MATERIAL_REF(/datum/material/iron)]/MINERAL_MATERIAL_AMOUNT) //Replace this shit later
 	var/glass_amt = round(custom_materials[GET_MATERIAL_REF(/datum/material/glass)]/MINERAL_MATERIAL_AMOUNT) //Replace this shit later
 
-	if(W.tool_behaviour == TOOL_WRENCH && (metal_amt || glass_amt))
-		to_chat(user, span_notice("You dismantle [src]."))
-		if(metal_amt)
-			new /obj/item/stack/sheet/iron(get_turf(src), metal_amt)
-		if(glass_amt)
-			new /obj/item/stack/sheet/glass(get_turf(src), glass_amt)
-		qdel(src)
+	if(!metal_amt && !glass_amt)
+		return FALSE
+	to_chat(user, span_notice("You dismantle [src]."))
+	if(metal_amt)
+		new /obj/item/stack/sheet/iron(get_turf(src), metal_amt)
+	if(glass_amt)
+		new /obj/item/stack/sheet/glass(get_turf(src), glass_amt)
+	qdel(src)
+	return TOOL_ACT_TOOLTYPE_SUCCESS
 
 /obj/item/electronics
 	desc = "Looks like a circuit. Probably is."

code/game/objects/structures/barsigns.dm
@@ -74,24 +74,26 @@
 		return
 	pick_sign(user)
 
-/obj/structure/sign/barsign/attackby(obj/item/I, mob/user)
-	if(I.tool_behaviour == TOOL_SCREWDRIVER)
-		if(!panel_open)
-			to_chat(user, span_notice("You open the maintenance panel."))
-			set_sign(new /datum/barsign/hiddensigns/signoff)
-			panel_open = TRUE
-		else
-			to_chat(user, span_notice("You close the maintenance panel."))
-			if(!broken)
-				if(!chosen_sign)
-					set_sign(new /datum/barsign/hiddensigns/signoff)
-				else
-					set_sign(chosen_sign)
-			else
-				set_sign(new /datum/barsign/hiddensigns/empbarsign)
-			panel_open = FALSE
+/obj/structure/sign/barsign/screwdriver_act(mob/living/user, obj/item/tool)
+	tool.play_tool_sound(src)
+	if(!panel_open)
+		to_chat(user, span_notice("You open the maintenance panel."))
+		set_sign(new /datum/barsign/hiddensigns/signoff)
+		panel_open = TRUE
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	to_chat(user, span_notice("You close the maintenance panel."))
 
-	else if(istype(I, /obj/item/stack/cable_coil) && panel_open)
+	if(broken)
+		set_sign(new /datum/barsign/hiddensigns/empbarsign)
+	else if(!chosen_sign)
+		set_sign(new /datum/barsign/hiddensigns/signoff)
+	else
+		set_sign(chosen_sign)
+	panel_open = FALSE
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/structure/sign/barsign/attackby(obj/item/I, mob/user)
+	if(istype(I, /obj/item/stack/cable_coil) && panel_open)
 		var/obj/item/stack/cable_coil/C = I
 		if(!broken)
 			to_chat(user, span_warning("This sign is functioning properly!"))
@@ -102,8 +104,11 @@
 			broken = FALSE
 		else
 			to_chat(user, span_warning("You need at least two lengths of cable!"))
-	else
-		return ..()
+		return TRUE
+
+	if (broken)
+		return TRUE
+	return ..()
 
 
 /obj/structure/sign/barsign/emp_act(severity)

code/game/objects/structures/bedsheet_bin.dm
@@ -539,6 +539,21 @@ LINEN BINS
 		update_appearance()
 	..()
 
+/obj/structure/bedsheetbin/screwdriver_act(mob/living/user, obj/item/tool)
+	if(flags_1 & NODECONSTRUCT_1)
+		return FALSE
+	if(amount)
+		to_chat(user, span_warning("The [src] must be empty first!"))
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	if(tool.use_tool(src, user, 0.5 SECONDS, volume=50))
+		to_chat(user, span_notice("You disassemble the [src]."))
+		new /obj/item/stack/rods(loc, 2)
+		qdel(src)
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/structure/bedsheetbin/wrench_act(mob/living/user, obj/item/tool)
+	return default_unfasten_wrench(user, tool, 5)
+
 /obj/structure/bedsheetbin/attackby(obj/item/I, mob/user, params)
 	if(istype(I, /obj/item/bedsheet))
 		if(!user.transferItemToLoc(I, src))
@@ -547,20 +562,6 @@ LINEN BINS
 		amount++
 		to_chat(user, span_notice("You put [I] in [src]."))
 		update_appearance()
-
-	else if(default_unfasten_wrench(user, I, 5))
-		return
-
-	else if(I.tool_behaviour == TOOL_SCREWDRIVER)
-		if(flags_1 & NODECONSTRUCT_1)
-			return
-		if(amount)
-			to_chat(user, span_warning("The [src] must be empty first!"))
-			return
-		if(I.use_tool(src, user, 5, volume=50))
-			to_chat(user, span_notice("You disassemble the [src]."))
-			new /obj/item/stack/rods(loc, 2)
-			qdel(src)
 
 	else if(amount && !hidden && I.w_class < WEIGHT_CLASS_BULKY) //make sure there's sheets to hide it among, make sure nothing else is hidden in there.
 		if(!user.transferItemToLoc(I, src))

code/game/objects/structures/false_walls.dm
@@ -83,30 +83,39 @@
 		qdel(src)
 	return T
 
+/obj/structure/falsewall/tool_act(mob/living/user, obj/item/tool)
+	if(!opening)
+		return ..()
+	to_chat(user, span_warning("You must wait until the door has stopped moving!"))
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/structure/falsewall/screwdriver_act(mob/living/user, obj/item/tool)
+	if(!density)
+		to_chat(user, span_warning("You can't reach, close it first!"))
+		return
+	var/turf/loc_turf = get_turf(src)
+	if(loc_turf.density)
+		to_chat(user, span_warning("[src] is blocked!"))
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	if(!isfloorturf(loc_turf))
+		to_chat(user, span_warning("[src] bolts must be tightened on the floor!"))
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	user.visible_message(span_notice("[user] tightens some bolts on the wall."), span_notice("You tighten the bolts on the wall."))
+	ChangeToWall()
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+
+/obj/structure/falsewall/welder_act(mob/living/user, obj/item/tool)
+	if(tool.use_tool(src, user, 0 SECONDS, volume=50))
+		dismantle(user, TRUE)
+		return TOOL_ACT_TOOLTYPE_SUCCESS
+	return
+
 /obj/structure/falsewall/attackby(obj/item/W, mob/user, params)
-	if(opening)
+	if(!opening)
 		to_chat(user, span_warning("You must wait until the door has stopped moving!"))
 		return
-
-	if(W.tool_behaviour == TOOL_SCREWDRIVER)
-		if(density)
-			var/turf/T = get_turf(src)
-			if(T.density)
-				to_chat(user, span_warning("[src] is blocked!"))
-				return
-			if(!isfloorturf(T))
-				to_chat(user, span_warning("[src] bolts must be tightened on the floor!"))
-				return
-			user.visible_message(span_notice("[user] tightens some bolts on the wall."), span_notice("You tighten the bolts on the wall."))
-			ChangeToWall()
-		else
-			to_chat(user, span_warning("You can't reach, close it first!"))
-
-	else if(W.tool_behaviour == TOOL_WELDER)
-		if(W.use_tool(src, user, 0, volume=50))
-			dismantle(user, TRUE)
-	else
-		return ..()
+	return ..()
 
 /obj/structure/falsewall/proc/dismantle(mob/user, disassembled=TRUE, obj/item/tool = null)
 	user.visible_message(span_notice("[user] dismantles the false wall."), span_notice("You dismantle the false wall."))

code/game/objects/structures/grille.dm
@@ -171,28 +171,38 @@
 	if(istype(caller))
 		. = . || (caller.pass_flags & PASSGRILLE)
 
+/obj/structure/grille/wirecutter_act(mob/living/user, obj/item/tool)
+	add_fingerprint(user)
+	if(shock(user, 100))
+		return
+	tool.play_tool_sound(src, 100)
+	deconstruct()
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
+/obj/structure/grille/screwdriver_act(mob/living/user, obj/item/tool)
+	if(!isturf(loc) || !anchored)
+		return FALSE
+	add_fingerprint(user)
+	if(shock(user, 90))
+		return FALSE
+	tool.play_tool_sound(src, 100)
+	set_anchored(!anchored)
+	user.visible_message(span_notice("[user] [anchored ? "fastens" : "unfastens"] [src]."), \
+		span_notice("You [anchored ? "fasten [src] to" : "unfasten [src] from"] the floor."))
+	return TOOL_ACT_TOOLTYPE_SUCCESS
+
 /obj/structure/grille/attackby(obj/item/W, mob/user, params)
 	user.changeNext_move(CLICK_CD_MELEE)
 	add_fingerprint(user)
-	if(W.tool_behaviour == TOOL_WIRECUTTER)
-		if(!shock(user, 100))
-			W.play_tool_sound(src, 100)
-			deconstruct()
-	else if((W.tool_behaviour == TOOL_SCREWDRIVER) && (isturf(loc) || anchored))
-		if(!shock(user, 90))
-			W.play_tool_sound(src, 100)
-			set_anchored(!anchored)
-			user.visible_message(span_notice("[user] [anchored ? "fastens" : "unfastens"] [src]."), \
-				span_notice("You [anchored ? "fasten [src] to" : "unfasten [src] from"] the floor."))
+	if(istype(W, /obj/item/stack/rods) && broken)
+		if(shock(user, 90))
 			return
-	else if(istype(W, /obj/item/stack/rods) && broken)
 		var/obj/item/stack/rods/R = W
-		if(!shock(user, 90))
-			user.visible_message(span_notice("[user] rebuilds the broken grille."), \
-				span_notice("You rebuild the broken grille."))
-			repair_grille()
-			R.use(1)
-			return
+		user.visible_message(span_notice("[user] rebuilds the broken grille."), \
+			span_notice("You rebuild the broken grille."))
+		repair_grille()
+		R.use(1)
+		return TRUE
 
 //window placing begin
 	else if(is_glass_sheet(W) || istype(W, /obj/item/stack/sheet/bronze))

commit 414425fbc36f6206afe068f7239ccaf84b8b3727

Fixes airlocks and windoors not being attackable with melee weapons (#65496)
Fixes #65499 (Axes can't bust down doors) Likely caused by (afc1e44ee2922a316feb958249f7806568953bbe)

Fixes an issue preventing melee weapons from being able to attack airlocks and windoors when in combat mode because of an early return cancelling the attack chain when the player is in combat mode.

airlock destruction good fireaxe good

code/game/machinery/doors/door.dm
@@ -264,8 +264,7 @@
 		try_to_crowbar(I, user, FALSE)
 		return TRUE
 	else if(I.item_flags & NOBLUDGEON || user.combat_mode)
-		return FALSE
-
+		return ..()
 	else if(try_to_activate_door(user))
 		return TRUE
 	return ..()