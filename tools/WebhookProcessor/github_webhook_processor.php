1389715c5cfc54b7081054d73a1b4def738d6def
tattle <article.disaster@gmail.com>
2022-03-27 16:34:48 -0700

    add GBP: No Update label when title includes [no gbp]

--- tools/WebhookProcessor/github_webhook_processor.php
+++ tools/WebhookProcessor/github_webhook_processor.php
@@ -244,7 +244,6 @@ function tag_pr($payload, $opened) {
 
 		if(strpos(strtolower($title), 'refactor') !== FALSE)
 			$tags[] = 'Refactor';
-
 		if(strpos(strtolower($title), 'revert') !== FALSE)
 			$tags[] = 'Revert';
 		if(strpos(strtolower($title), 'removes') !== FALSE)
@@ -271,6 +270,7 @@ function tag_pr($payload, $opened) {
 			$tags[] = $tag;
 
 	check_tag_and_replace($payload, '[dnm]', 'Do Not Merge', $tags);
+	check_tag_and_replace($payload, '[no gbp]', 'GBP: No Update', $tags);
 
 	return array($tags, $remove);
 }
@@ -336,7 +336,7 @@ function handle_pr($payload) {
 	global $discord_announce_blacklist;
 	global $no_changelog;
 	global $game_announce_whitelist;
-	
+
 	$action = 'opened';
 	$validated = validate_user($payload);
 	switch ($payload["action"]) {
