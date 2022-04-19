43d2715476176e63e6d9088d4b2a8a05227979aa
Aleksej Komarov <stylemistake@gmail.com>
2022-04-19 16:48:04 +0300

    Update NtosPhysScanner.js

--- tgui/packages/tgui/interfaces/NtosPhysScanner.js
+++ tgui/packages/tgui/interfaces/NtosPhysScanner.js
@@ -1,14 +1,7 @@
-/* eslint-disable max-len */
 import { useBackend } from '../backend';
 import { NtosWindow } from '../layouts';
 import { Section, Box, Dropdown } from '../components';
 import { sanitizeText } from '../sanitize';
-
-let decodeHtml = function (html) {
-  let txt = document.createElement("textarea");
-  txt.innerHTML = html;
-  return txt.value;
-};
 
 export const NtosPhysScanner = (props, context) => {
   const { act, data } = useBackend(context);
@@ -26,7 +19,8 @@ export const NtosPhysScanner = (props, context) => {
       height={350}>
       <NtosWindow.Content scrollable>
         <Section>
-          Tap something (right-click) with your tablet to use the physical scanner.
+          Tap something (right-click) with your tablet
+          to use the physical scanner.
         </Section>
         <Section>
           <Box bold>SELECTED MODE <br /> <br /></Box>
@@ -44,7 +38,9 @@ export const NtosPhysScanner = (props, context) => {
             <br />
             <br />
           </Box>
-          <Box style={{ "white-space": "pre-line" }} dangerouslySetInnerHTML={textHtml} />
+          <Box
+            style={{ "white-space": "pre-line" }}
+            dangerouslySetInnerHTML={textHtml} />
         </Section>
       </NtosWindow.Content>
     </NtosWindow>
