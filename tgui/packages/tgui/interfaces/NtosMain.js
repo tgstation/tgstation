0cce5cf1c92c2295fe4d6b07e6e6092a6e8572f5
JohnFulpWillard <53777086+JohnFulpWillard@users.noreply.github.com>
2022-04-26 17:59:00 -0400

    real tgui suggestion

--- tgui/packages/tgui/interfaces/NtosMain.js
+++ tgui/packages/tgui/interfaces/NtosMain.js
@@ -1,5 +1,5 @@
 import { useBackend } from '../backend';
-import { Button, ColorBox, Section, Table } from '../components';
+import { Button, ColorBox, Stack, Section, Table } from '../components';
 import { NtosWindow } from '../layouts';
 
 export const NtosMain = (props, context) => {
@@ -31,37 +31,35 @@ export const NtosMain = (props, context) => {
       <NtosWindow.Content scrollable>
         {(!!has_light || !!removable_media.length) && (
           <Section>
-            <Table>
-              <Table.Row>
-                {!!has_light && (
-                  <>
-                    <Button
-                      width="144px"
-                      icon="lightbulb"
-                      selected={light_on}
-                      onClick={() => act('PC_toggle_light')}>
-                      Flashlight: {light_on ? 'ON' : 'OFF'}
-                    </Button>
-                    <Button
-                      ml={1}
-                      onClick={() => act('PC_light_color')}>
-                      Color:
-                      <ColorBox ml={1} color={comp_light_color} />
-                    </Button>
-                  </>
-                )}
-                {removable_media.map(device => (
-                  <Table.Cell key={device}>
-                    <Button
-                      fluid
-                      icon="eject"
-                      content={device}
-                      onClick={() => act('PC_Eject_Disk', { name: device })}
-                      disabled={!device} />
-                  </Table.Cell>
-                ))}
-              </Table.Row>
-            </Table>
+            <Stack>
+              {!!has_light && (
+                <>
+                  <Button
+                    width="144px"
+                    icon="lightbulb"
+                    selected={light_on}
+                    onClick={() => act('PC_toggle_light')}>
+                    Flashlight: {light_on ? 'ON' : 'OFF'}
+                  </Button>
+                  <Button
+                    ml={1}
+                    onClick={() => act('PC_light_color')}>
+                    Color:
+                    <ColorBox ml={1} color={comp_light_color} />
+                  </Button>
+                </>
+              )}
+              {removable_media.map(device => (
+                <Stack.Item key={device}>
+                  <Button
+                    fluid
+                    icon="eject"
+                    content={device}
+                    onClick={() => act('PC_Eject_Disk', { name: device })}
+                    disabled={!device} />
+                </Stack.Item>
+              ))}
+            </Stack>
           </Section>
         )}
         {!!(cardholder && show_imprint) && (
