0f75611f6fa914cc2419628f1b40d12348bc717f
magatsuchi <gavin.blaker@outlook.com>
2022-05-14 19:57:11 -0500

    more stuff

--- html/statbrowser.js
+++ html/statbrowser.js
@@ -15,51 +15,6 @@ if (!String.prototype.trim) {
 	};
 }
 
-/*\
-|*|
-|*|  Polyfill which enables the passage of arbitrary arguments to the
-|*|  callback functions of JavaScript timers (HTML5 standard syntax).
-|*|
-|*|  https://developer.mozilla.org/en-US/docs/DOM/window.setInterval
-|*|
-|*|  Syntax:
-|*|  var timeoutID = window.setTimeout(func, delay[, arg1, arg2, ...]);
-|*|  var timeoutID = window.setTimeout(code, delay);
-|*|  var intervalID = window.setInterval(func, delay[, arg1, arg2, ...]);
-|*|  var intervalID = window.setInterval(code, delay);
-|*|
-\*/
-(function () {
-	setTimeout(function (arg1) {
-		if (arg1 === 'test') {
-			// feature test is passed, no need for polyfill
-			return;
-		}
-		var __nativeST__ = window.setTimeout;
-		window.setTimeout = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
-			var aArgs = Array.prototype.slice.call(arguments, 2);
-			return __nativeST__(vCallback instanceof Function ? function () {
-				vCallback.apply(null, aArgs);
-			} : vCallback, nDelay);
-		};
-	}, 0, 'test');
-
-	var interval = setInterval(function (arg1) {
-		clearInterval(interval);
-		if (arg1 === 'test') {
-			// feature test is passed, no need for polyfill
-			return;
-		}
-		var __nativeSI__ = window.setInterval;
-		window.setInterval = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
-			var aArgs = Array.prototype.slice.call(arguments, 2);
-			return __nativeSI__(vCallback instanceof Function ? function () {
-				vCallback.apply(null, aArgs);
-			} : vCallback, nDelay);
-		};
-	}, 0, 'test');
-}())
-
 // Status panel implementation ------------------------------------------------
 var status_tab_parts = ["Loading..."];
 var current_tab = null;
@@ -344,7 +299,9 @@ function tab_change(tab) {
 	} else {
 		statcontentdiv[textContentKey] = "Loading...";
 	}
-	window.location.href = "byond://winset?statbrowser.is-visible=true";
+	Byond.winset(Byond.windowId, {
+		'is-visible': true,
+	});
 }
 
 function set_byond_tab(tab) {
@@ -809,7 +766,9 @@ function set_style_sheet(sheet) {
 
 function restoreFocus() {
 	run_after_focus(function () {
-		window.location.href = "byond://winset?map.focus=true";
+		Byond.winset('map', {
+			focus: true,
+		});
 	});
 }
 
