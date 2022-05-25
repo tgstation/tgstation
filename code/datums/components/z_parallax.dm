tgstation/tgstation

commit a9d8be4d16c90bc4c219318ffba8140e815c6f12

adds new z-level trait to disable parallax (#66637)
* first push woohoo

* more stuff

* Update code/datums/components/z_parallax.dm

Co-authored-by: Mothblocks <35135081+Mothblocks@users.noreply.github.com>

* mothblockification

* fuck

* fuck 2

* uh

* uh yeah style stuff ig

* more changes

* last changes

* fuck

* fuck 2

* i hate potatopotato

* i hate potato * 2

* a

* god

* woops

* Update code/modules/mapping/space_management/traits.dm

Co-authored-by: Kylerace <kylerlumpkin1@gmail.com>

* Update code/modules/mapping/space_management/traits.dm

Co-authored-by: Kylerace <kylerlumpkin1@gmail.com>

* Update code/modules/mapping/space_management/traits.dm

Co-authored-by: Kylerace <kylerlumpkin1@gmail.com>

Co-authored-by: Mothblocks <35135081+Mothblocks@users.noreply.github.com>
Co-authored-by: Kylerace <kylerlumpkin1@gmail.com>

code/datums/components/z_parallax.dm
@@ -1,0 +1,60 @@
+/**
+ * Component that hooks into the client, listens for COMSIG_MOVABLE_Z_CHANGED, and depending on whether or not the
+ * Z-level has ZTRAIT_NOPARALLAX enabled, disable or reenable parallax.
+ */
+
+/datum/component/zparallax
+	dupe_mode = COMPONENT_DUPE_UNIQUE
+
+	var/client/tracked
+	var/mob/client_mob
+
+/datum/component/zparallax/Initialize(client/tracked)
+	. = ..()
+	if(!istype(tracked))
+		stack_trace("Component zparallax has been initialized outside of a client. Deleting.")
+		return COMPONENT_INCOMPATIBLE
+
+	src.tracked = tracked
+	client_mob = tracked.mob
+
+	RegisterSignal(client_mob, COMSIG_MOB_LOGOUT, .proc/mob_change)
+	RegisterSignal(client_mob, COMSIG_MOVABLE_Z_CHANGED, .proc/ztrait_checks)
+	RegisterSignal(client_mob, COMSIG_MOB_LOGIN, .proc/refresh_client)
+
+/datum/component/zparallax/Destroy()
+	. = ..()
+	unregister_signals()
+
+	tracked = null
+	client_mob = null
+
+/datum/component/zparallax/proc/unregister_signals()
+	if(!client_mob)
+		return
+
+	UnregisterSignal(client_mob, list(COMSIG_MOB_LOGOUT, COMSIG_MOVABLE_Z_CHANGED))
+
+/datum/component/zparallax/proc/refresh_client()
+	tracked = client_mob.client
+
+/datum/component/zparallax/proc/mob_change()
+	SIGNAL_HANDLER
+
+	if(client_mob.key)
+		return
+
+	unregister_signals()
+
+	client_mob = tracked.mob
+
+	RegisterSignal(client_mob, COMSIG_MOB_LOGOUT, .proc/mob_change)
+	RegisterSignal(client_mob, COMSIG_MOVABLE_Z_CHANGED, .proc/ztrait_checks)
+	RegisterSignal(client_mob, COMSIG_MOB_LOGIN, .proc/refresh_client)
+
+/datum/component/zparallax/proc/ztrait_checks()
+	SIGNAL_HANDLER
+
+	var/datum/hud/hud = client_mob.hud_used
+
+	hud.update_parallax_pref(client_mob)

code/__DEFINES/maps.dm
@@ -120,6 +120,9 @@ Always compile, always use that verb, and always make sure that it works for wha
 // string - type path of the z-level's baseturf (defaults to space)
 #define ZTRAIT_BASETURF "Baseturf"
 
+///boolean - does this z disable parallax?
+#define ZTRAIT_NOPARALLAX "No Parallax"
+
 // default trait definitions, used by SSmapping
 ///Z level traits for CentCom
 #define ZTRAITS_CENTCOM list(ZTRAIT_CENTCOM = TRUE, ZTRAIT_NOPHASE = TRUE)
@@ -130,6 +133,7 @@ Always compile, always use that verb, and always make sure that it works for wha
 ///Z level traits for Lavaland
 #define ZTRAITS_LAVALAND list(\
 	ZTRAIT_MINING = TRUE, \
+	ZTRAIT_NOPARALLAX = TRUE, \
 	ZTRAIT_ASHSTORM = TRUE, \
 	ZTRAIT_LAVA_RUINS = TRUE, \
 	ZTRAIT_BOMBCAP_MULTIPLIER = 2, \

code/_onclick/hud/hud.dm
@@ -117,6 +117,8 @@ GLOBAL_LIST_INIT(available_ui_styles, list(
 
 	owner.overlay_fullscreen("see_through_darkness", /atom/movable/screen/fullscreen/see_through_darkness)
 
+	AddComponent(/datum/component/zparallax, owner.client)
+
 /datum/hud/Destroy()
 	if(mymob.hud_used == src)
 		mymob.hud_used = null

code/_onclick/hud/parallax.dm
@@ -47,6 +47,8 @@
 /datum/hud/proc/apply_parallax_pref(mob/viewmob)
 	var/mob/screenmob = viewmob || mymob
 
+	if(SSmapping.level_trait(screenmob.z, ZTRAIT_NOPARALLAX))
+		return FALSE
 	if (SSlag_switch.measures[DISABLE_PARALLAX] && !HAS_TRAIT(viewmob, TRAIT_BYPASS_MEASURES))
 		return FALSE
 

code/controllers/subsystem/mapping.dm
@@ -67,6 +67,7 @@ SUBSYSTEM_DEF(mapping)
 			config = old_config
 	initialize_biomes()
 	loadWorld()
+	determine_fake_sale()
 	repopulate_sorted_areas()
 	process_teleport_locs() //Sets up the wizard teleport locations
 	preloadTemplates()
@@ -188,6 +189,13 @@ Used by the AI doomsday and the self-destruct nuke.
 	for(var/N in nuke_tiles)
 		var/turf/open/floor/circuit/C = N
 		C.update_appearance()
+
+/datum/controller/subsystem/mapping/proc/determine_fake_sale()
+	if(length(SSmapping.levels_by_all_traits(list(ZTRAIT_STATION, ZTRAIT_NOPARALLAX))))
+		GLOB.arcade_prize_pool += /obj/item/stack/tile/fakeice/loaded
+	else
+		GLOB.arcade_prize_pool += /obj/item/stack/tile/fakespace/loaded
+
 
 /datum/controller/subsystem/mapping/Recover()
 	flags |= SS_NO_INIT

code/game/machinery/computer/arcade/arcade.dm
@@ -35,7 +35,6 @@ GLOBAL_LIST_INIT(arcade_prize_pool, list(
 		/obj/item/toy/talking/owl = 2,
 		/obj/item/toy/talking/griffin = 2,
 		/obj/item/coin/antagtoken = 2,
-		/obj/item/stack/tile/fakespace/loaded = 2,
 		/obj/item/stack/tile/fakepit/loaded = 2,
 		/obj/item/stack/tile/eighties/loaded = 2,
 		/obj/item/toy/toy_xeno = 2,

code/game/objects/items/stacks/tiles/tile_types.dm
@@ -1,6 +1,6 @@
 /**
  * TILE STACKS
- * 
+ *
  * Allows us to place a turf on a plating.
  */
 /obj/item/stack/tile
@@ -65,7 +65,7 @@
 
 /**
  * Place our tile on a plating, or replace it.
- * 
+ *
  * Arguments:
  * * target_plating - Instance of the plating we want to place on. Replaced during sucessful executions.
  * * user - The mob doing the placing.
@@ -968,6 +968,19 @@
 	merge_type = /obj/item/stack/tile/fakepit
 
 /obj/item/stack/tile/fakepit/loaded
+	amount = 30
+
+/obj/item/stack/tile/fakeice
+	name = "fake ice"
+	singular_name = "fake ice tile"
+	desc = "A piece of tile with a convincing ice pattern."
+	icon_state = "tile_ice"
+	inhand_icon_state = "tile-diamond"
+	turf_type = /turf/open/floor/fakeice
+	resistance_flags = FLAMMABLE
+	merge_type = /obj/item/stack/tile/fakeice
+
+/obj/item/stack/tile/fakeice/loaded
 	amount = 30
 
 //High-traction

code/game/turfs/open/floor/fancy_floor.dm
@@ -772,6 +772,12 @@
 	underlay_appearance.icon_state = "basalt"
 	return TRUE
 
+/turf/open/floor/fakeice
+	desc = "Is it marble, polished to a mirror finish? Or just really, really grippy ice?"
+	icon = 'icons/turf/floors/ice_turf.dmi'
+	icon_state = "ice_turf-0"
+	base_icon_state = "ice_turf-0"
+
 /turf/open/floor/fakespace
 	icon = 'icons/turf/space.dmi'
 	icon_state = "0"

commit f98d965a5d812a934279232b56b00c9aceef8d1f

fixes small zparallax bug (#67244)
fixes an issue where if you attempted to regain control of your initial body after possessing another body it caused a stack trace due to MOB_LOGIN signal not being unregistered

(This was tsu's fault, introduced in a9d8be4d16c90bc4c219318ffba8140e815c6f12)

code/datums/components/z_parallax.dm
@@ -33,7 +33,7 @@
 	if(!client_mob)
 		return
 
-	UnregisterSignal(client_mob, list(COMSIG_MOB_LOGOUT, COMSIG_MOVABLE_Z_CHANGED))
+	UnregisterSignal(client_mob, list(COMSIG_MOB_LOGIN, COMSIG_MOB_LOGOUT, COMSIG_MOVABLE_Z_CHANGED))
 
 /datum/component/zparallax/proc/refresh_client()
 	tracked = client_mob.client