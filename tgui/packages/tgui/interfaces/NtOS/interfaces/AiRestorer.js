6fae7ab5247243855a7ac1e02fbc076a53e9923f
Jeremiah <jlsnow301@protonmail.com>
2022-05-25 01:21:06 -0700

    Reverts formatter psychic damage

--- tgui/packages/tgui/interfaces/NtOS/interfaces/AiRestorer.js
+++ tgui/packages/tgui/interfaces/NtOS/interfaces/AiRestorer.js
@@ -3,7 +3,9 @@ import { AiRestorerContent } from '../../AiRestorer';
 
 export const NtosAiRestorer = () => {
   return (
-    <NtosWindow width={370} height={400}>
+    <NtosWindow
+      width={370}
+      height={400}>
       <NtosWindow.Content scrollable>
         <AiRestorerContent />
       </NtosWindow.Content>
