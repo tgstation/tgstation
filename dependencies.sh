253d5108c23c63d9645beef2110503b6db0bcdf3
Y0SH1M4S73R <legoboyo@earthlink.net>
2022-05-30 21:37:29 -0400

    updates ci step for installing auxlua

--- dependencies.sh
+++ dependencies.sh
@@ -20,5 +20,8 @@ export SPACEMAN_DMM_VERSION=suite-1.7.1
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.7.9
 
+#auxlua repo (maintainers: change Y0SH1M4S73R to tgstation before merging)
+export AUXLUA_REPO=Y0SH1M4S73R/auxlua
+
 #auxlua git tag
-export AUXLUA_VERSION=0.1.0-beta.1
+export AUXLUA_VERSION=0.1.0-beta.2
--- tools/ci/install_auxlua.sh
+++ tools/ci/install_auxlua.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 source dependencies.sh
 
 mkdir -p ~/.byond/bin
-wget -nv -O ~/.byond/bin/libauxlua.so "https://github.com/tgstation/auxlua/releases/download/$AUXLUA_VERSION/libauxlua.so"
+wget -nv -O ~/.byond/bin/libauxlua.so "https://github.com/$AUXLUA_REPO/releases/download/$AUXLUA_VERSION/libauxlua.so"
 chmod +x ~/.byond/bin/libauxlua.so
 ldd ~/.byond/bin/libauxlua.so
