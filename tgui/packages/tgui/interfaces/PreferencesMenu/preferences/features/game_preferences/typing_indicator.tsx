a9f09730d4ced531d1f0b292341019a83dd43e7e
iamgoofball <iamgoofball@gmail.com>
2022-06-03 21:39:50 -0700

    aaa

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/typing_indicator.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from "../base";
+
+export const typingindicator: FeatureToggle = {
+  name: "Enable typing indicators for self",
+  category: "GAMEPLAY",
+  description: "Enable typing indicators, to show when you're typing a message.",
+  component: CheckboxInput,
+};
