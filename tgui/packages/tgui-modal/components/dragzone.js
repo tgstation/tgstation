9edb8d8709109c14871788775c688f447cb05c8e
Jeremiah <jlsnow301@protonmail.com>
2022-06-07 00:07:15 -0700

    Linter really

--- tgui/packages/tgui-modal/components/dragzone.js
+++ tgui/packages/tgui-modal/components/dragzone.js
@@ -1,8 +1,9 @@
 import { dragStartHandler } from 'tgui/drag';
 
 export const Dragzone = (props) => {
-  const direction =
-    (props.horizontal && 'horizontal') || (props.vertical && 'vertical');
+  const direction
+    = (props.horizontal && 'horizontal')
+    || (props.vertical && 'vertical');
   return (
     <div className={`dragzone-${direction}`} onmousedown={dragStartHandler} />
   );
