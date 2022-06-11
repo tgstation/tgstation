a72537a2f9ecb30736b80919afd5815322c97aed
Jeremiah <jlsnow301@protonmail.com>
2022-06-11 13:12:53 -0700

    scss rocks

--- tgui/packages/tgui-say/components/dragzone.tsx
+++ tgui/packages/tgui-say/components/dragzone.tsx
@@ -5,9 +5,11 @@ interface Props {
   vertical: boolean;
 }
 
+/** Creates a draggable edge. Props Req: horizontal or vertical. */
 export const Dragzone = (props: Partial<Props>) => {
-  const direction =
-    (props.horizontal && 'horizontal') || (props.vertical && 'vertical');
+  const direction
+    = (props.horizontal && 'horizontal') || (props.vertical && 'vertical');
+
   return (
     <div className={`dragzone-${direction}`} onmousedown={dragStartHandler} />
   );
