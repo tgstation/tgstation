d59a6067cb1ec2b2ff15a2c19d561f5f5942c73c
Jeremiah <jlsnow301@protonmail.com>
2022-06-17 21:21:37 -0700

    cleanup commit

--- tgui/.yarn/sdks/prettier/index.js
+++ tgui/.yarn/sdks/prettier/index.js
@@ -4,17 +4,17 @@ const {existsSync} = require(`fs`);
 const {createRequire, createRequireFromPath} = require(`module`);
 const {resolve} = require(`path`);
 
-const relPnpApiPath = "../../../../.pnp.cjs";
+const relPnpApiPath = "../../../.pnp.cjs";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = (createRequire || createRequireFromPath)(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
-    // Setup the environment to be able to require eslint
+    // Setup the environment to be able to require prettier/index.js
     require(absPnpApiPath).setup();
   }
 }
 
-// Defer to the real eslint your application uses
-module.exports = absRequire(`eslint`);
+// Defer to the real prettier/index.js your application uses
+module.exports = absRequire(`prettier/index.js`);
