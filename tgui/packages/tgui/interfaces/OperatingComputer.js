a88c0fadfaa2fbf98b5e5833e9f267cd183590a5
RandomGamer123 <31096837+RandomGamer123@users.noreply.github.com>
2022-06-19 16:49:26 -0400

    Fix display formatting issue with operating computer ui

--- tgui/packages/tgui/interfaces/OperatingComputer.js
+++ tgui/packages/tgui/interfaces/OperatingComputer.js
@@ -121,6 +121,8 @@ const PatientStateView = (props, context) => {
               {procedure.next_step}
               {procedure.chems_needed && (
                 <>
+                  <br />
+                  <br />
                   <b>Required Chemicals:</b>
                   <br />
                   {procedure.chems_needed}
@@ -132,6 +134,8 @@ const PatientStateView = (props, context) => {
                 {procedure.alternative_step}
                 {procedure.alt_chems_needed && (
                   <>
+                    <br />
+                    <br />
                     <b>Required Chemicals:</b>
                     <br />
                     {procedure.alt_chems_needed}
