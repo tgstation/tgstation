tgstation/tgstation

commit 0b7e5429273901c230bc5d8bf9864569232a1fa2

Fix deafness virus symptom to be permanent if resistance threshold is met (#68080)
* Fix deafness virus symptom to be permanent

* Refactor deafness disease trait

Co-authored-by: ATH1909 <42606352+ATH1909@users.noreply.github.com>

* Add disease trait on stage 5

Co-authored-by: ATH1909 <42606352+ATH1909@users.noreply.github.com>

code/datums/diseases/advance/symptoms/deafness.dm
@@ -22,6 +22,7 @@
 		"Resistance 9" = "Causes permanent deafness, instead of intermittent.",
 		"Stealth 4" = "The symptom remains hidden until active.",
 	)
+	var/causes_permanent_deafness = FALSE
 
 /datum/symptom/deafness/Start(datum/disease/advance/A)
 	. = ..()
@@ -30,7 +31,11 @@
 	if(A.totalStealth() >= 4)
 		suppress_warning = TRUE
 	if(A.totalResistance() >= 9) //permanent deafness
-		power = 2
+		causes_permanent_deafness = TRUE
+
+/datum/symptom/deafness/End(datum/disease/advance/advanced_disease)
+	REMOVE_TRAIT(advanced_disease.affected_mob, TRAIT_DEAF, DISEASE_TRAIT)
+	return ..()
 
 /datum/symptom/deafness/Activate(datum/disease/advance/A)
 	. = ..()
@@ -45,12 +50,22 @@
 			if(prob(base_message_chance) && !suppress_warning)
 				to_chat(M, span_warning("[pick("You hear a ringing in your ear.", "Your ears pop.")]"))
 		if(5)
-			if(power >= 2)
+			if(causes_permanent_deafness)
 				if(ears.damage < ears.maxHealth)
 					to_chat(M, span_userdanger("Your ears pop painfully and start bleeding!"))
 					// Just absolutely murder me man
 					ears.applyOrganDamage(ears.maxHealth)
 					M.emote("scream")
+					ADD_TRAIT(M.affected_mob, TRAIT_DEAF, DISEASE_TRAIT)
 			else
 				to_chat(M, span_userdanger("Your ears pop and begin ringing loudly!"))
 				ears.deaf = min(20, ears.deaf + 15)
+
+/datum/symptom/deafness/on_stage_change(datum/disease/advance/advanced_disease)
+	. = ..()
+	if(!.)
+		return FALSE
+	var/mob/living/carbon/infected_mob = advanced_disease.affected_mob
+	if(advanced_disease.stage < 5 || !causes_permanent_deafness)
+		REMOVE_TRAIT(infected_mob, TRAIT_DEAF, DISEASE_TRAIT)
+	return TRUE

commit f719e389a299dd1ad208d4b475ab14c16c41638f

Fixes a compile error caused by #68080 (0b7e5429273901c230bc5d8bf9864569232a1fa2)
Local man forgets to test, misnames a var
I cleaned up M here too because it pissed me off

Oh, also the unit tests didn't properly run on the last commit, which
fooled the merging maintainer into assuming it passed.

WTF happen there

code/datums/diseases/advance/symptoms/deafness.dm
@@ -41,24 +41,24 @@
 	. = ..()
 	if(!.)
 		return
-	var/mob/living/carbon/M = A.affected_mob
-	var/obj/item/organ/internal/ears/ears = M.getorganslot(ORGAN_SLOT_EARS)
+	var/mob/living/carbon/infected_mob = A.affected_mob
+	var/obj/item/organ/internal/ears/ears = infected_mob.getorganslot(ORGAN_SLOT_EARS)
 	if(!ears)
 		return //cutting off your ears to cure the deafness: the ultimate own
 	switch(A.stage)
 		if(3, 4)
 			if(prob(base_message_chance) && !suppress_warning)
-				to_chat(M, span_warning("[pick("You hear a ringing in your ear.", "Your ears pop.")]"))
+				to_chat(infected_mob, span_warning("[pick("You hear a ringing in your ear.", "Your ears pop.")]"))
 		if(5)
 			if(causes_permanent_deafness)
 				if(ears.damage < ears.maxHealth)
-					to_chat(M, span_userdanger("Your ears pop painfully and start bleeding!"))
+					to_chat(infected_mob, span_userdanger("Your ears pop painfully and start bleeding!"))
 					// Just absolutely murder me man
 					ears.applyOrganDamage(ears.maxHealth)
-					M.emote("scream")
-					ADD_TRAIT(M.affected_mob, TRAIT_DEAF, DISEASE_TRAIT)
+					infected_mob.emote("scream")
+					ADD_TRAIT(infected_mob, TRAIT_DEAF, DISEASE_TRAIT)
 			else
-				to_chat(M, span_userdanger("Your ears pop and begin ringing loudly!"))
+				to_chat(infected_mob, span_userdanger("Your ears pop and begin ringing loudly!"))
 				ears.deaf = min(20, ears.deaf + 15)
 
 /datum/symptom/deafness/on_stage_change(datum/disease/advance/advanced_disease)