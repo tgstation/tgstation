7f02700ea6a3ce1a7ad8c797c53e77d53516c764
Ákos Balázs <profakos@gmail.com>
2022-07-13 18:29:39 +0200

    Turns the menu into a list from tabs

--- tgui/packages/tgui/interfaces/ForceEvent.js
+++ tgui/packages/tgui/interfaces/ForceEvent.js
@@ -1,32 +1,23 @@
-
-import { useBackend, useSharedState } from '../backend';
-import { Box, Flex, Button, Section, Tabs, Table } from "../components";
+import { useBackend } from '../backend';
+import { Box, Stack, Button, Section, Table } from '../components';
 import { Window } from '../layouts';
 
 export const ForceEvent = (props, context) => {
-    return (
-      <Window title="Force Event" width={450} height={360}>
-        <Window.Content scrollable>
-            <EventContent />
-        </Window.Content>
-      </Window>
-    );
-}
+  return (
+    <Window title="Force Event" width={450} height={450}>
+      <Window.Content scrollable>
+        <EventContent />
+      </Window.Content>
+    </Window>
+  );
+};
 
-  export const EventContent = (props, context) => {
-  const { act, data } = useBackend(context);
-  
+export const EventContent = (props, context) => {
+  const { data } = useBackend(context);
+
   const categories = Object.values(data.categories);
 
-  const [activeCategoryName, setActiveCategoryName] = useSharedState(
-    context,
-    'category',
-    categories[0]?.name
-  );
-
-  const activeCategory = categories.find((category) => category.name === activeCategoryName);
-
-  const nameSorter = (a,b) => {
+  const nameSorter = (a, b) => {
     const nameA = a.name.toLowerCase();
     const nameB = b.name.toLowerCase();
     if (nameA < nameB) {
@@ -36,52 +27,60 @@ export const ForceEvent = (props, context) => {
       return 1;
     }
     return 0;
-  }
+  };
 
   return (
-    <Box>
-        <Section fitted>
-            <Flex>
-                <Flex.Item>
-                <Tabs vertical>
-                    {categories.sort(nameSorter).map((category) => (
-                    <Tabs.Tab
-                        key={category.name}
-                        selected={category.name === activeCategoryName}
-                        onClick={() => {
-                        setActiveCategoryName(category.name);
-                        }}>
-                        {category.name}
-                    </Tabs.Tab>
-                    ))}
-                </Tabs>
-                </Flex.Item>
-                <Flex.Item grow={1} basis={0}>
-                    <Table>
-                    {
-                            activeCategory?.events.sort(nameSorter).map((event) => {
-                                return (
-                                    <Table.Row key={event.name} className="candystripe">
-                                            <Table.Cell>{event.name}</Table.Cell>
-                                            <Table.Cell collapsing textAlign="right">
-                                                <Button
-                                                    content="Trigger"
-                                                    tooltip={ event.description || ''}
-                                                    tooltipPosition="right"
-                                                    onClick={() =>
-                                                        act('forceevent', {
-                                                        type: event.type,
-                                                        })}
-                                                />
-                                            </Table.Cell>
-                                    </Table.Row>     
-                            )})
-                        }
+      <Section>
+        <Stack vertical fill>
+            {categories.sort(nameSorter).map((category) => (
+              <Stack.Item mt={0.2}>
+                  <Section title={category.name}>
+                    <EventList events={category.events} />
+                  </Section>
+              </Stack.Item>
+            ))}
+        </Stack>
+      </Section>
+  );
+};
 
-                    </Table>
-                </Flex.Item>
-            </Flex>
-        </Section>
-    </Box>
-  )
-}
+export const EventList = (props, context) => {
+  const { act } = useBackend(context);
+  const { events } = props;
+
+  const nameSorter = (a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  };
+
+  return (
+    <Table>
+      {events.sort(nameSorter).map((event) => {
+        return (
+          <Table.Row key={event.name} className="candystripe">
+            <Table.Cell>{event.name}</Table.Cell>
+            <Table.Cell collapsing textAlign="right">
+              <Button
+                content="Trigger"
+                tooltip={event.description || ''}
+                tooltipPosition="right"
+                onClick={() =>
+                  act('forceevent', {
+                    type: event.type,
+                  })
+                }
+              />
+            </Table.Cell>
+          </Table.Row>
+        );
+      })}
+    </Table>
+  );
+};
