3fcc8fb3648c5c9302fe39c8fa6ac6df7e796030
san7890 <the@san7890.com>
2022-07-13 16:19:02 -0600

    rewrites all the error messages
    i was fed up with them being inconsistent so let's fix that

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -6,12 +6,14 @@ shopt -s globstar
 
 #ANSI Escape Codes for colors to increase contrast of errors
 RED="\033[0;31m"
+GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 NC="\033[0m" # No Color
 
 st=0
 
 echo -e "${BLUE}Checking for map issues...${NC}"
+
 if grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
 	echo
     echo -e "${RED}ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!${NC}"
@@ -35,7 +37,7 @@ if grep -P '/obj/merge_conflict_marker' _maps/**/*.dmm; then
 fi;
 if grep -P '^\ttag = \"icon' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: tag vars from icon state generation detected in maps, please remove them.${NC}"
+    echo -e "${RED}ERROR: Tag vars from icon state generation detected in maps, please remove them.${NC}"
     st=1
 fi;
 if grep -P 'step_[xy]' _maps/**/*.dmm;	then
@@ -50,7 +52,7 @@ if grep -P 'pixel_[^xy]' _maps/**/*.dmm;	then
 fi;
 if grep -P '/obj/structure/cable(/\w+)+\{' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: vareditted cables detected, please remove them.${NC}"
+    echo -e "${RED}ERROR: Variable editted cables detected, please remove them.${NC}"
     st=1
 fi;
 if grep -P '\td[1-2] =' _maps/**/*.dmm;	then
@@ -60,114 +62,114 @@ if grep -P '\td[1-2] =' _maps/**/*.dmm;	then
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/cable,\n[^)]*?/obj/structure/cable,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple cables on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple cables on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/lattice[/\w]*?,\n[^)]*?/obj/structure/lattice[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple lattices on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple lattices on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/machinery/atmospherics/pipe/(?<type>[/\w]*),\n[^)]*?/obj/machinery/atmospherics/pipe/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical pipes on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical pipes on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/barricade/(?<type>[/\w]*),\n[^)]*?/obj/structure/barricade/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical barricades on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical barricades on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/table/(?<type>[/\w]*),\n[^)]*?/obj/structure/table/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical tables on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical tables on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/chair/(?<type>[/\w]*),\n[^)]*?/obj/structure/chair/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical chairs on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical chairs on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/machinery/door/airlock[/\w]*?,\n[^)]*?/obj/machinery/door/airlock[/\w]*?,\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple airlocks on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple airlocks on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/machinery/door/firedoor[/\w]*?,\n[^)]*?/obj/machinery/door/firedoor[/\w]*?,\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple firelocks on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple firelocks on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/closet/(?<type>[/\w]*),\n[^)]*?/obj/structure/closet/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical closets on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical closets on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/grille/(?<type>[/\w]*),\n[^)]*?/obj/structure/grille/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical grilles on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical grilles on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/girder/(?<type>[/\w]*),\n[^)]*?/obj/structure/girder/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical girders on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical girders on the same tile, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/stairs/(?<type>[/\w]*),\n[^)]*?/obj/structure/stairs/\g{type},\n[^)]*?/area/.+\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found multiple identical stairs on the same tile, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found multiple identical stairs on the same tile, please remove them.${NC}"
 	st=1
 fi;
 if grep -rzoP 'machinery/door.*{([^}]|\n)*name = .*("|\s)(?!of|and|to)[a-z].*\n' _maps/**/*.dmm;	then
     echo
-    echo -e "${RED}ERROR: found door names without proper upper-casing. Please upper-case your door names.${NC}"
+    echo -e "${RED}ERROR: Found door names without proper upper-casing. Please upper-case your door names.${NC}"
     st=1
 fi;
 if grep -Pzo '/obj/machinery/power/apc[/\w]*?\{\n[^}]*?pixel_[xy] = -?[013-9]\d*?[^\d]*?\s*?\},?\n' _maps/**/*.dmm ||
 	grep -Pzo '/obj/machinery/power/apc[/\w]*?\{\n[^}]*?pixel_[xy] = -?\d+?[0-46-9][^\d]*?\s*?\},?\n' _maps/**/*.dmm ||
 	grep -Pzo '/obj/machinery/power/apc[/\w]*?\{\n[^}]*?pixel_[xy] = -?\d{3,1000}[^\d]*?\s*?\},?\n' _maps/**/*.dmm ;	then
 	echo
-    echo -e "${RED}ERROR: found an APC with a manually set pixel_x or pixel_y that is not +-25.${NC}"
+    echo -e "${RED}ERROR: Found an APC with a manually set pixel_x or pixel_y that is not +-25. Use the directional variants when possible.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/lattice[/\w]*?,\n[^)]*?/turf/closed/wall[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found lattice stacked with a wall, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found a lattice stacked with a wall, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/lattice[/\w]*?,\n[^)]*?/turf/closed[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found lattice stacked within a wall, please remove them.${NC}"
+    echo -e "${RED}ERROR: Found a lattice stacked within a wall, please remove them.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/window[/\w]*?,\n[^)]*?/turf/closed[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found a window stacked within a wall, please remove it.${NC}"
+    echo -e "${RED}ERROR: Found a window stacked within a wall, please remove it.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/machinery/door/airlock[/\w]*?,\n[^)]*?/turf/closed[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found an airlock stacked within a wall, please remove it.${NC}"
+    echo -e "${RED}ERROR: Found an airlock stacked within a wall, please remove it.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/obj/structure/stairs[/\w]*?,\n[^)]*?/turf/open/genturf[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found a staircase on top of a genturf. Please replace the genturf with a proper tile.${NC}"
+    echo -e "${RED}ERROR: Found a staircase on top of a gen_turf. Please replace the gen_turf with a proper turf.${NC}"
     st=1
 fi;
 if grep -Pzo '/obj/machinery/conveyor/inverted[/\w]*?\{\n[^}]*?dir = [1248];[^}]*?\},?\n' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: found an inverted conveyor belt with a cardinal dir. Please replace it with a normal conveyor belt.${NC}"
+    echo -e "${RED}ERROR: Found an inverted conveyor belt with a cardinal dir. Please replace it with a normal conveyor belt.${NC}"
     st=1
 fi;
 if grep -P '^/area/.+[\{]' _maps/**/*.dmm;	then
 	echo
-    echo -e "${RED}ERROR: Vareditted /area path use detected in maps, please replace with proper paths.${NC}"
+    echo -e "${RED}ERROR: Variable editted /area path use detected in a map, please replace with a proper area path.${NC}"
     st=1
 fi;
 if grep -P '\W\/turf\s*[,\){]' _maps/**/*.dmm; then
 	echo
-    echo -e "${RED}ERROR: base /turf path use detected in maps, please replace with proper paths.${NC}"
+    echo -e "${RED}ERROR: Base /turf path use detected in maps, please replace a with proper turf path.${NC}"
     st=1
 fi;
 if grep -Pzo '"\w+" = \(\n[^)]*?/turf/[/\w]*?,\n[^)]*?/turf/[/\w]*?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm; then
@@ -180,20 +182,27 @@ if grep -Pzo '"\w+" = \(\n[^)]*?/area/.+?,\n[^)]*?/area/.+?\)' _maps/**/*.dmm; t
     echo -e "${RED}ERROR: Multiple areas detected on the same tile! Please choose only one area!${NC}"
     st=1
 fi;
-if grep -P '^/*var/' code/**/*.dm; then
+if grep -ni 'nanotransen' _maps/**/*.dmm; then
 	echo
-    echo -e "${RED}ERROR: Unmanaged global var use detected in code, please use the helpers.${NC}"
+    echo -e "${RED}ERROR: Misspelling of Nanotrasen detected in maps, please remove the extra N(s).${NC}"
     st=1
 fi;
+if grep -i 'centcomm' _maps/**/*.dmm; then
+	echo
+    echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in maps, please remove the extra M(s).${NC}"
+    st=1
+fi;
+
 echo -e "${BLUE}Checking for whitespace issues...${NC}"
+
 if grep -P '(^ {2})|(^ [^ * ])|(^    +)' code/**/*.dm; then
 	echo
-    echo -e "${RED}ERROR: space indentation detected${NC}"
+    echo -e "${RED}ERROR: Space indentation detected, please use tab indentation.${NC}"
     st=1
 fi;
 if grep -P '^\t+ [^ *]' code/**/*.dm; then
 	echo
-    echo -e "${RED}ERROR: mixed <tab><space> indentation detected${NC}"
+    echo -e "${RED}ERROR: Mixed <tab><space> indentation detected, please stick to tab indentation.${NC}"
     st=1
 fi;
 nl='
@@ -203,49 +212,46 @@ while read f; do
     t=$(tail -c2 "$f"; printf x); r1="${nl}$"; r2="${nl}${r1}"
     if [[ ! ${t%x} =~ $r1 ]]; then
 		echo
-        echo -e "${RED}ERROR: file $f is missing a trailing newline${NC}"
+        echo -e "${RED}ERROR: file $f is missing a trailing newline.${NC}"
         st=1
     fi;
 done < <(find . -type f -name '*.dm')
+
 echo -e "${BLUE}Checking for common mistakes...${NC}"
+
+if grep -P '^/*var/' code/**/*.dm; then
+	echo
+    echo -e "${RED}ERROR: Unmanaged global var use detected in code, please use the helpers.${NC}"
+    st=1
+fi;
 if grep -P '^/[\w/]\S+\(.*(var/|, ?var/.*).*\)' code/**/*.dm; then
 	echo
-    echo -e "${RED}ERROR: changed files contains proc argument starting with 'var'${NC}"
+    echo -e "${RED}ERROR: Changed files contains a proc argument starting with 'var'.${NC}"
     st=1
 fi;
 if grep 'balloon_alert\(".+"\)' code/**/*.dm; then
 	echo
-	echo -e "${RED}ERROR: Balloon alert with improper arguments.${NC}"
+	echo -e "${RED}ERROR: Found a balloon alert with improper arguments.${NC}"
 	st=1
 fi;
 if grep -i 'centcomm' code/**/*.dm; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of CENTCOM detected in code, please remove the extra M(s).${NC}"
-    st=1
-fi;
-if grep -i 'centcomm' _maps/**/*.dmm; then
-	echo
-    echo -e "${RED}ERROR: Misspelling(s) of CENTCOM detected in maps, please remove the extra M(s).${NC}"
+    echo -e "${RED}ERROR: Misspelling(s) of CentCom detected in code, please remove the extra M(s).${NC}"
     st=1
 fi;
 if grep -ni 'nanotransen' code/**/*.dm; then
 	echo
-    echo -e "${RED}ERROR: Misspelling(s) of nanotrasen detected in code, please remove the extra N(s).${NC}"
-    st=1
-fi;
-if grep -ni 'nanotransen' _maps/**/*.dmm; then
-	echo
-    echo -e "${RED}ERROR: Misspelling(s) of nanotrasen detected in maps, please remove the extra N(s).${NC}"
+    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please remove the extra N(s).${NC}"
     st=1
 fi;
 if ls _maps/*.json | grep -P "[A-Z]"; then
 	echo
-    echo -e "${RED}ERROR: Uppercase in a map json detected, these must be all lowercase.${NC}"
+    echo -e "${RED}ERROR: Uppercase in a map .JSON file detected, these must be all lowercase.${NC}"
     st=1
 fi;
 if grep -i '/obj/effect/mapping_helpers/custom_icon' _maps/**/*.dmm; then
 	echo
-    echo -e "${RED}ERROR: Custom icon helper found. Please include dmis as standard assets instead for built-in maps.${NC}"
+    echo -e "${RED}ERROR: Custom icon helper found. Please include DMI files as standard assets instead for repository maps.${NC}"
     st=1
 fi;
 for json in _maps/*.json
@@ -256,10 +262,20 @@ do
         if [ ! -f $filename ]
         then
 			echo
-            echo -e "${RED}ERROR: found invalid file reference to $filename in _maps/$json ${NC}"
+            echo -e "${RED}ERROR: Found an invalid file reference to $filename in _maps/$json ${NC}"
             st=1
         fi
     done < <(jq -r '[.map_file] | flatten | .[]' $json)
 done
+
+if [ $st = 0 ]; then
+    echo
+    echo -e "${GREEN}No errors found!${NC}"
+fi;
+
+if [ $st = 1 ]; then
+    echo
+    echo -e "${RED}Errors found, please fix them and try again.${NC}"
+fi;
 
 exit $st
