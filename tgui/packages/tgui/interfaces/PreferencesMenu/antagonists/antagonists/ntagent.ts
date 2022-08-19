2117588f22fd9d0806cd83662caf70af8d7c8e70
robotduinom <robotduinom@proton.me>
2022-08-19 10:40:18 +0300

    nt agent ui kodlarında prettier çalıştırdım

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/ntagent.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/ntagent.ts
@@ -1,8 +1,7 @@
-import { Antagonist, Category } from "../base";
-import { multiline } from "common/string";
+import { Antagonist, Category } from '../base';
+import { multiline } from 'common/string';
 
-export const NTA_MECHANICAL_DESCRIPTION
-  = multiline`
+export const NTA_MECHANICAL_DESCRIPTION = multiline`
       Sen bir Nanotrasen ajanısın. Amacın istasyonda bir tehlike söz konusu
       olursa o tehlikenin peşine düşmektir. Onun dışında istasyonda mesleğini
       yerine getirmek zorundasın.
@@ -12,12 +11,9 @@ export const NTA_MECHANICAL_DESCRIPTION
     `;
 
 const NTAgent: Antagonist = {
-  key: "ntagent",
-  name: "NT Agent",
-  description: [
-    `Nanotrasene calisiyorsun.`,
-    NTA_MECHANICAL_DESCRIPTION,
-  ],
+  key: 'ntagent',
+  name: 'NT Agent',
+  description: [`Nanotrasene calisiyorsun.`, NTA_MECHANICAL_DESCRIPTION],
   category: Category.Roundstart,
 };
 
