tgstation/tgstation

commit a88a2f81320000962b1d492513d504411a320d68

Shadowpeople's healing is from their brain, splits up surgery.dmi (#69543)
About The Pull Request

    Shadowpeople
        brain now holds their healing properties.
            while possible to extract the brain and put them in another species, the burn-in-light downside really makes it a lot more worth it to just stay a shadowperson and enjoy their other benefits than to swap.
        Now use burning eyes from nightmares instead of an unsprited nightvision granting eyeball.
    surgery.dmi split up
        surgery_ui.dmi holds zone selection ui things for research
        surgery_tools.dmi holds surgical tools
        /organs folder holds organs.dmi, and species specific organ files for flies and shadowpeople
            flies don't put in their random organs because of dmi memes, all their UNIQUE organs will be in the .dmi

Why It's Good For The Game

moving behavior onto the organ moves us closer to species as a blueprint, not species as something that magically grants immutable bonuses.
surgery.dmi is poorly described, holds many different things, and conflicts often because of it.
Changelog

cl
add: Shadowpeople now heal from their brains! Their brain-tumor-thingy!
code: split up surgery.dmi
/cl

code/game/objects/structures/tables_racks.dm
@@ -671,7 +671,7 @@
 /obj/structure/table/optable
 	name = "operating table"
 	desc = "Used for advanced medical procedures."
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "optable"
 	buildstack = /obj/item/stack/sheet/mineral/silver
 	smoothing_flags = NONE

code/game/objects/items/debug_items.dm
@@ -57,15 +57,15 @@
 		"Analyzer" = image(icon = 'icons/obj/device.dmi', icon_state = "analyzer"),
 		"Pickaxe" = image(icon = 'icons/obj/mining.dmi', icon_state = "minipick"),
 		"Shovel" = image(icon = 'icons/obj/mining.dmi', icon_state = "spade"),
-		"Retractor" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "retractor"),
-		"Hemostat" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "hemostat"),
-		"Cautery" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "cautery"),
-		"Drill" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "drill"),
-		"Scalpel" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "scalpel"),
-		"Saw" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "saw"),
-		"Bonesetter" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "bone setter"),
+		"Retractor" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "retractor"),
+		"Hemostat" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "hemostat"),
+		"Cautery" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "cautery"),
+		"Drill" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "drill"),
+		"Scalpel" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "scalpel"),
+		"Saw" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "saw"),
+		"Bonesetter" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "bone setter"),
 		"Knife" = image(icon = 'icons/obj/kitchen.dmi', icon_state = "knife"),
-		"Blood Filter" = image(icon = 'icons/obj/medical/surgery.dmi', icon_state = "bloodfilter"),
+		"Blood Filter" = image(icon = 'icons/obj/medical/organs/organs.dmi', icon_state = "bloodfilter"),
 		"Rolling Pin" = image(icon = 'icons/obj/kitchen.dmi', icon_state = "rolling_pin"),
 		"Wire Brush" = image(icon = 'icons/obj/tools.dmi', icon_state = "wirebrush"),
 		)

code/game/objects/items/implants/implant_misc.dm
@@ -69,7 +69,7 @@
 
 /obj/item/implant/radio/slime
 	name = "slime radio"
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "adamantine_resonator"
 	radio_key = /obj/item/encryptionkey/headset_sci
 	subspace_transmission = TRUE

code/game/objects/items/melee/energy.dm
@@ -199,7 +199,7 @@
 /obj/item/melee/energy/sword/cyborg/saw //Used by medical Syndicate cyborgs
 	name = "energy saw"
 	desc = "For heavy duty cutting. It has a carbon-fiber blade in addition to a toggleable hard-light edge to dramatically increase sharpness."
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "esaw"
 	hitsound = 'sound/weapons/circsawhit.ogg'
 	force = 18

code/game/objects/items/stacks/medical.dm
@@ -366,7 +366,7 @@
 	singular_name = "bone gel"
 	desc = "A potent medical gel that, when applied to a damaged bone in a proper surgical setting, triggers an intense melding reaction to repair the wound. Can be directly applied alongside surgical sticky tape to a broken bone in dire circumstances, though this is very harmful to the patient and not recommended."
 
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/surgery_tools.dmi'
 	icon_state = "bone-gel"
 	lefthand_file = 'icons/mob/inhands/equipment/medical_lefthand.dmi'
 	righthand_file = 'icons/mob/inhands/equipment/medical_righthand.dmi'

code/game/objects/items/taster.dm
@@ -1,7 +1,7 @@
 /obj/item/taster
 	name = "taster"
 	desc = "Tastes things, so you don't have to!"
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "tonguenormal"
 
 	w_class = WEIGHT_CLASS_TINY

code/game/objects/items/toys.dm
@@ -1222,7 +1222,7 @@
 /obj/item/toy/braintoy
 	name = "squeaky brain"
 	desc = "A Mr. Monstrous brand toy made to imitate a human brain in smell and texture."
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "brain-old"
 	var/cooldown = 0
 

code/modules/antagonists/nightmare/nightmare_organs.dm
@@ -26,7 +26,7 @@
 /obj/item/organ/internal/heart/nightmare
 	name = "heart of darkness"
 	desc = "An alien organ that twists and writhes when exposed to light."
-	icon = 'icons/obj/medical/surgery.dmi'
+	icon = 'icons/obj/medical/organs/organs.dmi'
 	icon_state = "demon_heart-on"
 	visual = TRUE
 	color = "#1C1C1C"

commit 25967aeac0247d9a17ca3287358f945a09ec770a

Replaces Missing Surgery Table Sprite
Hey there,

It appears good ol' Armgulenbpgen (garmps) broke this earlier today in #69543 (a88a2f81320000962b1d492513d504411a320d68). This PR just quickly rescues the surgery table from the depths of obscurity and drags it back up to the land of the living.

code/game/objects/structures/tables_racks.dm
@@ -671,8 +671,8 @@
 /obj/structure/table/optable
 	name = "operating table"
 	desc = "Used for advanced medical procedures."
-	icon = 'icons/obj/medical/organs/organs.dmi'
-	icon_state = "optable"
+	icon = 'icons/obj/medical/surgery_table.dmi'
+	icon_state = "surgery_table"
 	buildstack = /obj/item/stack/sheet/mineral/silver
 	smoothing_flags = NONE
 	smoothing_groups = null