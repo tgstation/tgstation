cd3a7789153441a80bce6b627d460c73e648c8e4
LemonInTheDark <58055496+LemonInTheDark@users.noreply.github.com>
2022-10-19 03:34:45 -0700

    PKP Mind Virus: Glorfing Cigs edition (#69904)
    * PKP Mind Virus: Glorfing Cigs edition
    
    If you glorf while smoking you'll get the cig stuck in your throat, leaving you unable to breath, speak, or eat. Based off the sort of cig you have a random delay before you vomit up the cig. (Pipes last forever don't philoso-larp kids). Other players can help you by using the help action on you while you're hurt, which leads to something imitating the himelich manuver, a broken chest, and a vomited up blockage. Other forms of vomiting will work too, if that's even applicable.
    
    Oh I fixed a bug in vomit code. Because paralyzing you forces you to the ground, thus changing your dir, paralyzing vomits always went down. This is cringe, easy fix tho.
    
    Ah and I added some extra functionality to sound loops too. You can set them to pick and take from the provided list, then refilling it once it's empty. This keeps some variety while preventing the risk of repeat sounds
    
    Oh and I added some new signals. Some misc ones, for force say and a different kind of help then the existing one, which I renamed. One for breathing, which I used to clean up leroxin. I was planning to hook this but didn't need to in the end.
    
    One for vomiting too. and eating. Also added a way to update looping timer delays. Forgot about that, I also added variable delay for sound loops, because I thought it sounded better here.
    
    Features audio courtesy of our very own san7890, and the quake jump sound.

--- dependencies.sh
+++ dependencies.sh
@@ -15,7 +15,7 @@ export NODE_VERSION=14
 export NODE_VERSION_PRECISE=14.16.1
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.1
+export SPACEMAN_DMM_VERSION=suite-1.7.2
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.7.9
