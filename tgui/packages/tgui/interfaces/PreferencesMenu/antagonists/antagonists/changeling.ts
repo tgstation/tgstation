356b658b4d284716df1b4baa671dbced2e7659ab
Rhials <Datguy33456@gmail.com>
2022-11-06 17:47:02 -0500

    tgui progress??

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/changeling.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/changeling.ts
@@ -1,6 +1,11 @@
 import { Antagonist, Category } from '../base';
 import { multiline } from 'common/string';
 
+export const CHANGELING_MECHANICAL_DESCRIPTION = multiline`
+Transform yourself or others into different identities, and buy from an
+arsenal of biological weaponry with the DNA you collect.
+`;
+
 const Changeling: Antagonist = {
   key: 'changeling',
   name: 'Changeling',
@@ -9,11 +14,7 @@ const Changeling: Antagonist = {
       A highly intelligent alien predator that is capable of altering their
       shape to flawlessly resemble a human.
     `,
-
-    multiline`
-      Transform yourself or others into different identities, and buy from an
-      arsenal of biological weaponry with the DNA you collect.
-    `,
+    CHANGELING_MECHANICAL_DESCRIPTION,
   ],
   category: Category.Roundstart,
 };
