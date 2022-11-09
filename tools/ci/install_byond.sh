5307b8f1f08a95374c69972bf90a367bf69c509d
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2022-11-08 22:42:29 -0800

    Apply suggestions from code review
    Co-authored-by: Zephyr <12817816+ZephyrTFA@users.noreply.github.com>

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # BYOND_MAJOR and BYOND_MINOR can be explicitly set, such as in alt_byond_versions.txt
 if [ -z "${BYOND_MAJOR+x}" ]; then
-  source dependencies.sh;
+  source dependencies.sh
 fi
 
 if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
