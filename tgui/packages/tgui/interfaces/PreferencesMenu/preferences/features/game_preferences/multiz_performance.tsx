4dc67e1536dd173be404dcc4072f64863f561841
LemonInTheDark <58055496+LemonInTheDark@users.noreply.github.com>
2022-11-24 20:14:53 -0800

    ??

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/multiz_performance.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/multiz_performance.tsx
@@ -3,8 +3,7 @@ import { createDropdownInput, Feature } from '../base';
 export const multiz_performance: Feature<number> = {
   name: 'Multi-Z Detail',
   category: 'GAMEPLAY',
-  description:
-  'How detailed multi-z is. Lower this to improve performance',
+  description: 'How detailed multi-z is. Lower this to improve performance',
   component: createDropdownInput({
     [-1]: 'Standard',
     1: 'Medium',
