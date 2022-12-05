d335ef7f929b3639ac36c93fe5339f38a7beffc9
Cheshify <doedy2@gmail.com>
2022-12-04 23:16:31 -0500

    2 not -2

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/multiz_performance.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/multiz_performance.tsx
@@ -6,7 +6,7 @@ export const multiz_performance: Feature<number> = {
   description: 'How detailed multi-z is. Lower this to improve performance',
   component: createDropdownInput({
     [-1]: 'Standard',
-    [-2]: 'High',
+    2: 'High',
     1: 'Medium',
     0: 'Low',
   }),
