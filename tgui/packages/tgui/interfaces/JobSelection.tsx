b517e01fbf7cb964120d55cdb1dcd78bbf7a2292
RimiNosha <riminosha@gmail.com>
2022-12-09 18:55:16 +0000

    Fucking pain.

--- tgui/packages/tgui/interfaces/JobSelection.tsx
+++ tgui/packages/tgui/interfaces/JobSelection.tsx
@@ -109,10 +109,10 @@ export const JobSelection = (props, context) => {
     <Window
       width={1012}
       height={data.shuttle_status ? 690 : 666 /* Hahahahahaha */}
-      onComponentDidMount={
+      onComponentDidMount={() => {
         // Send a heartbeat back to DM to let it know the window is alive and well
-        act('ui_mounted_with_no_bluescreen')
-      }>
+        act('ui_mounted_with_no_bluescreen');
+      }}>
       <Window.Content scrollable>
         <StyleableSection
           title={
