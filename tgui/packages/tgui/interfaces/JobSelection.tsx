79817be05c82a2488f33ca99622529b747d9ed0c
RimiNosha <riminosha@gmail.com>
2022-12-09 17:30:18 +0000

    Fuck, I forgot the window resize-

--- tgui/packages/tgui/interfaces/JobSelection.tsx
+++ tgui/packages/tgui/interfaces/JobSelection.tsx
@@ -108,7 +108,7 @@ export const JobSelection = (props, context) => {
   return (
     <Window
       width={1012}
-      height={666 /* Hahahahahaha */}
+      height={data.shuttle_status ? 690 : 666 /* Hahahahahaha */}
       onComponentDidMount={
         // Send a heartbeat back to DM to let it know the window is alive and well
         act('ui_mounted_with_no_bluescreen')
@@ -120,7 +120,9 @@ export const JobSelection = (props, context) => {
               {data.shuttle_status && (
                 <NoticeBox info>{data.shuttle_status}</NoticeBox>
               )}
-              <span>It is currently {data.round_duration} into the shift.</span>
+              <span style={{ 'color': 'grey' }}>
+                It is currently {data.round_duration} into the shift.
+              </span>
               <Button
                 style={{ 'position': 'absolute', 'right': '1em' }}
                 onClick={() => act('select_job', { 'job': 'Random' })}
