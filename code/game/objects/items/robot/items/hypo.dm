tgstation/tgstation

commit ecde5633bdfdce4f64f6f614cedfa3038c040996

Refactors bar drink icons into datum singletons / unit tests them (#71810)
## About The Pull Request

- Refactors bar drink icons.
- Juice boxes no longer have a hard-coded list of a bunch of reagent
types in their update state, and use a system similar to bar drinks.
- Glass and shot glass icon information are no longer stored on the
drink. Instead, they are now stored in glass style datums. These datums
store name, description, icon, and icon state of a certain container +
reagent type.
- Glass styles are applied via the `takes_reagent_appearance` component.
Glasses, shot glasses, and juice boxes have this component.
- This comes with support for being able to have drink icons from
different files, rather than requiring the drinks DMI.
- The britmug is now a subtype of mug. 
   -  1 new icon: britmug filled.
- Various small code clean-up around drink reagents. 
- Unit tests icon state setups for glass styles as well as all `/drink`
reagent container subtypes.
- Splits up the massive `drinks.dmi` into separate files. 

*Disclaimer: Much of the drinking glass datums were written via script
automatically, so there may be errors present.*

## Why It's Good For The Game

- Much easier to add new drink styles, much more modular. 
- It is no longer necessary for new drinks to be added to the massive
`drinks.dmi`. People working with drinks in the future can simply add
their glass style datum and point it to their file wherever it may be.
- Expandable system. Adding a new type of reagent container that works
similarly to bar drinks but for different types of icons is a breeze.
- Ensures going forward no bar drinks have invisible sprites. 

## Changelog

:cl: Melbert
refactor: Refactored how bar drinks set their icons. Juice boxes now use
the same system.
/:cl:

code/game/objects/items/robot/items/hypo.dm
@@ -302,7 +302,7 @@
 /obj/item/reagent_containers/borghypo/borgshaker
 	name = "cyborg shaker"
 	desc = "An advanced drink synthesizer and mixer."
-	icon = 'icons/obj/drinks.dmi'
+	icon = 'icons/obj/drinks/bottles.dmi'
 	icon_state = "shaker"
 	possible_transfer_amounts = list(5,10,20)
 	// Lots of reagents all regenerating at once, so the charge cost is lower. They also regenerate faster.

_maps/RandomZLevels/SnowCabin.dmm
@@ -339,7 +339,7 @@
 /area/awaymission/cabin)
 "bc" = (
 /obj/structure/table/wood,
-/obj/item/reagent_containers/cup/glass/britcup,
+/obj/item/reagent_containers/cup/glass/mug/britcup,
 /turf/open/floor/carpet,
 /area/awaymission/cabin)
 "bd" = (

_maps/map_files/Deltastation/DeltaStation2.dmm
@@ -2442,6 +2442,27 @@
 	},
 /turf/open/floor/wood,
 /area/station/service/theater)
+"aBF" = (
+/obj/effect/decal/cleanable/dirt/dust,
+/obj/machinery/newscaster/directional/east,
+/obj/effect/spawner/random/structure/table_fancy,
+/obj/item/reagent_containers/cup/glass/bottle/beer{
+	desc = "Whatever it is, it reeks of foul, putrid froth.";
+	list_reagents = list(/datum/reagent/consumable/ethanol/bacchus_blessing = 15);
+	name = "Delta-Down";
+	pixel_x = 5;
+	pixel_y = 5
+	},
+/obj/item/reagent_containers/cup/glass/drinkingglass/shotglass{
+	pixel_x = -3;
+	pixel_y = 6
+	},
+/obj/item/reagent_containers/cup/glass/drinkingglass/shotglass{
+	pixel_x = -6;
+	pixel_y = 3
+	},
+/turf/open/floor/wood,
+/area/station/commons/dorms)
 "aBL" = (
 /obj/structure/disposalpipe/segment{
 	dir = 4
@@ -15161,18 +15182,6 @@
 /obj/item/clothing/mask/breath,
 /turf/open/floor/iron/dark/textured_large,
 /area/station/engineering/atmos/storage/gas)
-"dDe" = (
-/obj/structure/table/reinforced,
-/obj/machinery/newscaster/directional/north,
-/obj/item/clipboard,
-/obj/item/toy/figure/engineer{
-	pixel_x = -6
-	},
-/obj/item/toy/figure/atmos{
-	pixel_x = 6
-	},
-/turf/open/floor/wood,
-/area/station/engineering/break_room)
 "dDk" = (
 /obj/machinery/door/firedoor,
 /obj/structure/disposalpipe/segment{
@@ -27025,30 +27034,6 @@
 /obj/effect/spawner/random/food_or_drink/donkpockets,
 /turf/open/floor/iron,
 /area/station/maintenance/department/security)
-"gxk" = (
-/obj/item/paper_bin{
-	pixel_x = -7
-	},
-/obj/item/pen{
-	pixel_x = -7
-	},
-/obj/item/reagent_containers/cup/glass/britcup{
-	pixel_x = 6;
-	pixel_y = -4
-	},
-/obj/effect/turf_decal/stripes/line{
-	dir = 1
-	},
-/obj/structure/window{
-	dir = 1
-	},
-/obj/structure/table,
-/obj/structure/extinguisher_cabinet/directional/west,
-/obj/effect/turf_decal/tile/brown/anticorner/contrasted{
-	dir = 1
-	},
-/turf/open/floor/iron,
-/area/station/cargo/drone_bay)
 "gxv" = (
 /obj/structure/table,
 /obj/item/restraints/handcuffs,
@@ -45886,6 +45871,30 @@
 /obj/machinery/status_display/ai/directional/north,
 /turf/open/floor/iron,
 /area/station/security/prison/safe)
+"lde" = (
+/obj/item/paper_bin{
+	pixel_x = -7
+	},
+/obj/item/pen{
+	pixel_x = -7
+	},
+/obj/item/reagent_containers/cup/glass/mug/britcup{
+	pixel_x = 6;
+	pixel_y = -4
+	},
+/obj/effect/turf_decal/stripes/line{
+	dir = 1
+	},
+/obj/structure/window{
+	dir = 1
+	},
+/obj/structure/table,
+/obj/structure/extinguisher_cabinet/directional/west,
+/obj/effect/turf_decal/tile/brown/anticorner/contrasted{
+	dir = 1
+	},
+/turf/open/floor/iron,
+/area/station/cargo/drone_bay)
 "ldh" = (
 /obj/effect/turf_decal/tile/neutral/fourcorners,
 /obj/machinery/atmospherics/pipe/bridge_pipe/scrubbers/visible,
@@ -50584,6 +50593,23 @@
 /obj/machinery/light/directional/north,
 /turf/open/floor/iron,
 /area/station/engineering/main)
+"mnh" = (
+/obj/structure/table/reinforced,
+/obj/machinery/airalarm/directional/north,
+/obj/effect/spawner/random/entertainment/cigarette_pack{
+	pixel_x = 3;
+	pixel_y = 2
+	},
+/obj/effect/spawner/random/entertainment/cigarette_pack{
+	pixel_x = 3;
+	pixel_y = 2
+	},
+/obj/item/storage/box/coffeepack{
+	pixel_x = -13;
+	pixel_y = 7
+	},
+/turf/open/floor/wood,
+/area/station/engineering/break_room)
 "mnz" = (
 /obj/effect/turf_decal/tile/neutral/fourcorners,
 /obj/machinery/atmospherics/components/unary/vent_pump/on/layer4{
@@ -51573,6 +51599,18 @@
 	},
 /turf/open/floor/iron/dark,
 /area/station/command/corporate_showroom)
+"mzO" = (
+/obj/structure/table/reinforced,
+/obj/machinery/newscaster/directional/north,
+/obj/item/clipboard,
+/obj/item/toy/figure/engineer{
+	pixel_x = -6
+	},
+/obj/item/toy/figure/atmos{
+	pixel_x = 6
+	},
+/turf/open/floor/wood,
+/area/station/engineering/break_room)
 "mzV" = (
 /obj/effect/landmark/start/hangover,
 /obj/effect/turf_decal/bot,
@@ -65562,6 +65600,11 @@
 /obj/effect/turf_decal/tile/red/half/contrasted,
 /turf/open/floor/iron,
 /area/station/service/kitchen/abandoned)
+"pXB" = (
+/obj/structure/table/reinforced,
+/obj/machinery/coffeemaker/impressa,
+/turf/open/floor/wood,
+/area/station/engineering/break_room)
 "pXK" = (
 /obj/structure/cable,
 /obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
@@ -79628,27 +79671,6 @@
 /obj/machinery/atmospherics/pipe/smart/manifold4w/scrubbers/hidden/layer2,
 /turf/open/floor/iron,
 /area/station/medical/morgue)
-"twv" = (
-/obj/effect/decal/cleanable/dirt/dust,
-/obj/machinery/newscaster/directional/east,
-/obj/effect/spawner/random/structure/table_fancy,
-/obj/item/reagent_containers/cup/glass/bottle/beer{
-	desc = "Whatever it is, it reeks of foul, putrid froth.";
-	list_reagents = list(/datum/reagent/consumable/ethanol/bacchus_blessing = 15);
-	name = "Delta-Down";
-	pixel_x = 5;
-	pixel_y = 5
-	},
-/obj/item/reagent_containers/cup/glass/drinkingglass/shotglass{
-	pixel_x = -3;
-	pixel_y = 6
-	},
-/obj/item/reagent_containers/cup/glass/drinkingglass/shotglass{
-	pixel_x = -6;
-	pixel_y = 3
-	},
-/turf/open/floor/wood,
-/area/station/commons/dorms)
 "tww" = (
 /obj/structure/cable,
 /obj/effect/turf_decal/tile/neutral/fourcorners,
@@ -82157,23 +82179,6 @@
 	},
 /turf/open/floor/iron/dark,
 /area/station/engineering/atmos)
-"ubg" = (
-/obj/structure/table/reinforced,
-/obj/machinery/airalarm/directional/north,
-/obj/effect/spawner/random/entertainment/cigarette_pack{
-	pixel_x = 3;
-	pixel_y = 2
-	},
-/obj/effect/spawner/random/entertainment/cigarette_pack{
-	pixel_x = 3;
-	pixel_y = 2
-	},
-/obj/item/storage/box/coffeepack{
-	pixel_x = -13;
-	pixel_y = 7
-	},
-/turf/open/floor/wood,
-/area/station/engineering/break_room)
 "ubk" = (
 /obj/structure/cable,
 /obj/effect/turf_decal/stripes/line{
@@ -92832,11 +92837,6 @@
 /obj/effect/turf_decal/trimline/yellow/warning,
 /turf/open/floor/iron,
 /area/station/engineering/atmos/project)
-"wBr" = (
-/obj/structure/table/reinforced,
-/obj/machinery/coffeemaker/impressa,
-/turf/open/floor/wood,
-/area/station/engineering/break_room)
 "wBB" = (
 /obj/structure/cable,
 /obj/structure/disposalpipe/junction/yjunction{
@@ -122009,7 +122009,7 @@ tsB
 sch
 cKK
 vQj
-dDe
+mzO
 agA
 hHJ
 tgl
@@ -122266,7 +122266,7 @@ lKe
 nZW
 qAz
 vQj
-ubg
+mnh
 agA
 uzb
 jLx
@@ -122523,7 +122523,7 @@ ciD
 vYI
 lpv
 vQj
-wBr
+pXB
 gsf
 miE
 tgl
@@ -139717,7 +139717,7 @@ xeH
 qAV
 xYZ
 vGf
-gxk
+lde
 cAw
 sJd
 khn
@@ -152365,7 +152365,7 @@ aaa
 aad
 vop
 qqe
-twv
+aBF
 yaI
 fNn
 uEo

_maps/map_files/MetaStation/MetaStation.dmm
@@ -7438,6 +7438,16 @@
 /obj/structure/cable,
 /turf/closed/wall/r_wall,
 /area/station/maintenance/starboard/fore)
+"cMQ" = (
+/obj/machinery/door/poddoor/shutters/preopen{
+	id = "hopqueue";
+	name = "HoP Queue Shutters"
+	},
+/obj/effect/turf_decal/loading_area{
+	dir = 1
+	},
+/turf/open/floor/iron,
+/area/station/hallway/secondary/command)
 "cMX" = (
 /obj/machinery/door/firedoor,
 /obj/effect/turf_decal/tile/blue/fourcorners,
@@ -9459,18 +9469,6 @@
 	},
 /turf/open/floor/plating,
 /area/station/science/lab)
-"dzp" = (
-/obj/structure/table,
-/obj/effect/turf_decal/tile/purple,
-/obj/effect/turf_decal/tile/purple{
-	dir = 1
-	},
-/obj/structure/extinguisher_cabinet/directional/south,
-/obj/item/reagent_containers/cup/glass/britcup{
-	pixel_y = 2
-	},
-/turf/open/floor/iron,
-/area/station/science/research)
 "dzq" = (
 /obj/item/radio/intercom/directional/north,
 /obj/machinery/light/directional/north,
@@ -13425,6 +13423,20 @@
 /obj/item/radio/intercom/directional/south,
 /turf/open/floor/carpet,
 /area/station/command/heads_quarters/captain/private)
+"eWo" = (
+/obj/machinery/door/poddoor/shutters{
+	id = "teleshutter";
+	name = "Teleporter Access Shutter"
+	},
+/obj/effect/turf_decal/delivery,
+/obj/machinery/button/door/directional/east{
+	id = "teleshutter";
+	name = "Teleporter Shutter Control";
+	pixel_y = 5;
+	req_access = list("teleporter")
+	},
+/turf/open/floor/iron,
+/area/station/command/teleporter)
 "eWq" = (
 /obj/effect/turf_decal/trimline/blue/filled/line{
 	dir = 9
@@ -19118,6 +19130,18 @@
 "hbv" = (
 /turf/closed/wall/r_wall,
 /area/station/medical/coldroom)
+"hbC" = (
+/obj/structure/table,
+/obj/effect/turf_decal/tile/purple,
+/obj/effect/turf_decal/tile/purple{
+	dir = 1
+	},
+/obj/structure/extinguisher_cabinet/directional/south,
+/obj/item/reagent_containers/cup/glass/mug/britcup{
+	pixel_y = 2
+	},
+/turf/open/floor/iron,
+/area/station/science/research)
 "hbK" = (
 /turf/closed/wall,
 /area/station/maintenance/department/science/xenobiology)
@@ -26227,18 +26251,6 @@
 /obj/effect/turf_decal/tile/neutral/fourcorners,
 /turf/open/floor/iron/dark,
 /area/station/engineering/storage/tech)
-"jty" = (
-/obj/structure/disposalpipe/segment{
-	dir = 4
-	},
-/obj/effect/turf_decal/tile/neutral{
-	dir = 1
-	},
-/obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
-/obj/machinery/atmospherics/pipe/smart/manifold4w/scrubbers/hidden/layer2,
-/obj/machinery/ticket_machine/directional/north,
-/turf/open/floor/iron,
-/area/station/hallway/secondary/command)
 "jtA" = (
 /obj/structure/table/glass,
 /obj/effect/turf_decal/tile/blue/fourcorners,
@@ -35856,20 +35868,6 @@
 /obj/machinery/atmospherics/pipe/smart/manifold4w/scrubbers/hidden/layer2,
 /turf/open/floor/iron/white,
 /area/station/science/xenobiology)
-"mLY" = (
-/obj/machinery/door/poddoor/shutters{
-	id = "teleshutter";
-	name = "Teleporter Access Shutter"
-	},
-/obj/effect/turf_decal/delivery,
-/obj/machinery/button/door/directional/east{
-	id = "teleshutter";
-	name = "Teleporter Shutter Control";
-	pixel_y = 5;
-	req_access = list("teleporter")
-	},
-/turf/open/floor/iron,
-/area/station/command/teleporter)
 "mMl" = (
 /obj/structure/cable,
 /obj/effect/turf_decal/tile/neutral/fourcorners,
@@ -37988,6 +37986,15 @@
 /obj/machinery/bluespace_vendor/directional/west,
 /turf/open/floor/iron,
 /area/station/commons/dorms)
+"nwV" = (
+/obj/machinery/door/firedoor,
+/obj/machinery/door/airlock/grunge{
+	name = "Courtroom"
+	},
+/obj/structure/cable,
+/obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
+/turf/open/floor/iron/dark,
+/area/station/security/courtroom)
 "nwW" = (
 /obj/structure/closet/secure_closet/security/engine,
 /obj/machinery/airalarm/directional/east,
@@ -39214,18 +39221,6 @@
 /obj/machinery/atmospherics/components/unary/vent_pump/on/layer4,
 /turf/open/floor/iron,
 /area/station/commons/fitness/recreation)
-"nUr" = (
-/obj/structure/table,
-/obj/item/reagent_containers/cup/glass/britcup{
-	pixel_x = -6;
-	pixel_y = 11
-	},
-/obj/item/phone{
-	pixel_x = 6;
-	pixel_y = 1
-	},
-/turf/open/floor/iron,
-/area/station/cargo/drone_bay)
 "nUt" = (
 /obj/machinery/light/small/directional/east,
 /turf/open/floor/plating,
@@ -48314,6 +48309,18 @@
 /obj/effect/spawner/random/decoration/showcase,
 /turf/open/floor/carpet,
 /area/station/command/corporate_showroom)
+"rgL" = (
+/obj/structure/table,
+/obj/item/reagent_containers/cup/glass/mug/britcup{
+	pixel_x = -6;
+	pixel_y = 11
+	},
+/obj/item/phone{
+	pixel_x = 6;
+	pixel_y = 1
+	},
+/turf/open/floor/iron,
+/area/station/cargo/drone_bay)
 "rgN" = (
 /obj/structure/cable,
 /obj/effect/turf_decal/trimline/brown/filled/line{
@@ -61350,15 +61357,6 @@
 /obj/structure/window/reinforced,
 /turf/open/floor/iron/dark,
 /area/station/ai_monitored/aisat/exterior)
-"vCb" = (
-/obj/machinery/door/firedoor,
-/obj/machinery/door/airlock/grunge{
-	name = "Courtroom"
-	},
-/obj/structure/cable,
-/obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
-/turf/open/floor/iron/dark,
-/area/station/security/courtroom)
 "vCh" = (
 /obj/structure/disposalpipe/segment,
 /obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
@@ -64054,6 +64052,18 @@
 /obj/machinery/holopad,
 /turf/open/floor/iron,
 /area/station/science/research)
+"wzy" = (
+/obj/structure/disposalpipe/segment{
+	dir = 4
+	},
+/obj/effect/turf_decal/tile/neutral{
+	dir = 1
+	},
+/obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
+/obj/machinery/atmospherics/pipe/smart/manifold4w/scrubbers/hidden/layer2,
+/obj/machinery/ticket_machine/directional/north,
+/turf/open/floor/iron,
+/area/station/hallway/secondary/command)
 "wzF" = (
 /obj/structure/closet/secure_closet{
 	name = "contraband locker";
@@ -64256,16 +64266,6 @@
 /obj/machinery/atmospherics/pipe/smart/manifold4w/supply/hidden/layer4,
 /turf/open/floor/iron/dark,
 /area/station/ai_monitored/aisat/exterior)
-"wCE" = (
-/obj/machinery/door/poddoor/shutters/preopen{
-	id = "hopqueue";
-	name = "HoP Queue Shutters"
-	},
-/obj/effect/turf_decal/loading_area{
-	dir = 1
-	},
-/turf/open/floor/iron,
-/area/station/hallway/secondary/command)
 "wCH" = (
 /obj/effect/turf_decal/stripes/line{
 	dir = 10
@@ -89374,7 +89374,7 @@ fru
 wrG
 uzb
 phP
-nUr
+rgL
 jXu
 tfR
 jXu
@@ -92244,7 +92244,7 @@ jbd
 pJR
 qwY
 qIl
-jty
+wzy
 nIR
 tOh
 lTM
@@ -92500,7 +92500,7 @@ aVd
 nHn
 mig
 wMx
-wCE
+cMQ
 urA
 qAc
 knj
@@ -94310,7 +94310,7 @@ vnp
 vnp
 vnp
 exN
-mLY
+eWo
 gqX
 cfb
 laK
@@ -98650,7 +98650,7 @@ wwY
 wwY
 xCg
 qfE
-vCb
+nwV
 egb
 htd
 ggU
@@ -106158,7 +106158,7 @@ gcV
 nNe
 nbS
 aHM
-dzp
+hbC
 oWk
 ddu
 wmi

_maps/map_files/generic/CentCom.dmm
@@ -8924,7 +8924,7 @@
 /obj/item/reagent_containers/condiment/peppermill{
 	pixel_x = -8
 	},
-/obj/item/reagent_containers/cup/glass/britcup,
+/obj/item/reagent_containers/cup/glass/mug/britcup,
 /obj/effect/turf_decal/bot,
 /turf/open/floor/iron,
 /area/centcom/tdome/observation)
@@ -9968,7 +9968,7 @@
 "Du" = (
 /obj/structure/table/reinforced,
 /obj/machinery/door/firedoor,
-/obj/item/reagent_containers/cup/glass/britcup,
+/obj/item/reagent_containers/cup/glass/mug/britcup,
 /obj/structure/window/reinforced{
 	dir = 8
 	},

_maps/shuttles/emergency_cruise.dmm
@@ -412,7 +412,7 @@
 /area/shuttle/escape)
 "tJ" = (
 /obj/structure/table/reinforced,
-/obj/item/reagent_containers/cup/glass/britcup{
+/obj/item/reagent_containers/cup/glass/mug/britcup{
 	pixel_x = 8;
 	pixel_y = -1
 	},

code/datums/components/takes_reagent_appearance.dm
@@ -1,0 +1,157 @@
+/**
+ * ## "Takes reagent appearance" Component
+ *
+ * Bit of a mouthful, but when applied to an item that can hold reagents (primarily reagent containers),
+ * said item will take on an appearance based on the majority share reagent inside
+ *
+ * This is more than just "changing the color a bit" or "applies an overlay", this is
+ * an entire icon / icon state / name change, making it look like a different item entirely
+ *
+ * This is done by cross-referencing the glass style datums. See [/datum/glass_style] for more information about that.
+ *
+ * An example usage is bartender mixed drinks - each reagent gets its own fancy drink sprite
+ */
+/datum/component/takes_reagent_appearance
+	/// Icon file when attached to the item
+	var/icon_pre_change
+	/// Icon state when attached to the item
+	var/icon_state_pre_change
+	/// Optional callback invoked when when the item's appearance is changed
+	var/datum/callback/on_icon_changed
+	/// Optional callback invoked when our item has its appearance reverted to default
+	var/datum/callback/on_icon_reset
+
+/datum/component/takes_reagent_appearance/Initialize(
+	datum/callback/on_icon_changed,
+	datum/callback/on_icon_reset,
+)
+	if(!isitem(parent))
+		return COMPONENT_INCOMPATIBLE
+
+	var/obj/item/item_parent = parent
+	if(isnull(item_parent.reagents))
+		return COMPONENT_INCOMPATIBLE
+
+	icon_pre_change = item_parent.icon
+	icon_state_pre_change = item_parent.base_icon_state || item_parent.icon_state
+
+	src.on_icon_changed = on_icon_changed
+	src.on_icon_reset = on_icon_reset
+
+/datum/component/takes_reagent_appearance/Destroy()
+	QDEL_NULL(on_icon_changed)
+	QDEL_NULL(on_icon_reset)
+	return ..()
+
+/datum/component/takes_reagent_appearance/RegisterWithParent()
+	RegisterSignal(parent, COMSIG_ATOM_UPDATE_APPEARANCE, PROC_REF(on_update_appearance))
+
+/datum/component/takes_reagent_appearance/UnregisterFromParent()
+	UnregisterSignal(parent, COMSIG_ATOM_UPDATE_APPEARANCE)
+	if(QDELING(parent))
+		return
+
+	var/obj/item/item_parent = parent
+	item_parent.name = initial(item_parent.name)
+	item_parent.desc = initial(item_parent.desc)
+	item_parent.icon = icon_pre_change
+	item_parent.icon_state = icon_state_pre_change
+	on_icon_reset?.Invoke()
+	item_parent.update_appearance()
+
+/// Signal proc for [COMSIG_ATOM_UPDATE_APPEARANCE]
+/// We hook into the update appearance proc to perform our own update based on our glass style
+/// Preventing any further updates down the line on successes
+/datum/component/takes_reagent_appearance/proc/on_update_appearance(datum/source, updates)
+	SIGNAL_HANDLER
+
+	. = NONE
+
+	var/datum/glass_style/main_style = get_main_reagent_style()
+	if(updates & UPDATE_NAME)
+		. |= update_name(main_style)
+	if(updates & UPDATE_DESC)
+		. |= update_desc(main_style)
+	if(updates & UPDATE_ICON)
+		. |= update_icon(main_style)
+	return .
+
+/**
+ * Performs the name update.
+ *
+ * * Returns [COMSIG_ATOM_NO_UPDATE_NAME] if one was complete
+ * * Returns [NONE] if nothing happened
+ * * Returns [NONE] if the name was reset to initial state
+ */
+/datum/component/takes_reagent_appearance/proc/update_name(datum/glass_style/style)
+	var/obj/item/item_parent = parent
+	if(isnull(style))
+		// no style (reset)
+		item_parent.name = initial(item_parent.name)
+	else if(style.name)
+		// style
+		item_parent.name = style.name
+		return COMSIG_ATOM_NO_UPDATE_NAME
+
+	return NONE
+
+/**
+ * Performs the description update.
+ *
+ * * Returns [COMSIG_ATOM_NO_UPDATE_DESC] if one was complete
+ * * Returns [NONE] if nothing happened
+ * * Returns [NONE] if the description was reset to initial state
+ */
+/datum/component/takes_reagent_appearance/proc/update_desc(datum/glass_style/style)
+	var/obj/item/item_parent = parent
+	if(isnull(style))
+		// no style (reset)
+		item_parent.desc = initial(item_parent.desc)
+	else if(style.desc)
+		// style
+		item_parent.desc = style.desc
+		return COMSIG_ATOM_NO_UPDATE_DESC
+
+	return NONE
+
+/**
+ * Performs the icon update.
+ *
+ * * Returns [COMSIG_ATOM_NO_UPDATE_ICON] if an icon or icon state ocurred
+ * * Returns [NONE] if the icon or icon state was reset to base state
+ */
+/datum/component/takes_reagent_appearance/proc/update_icon(datum/glass_style/style)
+	var/obj/item/item_parent = parent
+	if(isnull(style))
+		// no style (reset)
+		item_parent.icon = icon_pre_change
+		item_parent.icon_state = icon_state_pre_change
+	else if(style.icon && style.icon_state)
+		// style
+		item_parent.icon = style.icon
+		item_parent.icon_state = style.icon_state
+		on_icon_changed?.InvokeAsync(style)
+		return COMSIG_ATOM_NO_UPDATE_ICON_STATE
+
+	// Reset gets invoked regardless, as further updates may "reset" the icon yet
+	on_icon_reset?.InvokeAsync()
+	return NONE
+
+/**
+ * Gets the correspinding style based on the parent's state and reagents within
+ *
+ * * Returns null if its reagents are empty
+ * * Returns null if no majority reagent was found
+ * * Otherwise returns a glass style datum
+ */
+/datum/component/takes_reagent_appearance/proc/get_main_reagent_style()
+	RETURN_TYPE(/datum/glass_style)
+
+	var/obj/item/item_parent = parent
+	if(item_parent.reagents.total_volume <= 0)
+		return null
+	var/datum/reagent/main_reagent = item_parent.reagents.get_master_reagent()
+	if(isnull(main_reagent))
+		return null
+
+	return GLOB.glass_style_singletons[parent.type][main_reagent.type]

commit b351d74e6877dc4611c1494afb9284ce02628021

Fixes master missing icon failure - threemileislandglass (#71990)
Caused by #71810
Fixes master.

My unit test works, rejoice!

code/game/objects/items/robot/items/hypo.dm
@@ -373,6 +373,7 @@
 	name = "cyborg shaker"
 	desc = "Will mix drinks that knock them dead."
 	icon_state = "threemileislandglass"
+	icon = 'icons/obj/drinks/mixed_drinks.dmi'
 	tgui_theme = "syndicate"
 	dispensed_temperature = WATER_MATTERSTATE_CHANGE_TEMP
 	default_reagent_types = HACKED_SERVICE_REAGENTS