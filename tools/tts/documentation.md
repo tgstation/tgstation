6c9675a7053925b9f163019f0bc0d7b3961ed2c5
Kyle Spier-Swenson <kyleshome@gmail.com>
2022-12-31 05:39:36 -0800

    Intel accelerated python. Fileless python ffmpeg handling. misc other shit. (#12)
    * Update documentation.md
    
    * Intel accelerated python. Fileless python ffmpeg handling.

--- tools/tts/documentation.md
+++ tools/tts/documentation.md
@@ -4,3 +4,4 @@ Documentation for the library can be found [here](https://tts.readthedocs.io/en/
 
 ## Basic documentation
 To run, simply do `docker-compose up -d`
+Do `docker-compose up -d --build` if you need to force a rebuild after making changes
--- tools/tts/tts-api.py
+++ tools/tts/tts-api.py
@@ -2,38 +2,56 @@ from flask import Flask, request, send_file
 import torch
 from TTS.api import TTS
 import os
+import io
+import gc
 import json
-import shlex
+import subprocess
 
 tts = TTS("tts_models/en/vctk/vits", progress_bar=False, gpu=False)
 
 app = Flask(__name__)
+request_count = 0;
 
 @app.route("/tts")
 def text_to_speech():
-    voice = request.args.get("voice", '')
-    text = request.json.get("text", '')
-    identifier = bytes.fromhex(request.args.get("identifier", '')).hex()
-    filter_complex = request.args.get("filter", '')
-    filter_complex = filter_complex.replace("\"", "")
+	global request_count
+	request_count += 1
+	
+	voice = request.args.get("voice", '')
+	text = request.json.get("text", '')
+	
+	filter_complex = request.args.get("filter", '')
+	filter_complex = filter_complex.replace("\"", "")
 
-    filter_statement = ""
-    if filter_complex != "":
-        filter_statement = "-filter_complex " + shlex.quote(filter_complex)
-
-    wav_file_loc = f'/tts_files/{identifier}.wav'
-    ogg_file_loc = f'/tts_files/{identifier}.ogg'
-
-    with torch.no_grad():
-        tts.tts_to_file(text=text, speaker=voice, file_path=f"/tts_files/{identifier}.wav")
-    os.system(f"ffmpeg -i {shlex.quote(wav_file_loc)} {filter_statement} -c:a libvorbis -b:a 64k {shlex.quote(ogg_file_loc)} -y")
-    os.remove(wav_file_loc)
-    return send_file(ogg_file_loc, mimetype="audio/wav")
+	result = None
+	with io.BytesIO() as wav_bytes:
+		with torch.no_grad():
+			tts.tts_to_file(text=text, speaker=voice, file_path=wav_bytes)
+		
+		ffmpeg_result = None
+		if filter_complex != "":
+			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-filter_complex", filter_complex, "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=wav_bytes.getvalue(), capture_output = True)
+		else:
+			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=wav_bytes.getvalue(), capture_output = True)
+		print(f"ffmpeg result size: {len(ffmpeg_result.stdout)} stderr = \n{ffmpeg_result.stderr.decode()}")
+		
+		result = send_file(io.BytesIO(ffmpeg_result.stdout), as_attachment=True, download_name='{identifier}.ogg', mimetype="audio/ogg")
+		
+	return result
 
 @app.route("/tts-voices")
 def voices_list():
-    return json.dumps(tts.speakers)
+	return json.dumps(tts.speakers)
+
+@app.route("/health-check")
+def tts_health_check():
+	gc.collect()
+	if request_count > 2048:
+		return f"EXPIRED: {request_count}", 500
+	return f"OK: {request_count}", 200
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5002)
+	if os.getenv('TTS_LD_LIBRARY_PATH', "") != "":
+		os.putenv('LD_LIBRARY_PATH', os.getenv('TTS_LD_LIBRARY_PATH'))
+	from waitress import serve
+	serve(app, host="0.0.0.0", port=5002)
