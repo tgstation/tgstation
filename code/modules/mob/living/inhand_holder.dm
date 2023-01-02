tgstation/tgstation

commit 63a9399650431f15abf43680d6924de51899822b

pAIs and other pets that can be picked up no longer know shadow clone jutsu. (#69625)
* Exiting as a held mob now immediately drops you no matter what.

code/modules/mob/living/inhand_holder.dm
@@ -88,6 +88,10 @@
 /obj/item/clothing/head/mob_holder/container_resist_act()
 	release()
 
+/obj/item/clothing/head/mob_holder/Exited(atom/movable/gone, direction)
+	if(held_mob && held_mob == gone)
+		release()
+
 /obj/item/clothing/head/mob_holder/on_found(mob/finder)
 	if(held_mob?.will_escape_storage())
 		to_chat(finder, span_warning("\A [held_mob.name] pops out! "))

commit 584f07f0a9921ca554c1675dd75f008462958767

Fixes wearable mob deafness bug (#72297)
## About The Pull Request

Fixes pAIs and simplemobs getting deaf when moving out of a headwear
slot. Bug introduced by #69625

## Why It's Good For The Game

Closes #70405
## Changelog
:cl:
fix: fixed pAIs and simplemobs getting deaf when moving out of a
headwear slot.
/:cl:

Co-authored-by: Fikou <23585223+Fikou@users.noreply.github.com>

code/modules/mob/living/inhand_holder.dm
@@ -89,6 +89,7 @@
 	release()
 
 /obj/item/clothing/head/mob_holder/Exited(atom/movable/gone, direction)
+	. = ..()
 	if(held_mob && held_mob == gone)
 		release()
 