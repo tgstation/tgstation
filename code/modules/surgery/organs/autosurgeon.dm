tgstation/tgstation

commit e0ad10462076a769e6eaf73a65699ba84d634a0c

Autosurgeon code improvement and light overlay (#68379)
Co-authored-by: Kylerace <kylerlumpkin1@gmail.com>

code/modules/surgery/organs/autosurgeon.dm
@@ -3,124 +3,166 @@
 /obj/item/autosurgeon
 	name = "autosurgeon"
 	desc = "A device that automatically inserts an implant, skillchip or organ into the user without the hassle of extensive surgery. \
-		It has a screwdriver slot for removing accidentally added items."
+		It has a slot to insert implants or organs and a screwdriver slot for removing accidentally added items."
 	icon = 'icons/obj/device.dmi'
-	icon_state = "autoimplanter"
+	icon_state = "autosurgeon"
 	inhand_icon_state = "nothing"
 	w_class = WEIGHT_CLASS_SMALL
 
+	/// How many times you can use the autosurgeon before it becomes useless
 	var/uses = INFINITE
+	/// What organ will the autosurgeon sub-type will start with. ie, CMO autosurgeon start with a medi-hud.
+	var/starting_organ
+	/// The organ currently loaded in the autosurgeon, ready to be implanted.
+	var/obj/item/organ/stored_organ
+	/// The list of organs and their children we allow into the autosurgeon. An empty list means no whitelist.
+	var/list/organ_whitelist = list()
+	/// The percentage modifier for how fast you can use the autosurgeon to implant other people.
+	var/surgery_speed = 1
+	/// The overlay that shows when the autosurgeon has an organ inside of it.
+	var/loaded_overlay = "autosurgeon_loaded_overlay"
 
 /obj/item/autosurgeon/attack_self_tk(mob/user)
 	return //stops TK fuckery
 
-/obj/item/autosurgeon/organ
-	name = "implant autosurgeon"
-	desc = "A device that automatically inserts an implant or organ into the user without the hassle of extensive surgery. \
-		It has a slot to insert implants or organs and a screwdriver slot for removing accidentally added items."
-
-	var/organ_type = /obj/item/organ
-	var/starting_organ
-	var/obj/item/organ/storedorgan
-	var/surgery_speed = 1
-
-/obj/item/autosurgeon/organ/syndicate
-	name = "suspicious implant autosurgeon"
-	icon_state = "syndicate_autoimplanter"
-	surgery_speed = 0.75
-
-/obj/item/autosurgeon/organ/Initialize(mapload)
+/obj/item/autosurgeon/Initialize(mapload)
 	. = ..()
 	if(starting_organ)
-		insert_organ(new starting_organ(src))
+		load_organ(new starting_organ(src))
 
-/obj/item/autosurgeon/organ/proc/insert_organ(obj/item/item)
-	storedorgan = item
-	item.forceMove(src)
-	name = "[initial(name)] ([storedorgan.name])"
+/obj/item/autosurgeon/update_overlays()
+	. = ..()
+	if(stored_organ)
+		. += loaded_overlay
+		. += emissive_appearance(icon, loaded_overlay)
 
-/obj/item/autosurgeon/organ/proc/use_autosurgeon()
-	storedorgan = null
-	name = initial(name)
-	if(uses != INFINITE)
-		uses--
-	if(!uses)
-		desc = "[initial(desc)] Looks like it's been used up."
+/obj/item/autosurgeon/proc/load_organ(obj/item/organ/loaded_organ, mob/living/user)
+	if(user)
+		if(stored_organ)
+			to_chat(user, span_alert("[src] already has an implant stored."))
+			return
 
-/obj/item/autosurgeon/organ/attack_self(mob/user)//when the object it used...
+		if(uses == 0)
+			to_chat(user, span_alert("[src] is used up and cannot be loaded with more implants."))
+			return
+
+		if(organ_whitelist.len)
+			var/organ_whitelisted
+			for(var/whitelisted_organ in organ_whitelist)
+				if(istype(loaded_organ, whitelisted_organ))
+					organ_whitelisted = TRUE
+					break
+			if(!organ_whitelisted)
+				to_chat(user, span_alert("[src] is not compatible with [loaded_organ]."))
+				return
+
+		if(!user.transferItemToLoc(loaded_organ, src))
+			to_chat(user, span_alert("[loaded_organ] is stuck to your hand!"))
+			return
+
+	stored_organ = loaded_organ
+	loaded_organ.forceMove(src)
+
+	name = "[initial(name)] ([stored_organ.name])" //to tell you the organ type, like "suspicious autosurgeon (Reviver implant)"
+	update_appearance()
+
+/obj/item/autosurgeon/proc/use_autosurgeon(mob/living/target, mob/living/user, implant_time)
+	if(!stored_organ)
+		to_chat(user, span_alert("[src] currently has no implant stored."))
+		return
+
 	if(!uses)
 		to_chat(user, span_alert("[src] has already been used. The tools are dull and won't reactivate."))
 		return
-	else if(!storedorgan)
-		to_chat(user, span_alert("[src] currently has no implant stored."))
-		return
-	storedorgan.Insert(user)//insert stored organ into the user
-	user.visible_message(span_notice("[user] presses a button on [src], and you hear a short mechanical noise."), span_notice("You feel a sharp sting as [src] plunges into your body."))
-	playsound(get_turf(user), 'sound/weapons/circsawhit.ogg', 50, TRUE)
-	use_autosurgeon()
 
-/obj/item/autosurgeon/organ/attackby(obj/item/weapon, mob/user, params)
-	if(istype(weapon, organ_type))
-		if(storedorgan)
-			to_chat(user, span_alert("[src] already has an implant stored."))
+	if(implant_time)
+		user.visible_message( "[user] prepares to use [src] on [target].", "You begin to prepare to use [src] on [target].")
+		if(!do_after(user, (8 SECONDS * surgery_speed), target))
 			return
-		else if(!uses)
-			to_chat(user, span_alert("[src] has already been used up."))
-			return
-		if(!user.transferItemToLoc(weapon, src))
-			return
-		storedorgan = weapon
-		to_chat(user, span_notice("You insert the [weapon] into [src]."))
+
+	if(target != user)
+		log_combat(user, target, "autosurgeon implanted [stored_organ] into", "[src]", "in [AREACOORD(target)]")
+		user.visible_message(span_notice("[user] presses a button on [src] as it plunges into [target]'s body."), span_notice("You press a button on [src] as it plunges into [target]'s body."))
+	else
+		user.visible_message(span_notice("[user] pressses a button on [src] as it plunges into [user.p_their()] body."), "You press a button on [src] as it plunges into your body.")
+
+	stored_organ.Insert(target)//insert stored organ into the user
+	stored_organ = null
+	name = initial(name) //get rid of the organ in the name
+	playsound(target.loc, 'sound/weapons/circsawhit.ogg', 50, vary = TRUE)
+	update_appearance()
+
+	if(uses)
+		uses--
+	if(uses == 0)
+		desc = "[initial(desc)] Looks like it's been used up."
+
+/obj/item/autosurgeon/attack_self(mob/user)//when the object it used...
+	use_autosurgeon(user, user)
+
+/obj/item/autosurgeon/attack(mob/living/target, mob/living/user, params)
+	add_fingerprint(user)
+	use_autosurgeon(target, user, 8 SECONDS)
+
+/obj/item/autosurgeon/attackby(obj/item/attacking_item, mob/user, params)
+	if(istype(attacking_item, /obj/item/organ))
+		load_organ(attacking_item, user)
 	else
 		return ..()
 
-/obj/item/autosurgeon/organ/attack(mob/living/target, mob/living/user, params)
-	add_fingerprint(user)
-	user.visible_message(
-		"[user] prepares to use [src] on [target].",
-		"You begin to prepare to use [src] on [target]."
-	)
-	if(!do_after(user, (8 SECONDS * surgery_speed), target))
-		return
-	user.visible_message(span_notice("[user] presses a button on [src], and you hear a short mechanical noise."), span_notice("You press a button on [src] as it plunges into [target]'s body."))
-	to_chat(target, span_notice("You feel a sharp sting as something plunges into your body!"))
-	playsound(get_turf(user), 'sound/weapons/circsawhit.ogg', 50, vary = TRUE)
-	storedorgan.Insert(target) //let's actually get the organ into the target, yeah?
-	use_autosurgeon()
 
-/obj/item/autosurgeon/organ/screwdriver_act(mob/living/user, obj/item/screwtool)
+
+/obj/item/autosurgeon/screwdriver_act(mob/living/user, obj/item/screwtool)
 	if(..())
 		return TRUE
-	if(!storedorgan)
+	if(!stored_organ)
 		to_chat(user, span_warning("There's no implant in [src] for you to remove!"))
 	else
 		var/atom/drop_loc = user.drop_location()
 		for(var/atom/movable/stored_implant as anything in src)
 			stored_implant.forceMove(drop_loc)
+			stored_organ = null
 
-		to_chat(user, span_notice("You remove the [storedorgan] from [src]."))
+		to_chat(user, span_notice("You remove the [stored_organ] from [src]."))
 		screwtool.play_tool_sound(src)
-		use_autosurgeon()
 	return TRUE
 
-/obj/item/autosurgeon/organ/cmo
+/obj/item/autosurgeon/medical_hud
+	name = "autosurgeon"
 	desc = "A single use autosurgeon that contains a medical heads-up display augment. A screwdriver can be used to remove it, but implants can't be placed back in."
 	uses = 1
 	starting_organ = /obj/item/organ/internal/cyberimp/eyes/hud/medical
 
-/obj/item/autosurgeon/organ/syndicate/laser_arm
+
+/obj/item/autosurgeon/syndicate
+	name = "suspicious autosurgeon"
+	icon_state = "autosurgeon_syndicate"
+	surgery_speed = 0.75
+	loaded_overlay = "autosurgeon_syndicate_loaded_overlay"
+
+/obj/item/autosurgeon/syndicate/laser_arm
 	desc = "A single use autosurgeon that contains a combat arms-up laser augment. A screwdriver can be used to remove it, but implants can't be placed back in."
 	uses = 1
 	starting_organ = /obj/item/organ/internal/cyberimp/arm/gun/laser
 
-/obj/item/autosurgeon/organ/syndicate/thermal_eyes
+/obj/item/autosurgeon/syndicate/thermal_eyes
 	starting_organ = /obj/item/organ/internal/eyes/robotic/thermals
 
-/obj/item/autosurgeon/organ/syndicate/xray_eyes
+/obj/item/autosurgeon/syndicate/xray_eyes
 	starting_organ = /obj/item/organ/internal/eyes/robotic/xray
 
-/obj/item/autosurgeon/organ/syndicate/anti_stun
+/obj/item/autosurgeon/syndicate/anti_stun
 	starting_organ = /obj/item/organ/internal/cyberimp/brain/anti_stun
 
-/obj/item/autosurgeon/organ/syndicate/reviver
+/obj/item/autosurgeon/syndicate/reviver
 	starting_organ = /obj/item/organ/internal/cyberimp/chest/reviver
+
+/obj/item/autosurgeon/syndicate/commsagent
+	desc = "A device that automatically - painfully - inserts an implant. It seems someone's specially \
+	modified this one to only insert... tongues. Horrifying."
+	starting_organ = /obj/item/organ/internal/tongue
+
+
+/obj/item/autosurgeon/syndicate/commsagent/Initialize(mapload)
+	. = ..()
+	organ_whitelist += /obj/item/organ/internal/tongue

code/game/objects/structures/crates_lockers/closets/secure/medical.dm
@@ -85,7 +85,7 @@
 	new /obj/item/healthanalyzer/advanced(src)
 	new /obj/item/assembly/flash/handheld(src)
 	new /obj/item/reagent_containers/hypospray/cmo(src)
-	new /obj/item/autosurgeon/organ/cmo(src)
+	new /obj/item/autosurgeon/medical_hud(src)
 	new /obj/item/door_remote/chief_medical_officer(src)
 	new /obj/item/clothing/neck/petcollar(src)
 	new /obj/item/pet_carrier(src)

code/modules/mapfluff/ruins/lavalandruin_code/syndicate_base.dm
@@ -24,11 +24,6 @@
 /obj/modular_map_root/syndicatebase
 	config_file = "strings/modular_maps/syndicatebase.toml"
 
-/obj/item/autosurgeon/organ/syndicate/commsagent
-	desc = "A device that automatically - painfully - inserts an implant. It seems someone's specially \
-	modified this one to only insert... tongues. Horrifying."
-	organ_type = /obj/item/organ/internal/tongue
-
 /obj/structure/closet/crate/secure/freezer/commsagent
 	name = "Assorted Tongues And Tongue Accessories"
 	desc = "Unearthing this was probably a mistake."
@@ -43,7 +38,7 @@
 	new /obj/item/organ/internal/tongue/robot(src) //DANGER! CRYSTAL HYPERSTRUCTURE-
 	new /obj/item/organ/internal/tongue/ethereal(src)
 	new /obj/item/organ/internal/tongue/tied(src)
-	new /obj/item/autosurgeon/organ/syndicate/commsagent(src)
+	new /obj/item/autosurgeon/syndicate/commsagent(src)
 	new	/obj/item/clothing/gloves/radio(src)
 
 /obj/machinery/power/supermatter_crystal/shard/syndicate

code/modules/surgery/organs/augments_internal.dm
@@ -172,10 +172,10 @@
 	desc = "A sleek, sturdy box."
 	icon_state = "cyber_implants"
 	var/list/boxed = list(
-		/obj/item/autosurgeon/organ/syndicate/thermal_eyes,
-		/obj/item/autosurgeon/organ/syndicate/xray_eyes,
-		/obj/item/autosurgeon/organ/syndicate/anti_stun,
-		/obj/item/autosurgeon/organ/syndicate/reviver)
+		/obj/item/autosurgeon/syndicate/thermal_eyes,
+		/obj/item/autosurgeon/syndicate/xray_eyes,
+		/obj/item/autosurgeon/syndicate/anti_stun,
+		/obj/item/autosurgeon/syndicate/reviver)
 	var/amount = 5
 
 /obj/item/storage/box/cyber_implants/PopulateContents()

code/modules/uplink/uplink_items/job.dm
@@ -209,7 +209,7 @@
 	desc = "An implant that grants you a recharging laser gun inside your arm. Weak to EMPs. Comes with a syndicate autosurgeon for immediate self-application."
 	progression_minimum = 30 MINUTES
 	cost = 10
-	item = /obj/item/autosurgeon/organ/syndicate/laser_arm
+	item = /obj/item/autosurgeon/syndicate/laser_arm
 	restricted_roles = list(JOB_ROBOTICIST, JOB_RESEARCH_DIRECTOR)
 
 /datum/uplink_item/role_restricted/chemical_gun

code/modules/uplink/uplink_items/nukeops.dm
@@ -741,7 +741,7 @@
 /datum/uplink_item/implants/antistun
 	name = "CNS Rebooter Implant"
 	desc = "This implant will help you get back up on your feet faster after being stunned. Comes with an autosurgeon."
-	item = /obj/item/autosurgeon/organ/syndicate/anti_stun
+	item = /obj/item/autosurgeon/syndicate/anti_stun
 	cost = 12
 	surplus = 0
 	purchasable_from = UPLINK_NUKE_OPS
@@ -767,7 +767,7 @@
 /datum/uplink_item/implants/reviver
 	name = "Reviver Implant"
 	desc = "This implant will attempt to revive and heal you if you lose consciousness. Comes with an autosurgeon."
-	item = /obj/item/autosurgeon/organ/syndicate/reviver
+	item = /obj/item/autosurgeon/syndicate/reviver
 	cost = 8
 	surplus = 0
 	purchasable_from = UPLINK_NUKE_OPS
@@ -775,7 +775,7 @@
 /datum/uplink_item/implants/thermals
 	name = "Thermal Eyes"
 	desc = "These cybernetic eyes will give you thermal vision. Comes with a free autosurgeon."
-	item = /obj/item/autosurgeon/organ/syndicate/thermal_eyes
+	item = /obj/item/autosurgeon/syndicate/thermal_eyes
 	cost = 8
 	surplus = 0
 	purchasable_from = UPLINK_NUKE_OPS
@@ -783,7 +783,7 @@
 /datum/uplink_item/implants/xray
 	name = "X-ray Vision Implant"
 	desc = "These cybernetic eyes will give you X-ray vision. Comes with an autosurgeon."
-	item = /obj/item/autosurgeon/organ/syndicate/xray_eyes
+	item = /obj/item/autosurgeon/syndicate/xray_eyes
 	cost = 10
 	surplus = 0
 	purchasable_from = UPLINK_NUKE_OPS

commit 3e94ec14b4a7233c752e9f970d1cd7c5816ff568

Removing an organ from a autosurgeon now correctly updates its overlays + 2 more bonus autosurgeon fixes (#72471)
Removing an organ from an auto-surgeon with a screwdriver didn't call
update_appearance() and thus it kept its overlay as if it was full.

Fixes single use autosurgeons not consuming uses when their implant is
removed, this was a bug introduced by #68379
Autosurgeons now actually tell you what organ was removed from them.

code/modules/surgery/organs/autosurgeon.dm
@@ -119,10 +119,15 @@
 		var/atom/drop_loc = user.drop_location()
 		for(var/atom/movable/stored_implant as anything in src)
 			stored_implant.forceMove(drop_loc)
+			to_chat(user, span_notice("You remove the [stored_organ] from [src]."))
 			stored_organ = null
 
-		to_chat(user, span_notice("You remove the [stored_organ] from [src]."))
 		screwtool.play_tool_sound(src)
+		if (uses)
+			uses--
+		if(!uses)
+			desc = "[initial(desc)] Looks like it's been used up."
+		update_appearance(UPDATE_ICON)
 	return TRUE
 
 /obj/item/autosurgeon/medical_hud