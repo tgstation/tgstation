tgstation/tgstation

commit 519bf698697c2f45cdcd9c2e5c186c36f9392daf

Dynamic Human Icon Generation For Simple/Basic Mobs (& Cardboard Cutouts) (#72517)
## About The Pull Request
revive of #68760
this time a proc, not an element
this time supports cardboard cutouts
this time supports mob corpses

![image](https://user-images.githubusercontent.com/23585223/211064291-81070650-189f-4afa-8116-81b687e3ea35.png)

## Why It's Good For The Game
prevents these icons ever being outdated, they'll always look what they
are supposed to, saves spriting work

## Changelog
:cl: Fikou, a hood by Viro
refactor: humanoid mobs and cardboard cutouts automatically generate
their sprites, they no longer will be outdated
/:cl:

Co-authored-by: Time-Green <timkoster1@hotmail.com>

code/modules/mod/mod_control.dm
@@ -417,9 +417,11 @@
 	if(wearer.stat < UNCONSCIOUS && prob(10))
 		wearer.emote("scream")
 
+/obj/item/mod/control/visual_equipped(mob/user, slot, initial = FALSE)
+	if(slot & slot_flags)
+		set_wearer(user)
+
 /obj/item/mod/control/on_outfit_equip(mob/living/carbon/human/outfit_wearer, visuals_only, item_slot)
-	if(visuals_only)
-		set_wearer(outfit_wearer) //we need to set wearer manually since it doesnt call equipped
 	quick_activation()
 
 /obj/item/mod/control/doStrip(mob/stripper, mob/owner)

_maps/map_files/generic/CentCom.dmm
@@ -5958,10 +5958,7 @@
 /area/centcom/central_command_areas/evacuation)
 "Cg" = (
 /obj/item/cardboard_cutout{
-	desc = "They seem to be ignoring you... Typical.";
-	dir = 1;
-	icon_state = "cutout_ntsec";
-	name = "Private Security Officer"
+	starting_cutout = "Private Security Officer"
 	},
 /obj/effect/turf_decal/tile/red{
 	dir = 1
@@ -8855,9 +8852,7 @@
 /area/centcom/central_command_areas/admin)
 "UA" = (
 /obj/item/cardboard_cutout{
-	desc = "They seem to be ignoring you... Typical.";
-	icon_state = "cutout_ntsec";
-	name = "Private Security Officer"
+	starting_cutout = "Private Security Officer"
 	},
 /obj/effect/turf_decal/tile/neutral/fourcorners,
 /turf/open/floor/iron/dark,

_maps/shuttles/whiteship_box.dmm
@@ -301,7 +301,7 @@
 	melee_damage_lower = 11;
 	melee_damage_upper = 11;
 	name = "Rotting Carcass";
-	zombiejob = "Assistant"
+	outfit = /datum/outfit/corpse_assistant
 	},
 /turf/open/floor/iron/white/corner{
 	dir = 1
@@ -1546,7 +1546,7 @@
 	melee_damage_lower = 11;
 	melee_damage_upper = 11;
 	name = "Rotting Carcass";
-	zombiejob = "Assistant"
+	outfit = /datum/outfit/corpse_assistant
 	},
 /turf/open/floor/iron,
 /area/shuttle/abandoned/medbay)

_maps/shuttles/whiteship_personalshuttle.dmm
@@ -74,7 +74,7 @@
 	melee_damage_lower = 11;
 	melee_damage_upper = 11;
 	name = "Rotting Carcass";
-	zombiejob = "Assistant"
+	outfit = /datum/outfit/corpse_assistant
 	},
 /turf/open/floor/plating,
 /area/shuttle/abandoned/engine)
@@ -299,7 +299,7 @@
 	melee_damage_lower = 11;
 	melee_damage_upper = 11;
 	name = "Rotting Carcass";
-	zombiejob = "Assistant"
+	outfit = /datum/outfit/corpse_assistant
 	},
 /turf/open/floor/mineral/titanium,
 /area/shuttle/abandoned/engine)
@@ -531,7 +531,7 @@
 	melee_damage_lower = 11;
 	melee_damage_upper = 11;
 	name = "Rotting Carcass";
-	zombiejob = "Assistant"
+	outfit = /datum/outfit/corpse_assistant
 	},
 /turf/open/floor/mineral/plastitanium,
 /area/shuttle/abandoned/bridge)

_maps/templates/holodeck_spacecheckers.dmm
@@ -3,10 +3,7 @@
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "c" = (
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_greytide";
-	name = "White Pawn"
-	},
+/obj/structure/chess/checker/whiteman,
 /turf/open/floor/holofloor/dark,
 /area/template_noop)
 "d" = (
@@ -19,37 +16,13 @@
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "g" = (
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_clown";
-	name = "White King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_clown";
-	name = "White King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_clown";
-	name = "White King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_clown";
-	name = "White King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_clown";
-	name = "White King"
-	},
 /obj/effect/turf_decal/board_letter/h{
 	dir = 1
 	},
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "i" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /obj/effect/turf_decal/board_letter/f{
 	dir = 1
 	},
@@ -81,11 +54,16 @@
 	},
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
+"q" = (
+/obj/structure/chess/checker/whiteking,
+/obj/structure/chess/checker/whiteking,
+/obj/structure/chess/checker/whiteking,
+/obj/structure/chess/checker/whiteking,
+/obj/structure/chess/checker/whiteking,
+/turf/open/floor/holofloor/plating,
+/area/template_noop)
 "r" = (
-/obj/item/cardboard_cutout/adaptive{
-	icon_state = "cutout_greytide";
-	name = "White Pawn"
-	},
+/obj/structure/chess/checker/whiteman,
 /obj/effect/turf_decal/board_number/two{
 	dir = 4
 	},
@@ -122,31 +100,11 @@
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "z" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_clown";
-	name = "Black King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_clown";
-	name = "Black King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_clown";
-	name = "Black King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_clown";
-	name = "Black King"
-	},
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_clown";
-	name = "Black King"
-	},
+/obj/structure/chess/checker/blackking,
+/obj/structure/chess/checker/blackking,
+/obj/structure/chess/checker/blackking,
+/obj/structure/chess/checker/blackking,
+/obj/structure/chess/checker/blackking,
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "A" = (
@@ -158,11 +116,7 @@
 	},
 /area/template_noop)
 "B" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /obj/effect/turf_decal/board_letter/h{
 	dir = 1
 	},
@@ -223,19 +177,11 @@
 /turf/open/floor/holofloor/dark,
 /area/template_noop)
 "M" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /turf/open/floor/holofloor/dark,
 /area/template_noop)
 "N" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /obj/effect/turf_decal/board_number/six{
 	dir = 4
 	},
@@ -256,22 +202,14 @@
 /turf/open/floor/holofloor/plating,
 /area/template_noop)
 "T" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /obj/effect/turf_decal/board_letter/b{
 	dir = 1
 	},
 /turf/open/floor/holofloor/dark,
 /area/template_noop)
 "V" = (
-/obj/item/cardboard_cutout/adaptive{
-	color = "#9999BB";
-	icon_state = "cutout_greytide";
-	name = "Black Pawn"
-	},
+/obj/structure/chess/checker/blackman,
 /obj/effect/turf_decal/board_letter/d{
 	dir = 1
 	},
@@ -309,7 +247,7 @@
 /area/template_noop)
 
 (1,1,1) = {"
-a
+z
 Y
 w
 G
@@ -318,7 +256,7 @@ k
 K
 v
 x
-a
+q
 "}
 (2,1,1) = {"
 a
@@ -405,7 +343,7 @@ c
 P
 "}
 (9,1,1) = {"
-z
+a
 B
 n
 N

_maps/templates/shelter_2.dmm
@@ -74,9 +74,7 @@
 "o" = (
 /obj/structure/chair/comfy/black,
 /obj/item/cardboard_cutout{
-	desc = "A cardboard cutout of a xenomorph maid.";
-	icon_state = "cutout_lusty";
-	name = "lusty xenomorph maid"
+	starting_cutout = "Xenomorph Maid"
 	},
 /obj/structure/window/reinforced/survival_pod{
 	dir = 1

code/__DEFINES/mobs.dm
@@ -771,3 +771,6 @@
 /// Checking flags for [/mob/proc/can_read()]
 #define READING_CHECK_LITERACY (1<<0)
 #define READING_CHECK_LIGHT (1<<1)
+
+/// In dynamic human icon gen we don't replace the held item.
+#define NO_REPLACE 0

code/__HELPERS/dynamic_human_icon_gen.dm
@@ -1,0 +1,60 @@
+///Global list of all dynamically generated icons, for caching, so we don't have to generate multiple times.
+GLOBAL_LIST_EMPTY(dynamic_human_icons)
+
+///This proc takes an amount of arguments and creates a human dummy using the paths, for visuals you want!
+/proc/get_dynamic_human_icon(outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE, list/generated_dirs = GLOB.cardinals, animated = TRUE)
+	if(!species_path)
+		return FALSE
+	var/arg_string = "[outfit_path]_[species_path]_[mob_spawn_path]_[l_hand]_[r_hand]_[bloody_slots]_[generated_dirs.Join(",")]"
+	if(GLOB.dynamic_human_icons[arg_string]) //if already exists in our cache, just return that
+		return GLOB.dynamic_human_icons[arg_string]
+	var/mob/living/carbon/human/dummy/consistent/dummy = new()
+	dummy.set_species(species_path)
+	dummy.stat = DEAD //this is to avoid side effects of mob spawners
+	dummy.underwear = "Nude"
+	dummy.undershirt = "Nude"
+	dummy.socks = "Nude"
+	if(outfit_path)
+		var/datum/outfit/outfit = new outfit_path()
+		if(r_hand != NO_REPLACE) //we can still override to be null, no replace means just use outfit's
+			outfit.r_hand = r_hand
+		if(l_hand != NO_REPLACE)
+			outfit.l_hand = l_hand
+		dummy.equipOutfit(outfit)
+	else if(mob_spawn_path)
+		var/obj/effect/mob_spawn/spawner = new mob_spawn_path(null, TRUE)
+		spawner.outfit_override = list()
+		if(r_hand != NO_REPLACE)
+			spawner.outfit_override["r_hand"] = r_hand
+		if(l_hand != NO_REPLACE)
+			spawner.outfit_override["l_hand"] = l_hand
+		spawner.special(dummy, dummy)
+		spawner.equip(dummy)
+	for(var/obj/item/carried_item in dummy)
+		if(dummy.is_holding(carried_item))
+			var/datum/component/two_handed/twohanded = carried_item.GetComponent(/datum/component/two_handed)
+			if(twohanded)
+				twohanded.wield(dummy)
+			var/datum/component/transforming/transforming = carried_item.GetComponent(/datum/component/transforming)
+			if(transforming)
+				transforming.set_active(carried_item)
+		if(bloody_slots & carried_item.slot_flags)
+			carried_item.add_mob_blood(dummy)
+	dummy.update_held_items()
+	var/icon/output = icon('icons/blanks/32x32.dmi', "nothing")
+	for(var/direction in generated_dirs)
+		var/icon/partial = getFlatIcon(dummy, defdir = direction, no_anim = !animated)
+		output.Insert(partial, dir = direction)
+	GLOB.dynamic_human_icons[arg_string] = output
+	qdel(dummy)
+	return output
+
+///This exists to apply the icons async, as that cannot be done in Initialize because of possible sleeps.
+/proc/apply_dynamic_human_icon(atom/target, outfit_path, species_path = /datum/species/human, mob_spawn_path, r_hand, l_hand, bloody_slots = NONE, generated_dirs = GLOB.cardinals)
+	INVOKE_ASYNC(GLOBAL_PROC, GLOBAL_PROC_REF(set_dynamic_human_icon), args)
+
+///This proc gets an argument of a target and runs
+/proc/set_dynamic_human_icon(list/arguments)
+	var/atom/target = arguments[1] //1st argument is the target
+	var/icon/dynamic_icon = get_dynamic_human_icon(arglist(arguments.Copy(2))) //the rest of the arguments starting from 2 matter to the proc
+	target.icon = dynamic_icon

commit 54eae1e713f6c2971d42cc7c4117977bdac542c7

Fix signal override merge skew (#72882)
Caused by https://github.com/tgstation/tgstation/pull/72517, with
https://github.com/tgstation/tgstation/pull/72561 raising an error that
was being hidden before

This changes equip logic somewhat so that set_wearer was being called
twice.

I don't think this should be being run in visualsOnly at all, as it does
a ton of stuff like registering signals, updating UI, etc etc etc. Don't
have time to investigate further.

code/modules/mod/mod_control.dm
@@ -446,6 +446,15 @@
 	return ..()
 
 /obj/item/mod/control/proc/set_wearer(mob/living/carbon/human/user)
+	if (wearer == user)
+		// This should also not happen.
+		// This path is hit when equipping an outfit with visualsOnly, but only sometimes, and this eventually gets called twice.
+		// I'm not sure this proc should ever be being called by visualsOnly, but it is,
+		// and this was an emergency patch.
+		return
+	else if (!isnull(wearer))
+		stack_trace("set_wearer() was called with a new wearer without unset_wearer() being called")
+
 	wearer = user
 	SEND_SIGNAL(src, COMSIG_MOD_WEARER_SET, wearer)
 	RegisterSignal(wearer, COMSIG_ATOM_EXITED, PROC_REF(on_exit))

code/modules/unit_tests/_unit_tests.dm
@@ -174,6 +174,7 @@
 #include "say.dm"
 #include "screenshot_antag_icons.dm"
 #include "screenshot_basic.dm"
+#include "screenshot_dynamic_human_icons.dm"
 #include "screenshot_humanoids.dm"
 #include "screenshot_husk.dm"
 #include "screenshot_saturnx.dm"

code/modules/unit_tests/screenshot_dynamic_human_icons.dm
@@ -1,0 +1,7 @@
+/// A screenshot test for specific dynamic human icons
+/datum/unit_test/screenshot_dynamic_human_icons
+
+/datum/unit_test/screenshot_dynamic_human_icons/Run()
+	// Complicated MODsuit setup
+	var/icon/icon = get_dynamic_human_icon(/datum/outfit/syndicatecommandocorpse)
+	test_screenshot("syndicate_commando", icon)