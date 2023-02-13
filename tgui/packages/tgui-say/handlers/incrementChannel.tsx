669c94ccef28192b9b708943d142e0dbc507a920
Kubisopplay <38842052+Kubisopplay@users.noreply.github.com>
2023-02-13 02:53:11 +0100

    General refactor?
    Im going to sleep

--- tgui/packages/tgui-say/handlers/incrementChannel.tsx
+++ tgui/packages/tgui-say/handlers/incrementChannel.tsx
@@ -1,54 +1,49 @@
 import { CHANNELS } from '../constants';
 import { Modal } from '../types';
-const ADMIN_CHANNEL = CHANNELS.findIndex(
-  (channel) => channel === 'Admin'
-) as number;
+// Insert the names of channels you want to not cycle on tab here
+const BLACKLIST = ['Admin'];
+const BLACKLISTED_CHANNELS = [] as number[];
+CHANNELS.forEach((channel, index) => {
+  if (BLACKLIST.includes(channel)) {
+    BLACKLISTED_CHANNELS.push(index);
+  }
+});
+
 /**
  * Increments the channel or resets to the beginning of the list.
  * If the user switches between IC/OOC, messages Byond to toggle thinking
  * indicators.
  */
 export const handleIncrementChannel = function (this: Modal) {
-  const { channel } = this.state;
+  let { channel } = this.state;
   const { radioPrefix } = this.fields;
   if (radioPrefix === ':b ') {
     this.timers.channelDebounce({ mode: true });
   }
-  if (channel === ADMIN_CHANNEL) {
+  this.fields.radioPrefix = '';
+  if (BLACKLISTED_CHANNELS.includes(channel)) {
+    return;
+  }
+  if (BLACKLISTED_CHANNELS.length === CHANNELS.length) {
     this.setState({
       buttonContent: CHANNELS[channel],
       channel: channel,
     });
     return;
   }
-  this.fields.radioPrefix = '';
-  if (
-    channel === CHANNELS.length - 1 ||
-    (channel === ADMIN_CHANNEL - 1 && CHANNELS.length - 1 === ADMIN_CHANNEL)
-  ) {
-    this.timers.channelDebounce({ mode: true });
-    this.setState({
-      buttonContent: CHANNELS[0],
-      channel: 0,
-    });
-  } else {
-    if (channel === 2) {
-      // Disables thinking indicator for OOC channel
-      this.timers.channelDebounce({ mode: false });
+  do {
+    channel++;
+    if (channel === CHANNELS.length) {
+      this.timers.channelDebounce({ mode: true });
+      channel = 0;
     }
-    if (
-      channel === ADMIN_CHANNEL - 1 &&
-      CHANNELS.length - 1 !== ADMIN_CHANNEL
-    ) {
-      this.setState({
-        buttonContent: CHANNELS[channel + 2],
-        channel: channel + 2,
-      });
-    } else {
-      this.setState({
-        buttonContent: CHANNELS[channel + 1],
-        channel: channel + 1,
-      });
-    }
+  } while (BLACKLISTED_CHANNELS.includes(channel));
+  if (channel === CHANNELS.indexOf('OOC')) {
+    // Disables thinking indicator for OOC channel
+    this.timers.channelDebounce({ mode: false });
   }
+  this.setState({
+    buttonContent: CHANNELS[channel],
+    channel: channel,
+  });
 };
