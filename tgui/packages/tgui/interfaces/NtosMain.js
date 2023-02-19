dc8db27fa4322e95b9c283f1be6b405208bcdc97
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2023-02-19 05:50:40 +0000

    Fixes NtosNet Downloader & App themes (#73500)
    ## About The Pull Request
    
    NtOS' downloading app is the only application where PC device theme is
    actually used by the app directly, instead of handled by NtOS Window. I
    forgot about this, and removed it from the data, breaking the TGUI page.
    
    Also two edits;
    - NtosWindow now takes their theme directly from PC_device_theme, this
    means we're not relying anymore on apps to individually give the theme
    they want to have, and tablets will get alerts of downloading illegal
    programs if they are on any theme but the syndicate one (instead of not
    getting the message if theyre on any but the nt one).
    - Removes bad uses of ``as anything`` on stored files, because it holds
    more than just programs (like ordnance data).
    
    ## Why It's Good For The Game
    
    the NtOS app now works again, sorry for the issue.
    
    Closes https://github.com/tgstation/tgstation/issues/73493
    Closes https://github.com/tgstation/tgstation/issues/73507
    
    ## Changelog
    
    :cl:
    fix: NtOS program downloader now works again, and will now properly
    alert you of downloading illegal ROMs if you change your theme to
    anything that isn't Syndicate.
    fix: NtOS themes are now recognized by all windows.
    /:cl:

--- tgui/packages/tgui/interfaces/NtosMain.js
+++ tgui/packages/tgui/interfaces/NtosMain.js
@@ -5,7 +5,7 @@ import { NtosWindow } from '../layouts';
 export const NtosMain = (props, context) => {
   const { act, data } = useBackend(context);
   const {
-    device_theme,
+    PC_device_theme,
     show_imprint,
     programs = [],
     has_light,
@@ -22,9 +22,9 @@ export const NtosMain = (props, context) => {
   return (
     <NtosWindow
       title={
-        (device_theme === 'syndicate' && 'Syndix Main Menu') || 'NtOS Main Menu'
+        (PC_device_theme === 'syndicate' && 'Syndix Main Menu') ||
+        'NtOS Main Menu'
       }
-      theme={device_theme}
       width={400}
       height={500}>
       <NtosWindow.Content scrollable>
