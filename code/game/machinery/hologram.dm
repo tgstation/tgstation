tgstation/tgstation

commit cf02f622986932af8fb09e48cbdf5ec0ac567cf5

useless update_appearance reduction, emissive_blocker micro optimization (saves a second of init) (#71658)
## About The Pull Request

[Saves 0.2 seconds of init time. 50% of emissive
blockers](https://github.com/tgstation/tgstation/commit/8318b648f6d32844aacbfb4c309152cd45801f5c)

Emissive blockers are a decent expense during init, even these, which
are the ones that update outside of initialize.
I've inlined them, removed some redundant vars and checks, reduced the
arg count, and shifted some things around. This ends up saving 200ms, or
50% of its total cost.

I also shifted mutable_appearance about a bit. it's not a massive
saving, but it is technically faster

[Prevents a few redundant appearance_updates, saves 0.8 seconds of
init](https://github.com/tgstation/tgstation/commit/5475cd778b66b22b1e2c8d86b2c6d59fb84f219a)

Prequisit info: update_appearance is decently expensive
It's good then to only do it if we have a reason to, right?

Me and moth were shooting the shit about just general init time, and we
came up with the idea of tracking which update_appearances actually
"worked" and which didn't.

That bit comes later, let's enjoy the fruits of that work first

First, holograms were calling update_appearance on process, for almost
no reason.
I patched the one event they don't already "react" to, and then locked
it behind a change decection if.
good for live, doesn't impact init.

Next, decals. If you add a decal to something before it inits, it'll
react to the after successful init signal.
The trouble is the same atom could have its appearance updated from this
MORE then once, since decals can be stacked on tiles, and signal
unregisters don't work once the signal is sent.
So we add a flag to track if we've had this happen to us or not, so it
only happens once.
saves 80 ms

Power! lots of things call power_change on init, often more then once.
We'll update appearance for each of those calls, even if only one is an

code/game/machinery/hologram.dm
@@ -438,6 +438,7 @@ Possible to do for anyone motivated enough:
 		if(!LAZYLEN(holo_calls))
 			set_can_hear_flags(CAN_HEAR_ACTIVE_HOLOCALLS, FALSE)
 
+	update_appearance(UPDATE_ICON_STATE)
 	return TRUE
 
 /**
@@ -487,7 +488,7 @@ Possible to do for anyone motivated enough:
 	if(outgoing_call)
 		outgoing_call.Check()
 
-	ringing = FALSE
+	var/are_ringing = FALSE
 
 	for(var/datum/holocall/holocall as anything in holo_calls)
 		if(holocall.connected_holopad == src)
@@ -503,9 +504,11 @@ Possible to do for anyone motivated enough:
 			holocall.Disconnect(src)//can't answer calls while calling
 		else
 			playsound(src, 'sound/machines/twobeep.ogg', 100) //bring, bring!
-			ringing = TRUE
+			are_ringing = TRUE
 
-	update_appearance(UPDATE_ICON_STATE)
+	if(ringing != are_ringing)
+		update_appearance(UPDATE_ICON_STATE)
+		ringing = are_ringing
 
 /obj/machinery/holopad/proc/activate_holo(mob/living/user)
 	var/mob/living/silicon/ai/AI = user

code/__DEFINES/appearance.dm
@@ -1,0 +1,7 @@
+// wrapper for update_appearance that lets me capture it in debug mode
+// Does not work for callbacks unfortunately, not sure how to feel about that
+// This only works if definitions of the proc get changed to _update_appearance with REGEX
+// See the _compile_options comment for the regexes to use for that
+#ifdef APPEARANCE_SUCCESS_TRACKING
+#define update_appearance(arguments...) wrap_update_appearance(__FILE__, __LINE__, ##arguments)
+#endif

code/__DEFINES/flags.dm
@@ -56,8 +56,13 @@ GLOBAL_LIST_INIT(bitflags, list(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 204
 #define IS_PLAYER_COLORABLE_1 (1<<17)
 /// Whether or not this atom has contextual screentips when hovered OVER
 #define HAS_CONTEXTUAL_SCREENTIPS_1 (1<<18)
-// Whether or not this atom is storing contents for a disassociated storage object
+/// Whether or not this atom is storing contents for a disassociated storage object
 #define HAS_DISASSOCIATED_STORAGE_1 (1<<19)
+/// If this atom has experienced a decal element "init finished" sourced appearance update
+/// We use this to ensure stacked decals don't double up appearance updates for no rasin
+/// Flag as an optimization, don't make this a trait without profiling
+/// Yes I know this is a stupid flag, no you can't take him from me
+#define DECAL_INIT_UPDATE_EXPERIENCED_1 (1<<20)
 
 // Update flags for [/atom/proc/update_appearance]
 /// Update the atom's name

code/__DEFINES/stat_tracking.dm
@@ -10,23 +10,29 @@
 	STAT_ENTRY[STAT_ENTRY_TIME] += STAT_TIME;\
 	STAT_ENTRY[STAT_ENTRY_COUNT] += 1;
 
-// Cost tracking macros, to be used in one proc
-// The static lists are under the assumption that costs and counting are global lists, and will therefor
-// Break during world init
+// Cost tracking macros, to be used in one proc. If you're using this raw you'll want to use global lists
+// If you don't you'll need another way of reading it
 #define INIT_COST(costs, counting) \
 	var/list/_costs = costs; \
 	var/list/_counting = counting; \
 	var/usage = TICK_USAGE;
 
-// Cost tracking macro for global lists, prevents erroring if GLOB has not yet been initialized
-#define INIT_COST_GLOBAL(costs, counting) \
+// STATIC cost tracking macro. Uses static lists instead of the normal global ones
+// Good for debug stuff, and for running before globals init
+#define INIT_COST_STATIC(...) \
 	var/static/list/hidden_static_list_for_fun1 = list(); \
 	var/static/list/hidden_static_list_for_fun2 = list(); \
+	INIT_COST(hidden_static_list_for_fun1, hidden_static_list_for_fun2)
+
+// Cost tracking macro for global lists, prevents erroring if GLOB has not yet been initialized
+#define INIT_COST_GLOBAL(costs, counting) \
+	INIT_COST_STATIC() \
 	if(GLOB){\
 		costs = hidden_static_list_for_fun1; \
 		counting = hidden_static_list_for_fun2 ; \
 	} \
-	INIT_COST(hidden_static_list_for_fun1, hidden_static_list_for_fun2)
+	usage = TICK_USAGE;
+
 
 #define SET_COST(category) \
 	do { \
@@ -38,7 +44,10 @@
 
 #define SET_COST_LINE(...) SET_COST("[__LINE__]")
 
-#define EXPORT_STATS_TO_FILE_LATER(filename, costs, counts) \
+#define EXPORT_STATS_TO_JSON_LATER(filename, costs, counts) EXPORT_STATS_TO_FILE_LATER(filename, costs, counts, stat_tracking_export_to_json_later)
+#define EXPORT_STATS_TO_CSV_LATER(filename, costs, counts) EXPORT_STATS_TO_FILE_LATER(filename, costs, counts, stat_tracking_export_to_csv_later)
+
+#define EXPORT_STATS_TO_FILE_LATER(filename, costs, counts, proc) \
 	do { \
 		var/static/last_export = 0; \
 		if (world.time - last_export > 1.1 SECONDS) { \
@@ -46,7 +55,7 @@
 			/* spawn() is used here because this is often used to track init times, where timers act oddly. */ \
 			/* I was making timers and even after init times were complete, the timers didn't run :shrug: */ \
 			spawn (1 SECONDS) { \
-				stat_tracking_export_to_file_later(filename, costs, counts); \
+				##proc(filename, costs, counts); \
 			} \
 		} \
 	} while (FALSE);

code/__HELPERS/_planes.dm
@@ -47,6 +47,9 @@
 			if(_our_turf){\
 				thing.plane = GET_NEW_PLANE(_cached_plane, GET_Z_PLANE_OFFSET(_our_turf.z));\
 			}\
+			else {\
+				thing.plane = new_value;\
+			}\
 		}\
 		else {\
 			thing.plane = new_value;\

code/__HELPERS/icon_smoothing.dm
@@ -127,6 +127,12 @@ DEFINE_BITFIELD(smoothing_junction, list(
 		CRASH("smooth_icon called for [src] with smoothing_flags == [smoothing_flags]")
 	SEND_SIGNAL(src, COMSIG_ATOM_SMOOTHED_ICON)
 
+// As a rule, movables will most always care about smoothing changes
+// Turfs on the other hand, don't, so we don't do the update for THEM unless they explicitly request it
+/atom/movable/smooth_icon()
+	. = ..()
+	update_appearance(~UPDATE_SMOOTHING)
+
 /atom/proc/corners_diagonal_smooth(adjacencies)
 	switch(adjacencies)
 		if(NORTH_JUNCTION|WEST_JUNCTION)

code/__HELPERS/lighting.dm
@@ -1,8 +1,28 @@
 /// Produces a mutable appearance glued to the [EMISSIVE_PLANE] dyed to be the [EMISSIVE_COLOR].
 /proc/emissive_appearance(icon, icon_state = "", atom/offset_spokesman, layer = FLOAT_LAYER, alpha = 255, appearance_flags = NONE, offset_const)
-	var/mutable_appearance/appearance = mutable_appearance(icon, icon_state, layer, offset_spokesman, EMISSIVE_PLANE, alpha, appearance_flags | EMISSIVE_APPEARANCE_FLAGS, offset_const)
+	// Note: alpha doesn't "do" anything, since it's overriden by the color set shortly after
+	// Consider removing it someday? (I wonder if we made emissives blend right we could make alpha actually matter. dreams man, dreams)
+	var/mutable_appearance/appearance = mutable_appearance(icon, icon_state, layer, offset_spokesman, EMISSIVE_PLANE, 255, appearance_flags | EMISSIVE_APPEARANCE_FLAGS, offset_const)
 	appearance.color = GLOB.emissive_color
 	return appearance
+
+// This is a semi hot proc, so we micro it. saves maybe 150ms
+// sorry :)
+/proc/fast_emissive_blocker(atom/make_blocker)
+	// Note: alpha doesn't "do" anything, since it's overriden by the color set shortly after
+	// Consider removing it someday?
+	var/mutable_appearance/blocker = new()
+	blocker.icon = make_blocker.icon
+	blocker.icon_state = make_blocker.icon_state
+	blocker.layer = make_blocker.layer
+	blocker.appearance_flags |= make_blocker.appearance_flags | EMISSIVE_APPEARANCE_FLAGS
+	blocker.dir = make_blocker.dir
+	blocker.color = GLOB.em_block_color
+
+	// Note, we are ok with null turfs, that's not an error condition we'll just default to 0, the error would be
+	// Not passing ANYTHING in, key difference
+	SET_PLANE_EXPLICIT(blocker, EMISSIVE_PLANE, make_blocker)
+	return blocker
 
 /// Produces a mutable appearance glued to the [EMISSIVE_PLANE] dyed to be the [EM_BLOCK_COLOR].
 /proc/emissive_blocker(icon, icon_state = "", atom/offset_spokesman, layer = FLOAT_LAYER, alpha = 255, appearance_flags = NONE, offset_const)

code/__HELPERS/stat_tracking.dm
@@ -1,3 +1,4 @@
+// For use with the stopwatch defines
 /proc/render_stats(list/stats, user, sort = GLOBAL_PROC_REF(cmp_generic_stat_item_time))
 	sortTim(stats, sort, TRUE)
 
@@ -12,7 +13,8 @@
 
 	. = lines.Join("\n")
 
-/proc/stat_tracking_export_to_file_later(filename, costs, counts)
+// For use with the set_cost defines
+/proc/stat_tracking_export_to_json_later(filename, costs, counts)
 	if (IsAdminAdvancedProcCall())
 		return
 
@@ -25,3 +27,15 @@
 		)
 
 	rustg_file_write(json_encode(output), "[GLOB.log_directory]/[filename]")
+
+/proc/stat_tracking_export_to_csv_later(filename, costs, counts)
+	if (IsAdminAdvancedProcCall())
+		return
+
+	var/list/output = list()
+
+	output += "key, cost, count"
+	for (var/key in costs)
+		output += "[replacetext(key, ",", "")], [replacetext(costs[key], ",", "")], [replacetext(counts[key], ",", "")]"
+
+	rustg_file_write(output.Join("\n"), "[GLOB.log_directory]/[filename]")

commit 38f6e54642a27a71af1e15d600978c7f1e693477

Fixes holopads using incorrect icon states (#73529)
## About The Pull Request

Fixes two bugs with holopads. Holopads were permanently entering a
"ringing" state after being called once and they were using the on call
icon when they were supposed to be ringing. Issues seemed to be caused
by #71658 setting a var and updating the icon state in the wrong order.
## Why It's Good For The Game

Bug fix and you cant call every holopad on the station to make them
blink forever anymore.
## Changelog
:cl:
fix: Holopads will blink when being called rather than just glowing.
fix: Holopads will not blink forever if you missed a call.
/:cl:

code/game/machinery/hologram.dm
@@ -514,8 +514,8 @@ Possible to do for anyone motivated enough:
 			are_ringing = TRUE
 
 	if(ringing != are_ringing)
-		update_appearance(UPDATE_ICON_STATE)
 		ringing = are_ringing
+		update_appearance(UPDATE_ICON_STATE)
 
 /obj/machinery/holopad/proc/activate_holo(mob/living/user)
 	var/mob/living/silicon/ai/AI = user