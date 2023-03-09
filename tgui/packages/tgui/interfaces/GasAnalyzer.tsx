bfd1286eb141eacd863981df10b04ffce1d492ad
larentoun <larentoun@gmail.com>
2023-03-09 19:02:47 +0300

    feat: Gas Analyzer TGUI auto-updates
    fix: Gas Analyzer auto-update button only on Tool
    
    ref: removing unused var
    
    fix: grammar fix

--- tgui/packages/tgui/interfaces/GasAnalyzer.tsx
+++ tgui/packages/tgui/interfaces/GasAnalyzer.tsx
@@ -1,12 +1,14 @@
 import { useBackend } from '../backend';
 import { GasmixParser } from './common/GasmixParser';
 import type { Gasmix } from './common/GasmixParser';
+import { Box, Button } from '../components';
 import { AtmosHandbookContent, atmosHandbookHooks } from './common/AtmosHandbook';
 import { Window } from '../layouts';
 import { Section } from '../components';
 
 export type GasAnalyzerData = {
   gasmixes: Gasmix[];
+  autoUpdating: boolean;
 };
 
 export const GasAnalyzerContent = (props, context) => {
@@ -30,9 +32,24 @@ export const GasAnalyzerContent = (props, context) => {
 };
 
 export const GasAnalyzer = (props, context) => {
+  const { act, data } = useBackend<GasAnalyzerData>(context);
+  const { autoUpdating } = data;
   return (
     <Window width={500} height={450}>
       <Window.Content scrollable>
+        <Button
+          icon={autoUpdating ? 'sync-alt' : 'times'}
+          content={
+            autoUpdating
+              ? 'Auto-updating is enabled. Click to switch.'
+              : 'Auto-updating is disabled. Click to switch.'
+          }
+          onClick={() => act('autoscantoggle')}
+          fluid
+          textAlign="center"
+          selected={autoUpdating}
+        />
+        <Box color="bad" />
         <GasAnalyzerContent />
       </Window.Content>
     </Window>
