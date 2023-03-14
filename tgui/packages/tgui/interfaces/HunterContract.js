a46f05dfd0909579376bee03acc7643e015d9e08
SuperSlayer <91609255+SuperSlayer0@users.noreply.github.com>
2023-03-14 12:31:11 +0200

    govno

---
+++ tgui/packages/tgui/interfaces/HunterContract.js
@@ -0,0 +1,110 @@
+import { useBackend } from '../../tgui/backend';
+import { Box, Button, Section, Stack, Icon } from '../../tgui/components';
+import { Window } from '../../tgui/layouts';
+
+const HunterObjectives = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { objectives = [], all_completed, rabbits_found, used_up } = data;
+  return (
+    <Stack vertical fill>
+      <Stack.Item grow>
+        <Section fill title="Objectives">
+          {objectives.map((objective) => (
+            <Box key={objective.explanation}>
+              <Stack align="baseline">
+                <Stack.Item grow bold>
+                  {objective.explanation}
+                </Stack.Item>
+              </Stack>
+              <Icon
+                name={objective.completed ? 'check' : 'times'}
+                color={objective.completed ? 'good' : 'bad'}
+              />
+            </Box>
+          ))}
+          <Box>
+            <Button
+              fluid
+              textAlign="center"
+              align="center"
+              width={50}
+              content={'Commence Apocalypse'}
+              fontSize="200%"
+              disabled={!all_completed || !rabbits_found || used_up}
+              onClick={() => act('claim_reward')}
+            />
+          </Box>
+        </Section>
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+export const HunterContract = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { items = [], bought, number_of_rabbits } = data;
+  return (
+    <Window width={670} height={400}>
+      <Window.Content scrollable>
+        <Section title="Hunter's Contract" />
+        {
+          <Stack vertical fill>
+            <Stack.Item fontSize="20px" textAlign="center">
+              Pick your Hunter tool
+            </Stack.Item>
+            <Stack.Item grow>
+              {items.map((item) => (
+                <Box key={item.name} className="candystripe" p={1} pb={2}>
+                  <Stack align="baseline">
+                    <Stack.Item grow bold>
+                      {item.name}
+                    </Stack.Item>
+                    <Stack.Item>
+                      <Button
+                        content="Claim"
+                        disabled={bought}
+                        onClick={() =>
+                          act('select', {
+                            item: item.id,
+                          })
+                        }
+                      />
+                    </Stack.Item>
+                  </Stack>
+                  {item.desc}
+                </Box>
+              ))}
+            </Stack.Item>
+            <Stack.Item>
+              <Section fill title="Hunter's Guide">
+                <Stack vertical fill>
+                  <Stack.Item>
+                    <span>
+                      Look for the white rabbits! Use their eyes to upgrade your
+                      hunter&#39;s weapon, the red queen&#39;s card will guide
+                      you!{' '}
+                      <span className={'color-red'}>
+                        {' '}
+                        YOU HAVE FOUND {number_of_rabbits}{' '}
+                        {number_of_rabbits === 1 ? 'RABBIT' : 'RABBITS'}{' '}
+                      </span>
+                      Only once the contract is fullfilled and the rabbits are
+                      found will you be able to ascend to your true
+                      <span className={'color-green'}> Divine form </span>!
+                    </span>
+                    <br />
+                  </Stack.Item>
+                </Stack>
+              </Section>
+            </Stack.Item>
+            <Stack.Item>
+              <Box>
+                <HunterObjectives />
+              </Box>
+            </Stack.Item>
+          </Stack>
+        }
+      </Window.Content>
+    </Window>
+  );
+};
