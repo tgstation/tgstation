fef177bcedf580ef7d98c5fb9b7448cedcf4a143
san7890 <the@san7890.com>
2023-03-28 17:47:32 -0600

    makes it work a bit better

--- tools/define_sanity/check.py
+++ tools/define_sanity/check.py
@@ -4,16 +4,30 @@ import sys
 import re
 
 parent_directory = "code/**/*.dm"
-# This two directories are expected to have "global" defines, so they must be exempt from this check.
-defines_directory = "code/__DEFINES/*.dm"
-helpers_directory = "code/__HELPERS/*.dm"
+# This files/directories are expected to have "global" defines, so they must be exempt from this check.
+excluded_files = [
+    "code/__DEFINES/*.dm",
+    "code/__HELPERS/*.dm",
+    "code/modules/tgs/v5/_defines.dm",
+    "code/modules/unit_tests/_unit_tests.dm",
+    "code/modules/tgs/v4/api.dm",
+    "code/modules/tgs/v3210/commands.dm",
+    # In an ideal world, anything below this line shouldn't be the way it is and moved to the __DEFINES directory. This is not an ideal world.
+    "code/modules/atmospherics/machinery/components/fusion/hfr_defines.dm"
+]
 
 define_regex = re.compile("#define\s([A-Z0-9_]+)\s")
 
 filtered_files = []
 
 for code_file in glob.glob(parent_directory, recursive=True):
-    if not fnmatch.fnmatch(code_file, defines_directory) and not fnmatch.fnmatch(code_file, helpers_directory):
+    in_exempt_directory = False
+    for exempt_directory in excluded_files:
+        if fnmatch.fnmatch(code_file, exempt_directory):
+            in_exempt_directory = True
+            break
+
+    if not in_exempt_directory:
         filtered_files.append(code_file)
 
 error_found = False
