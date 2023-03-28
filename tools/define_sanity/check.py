21a8626e3bebe991b659b53be2cdda692c2f20ae
san7890 <the@san7890.com>
2023-03-28 17:41:44 -0600

    cleanup

--- tools/define_sanity/check.py
+++ tools/define_sanity/check.py
@@ -1,13 +1,12 @@
 import fnmatch
-import functools
 import glob
 import sys
 import re
 
 parent_directory = "code/**/*.dm"
 # This two directories are expected to have "global" defines, so they must be exempt from this check.
-defines_directory = parent_directory + "/__DEFINES"
-helpers_directory = parent_directory + "/__HELPERS"
+defines_directory = "code/__DEFINES/*.dm"
+helpers_directory = "code/__HELPERS/*.dm"
 
 define_regex = re.compile("#define\s([A-Z0-9_]+)\s")
 
@@ -21,16 +20,14 @@ error_found = False
 
 for applicable_file in filtered_files:
     add_file_to_list = False
-    with open(applicable_file, "r") as file:
+    with open(applicable_file, encoding="utf8") as file:
         file_contents = file.read()
-        for define in define_regex.findall(file_contents):
+        for define in define_regex.finditer(file_contents):
             define_name = define.group(1)
-            if not re.search("#undef\s" + define, file_contents):
+            if not re.search("#undef\s" + define_name, file_contents):
                 print(f"{define_name} is defined in {applicable_file} but not undefined!")
                 error_found = True
 
 if error_found:
     print(f"Please #undef the above defines or remake them as global defines in the /__DEFINES directory.")
     sys.exit(1)
-
-
