e25951d0da5fdc70c8f05f9c1a12d9f6f4e862b5
san7890 <the@san7890.com>
2023-03-28 17:57:05 -0600

    boah what the hell boah

--- tools/define_sanity/check.py
+++ tools/define_sanity/check.py
@@ -8,12 +8,17 @@ parent_directory = "code/**/*.dm"
 excluded_files = [
     "code/__DEFINES/*.dm",
     "code/__HELPERS/*.dm",
+    "code/_globalvars/*.dm",
+    "code/_compile_options.dm",
+    "code/__byond_version_compat.dm",
     "code/modules/tgs/v5/_defines.dm",
     "code/modules/unit_tests/_unit_tests.dm",
     "code/modules/tgs/v4/api.dm",
     "code/modules/tgs/v3210/commands.dm",
     # In an ideal world, anything below this line shouldn't be the way it is and moved to the __DEFINES directory. This is not an ideal world.
     "code/modules/atmospherics/machinery/components/fusion/hfr_defines.dm"
+    "code/datums/keybinding/_defines.dm",
+    "code/game/machinery/computer/atmos_computers/__identifiers.dm",
 ]
 
 define_regex = re.compile("#define\s([A-Z0-9_]+)\s")
@@ -32,6 +37,8 @@ for code_file in glob.glob(parent_directory, recursive=True):
 
 error_found = False
 
+strings_to_output = [] #remove me
+
 for applicable_file in filtered_files:
     add_file_to_list = False
     with open(applicable_file, encoding="utf8") as file:
@@ -39,9 +46,15 @@ for applicable_file in filtered_files:
         for define in define_regex.finditer(file_contents):
             define_name = define.group(1)
             if not re.search("#undef\s" + define_name, file_contents):
-                print(f"{define_name} is defined in {applicable_file} but not undefined!")
+                string.replace("\\", "/") #remove me
+                string = f"{define_name} is defined in {applicable_file} but not undefined!" #remove me
+                strings_to_output.append(string + "\n") #remove me
+                print(string)
                 error_found = True
 
 if error_found:
     print(f"Please #undef the above defines or remake them as global defines in the /__DEFINES directory.")
+    with open('output.txt', 'w') as f: #remove me
+        for string in strings_to_output:
+            f.write(string)
     sys.exit(1)
