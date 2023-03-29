d70a3a4a544aa7c19a38da6cb0442dcea31be1e8
san7890 <the@san7890.com>
2023-03-28 21:29:36 -0600

    tuple time

--- tools/define_sanity/check.py
+++ tools/define_sanity/check.py
@@ -10,6 +10,9 @@ def green(text):
 def red(text):
     return "\033[31m" + str(text) + "\033[0m"
 
+def blue(text):
+    return "\033[34m" + str(text) + "\033[0m"
+
 def post_error(define_name, file, github_error_style):
     if github_error_style:
         print(f"::error file={file},title=Define Sanity::{define_name} is defined locally in {file} but not undefined locally!")
@@ -48,7 +51,11 @@ define_regex = re.compile("#define\s?([A-Z0-9_]+)\(?(.+)\)?\s")
 
 filtered_files = []
 
-github_error_style = os.getenv("GITHUB_ACTIONS") == "true"
+# simple way to check if we're running on github actions, or on a local machine
+on_github = os.getenv("GITHUB_ACTIONS") == "true"
+
+if not on_github:
+    print(blue(f"Running define sanity check outside of Github Actions.\nFor assistance, a 'define_sanity_output.txt' file will be generated at the root of your directory if any errors are detected."))
 
 for code_file in glob.glob(parent_directory, recursive=True):
     in_exempt_directory = False
@@ -58,7 +65,10 @@ for code_file in glob.glob(parent_directory, recursive=True):
             break
     if not in_exempt_directory:
         filtered_files.append(code_file)
+
 error_found = False
+
+located_error_tuples = []
 
 for applicable_file in filtered_files:
     with open(applicable_file, encoding="utf8") as file:
@@ -66,10 +76,20 @@ for applicable_file in filtered_files:
         for define in define_regex.finditer(file_contents):
             define_name = define.group(1)
             if not re.search("#undef\s" + define_name, file_contents):
-                post_error(define_name, applicable_file, github_error_style)
+                located_error_tuples.append((define_name, applicable_file))
                 error_found = True
 
 if error_found:
+
+    if on_github:
+        for error in located_error_tuples:
+            post_error(error[0], error[1], True)
+    else:
+        with open("define_sanity_output.txt", "w") as output_file:
+            for error in located_error_tuples:
+                post_error(error[0], error[1], False)
+                output_file.write(f"{error[0]} is defined locally in {error[1]} but not undefined locally!\n")
+
     print(red(f"Please #undef the above defines or remake them as global defines in the code/__DEFINES directory."))
     sys.exit(1)
 else:
