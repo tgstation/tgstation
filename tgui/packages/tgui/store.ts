51af442219e19fc07c472263b70cc57f9e7f399a
Jeremiah <jlsnow301@pm.me>
2023-04-06 17:09:08 -0700

    opt

--- tgui/packages/tgui/store.ts
+++ tgui/packages/tgui/store.ts
@@ -32,44 +32,48 @@ type StoreProviderProps = {
 const logger = createLogger('store');
 
 export const configureStore = (options: ConfigureStoreOptions = {}): Store => {
-  const { sideEffects = true } = options;
-  const reducer: Reducer = flow([
+  const { sideEffects = true, reducer, middleware } = options;
+  const rootReducer: Reducer = flow([
     combineReducers({
       debug: debugReducer,
       backend: backendReducer,
     }),
-    options.reducer,
+    reducer,
   ]);
-  const middleware: Middleware[] = !sideEffects
+
+  const middlewares: Middleware[] = !sideEffects
     ? []
     : [
-      ...(options.middleware?.pre || []),
+      ...(middleware?.pre || []),
       assetMiddleware,
       backendMiddleware,
-      ...(options.middleware?.post || []),
+      ...(middleware?.post || []),
     ];
+
   if (process.env.NODE_ENV !== 'production') {
     // We are using two if statements because Webpack is capable of
     // removing this specific block as dead code.
     if (sideEffects) {
-      middleware.unshift(loggingMiddleware, debugMiddleware, relayMiddleware);
+      middlewares.unshift(loggingMiddleware, debugMiddleware, relayMiddleware);
     }
   }
-  const enhancer = applyMiddleware(...middleware);
-  const store = createStore(reducer, enhancer);
+
+  const enhancer = applyMiddleware(...middlewares);
+  const store = createStore(rootReducer, enhancer);
+
   // Globals
   window.__store__ = store;
   window.__augmentStack__ = createStackAugmentor(store);
+
   return store;
 };
 
 const loggingMiddleware: Middleware = (store) => (next) => (action) => {
   const { type } = action;
-  if (type === 'update' || type === 'backend/update') {
-    logger.debug('action', { type });
-  } else {
-    logger.debug('action', action);
-  }
+  logger.debug(
+    'action',
+    type === 'update' || type === 'backend/update' ? { type } : action
+  );
   return next(action);
 };
 
@@ -80,25 +84,24 @@ const loggingMiddleware: Middleware = (store) => (next) => (action) => {
 const createStackAugmentor =
   (store: Store): StackAugmentor =>
   (stack, error) => {
-    if (!error) {
-      error = new Error(stack.split('\n')[0]);
-      error.stack = stack;
-    } else if (typeof error === 'object' && !error.stack) {
-      error.stack = stack;
-    }
+    error = error || new Error(stack.split('\n')[0]);
+    error.stack = error.stack || stack;
+
     logger.log('FatalError:', error);
     const state = store.getState();
     const config = state?.backend?.config;
-    let augmentedStack = stack;
-    augmentedStack += '\nUser Agent: ' + navigator.userAgent;
-    augmentedStack +=
+
+    return (
+      stack +
+      '\nUser Agent: ' +
+      navigator.userAgent +
       '\nState: ' +
       JSON.stringify({
         ckey: config?.client?.ckey,
         interface: config?.interface,
         window: config?.window,
-      });
-    return augmentedStack;
+      })
+    );
   };
 
 /**
