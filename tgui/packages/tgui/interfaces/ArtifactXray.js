c1837eeada9f672639f8dc0d7ee8f2b3b2c42258
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2023-04-27 16:08:47 +0200

    prettier

--- tgui/packages/tgui/interfaces/ArtifactXray.js
+++ tgui/packages/tgui/interfaces/ArtifactXray.js
@@ -1,18 +1,17 @@
-import { useBackend } from "../backend";
-import {
-  Slider,
-  Button,
-  Stack,
-  NoticeBox,
-  Section,
-  Box,
-} from "../components";
-import { Window } from "../layouts";
+import { useBackend } from '../backend';
+import { Slider, Button, Stack, NoticeBox, Section, Box } from '../components';
+import { Window } from '../layouts';
 
 export const ArtifactXray = (props, context) => {
   const { act, data } = useBackend(context);
-  const { is_open, artifact_name, pulsing, current_strength, max_strength, results } =
-    data;
+  const {
+    is_open,
+    artifact_name,
+    pulsing,
+    current_strength,
+    max_strength,
+    results,
+  } = data;
   return (
     <Window width={400} height={220}>
       <Window.Content>
@@ -21,46 +20,46 @@ export const ArtifactXray = (props, context) => {
           textAlign="center"
           buttons={
             <Button
-              content={is_open ? "Close" : "Open"}
+              content={is_open ? 'Close' : 'Open'}
               selected={!is_open}
-              onClick={() => act("toggleopen")}
+              onClick={() => act('toggleopen')}
             />
-          }
-        >
+          }>
           {(!artifact_name && <NoticeBox>No artifact detected.</NoticeBox>) || (
             <>
               <Box color="label" mt={1}>
                 <b>Currently loaded object:</b> {artifact_name}
               </Box>
-			  <Stack fill>
-            <Stack.Item grow>
-              <Slider
-                minValue={0}
-                maxValue={max_strength}
-                value={current_strength}
-                stepPixelSize={35}
-                step={0.5}
-				unit={"Pulse Strength"}
-                onDrag={(e, nu) => act("change_rate", { target: nu })}
-              />
-            </Stack.Item>
-            <Stack.Item>
-              <Button
-                content={"Pulse"}
-                disabled={is_open || pulsing}
-                color="green"
-                onClick={() => act("pulse")} />
-            </Stack.Item>
-     </Stack>
-          )}
-		  <Section title={"Last Scan Results"} backgroundColor="black">
-              {results.map((result) => (
-                <Box mb={1} key={result} color="green">
-                  {result}
-                </Box>
-              ))}
-    </Section>
+              <Stack fill>
+                <Stack.Item grow>
+                  <Slider
+                    minValue={0}
+                    maxValue={max_strength}
+                    value={current_strength}
+                    stepPixelSize={35}
+                    step={0.5}
+                    unit={'Pulse Strength'}
+                    onDrag={(e, nu) => act('change_rate', { target: nu })}
+                  />
+                </Stack.Item>
+                <Stack.Item>
+                  <Button
+                    content={'Pulse'}
+                    disabled={is_open || pulsing}
+                    color="green"
+                    onClick={() => act('pulse')}
+                  />
+                </Stack.Item>
+              </Stack>
             </>
+          )}
+          <Section title={'Last Scan Results'} backgroundColor="black">
+            {results.map((result) => (
+              <Box mb={1} key={result} color="green">
+                {result}
+              </Box>
+            ))}
+          </Section>
         </Section>
       </Window.Content>
     </Window>
