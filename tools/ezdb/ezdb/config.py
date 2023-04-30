2439c5d7dc8bbb3386c781086f0fd4ad432e84dc
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2023-04-30 02:05:46 -0700

    Early return

--- tools/ezdb/ezdb/config.py
+++ tools/ezdb/ezdb/config.py
@@ -3,20 +3,20 @@ from typing import Optional
 
 def read_config() -> Optional[dict[str, str]]:
     config_path = get_config_path()
-    if config_path.exists():
-        with config_path.open('r') as file:
-            lines = file.readlines()
-            entries = {}
-
-            for line in lines:
-                if line.startswith("#"):
-                    continue
-                if " " not in line:
-                    continue
-
-                key, value = line.split(" ", 1)
-                entries[key.strip()] = value.strip()
-
-            return entries
-    else:
+    if not config_path.exists():
         return None
+
+    with config_path.open('r') as file:
+        lines = file.readlines()
+        entries = {}
+
+        for line in lines:
+            if line.startswith("#"):
+                continue
+            if " " not in line:
+                continue
+
+            key, value = line.split(" ", 1)
+            entries[key.strip()] = value.strip()
+
+        return entries
