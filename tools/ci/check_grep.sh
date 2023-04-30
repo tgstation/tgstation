6bd192fe49a5c9bd217d1599c9e9a940413828fe
BlueMemesauce <47338680+BlueMemesauce@users.noreply.github.com>
2023-04-30 00:16:29 -0400

    Fixes some miscapitalization of Nanotrasen  (#75026)
    ## About The Pull Request
    
    Fixes #75019
    
    Also fixes it in a drink, the ethereal lore, and some photocopier
    paperwork.
    
    And also adds "NanoTrasen" to common spelling mistakes
    
    ## Why It's Good For The Game
    
    Nanotrasen is the correct capitalization
    ## Changelog
    :cl:
    spellcheck: Fixed Nanotrasen being miscapitalized in traitor posters, a
    drink, the ethereal lore, and some photocopier paperwork
    /:cl:

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -79,7 +79,12 @@ fi;
 part "common spelling mistakes"
 if $grep -i 'nanotransen' $map_files; then
 	echo
-    echo -e "${RED}ERROR: Misspelling of Nanotrasen detected in maps, please remove the extra N(s).${NC}"
+    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please remove the extra N(s).${NC}"
+    st=1
+fi;
+if $grep 'NanoTrasen' $map_files; then
+	echo
+    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in maps, please uncapitalize the T(s).${NC}"
     st=1
 fi;
 if $grep -i'centcomm' $map_files; then
@@ -164,6 +169,11 @@ fi;
 if $grep -ni 'nanotransen' $code_files; then
 	echo
     echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please remove the extra N(s).${NC}"
+    st=1
+fi;
+if $grep 'NanoTrasen' $code_files; then
+	echo
+    echo -e "${RED}ERROR: Misspelling(s) of Nanotrasen detected in code, please uncapitalize the T(s).${NC}"
     st=1
 fi;
 part "map json naming"
