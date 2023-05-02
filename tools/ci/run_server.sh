5977e6648426d203791d2259f3f860e7a581518b
Dominion <Cyberboss@users.noreply.github.com>
2023-05-01 23:19:20 -0400

    Fixes

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -16,7 +16,7 @@ cp tools/ci/ci_config.txt ci_test/config/config.txt
 cp _maps/$MAP.json ci_test/data/next_map.json
 
 cd ci_test
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
+DreamDaemon tgstation.flat.dmb -close -trusted -verbose -params "log-directory=ci"
 
 cd ..
 
