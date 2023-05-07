aa067ec6f2350ea56a83516f94f1d11b8a8d37fb
Dominion <Cyberboss@users.noreply.github.com>
2023-05-07 14:18:07 -0400

    Temporarily use my fork of SpacemanDMM for linting until https://github.com/SpaceManiac/SpacemanDMM/pull/356 is released

--- tools/ci/install_spaceman_dmm.sh
+++ tools/ci/install_spaceman_dmm.sh
@@ -7,8 +7,8 @@ if [ ! -f ~/$1 ]; then
 	mkdir -p "$HOME/SpacemanDMM"
 	CACHEFILE="$HOME/SpacemanDMM/$1"
 
-	if ! [ -f "$CACHEFILE.version" ] || ! grep -Fxq "$SPACEMAN_DMM_VERSION" "$CACHEFILE.version"; then
-		wget -O "$CACHEFILE" "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMAN_DMM_VERSION/$1"
+	if ! [ -f "$CACHEFILE.version" ] || ! grep -Fxq "$SPACEMAN_DMM_VERSION-Cyberboss" "$CACHEFILE.version"; then # FIX THIS LINE TOO
+		wget -O "$CACHEFILE" "https://github.com/Cyberboss/SpacemanDMM/releases/download/$SPACEMAN_DMM_VERSION/$1" # DO NOT MERGE UNTIL THIS IS SET BACK TO SPACEMANIAC'S VERSION OF THE REPO
 		chmod +x "$CACHEFILE"
 		echo "$SPACEMAN_DMM_VERSION" >"$CACHEFILE.version"
 	fi
