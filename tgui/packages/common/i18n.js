21936f2106e8e4a61850dd828cda8a9d5fbed38a
AddRaiser <49074954+AddRaiser@users.noreply.github.com>
2023-05-07 16:20:52 +0300

    Basic i18n

---
+++ tgui/packages/common/i18n.js
@@ -0,0 +1,77 @@
+export const i18n = (translations) => {
+  const fallbackLang = LANGUAGES.EN;
+  let currentLang = LANGUAGES.RU;
+
+  const changeLanguage = (newLang) => {
+    currentLang = newLang;
+  };
+
+  const t = (stringName, values = {}) => {
+    const rawString = translations[stringName][currentLang] || translations[stringName][fallbackLang];
+    return fillStringValues(rawString, values);
+  };
+
+  const plural = (stringName, values = {}) => {
+    const validLanguage = translations[stringName][currentLang] ? currentLang : fallbackLang;
+
+    if (typeof translations[stringName][currentLang] === 'string' || !values) {
+      return t(stringName);
+    }
+
+    let rawString;
+    switch (validLanguage) {
+      case LANGUAGES.RU:
+        rawString = russianPlural(translations[stringName][validLanguage], values.n);
+        break;
+      case LANGUAGES.EN:
+      default: {
+        rawString = englishPlural(translations[stringName][validLanguage], values.n);
+      }
+    }
+
+    return fillStringValues(rawString, values);
+  };
+
+  const fillStringValues = (string, values) => {
+    let editedString = string;
+    Object.entries(values).forEach(([key, value]) => {
+      editedString = editedString.replace(RegExp(`{${key}}`, "g"), String(value));
+    });
+    return editedString;
+  };
+
+  const englishPlural = (translation, n) => {
+    switch (n) {
+      case 0:
+        return translation.zero;
+      case 1:
+        return translation.one;
+      default:
+        return translation.other;
+    }
+  };
+
+  const russianPlural = (translation, n) => {
+    switch (n % 10) {
+      case 1:
+        return translation.one;
+      case 2:
+      case 3:
+      case 4:
+        return translation.few;
+      default:
+        return translation.other;
+    }
+  };
+
+  return {
+    changeLanguage,
+    t,
+    plural,
+  };
+};
+
+const LANGUAGES = {
+  EN: 'en',
+  RU: 'ru',
+};
