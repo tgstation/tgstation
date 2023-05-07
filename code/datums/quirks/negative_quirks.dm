tgstation/tgstation

commit 5401d0798fb4f88990591bd195399056ed6f82b8

Removes the random prosthetic quirk from the character preview page (#75050)
## About The Pull Request

The prosthetic limb quirk chooses a random limb to make prosthetic at
roundstart.

It was extremely buggy with the way quirks are displayed and the way the
dummy preview works and would frequently carry over to other characters
and not update correctly.

<details>
<summary>Eventually you end up getting this if you switch enough times
</summary>


![hDq983y2T0](https://user-images.githubusercontent.com/13398309/235334283-d4fc4c1f-c1de-4460-b6f7-7ef95a587cb4.gif)

</details>

I started fixing the issue when I realized this really shouldn't be
shown on the dummy to begin with, because what you see on the dummy will
not necessarily be what you get at roundstart. It's misleading.

I did however leave part of the fix that I was working on, because I
think it is useful to be able to undo the quirk via adminnery.

Closes https://github.com/Skyrat-SS13/Skyrat-tg/issues/20791

## Why It's Good For The Game
## Changelog
:cl:
fix: prosthetic limb quirk will no longer display a prosthetic on your
character preview because it is buggy and hardly ever matches what you
end up getting upon spawning
code: removing the prosthetic limb quirk will restore your original limb
/:cl:

code/datums/quirks/negative_quirks.dm
@@ -529,11 +529,14 @@
 	desc = "An accident caused you to lose one of your limbs. Because of this, you now have a random prosthetic!"
 	icon = "tg-prosthetic-leg"
 	value = -3
-	var/slot_string = "limb"
 	medical_record_text = "During physical examination, patient was found to have a prosthetic limb."
 	hardcore_value = 3
-	quirk_flags = QUIRK_HUMAN_ONLY|QUIRK_CHANGES_APPEARANCE
+	quirk_flags = QUIRK_HUMAN_ONLY // while this technically changes appearance, we don't want it to be shown on the dummy because it's randomized at roundstart
 	mail_goodies = list(/obj/item/weldingtool/mini, /obj/item/stack/cable_coil/five)
+	/// The slot to replace, in string form
+	var/slot_string = "limb"
+	/// the original limb from before the prosthetic was applied
+	var/obj/item/bodypart/old_limb
 
 /datum/quirk/prosthetic_limb/add_unique(client/client_source)
 	var/limb_slot = pick(BODY_ZONE_L_ARM, BODY_ZONE_R_ARM, BODY_ZONE_L_LEG, BODY_ZONE_R_LEG)
@@ -552,11 +555,16 @@
 		if(BODY_ZONE_R_LEG)
 			prosthetic = new /obj/item/bodypart/leg/right/robot/surplus
 			slot_string = "right leg"
-	human_holder.del_and_replace_bodypart(prosthetic)
+	old_limb = human_holder.return_and_replace_bodypart(prosthetic)
 
 /datum/quirk/prosthetic_limb/post_add()
 	to_chat(quirk_holder, span_boldannounce("Your [slot_string] has been replaced with a surplus prosthetic. It is fragile and will easily come apart under duress. Additionally, \
 	you need to use a welding tool and cables to repair it, instead of bruise packs and ointment."))
+
+/datum/quirk/prosthetic_limb/remove()
+	var/mob/living/carbon/human/human_holder = quirk_holder
+	human_holder.del_and_replace_bodypart(old_limb)
+	old_limb = null
 
 /datum/quirk/quadruple_amputee
 	name = "Quadruple Amputee"

code/modules/surgery/bodyparts/helpers.dm
@@ -11,12 +11,19 @@
 		if(bodypart.body_zone == zone)
 			return bodypart
 
-///Replaces a single limb and deletes the old one if there was one
+/// Replaces a single limb and deletes the old one if there was one
 /mob/living/carbon/proc/del_and_replace_bodypart(obj/item/bodypart/new_limb, special)
 	var/obj/item/bodypart/old_limb = get_bodypart(new_limb.body_zone)
 	if(old_limb)
 		qdel(old_limb)
 	new_limb.try_attach_limb(src, special = special)
+
+/// Replaces a single limb and returns the old one if there was one
+/// Note: the old limb gets sent to nullspace during try_attach_limb
+/mob/living/carbon/proc/return_and_replace_bodypart(obj/item/bodypart/new_limb, special)
+	var/obj/item/bodypart/old_limb = get_bodypart(new_limb.body_zone)
+	new_limb.try_attach_limb(src, special = special)
+	return old_limb
 
 /mob/living/carbon/has_hand_for_held_index(i)
 	if(!i)

commit fdd7ed4d4c3f5b1f747ccfaf0dc7c23017e0c73a

Fixes prosthetic limbs leaving the old one behind (#75176)
## About The Pull Request

Fixes #75153
Caused by #75050 

`Note: the old limb gets sent to nullspace during try_attach_limb`
...This assertion is completely wrong, and I'm not sure where it came
from. Perhaps it's different on their downstream.

`try_attach_limb` does not care about limbs that occupy the same body
zone and will happy attach itself, giving you two left arms or two right
legs.

`del_and_replace_bodypart` handles this by deleting the existing limb,
`return_and_replace_bodypart` did not handle this whatsoever. So I added
that missing handling.

I'm not sure if we *actually want* `try_attach_limb` to disregard limbs
that occupy that slot already. It seems like weird behavior but also
consumers should probably know what they're doing in regards to that?

While I was here, I made both prosthetic related quirks use `special =
TRUE`. The application of these limbs are supposed to be quick swapping
no-side-effects, since it's done at mob creation, so it should be using
special.

## Why It's Good For The Game

Three arms is bad, okay?

## Changelog

:cl: Melbert
fix: Fixed Prosthetic Quirk not removing the limb before giving the
prosthetic
/:cl:

code/datums/quirks/negative_quirks.dm
@@ -555,7 +555,7 @@
 		if(BODY_ZONE_R_LEG)
 			prosthetic = new /obj/item/bodypart/leg/right/robot/surplus
 			slot_string = "right leg"
-	old_limb = human_holder.return_and_replace_bodypart(prosthetic)
+	old_limb = human_holder.return_and_replace_bodypart(prosthetic, special = TRUE)
 
 /datum/quirk/prosthetic_limb/post_add()
 	to_chat(quirk_holder, span_boldannounce("Your [slot_string] has been replaced with a surplus prosthetic. It is fragile and will easily come apart under duress. Additionally, \
@@ -563,7 +563,7 @@
 
 /datum/quirk/prosthetic_limb/remove()
 	var/mob/living/carbon/human/human_holder = quirk_holder
-	human_holder.del_and_replace_bodypart(old_limb)
+	human_holder.del_and_replace_bodypart(old_limb, special = TRUE)
 	old_limb = null
 
 /datum/quirk/quadruple_amputee
@@ -577,10 +577,10 @@
 
 /datum/quirk/quadruple_amputee/add_unique(client/client_source)
 	var/mob/living/carbon/human/human_holder = quirk_holder
-	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/arm/left/robot/surplus)
-	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/arm/right/robot/surplus)
-	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/leg/left/robot/surplus)
-	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/leg/right/robot/surplus)
+	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/arm/left/robot/surplus, special = TRUE)
+	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/arm/right/robot/surplus, special = TRUE)
+	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/leg/left/robot/surplus, special = TRUE)
+	human_holder.del_and_replace_bodypart(new /obj/item/bodypart/leg/right/robot/surplus, special = TRUE)
 
 /datum/quirk/quadruple_amputee/post_add()
 	to_chat(quirk_holder, span_boldannounce("All your limbs have been replaced with surplus prosthetics. They are fragile and will easily come apart under duress. Additionally, \

code/modules/surgery/bodyparts/helpers.dm
@@ -19,11 +19,14 @@
 	new_limb.try_attach_limb(src, special = special)
 
 /// Replaces a single limb and returns the old one if there was one
-/// Note: the old limb gets sent to nullspace during try_attach_limb
 /mob/living/carbon/proc/return_and_replace_bodypart(obj/item/bodypart/new_limb, special)
 	var/obj/item/bodypart/old_limb = get_bodypart(new_limb.body_zone)
+	if(!isnull(old_limb))
+		old_limb.drop_limb(special = special)
+		old_limb.moveToNullspace()
+
 	new_limb.try_attach_limb(src, special = special)
-	return old_limb
+	return old_limb // can be null
 
 /mob/living/carbon/has_hand_for_held_index(i)
 	if(!i)