tgstation/tgstation

commit b8fc54a9140c021cad9d348d0ff29e3015c5bb88

Dropdowns v2 (#75164)
## About The Pull Request

Complete rewrite of dropdowns to use popper elements that are injected
at the body level.
This means they are now horribly overcomplicated and also suck a lot
less

![2023-05-04_06-57-12](https://user-images.githubusercontent.com/5194834/236229806-2f676e7b-d6c5-427a-af2f-520d883be790.gif)
Also ported `Icon` to typescript for a reason that didn't end up making
it in the PR

## Why It's Good For The Game

![image](https://user-images.githubusercontent.com/5194834/236226086-35ec2009-e9a9-404b-9b52-3ab8de0a0ae6.png)

![image](https://user-images.githubusercontent.com/5194834/236226573-880c0380-0f70-41c7-90c6-d51ba4238561.png)

## Changelog
:cl:
add: good tgui dropdowns
del: tgui dropdown suck
qol: tgui dropdowns suck less
balance: buffed tgui dropdowns
/:cl:

tgui/packages/tgui/components/Icon.tsx
@@ -7,24 +7,37 @@
  */
 
 import { classes, pureComponentHooks } from 'common/react';
-import { computeBoxClassName, computeBoxProps } from './Box';
+import { InfernoNode } from 'inferno';
+import { BoxProps, computeBoxClassName, computeBoxProps } from './Box';
 
 const FA_OUTLINE_REGEX = /-o$/;
 
-export const Icon = (props) => {
-  const { name, size, spin, className, rotation, inverse, ...rest } = props;
+type IconPropsUnique = {
+  name: string;
+  size?: number;
+  spin?: boolean;
+  className?: string;
+  rotation?: number;
+  style?: string | CSSProperties;
+};
+
+export type IconProps = IconPropsUnique & BoxProps;
+
+export const Icon = (props: IconProps) => {
+  let { style, ...restlet } = props;
+  const { name, size, spin, className, rotation, ...rest } = restlet;
 
   if (size) {
-    if (!rest.style) {
-      rest.style = {};
+    if (!style) {
+      style = {};
     }
-    rest.style['font-size'] = size * 100 + '%';
+    style['font-size'] = size * 100 + '%';
   }
-  if (typeof rotation === 'number') {
-    if (!rest.style) {
-      rest.style = {};
+  if (rotation) {
+    if (!style) {
+      style = {};
     }
-    rest.style['transform'] = `rotate(${rotation}deg)`;
+    style['transform'] = `rotate(${rotation}deg)`;
   }
 
   const boxProps = computeBoxProps(rest);
@@ -63,7 +76,14 @@ export const Icon = (props) => {
 
 Icon.defaultHooks = pureComponentHooks;
 
-export const IconStack = (props) => {
+type IconStackUnique = {
+  children: InfernoNode;
+  className?: string;
+};
+
+export type IconStackProps = IconStackUnique & BoxProps;
+
+export const IconStack = (props: IconStackProps) => {
   const { className, children, ...rest } = props;
   return (
     <span

tgui/packages/tgui/components/Dropdown.js
@@ -1,161 +1,0 @@
-/**
- * @file
- * @copyright 2020 Aleksej Komarov
- * @license MIT
- */
-
-import { classes } from 'common/react';
-import { Component } from 'inferno';
-import { Box } from './Box';
-import { Icon } from './Icon';
-
-export class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: props.selected,
-      open: false,
-    };
-    this.handleClick = () => {
-      if (this.state.open) {
-        this.setOpen(false);
-      }
-    };
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('click', this.handleClick);
-  }
-
-  setOpen(open) {
-    this.setState({ open: open });
-    if (open) {
-      setTimeout(() => {
-        window.addEventListener('click', this.handleClick);
-      });
-      this.menuRef.focus();
-    } else {
-      window.removeEventListener('click', this.handleClick);
-    }
-  }
-
-  setSelected(selected) {
-    this.setState({
-      selected: selected,
-    });
-    this.setOpen(false);
-    this.props.onSelected(selected);
-  }
-
-  buildMenu() {
-    const { options = [] } = this.props;
-    const ops = options.map((option) => {
-      let displayText, value;
-
-      if (typeof option === 'string') {
-        displayText = option;
-        value = option;
-      } else {
-        displayText = option.displayText;
-        value = option.value;
-      }
-
-      return (
-        <Box
-          key={value}
-          className="Dropdown__menuentry"
-          onClick={() => {
-            this.setSelected(value);
-          }}>
-          {displayText}
-        </Box>
-      );
-    });
-    return ops.length ? ops : 'No Options Found';
-  }
-
-  render() {
-    const { props } = this;
-    const {
-      icon,
-      iconRotation,
-      iconSpin,
-      clipSelectedText = true,
-      color = 'default',
-      dropdownStyle,
-      over,
-      noscroll,
-      nochevron,
-      width,
-      openWidth = width,
-      onClick,
-      onOpen,
-      selected,
-      disabled,
-      displayText,
-      ...boxProps
-    } = props;
-    const { className, ...rest } = boxProps;
-
-    const adjustedOpen = over ? !this.state.open : this.state.open;
-
-    const menu = this.state.open ? (
-      <div
-        ref={(menu) => {
-          this.menuRef = menu;
-        }}
-        tabIndex="-1"
-        style={{
-          'width': openWidth,
-        }}
-        className={classes([
-          (noscroll && 'Dropdown__menu-noscroll') || 'Dropdown__menu',
-          over && 'Dropdown__over',
-        ])}>
-        {this.buildMenu()}
-      </div>
-    ) : null;
-
-    return (
-      <div className="Dropdown" style={dropdownStyle}>
-        <Box
-          width={this.state.open ? openWidth : width}
-          className={classes([
-            'Dropdown__control',
-            'Button',
-            'Button--color--' + color,
-            disabled && 'Button--disabled',
-            className,
-          ])}
-          {...rest}
-          onClick={(event) => {
-            if (disabled && !this.state.open) {
-              return;
-            }
-            this.setOpen(!this.state.open);
-
-            if (props.onOpen) {
-              props.onOpen(event);
-            }
-          }}>
-          {icon && (
-            <Icon name={icon} rotation={iconRotation} spin={iconSpin} mr={1} />
-          )}
-          <span
-            className="Dropdown__selected-text"
-            style={{
-              'overflow': clipSelectedText ? 'hidden' : 'visible',
-            }}>
-            {displayText ? displayText : this.state.selected}
-          </span>
-          {!!nochevron || (
-            <span className="Dropdown__arrow-button">
-              <Icon name={adjustedOpen ? 'chevron-up' : 'chevron-down'} />
-            </span>
-          )}
-        </Box>
-        {menu}
-      </div>
-    );
-  }
-}

tgui/packages/tgui/components/Dropdown.tsx
@@ -1,0 +1,298 @@
+import { createPopper, VirtualElement } from '@popperjs/core';
+import { classes } from 'common/react';
+import { Component, findDOMfromVNode, InfernoNode, render } from 'inferno';
+import { Box, BoxProps } from './Box';
+import { Icon } from './Icon';
+
+export interface DropdownEntry {
+  displayText: string | number | InfernoNode;
+  value: string | number | Enumerator;
+}
+
+type DropdownUniqueProps = {
+  options: string[] | DropdownEntry[];
+  icon?: string;
+  iconRotation?: number;
+  clipSelectedText?: boolean;
+  width?: string;
+  menuWidth?: string;
+  over?: boolean;
+  color?: string;
+  nochevron?: boolean;
+  displayText?: string | number | InfernoNode;
+  onClick?: (event) => void;
+  // you freaks really are just doing anything with this shit
+  selected?: any;
+  onSelected?: (selected: any) => void;
+};
+
+export type DropdownProps = BoxProps & DropdownUniqueProps;
+
+const DEFAULT_OPTIONS = {
+  placement: 'left-start',
+  modifiers: [
+    {
+      name: 'eventListeners',
+      enabled: false,
+    },
+  ],
+};
+const NULL_RECT: DOMRect = {
+  width: 0,
+  height: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  x: 0,
+  y: 0,
+  toJSON: () => null,
+} as const;
+
+type DropdownState = {
+  selected?: string;
+  open: boolean;
+};
+
+const DROPDOWN_DEFAULT_CLASSNAMES = 'Layout Dropdown__menu';
+const DROPDOWN_SCROLL_CLASSNAMES = 'Layout Dropdown__menu-scroll';
+
+export class Dropdown extends Component<DropdownProps, DropdownState> {
+  static renderedMenu: HTMLDivElement | undefined;
+  static singletonPopper: ReturnType<typeof createPopper> | undefined;
+  static currentOpenMenu: Element | undefined;
+  static virtualElement: VirtualElement = {
+    getBoundingClientRect: () =>
+      Dropdown.currentOpenMenu?.getBoundingClientRect() ?? NULL_RECT,
+  };
+  menuContents: any;
+  handleClick: any;
+  state: DropdownState = {
+    open: false,
+  };
+
+  constructor() {
+    super();
+
+    this.handleClick = () => {
+      if (this.state.open) {
+        this.setOpen(false);
+      }
+    };
+  }
+
+  getDOMNode() {
+    return findDOMfromVNode(this.$LI, true);
+  }
+
+  componentDidMount() {
+    const domNode = this.getDOMNode();
+
+    if (!domNode) {
+      return;
+    }
+  }
+
+  openMenu() {
+    let renderedMenu = Dropdown.renderedMenu;
+    if (renderedMenu === undefined) {
+      renderedMenu = document.createElement('div');
+      renderedMenu.className = DROPDOWN_DEFAULT_CLASSNAMES;
+      document.body.appendChild(renderedMenu);
+      Dropdown.renderedMenu = renderedMenu;
+    }
+
+    const domNode = this.getDOMNode()!;
+    Dropdown.currentOpenMenu = domNode;
+
+    renderedMenu.scrollTop = 0;
+    renderedMenu.style.width =
+      this.props.menuWidth ||
+      // Hack, but domNode should *always* be the parent control meaning it will have width
+      // @ts-ignore
+      `${domNode.offsetWidth}px`;
+    renderedMenu.style.opacity = '1';
+    renderedMenu.style.pointerEvents = 'auto';
+
+    // ie hack
+    // ie has this bizarre behavior where focus just silently fails if the
+    // element being targeted "isn't ready"
+    // 400 is probably way too high, but the lack of hotloading is testing my
+    // patience on tuning it
+    // I'm beyond giving a shit at this point it fucking works whatever
+    setTimeout(() => {
+      Dropdown.renderedMenu?.focus();
+    }, 400);
+    this.renderMenuContent();
+  }
+
+  closeMenu() {
+    if (Dropdown.currentOpenMenu !== this.getDOMNode()) {
+      return;
+    }
+
+    Dropdown.currentOpenMenu = undefined;
+    Dropdown.renderedMenu!.style.opacity = '0';
+    Dropdown.renderedMenu!.style.pointerEvents = 'none';
+  }
+
+  componentWillUnmount() {
+    this.closeMenu();
+    this.setOpen(false);
+  }
+
+  renderMenuContent() {
+    const renderedMenu = Dropdown.renderedMenu;
+    if (!renderedMenu) {
+      return;
+    }
+    if (renderedMenu.offsetHeight > 200) {
+      renderedMenu.className = DROPDOWN_SCROLL_CLASSNAMES;
+    } else {
+      renderedMenu.className = DROPDOWN_DEFAULT_CLASSNAMES;
+    }
+
+    const { options = [] } = this.props;
+    const ops = options.map((option) => {
+      let value, displayText;
+
+      if (typeof option === 'string') {
+        displayText = option;
+        value = option;
+      } else if (option !== null) {
+        displayText = option.displayText;
+        value = option.value;
+      }
+
+      return (
+        <div
+          key={value}
+          className="Dropdown__menuentry"
+          onClick={() => {
+            this.setSelected(value);
+          }}>
+          {displayText}
+        </div>
+      );
+    });
+
+    const to_render = ops.length ? ops : 'No Options Found';
+
+    render(
+      <div>{to_render}</div>,
+      renderedMenu,
+      () => {
+        let singletonPopper = Dropdown.singletonPopper;
+        if (singletonPopper === undefined) {
+          singletonPopper = createPopper(
+            Dropdown.virtualElement,
+            renderedMenu!,
+            {
+              ...DEFAULT_OPTIONS,
+              placement: 'bottom-start',
+            }
+          );
+
+          Dropdown.singletonPopper = singletonPopper;
+        } else {
+          singletonPopper.setOptions({
+            ...DEFAULT_OPTIONS,
+            placement: 'bottom-start',
+          });
+
+          singletonPopper.update();
+        }
+      },
+      this.context
+    );
+  }
+
+  setOpen(open: boolean) {
+    this.setState((state) => ({
+      ...state,
+      open,
+    }));
+    if (open) {
+      setTimeout(() => {
+        this.openMenu();
+        window.addEventListener('click', this.handleClick);
+      });
+    } else {
+      this.closeMenu();
+      window.removeEventListener('click', this.handleClick);
+    }
+  }
+
+  setSelected(selected: string) {
+    this.setState((state) => ({
+      ...state,
+      selected,
+    }));
+    this.setOpen(false);
+    if (this.props.onSelected) {
+      this.props.onSelected(selected);
+    }
+  }
+
+  render() {
+    const { props } = this;
+    const {
+      icon,
+      iconRotation,
+      iconSpin,
+      clipSelectedText = true,
+      color = 'default',
+      dropdownStyle,
+      over,
+      nochevron,
+      width,
+      onClick,
+      onSelected,
+      selected,
+      disabled,
+      displayText,
+      ...boxProps
+    } = props;
+    const { className, ...rest } = boxProps;
+
+    const adjustedOpen = over ? !this.state.open : this.state.open;
+
+    return (
+      <Box
+        width={width}
+        className={classes([
+          'Dropdown__control',
+          'Button',
+          'Button--color--' + color,
+          disabled && 'Button--disabled',
+          className,
+        ])}
+        onClick={(event) => {
+          if (disabled && !this.state.open) {
+            return;
+          }
+          this.setOpen(!this.state.open);
+          if (onClick) {
+            onClick(event);
+          }
+        }}
+        {...rest}>
+        {icon && (
+          <Icon name={icon} rotation={iconRotation} spin={iconSpin} mr={1} />
+        )}
+        <span
+          className="Dropdown__selected-text"
+          style={{
+            overflow: clipSelectedText ? 'hidden' : 'visible',
+          }}>
+          {displayText || this.state.selected}
+        </span>
+        {nochevron || (
+          <span className="Dropdown__arrow-button">
+            <Icon name={adjustedOpen ? 'chevron-up' : 'chevron-down'} />
+          </span>
+        )}
+      </Box>
+    );
+  }
+}

tgui/packages/tgui/interfaces/AirlockController.tsx
@@ -111,7 +111,7 @@ const PressureIndicator = (props, context) => {
   const {
     currentStatus: { icon, color },
   } = props;
-  let spin = icon === 'fan' ? 1 : 0;
+  let spin = icon === 'fan';
 
   return (
     <Box color={color}>

tgui/packages/tgui/interfaces/HoloPay.tsx
@@ -109,7 +109,7 @@ const TerminalDisplay = (props, context) => {
       title="Terminal">
       <Stack fill vertical>
         <Stack.Item align="center">
-          <Icon color="good" name={shop_logo} size="5" />
+          <Icon color="good" name={shop_logo} size={5} />
         </Stack.Item>
         <Stack.Item grow textAlign="center">
           <Tooltip content={description} position="bottom">

tgui/packages/tgui/interfaces/IntegratedCircuit/FundamentalTypes.js
@@ -87,8 +87,7 @@ export const FUNDAMENTAL_DATA_TYPES = {
         options={data}
         onSelected={setValue}
         displayText={value}
-        openWidth={large ? '200px' : undefined}
-        noscroll
+        menuWidth={large ? '200px' : undefined}
       />
     );
   },

tgui/packages/tgui/interfaces/NavBeacon.tsx
@@ -25,7 +25,7 @@ export type DisabledProps = {
 };
 
 export type NavBeaconStaticControl = {
-  direction_options: String[];
+  direction_options: string[];
   has_codes: BooleanLike;
 };
 

tgui/packages/tgui/interfaces/ParticleEdit/Generators.tsx
@@ -181,7 +181,7 @@ export const GeneratorListEntry = (props: GeneratorProps, context) => {
         <Dropdown
           options={RandTypes}
           selected={rand_type}
-          onSelected={(e, value) =>
+          onSelected={(value) =>
             act('edit', {
               var: var_name,
               var_mod: P_DATA_GENERATOR,

commit 0f15d90d7fb782f058dab15fb4ab56018d417fb7

Icon font size & rotation are applied correctly (#75367)
## About The Pull Request
Fixes #75264

GPS arrows work again
![Screenshot
(208)](https://github.com/tgstation/tgstation/assets/110812394/0d9b76f9-389e-4936-b576-b85e3fdd10e0)

Fixes #75292

All icons using `font-size` are back to normal size
![Screenshot
(207)](https://github.com/tgstation/tgstation/assets/110812394/58b7c6bd-1c3d-4789-8bf8-5e67f24e8c01)

Also fixes reflector UI. That bottom arrow that is out of place
Before

![Before](https://github.com/tgstation/tgstation/assets/110812394/b02f06f7-5524-443c-a263-7e9dbd1144a9)
After

![After](https://github.com/tgstation/tgstation/assets/110812394/68f9af02-6a8c-4d9e-ba66-fe4415245131)



Broken by #75164

## Changelog
:cl:
fix: gps arrows rotate correctly
fix: icons have their correct sizes applied again
/:cl:

tgui/packages/tgui/components/Icon.tsx
@@ -39,6 +39,7 @@ export const Icon = (props: IconProps) => {
     }
     style['transform'] = `rotate(${rotation}deg)`;
   }
+  rest.style = style;
 
   const boxProps = computeBoxProps(rest);
 