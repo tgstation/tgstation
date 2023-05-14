54b26b135aec94f55994dd4dc09861de11b3c34a
Jeremiah <jlsnow301@pm.me>
2023-05-13 20:29:26 -0700

    window fns

--- tgui/packages/tgui-say/helpers.ts
+++ tgui/packages/tgui-say/helpers.ts
@@ -3,6 +3,7 @@ import { WINDOW_SIZES } from './constants';
 /**
  * Window functions
  */
+
 /**
  * Once byond signals this via keystroke, it
  * ensures window size, visibility, and focus.
@@ -35,31 +36,35 @@ export const windowLoad = () => {
  * Parameters:
  *  size - The size of the window in pixels. Optional.
  */
-export const windowSet = (size: number = WINDOW_SIZES.small) => {
-  Byond.winset('tgui_say', { size: `${WINDOW_SIZES.width}x${size}` });
-  Byond.winset('tgui_say.browser', { size: `${WINDOW_SIZES.width}x${size}` });
+export const windowSet = (size = WINDOW_SIZES.small) => {
+  setWindowSizeAndVisibility(true, size);
 };
 
 /** Private functions */
 /** Sets the skin props as opened. Focus might be a placebo here. */
 const setOpen = () => {
-  Byond.winset('tgui_say', {
-    'is-visible': true,
-    size: `${WINDOW_SIZES.width}x${WINDOW_SIZES.small}`,
-  });
-  Byond.winset('tgui_say.browser', {
-    'is-visible': true,
-    size: `${WINDOW_SIZES.width}x${WINDOW_SIZES.small}`,
-  });
+  setWindowSizeAndVisibility(true, WINDOW_SIZES.small);
 };
 
 /** Sets the skin props as closed.  */
 const setClosed = () => {
-  Byond.winset('tgui_say', {
-    'is-visible': false,
-    size: `${WINDOW_SIZES.width}x${WINDOW_SIZES.small}`,
-  });
+  setWindowSizeAndVisibility(false, WINDOW_SIZES.small);
   Byond.winset('map', {
     focus: true,
+  });
+};
+
+// Helper function
+const setWindowSizeAndVisibility = (isVisible: boolean, size: number) => {
+  const sizeStr = `${WINDOW_SIZES.width}x${size}`;
+
+  Byond.winset('tgui_say', {
+    'is-visible': isVisible,
+    size: sizeStr,
+  });
+
+  Byond.winset('tgui_say.browser', {
+    'is-visible': isVisible,
+    size: sizeStr,
   });
 };
