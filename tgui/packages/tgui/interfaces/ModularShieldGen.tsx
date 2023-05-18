c51e298e403ed93ae47943b030d66d43a6119eb3
moocowswag <nspruell13@gmail.com>
2023-05-17 22:54:22 -0400

    uirework

--- tgui/packages/tgui/interfaces/ModularShieldGen.tsx
+++ tgui/packages/tgui/interfaces/ModularShieldGen.tsx
@@ -1,6 +1,6 @@
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
-import { Stack, Section, ProgressBar, Button, NumberInput } from '../components';
+import { Stack, Section, ProgressBar, Button, NumberInput, LabeledList } from '../components';
 import { BooleanLike } from 'common/react';
 
 type ModularShieldGenData = {
@@ -35,85 +35,94 @@ export const ModularShieldGen = (props, context) => {
   return (
     <Window title="Modular Shield Generator" width={600} height={400}>
       <Window.Content scrollable>
-        <Stack vertical fill>
-          <Stack.Item>
-            <Section
-              title={'Shield Strength'}
-              color={recovering ? 'red' : 'white'}>
-              <ProgressBar
-                title="Shield Strength"
-                value={current_strength}
-                minValue={0}
-                maxValue={max_strength}
-                height={'30px'}
-                ranges={{
-                  'good': [max_strength * 0.75, max_strength],
-                  'average': [max_strength * 0.25, max_strength * 0.75],
-                  'bad': [0, max_strength * 0.25],
-                }}>
-                {current_strength}/{max_strength}
-              </ProgressBar>
-            </Section>
-            <Section
-              horizontal
-              height={'95px'}
-              title={'Regeneration and Radius'}>
-              <Section height={'20px'}>
-                <ProgressBar
-                  height={'20px'}
-                  title="Regeneration rate"
-                  value={current_regeneration}
-                  minValue={0}
-                  maxValue={max_regeneration}
-                  ranges={{
-                    'good': [max_regeneration * 0.75, max_regeneration],
-                    'average': [
-                      max_regeneration * 0.25,
-                      max_regeneration * 0.75,
-                    ],
-                    'bad': [0, max_regeneration * 0.25],
-                  }}>
-                  Regeneration {current_regeneration}/{max_regeneration}
-                </ProgressBar>
-              </Section>
-              <Section>
-                <ProgressBar
-                  height={'20px'}
-                  title="Shield radius"
-                  value={current_radius}
-                  minValue={0}
-                  maxValue={max_radius}
-                  ranges={{
-                    'good': [max_radius * 0.75, max_radius],
-                    'average': [max_radius * 0.25, max_radius * 0.75],
-                    'bad': [0, max_radius * 0.25],
-                  }}>
-                  Radius {current_radius}/{max_radius}
-                </ProgressBar>
-              </Section>
-              <Section title={'Settings'} bold={1}>
-                <Section>
-                  <Section>
+        <Section title={'Shield Strength'} color={recovering ? 'red' : 'white'}>
+          <ProgressBar
+            title="Shield Strength"
+            value={current_strength}
+            minValue={0}
+            maxValue={max_strength}
+            height={'30px'}
+            ranges={{
+              'good': [max_strength * 0.75, max_strength],
+              'average': [max_strength * 0.25, max_strength * 0.75],
+              'bad': [0, max_strength * 0.25],
+            }}>
+            {current_strength}/{max_strength}
+          </ProgressBar>
+        </Section>
+        <Section title={'Regeneration and Radius'}>
+          <ProgressBar
+            height={'20px'}
+            title="Regeneration rate"
+            value={current_regeneration}
+            minValue={0}
+            maxValue={max_regeneration}
+            ranges={{
+              'good': [max_regeneration * 0.75, max_regeneration],
+              'average': [max_regeneration * 0.25, max_regeneration * 0.75],
+              'bad': [0, max_regeneration * 0.25],
+            }}>
+            Regeneration {current_regeneration}/{max_regeneration}
+          </ProgressBar>
+          <Section>
+            <ProgressBar
+              height={'20px'}
+              title="Shield radius"
+              value={current_radius}
+              minValue={0}
+              maxValue={max_radius}
+              ranges={{
+                'good': [max_radius * 0.75, max_radius],
+                'average': [max_radius * 0.25, max_radius * 0.75],
+                'bad': [0, max_radius * 0.25],
+              }}>
+              Radius {current_radius}/{max_radius}
+            </ProgressBar>
+          </Section>
+        </Section>
+        <Section title={'Settings'}>
+          <Stack>
+            <Section>
+              <LabeledList>
+                <LabeledList.Item label="Set Radius">
+                  <Section right={0.75}>
                     <NumberInput
-                      title={'Set Radius'}
                       disabled={active}
                       value={current_radius}
                       minValue={3}
                       maxValue={max_radius}
-                      stepPixelSize={10}
-                      lineHeight="50px"
-                      fontSize="20px"
-                      width="120px"
-                      height="50px"
+                      lineHeight="30px"
+                      fontSize="15px"
+                      width="60px"
+                      height="20px"
                       onChange={(e, value) =>
                         act('set_radius', {
                           new_radius: value,
                         })
                       }
                     />
+                  </Section>
+                </LabeledList.Item>
+              </LabeledList>
+            </Section>
+            <Section>
+              <LabeledList>
+                <LabeledList.Item label="Placement limitations">
+                  <Section right={0.75}>
                     <Button
-                      top={3.25}
-                      right={-16.75}
+                      disabled={active}
+                      onClick={() => act('toggle_exterior')}>
+                      {exterior_only ? 'External only' : 'Internal & External'}
+                    </Button>
+                  </Section>
+                </LabeledList.Item>
+              </LabeledList>
+            </Section>
+            <Section>
+              <LabeledList>
+                <LabeledList.Item label="Set Radius">
+                  <Section right={0.75}>
+                    <Button
                       bold={1}
                       disabled={recovering || initiating_field}
                       selected={active}
@@ -121,27 +130,21 @@ export const ModularShieldGen = (props, context) => {
                       icon="power-off"
                       onClick={() => act('toggle_shields')}
                     />
-                    <Button
-                      disabled={active}
-                      right={4.25}
-                      top={3.25}
-                      onClick={() => act('toggle_exterior')}>
-                      {exterior_only ? 'External only' : 'Internal & External'}
-                    </Button>
-
-                    <Section title={'Set Radius'} right={-10.5} top={-4.25}>
-                      Toggle Placement
-                    </Section>
-                    <Section right={-25} top={-7.35}>
-                      Toggle Shields
-                    </Section>
                   </Section>
-                </Section>
-              </Section>
+                </LabeledList.Item>
+              </LabeledList>
             </Section>
-          </Stack.Item>
-        </Stack>
+          </Stack>
+        </Section>
       </Window.Content>
     </Window>
   );
+  <Button
+    bold={1}
+    disabled={recovering || initiating_field}
+    selected={active}
+    content={active ? 'On' : 'Off'}
+    icon="power-off"
+    onClick={() => act('toggle_shields')}
+  />;
 };
