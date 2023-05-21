5a8057361c270f56e2ed7dbbf77472434e9ae03d
Dominion <Cyberboss@users.noreply.github.com>
2023-05-21 00:28:05 -0400

    Fix file names

--- tools/ci/install_auxlua.sh
+++ tools/ci/install_auxlua.sh
@@ -5,5 +5,5 @@ source dependencies.sh
 
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/libauxlua.so "https://github.com/$AUXLUA_REPO/releases/download/$AUXLUA_VERSION/libauxlua-coverage.so"
-chmod +x ~/.byond/bin/libauxcov.so
-ldd ~/.byond/bin/libauxcov.so
+chmod +x ~/.byond/bin/libauxlua.so
+ldd ~/.byond/bin/libauxlua.so
