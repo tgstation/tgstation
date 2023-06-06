8fda0f53372fdc72e09159344f703c64a7d33e43
Ghommie <42542238+Ghommie@users.noreply.github.com>
2023-06-06 23:28:55 +0200

    AH

--- tools/UpdatePaths/__main__.py
+++ tools/UpdatePaths/__main__.py
@@ -13,7 +13,7 @@ Replacement syntax example:
     /turf/open/floor/iron/warningline : /obj/effect/turf_decal {@OLD} , /obj/thing {icon_state = @OLD:name; name = "meme"}
     /turf/open/floor/iron/warningline{dir=2} : /obj/thing
     /obj/effect/landmark/start/virologist : @DELETE
-    /mob/living{resize:@ANY} : /mob/living{@OLD; resize = @SKIP, current_size = @OLD:resize}
+    /mob/living{resize = @ANY} : /mob/living{@OLD; resize = @SKIP, current_size = @OLD:resize}
 Syntax for subtypes also exist, to update a path's type but maintain subtypes:
     /obj/structure/closet/crate/@SUBTYPES : /obj/structure/new_box/@SUBTYPES {@OLD}
 New paths properties:
