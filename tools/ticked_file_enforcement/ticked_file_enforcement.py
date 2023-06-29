5fc096d1ceb0f0a671e6dc3be2c24ac2c221620c
san7890 <the@san7890.com>
2023-06-28 22:45:24 -0600

    more sanity

--- tools/ticked_file_enforcement/ticked_file_enforcement.py
+++ tools/ticked_file_enforcement/ticked_file_enforcement.py
@@ -18,18 +18,26 @@ def red(text):
 def blue(text):
     return "\033[34m" + str(text) + "\033[0m"
 
-def post_error(string):
-    print(red("Ticked File Enforcement: " + string))
-    if on_github:
-        print(f"::error file={file_reference},line=1,title=Ticked File Enforcement::{string}")
-
 schema = json.load(sys.stdin)
 file_reference = schema["file"]
 file_reference_basename = os.path.basename(file_reference)
 scannable_directory = schema["scannable_directory"]
 subdirectories = schema["subdirectories"]
-excluded_files = schema["excluded_files"]
 FORBIDDEN_INCLUDES = schema["forbidden_includes"]
+excluded_files = schema["excluded_files"]
+
+def post_error(string):
+    print(red(f"Ticked File Enforcement [{file_reference}]: " + string))
+    if on_github:
+        print(f"::error file={file_reference},line=1,title=Ticked File Enforcement::{string}")
+
+for excluded_file in excluded_files:
+    trimmed_file_name = excluded_file[:-3]
+    full_file_path = scannable_directory.replace('*', trimmed_file_name)
+    if not os.path.isfile(excluded_file):
+        post_error(f"Excluded file {full_file_path} does not exist, please remove it!")
+        sys.exit(1)
+
 
 reading = False
 lines = []
@@ -55,7 +63,7 @@ if lines == []:
     sys.exit(1)
 
 offset = total - len(lines)
-print(blue(f"Ticked File Enforcement: {offset} lines were ignored in output for {file_reference}."))
+print(blue(f"Ticked File Enforcement: {offset} lines were ignored in output for [{file_reference}]."))
 fail_no_include = False
 
 for code_file in glob.glob(scannable_directory, recursive=True):
@@ -76,7 +84,7 @@ for code_file in glob.glob(scannable_directory, recursive=True):
         forbid_include = True
 
         if included:
-            post_error(f"[{file_reference}] {dm_path} should NOT be included.")
+            post_error(f"{dm_path} should NOT be included.")
             fail_no_include = True
 
     if forbid_include:
@@ -89,7 +97,7 @@ for code_file in glob.glob(scannable_directory, recursive=True):
         if(dm_path in excluded_files):
             continue
 
-        post_error(f"[{file_reference}] Missing include for {dm_path}.")
+        post_error(f"Missing include for {dm_path}.")
         fail_no_include = True
 
 if fail_no_include:
@@ -124,7 +132,7 @@ def compare_lines(a, b):
 sorted_lines = sorted(lines, key = functools.cmp_to_key(compare_lines))
 for (index, line) in enumerate(lines):
     if sorted_lines[index] != line:
-        post_error(f"[{file_reference}] The include at line {index + offset} is out of order ({line}, expected {sorted_lines[index]})")
+        post_error(f"The include at line {index + offset} is out of order ({line}, expected {sorted_lines[index]})")
         sys.exit(1)
 
 print(green(f"Ticked File Enforcement: [{file_reference}] All includes are in order!"))
