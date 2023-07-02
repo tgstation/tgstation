5eaeb9edd3145db3b86735aca83cc66a78add725
Iamgoofball <iamgoofball@gmail.com>
2023-07-01 20:29:40 -0700

    Update tts-api.py

--- tools/tts/tts-api/tts-api.py
+++ tools/tts/tts-api/tts-api.py
@@ -4,7 +4,11 @@ import gc
 import subprocess
 import requests
 import re
+import pysbd
+import pydub
+import string
 from flask import Flask, request, send_file, abort, make_response
+tts_sample_rate = 22050 # Set to 40000 if you're using RVC, or whatever sample rate your endpoint is going to send the audio in.
 
 app = Flask(__name__)
 
@@ -19,18 +23,28 @@ def hhmmss_to_seconds(string):
 
 def text_to_speech_handler(endpoint, voice, text, filter_complex, pitch, silicon = False):
 	filter_complex = filter_complex.replace("\"", "")
-	response = requests.get(f"http://tts-container:5003/" + endpoint, json={ 'text': text, 'voice': voice, 'pitch': pitch })
-	if response.status_code != 200:
-		abort(500)
-
+	data_bytes = io.BytesIO()
+	final_audio = pydub.AudioSegment.empty()
+	for sentence in segmenter.segment(text):
+		response = requests.get(f"http://127.0.0.1:5003/" + endpoint, json={ 'text': sentence, 'voice': voice, 'pitch': pitch })
+		if response.status_code != 200:
+			abort(500)
+		sentence_audio = pydub.AudioSegment.from_file(io.BytesIO(response.content), "wav")
+		sentence_silence = pydub.AudioSegment.silent(250, 40000)
+		sentence_audio += sentence_silence
+		final_audio += sentence_audio
+		# ""Goldman-Eisler (1968) determined that typical speakers paused for an average of 250 milliseconds (ms), with a range from 150 to 400 ms.""
+		# (https://scholarsarchive.byu.edu/cgi/viewcontent.cgi?article=10153&context=etd)
+	final_audio.export(data_bytes, format="wav")
+	filter_complex = filter_complex.replace("%SAMPLE_RATE%", str(tts_sample_rate))
 	ffmpeg_result = None
 	if filter_complex != "":
-		ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-filter_complex", filter_complex, "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=response.content, capture_output = True)
+		ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-filter_complex", filter_complex, "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=data_bytes.read(), capture_output = True)
 	else:
 		if silicon:
-			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-i", "./SynthImpulse.wav", "-i", "./RoomImpulse.wav", "-filter_complex", "[0] aresample=44100 [re_1]; [re_1] apad=pad_dur=2 [in_1]; [in_1] asplit=2 [in_1_1] [in_1_2]; [in_1_1] [1] afir=dry=10:wet=10 [reverb_1]; [in_1_2] [reverb_1] amix=inputs=2:weights=8 1 [mix_1]; [mix_1] asplit=2 [mix_1_1] [mix_1_2]; [mix_1_1] [2] afir=dry=1:wet=1 [reverb_2]; [mix_1_2] [reverb_2] amix=inputs=2:weights=10 1 [mix_2]; [mix_2] equalizer=f=7710:t=q:w=0.6:g=-6,equalizer=f=33:t=q:w=0.44:g=-10 [out]; [out] alimiter=level_in=1:level_out=1:limit=0.5:attack=5:release=20:level=disabled", "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=response.content, capture_output = True)
+			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-i", "./SynthImpulse.wav", "-i", "./RoomImpulse.wav", "-filter_complex", "[0] aresample=44100 [re_1]; [re_1] apad=pad_dur=2 [in_1]; [in_1] asplit=2 [in_1_1] [in_1_2]; [in_1_1] [1] afir=dry=10:wet=10 [reverb_1]; [in_1_2] [reverb_1] amix=inputs=2:weights=8 1 [mix_1]; [mix_1] asplit=2 [mix_1_1] [mix_1_2]; [mix_1_1] [2] afir=dry=1:wet=1 [reverb_2]; [mix_1_2] [reverb_2] amix=inputs=2:weights=10 1 [mix_2]; [mix_2] equalizer=f=7710:t=q:w=0.6:g=-6,equalizer=f=33:t=q:w=0.44:g=-10 [out]; [out] alimiter=level_in=1:level_out=1:limit=0.5:attack=5:release=20:level=disabled", "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=data_bytes.read(), capture_output = True)
 		else:
-			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input=response.content, capture_output = True)
+			ffmpeg_result = subprocess.run(["ffmpeg", "-f", "wav", "-i", "pipe:0", "-c:a", "libvorbis", "-b:a", "64k", "-f", "ogg", "pipe:1"], input= data_bytes.read(), capture_output = True)
 	ffmpeg_metadata_output = ffmpeg_result.stderr.decode()
 	print(f"ffmpeg result size: {len(ffmpeg_result.stdout)} stderr = \n{ffmpeg_metadata_output}")
 
