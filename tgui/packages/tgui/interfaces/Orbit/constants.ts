6d02887d312b27819d3851935d4cad23f62aa054
Rhials <Datguy33456@gmail.com>
2023-07-10 22:27:31 -0400

    and why he ourple :3

--- tgui/packages/tgui/interfaces/Orbit/constants.ts
+++ tgui/packages/tgui/interfaces/Orbit/constants.ts
@@ -6,6 +6,7 @@ export const ANTAG2COLOR = {
   'Emergency Response Team': 'teal',
   'Escaped Fugitives': 'orange',
   'Bounty Hunters': 'yellow',
+  'Xenomorph Infestation': 'purple',
 } as const;
 
 export const THREAT = {
