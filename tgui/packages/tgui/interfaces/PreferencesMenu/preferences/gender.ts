dfec263578c7c5933809bd6ced0ef402e669e630
Vekter <spacialvekter@gmail.com>
2023-07-12 17:32:54 -0500

    Adds disclaimer in case we have an oopsie

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/gender.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/gender.ts
@@ -23,6 +23,6 @@ export const GENDERS = {
 
   [Gender.Other2]: {
     icon: 'neuter',
-    text: 'It/Its',
+    text: 'It/Its NOTE: BYOND does not have in-built support for this pronoun option. In rare cases, the game may default back to they/them pronouns.',
   },
 };
