141dd574130903e540c7693b385d176071972a8a
san7890 <the@san7890.com>
2023-07-17 17:30:59 -0600

    readd

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -5,6 +5,7 @@ source dependencies.sh
 
 sudo dpkg --add-architecture i386
 sudo apt update
+sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
 
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
