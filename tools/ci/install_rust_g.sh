f35fa6bbe9ee9414d4bc242d18f7603f5850ef66
san7890 <the@san7890.com>
2023-07-17 16:53:29 -0600

    test a removal of the libssl dependency
    also unifies everything into `install_rust_g.sh` because that's easier and i feel like we should have been doing it from the start

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 source dependencies.sh
 
+sudo dpkg --add-architecture i386
+sudo apt update
+
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ~/.byond/bin/librust_g.so
