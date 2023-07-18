943f71624e38b2c8a9fb3b06404a6c1351196ebc
san7890 <the@san7890.com>
2023-07-17 19:59:55 -0600

    removes the libssl thing again

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -5,7 +5,6 @@ source dependencies.sh
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -o APT::Immediate-Configure=false libssl1.1:i386
 
 mkdir -p ~/.byond/bin
 wget -nv -O ~/.byond/bin/librust_g.so "https://file.house/jZXt.so"
