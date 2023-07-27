c0938cca01f6ba806f319c5e93b94a60c99873be
larentoun <31931237+larentoun@users.noreply.github.com>
2023-07-27 22:06:34 +0900

    Initial Modularity Support (#1)
    <!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
    not be viewable. -->
    <!-- You can view Contributing.MD for a detailed description of the pull
    request process. -->
    
    <!-- Describe The Pull Request. Please be sure every change is
    documented or this can delay review and even discourage maintainers from
    merging your PR! -->
    
    В чейнджлоге
    
    <!-- Argue for the merits of your changes and how they benefit the game,
    especially if they are controversial and/or far reaching. If you can't
    actually explain WHY what you are doing will improve the game, then it
    probably isn't good for the game in the first place. -->
    
    Первый шаг к светлому будущему
    
    <!-- If your PR modifies aspects of the game that can be concretely
    observed by players or admins you should add a changelog. If your change
    does NOT meet this description, remove this section. Be sure to properly
    mark your PRs to prevent unnecessary GBP loss. You can read up on GBP
    and it's effects on PRs in the tgstation guides for contributors. Please
    note that maintainers freely reserve the right to remove and add tags
    should they deem it appropriate. You can attempt to finagle the system
    all you want, but it's best to shoot for clear communication right off
    the bat. -->
    
    :cl:
    code: Добавлена поддержка модпаков
    config: Добавлен файл конфига для модпаков
    code: Обновлен CI под модпаки
    code: Изменен метод обнаружения мердж конфликтов
    /:cl:
    
    <!-- Both :cl:'s are required for the changelog to work! You can put
    your name to the right of the first :cl: if you want to overwrite your
    GitHub username as author ingame. -->
    <!-- You can use multiple of the same prefix (they're only used for the
    icon ingame) and delete the unneeded ones. Despite some of the tags,
    changelogs should generally represent how a player might be affected by
    the changes rather than a summary of the PR's contents. -->

--- tools/build/build.js
+++ tools/build/build.js
@@ -80,6 +80,7 @@ export const DmTarget = new Juke.Target({
     'html/**',
     'icons/**',
     'interface/**',
+    'modular_bandastation/**', // BANDASTATION EDIT ADDITION - Making the CBT work
     `${DME_NAME}.dme`,
     NamedVersionFile,
   ],
