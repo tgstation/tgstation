bb9421860f47970b2122ba2f823a4e0ac6c2bb1e
Niko <20830349+Peliex@users.noreply.github.com>
2023-07-30 11:47:30 -0700

    adds task to build all maps, probably will never be used but oh well

--- tools/UpdatePaths/Update Paths.bat
+++ tools/UpdatePaths/Update Paths.bat
@@ -1,19 +1,39 @@
 @echo off
-
+setlocal EnableDelayedExpansion
+if "%~1"=="-tooltask" (
+	goto run-updatepaths
+)
 if "%~1"=="" (
+	:run-updatepaths
 	echo Running UpdatePaths with all scripts in the Scripts directory.
 	echo This will take a while, and cannot be interrupted without data corruption.
 	echo Make sure you commit all changes first!
+
+	:ask-to-continue
+	set "continue="
 	echo Are you sure you want to continue? ^(y / n^)
 	set /p "continue="
 
-	if /i "%continue%"=="y" (
+	if "!continue!"=="" (
+		goto ask-to-continue
+	) else if /i "!continue!"=="y" (
 		for /f "delims=" %%F in ('dir /b /on /a-d scripts\*_*.txt') do (
       		call "%~dp0\..\bootstrap\python" -m UpdatePaths "scripts\%%~nxF"
 		)
+	) else if /i "!continue!"=="n" (
+		echo usage: ^"Update Paths.bat^" [-h] [--map MAP] [--directory DIRECTORY] [--inline] [--verbose] script_path
 	) else (
-		echo usage: ^"Update Paths.bat^" [-h] [--map MAP] [--directory DIRECTORY] [--inline] [--verbose] script
+		goto ask-to-continue
 	)
+	goto stop
 ) else (
 	call "%~dp0\..\bootstrap\python" -m UpdatePaths %*
 )
+
+:stop
+if "%~1"=="-tooltask" (
+	goto eof
+)
+echo %CMDCMDLINE% | findstr /C:"/c">nul && pause
+
+:eof
