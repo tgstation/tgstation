1ee411f3b2c4c858b73ff9b03e83987ccf206bd6
Niko <20830349+Peliex@users.noreply.github.com>
2023-07-30 10:50:10 -0700

    change update paths logic, currently causes python to fail

--- tools/UpdatePaths/Update Paths.bat
+++ tools/UpdatePaths/Update Paths.bat
@@ -1,3 +1,19 @@
 @echo off
-call "%~dp0\..\bootstrap\python" -m UpdatePaths %*
-pause
+
+if "%~1"=="" (
+	echo Running UpdatePaths with all scripts in the Scripts directory.
+	echo This will take a while, and cannot be interrupted without data corruption.
+	echo Make sure you commit all changes first!
+	echo Are you sure you want to continue? ^(y / n^)
+	set /p "continue="
+
+	if /i "%continue%"=="y" (
+		for /f "delims=" %%F in ('dir /b /on /a-d scripts\*_*.txt') do (
+      		call "%~dp0\..\bootstrap\python" -m UpdatePaths "scripts\%%~nxF"
+		)
+	) else (
+		echo usage: ^"Update Paths.bat^" [-h] [--map MAP] [--directory DIRECTORY] [--inline] [--verbose] script
+	)
+) else (
+	call "%~dp0\..\bootstrap\python" -m UpdatePaths %*
+)
