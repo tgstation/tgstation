tgstation/tgstation

commit 3902973978a78064a495bd7e9769dc3f77d25344

Unit Test Font Awesome icons and Quirk icons (#74573)
## About The Pull Request

It's helpful to know what icons we have, and if any are invalid

## Why It's Good For The Game
## Changelog

---------

Co-authored-by: Mothblocks <35135081+Mothblocks@users.noreply.github.com>

tools/define_sanity/check.py
@@ -38,7 +38,7 @@ excluded_files = [
     "code/modules/tgs/**/*.dm",
 ]
 
-define_regex = re.compile(r"#define\s?([A-Z0-9_]+)\(?(.+)\)?")
+define_regex = re.compile(r"^(\s+)?#define\s?([A-Z0-9_]+)\(?(.+)\)?")
 
 files_to_scan = []
 
@@ -67,7 +67,7 @@ for applicable_file in files_to_scan:
     with open(applicable_file, encoding="utf8") as file:
         file_contents = file.read()
         for define in define_regex.finditer(file_contents):
-            define_name = define.group(1)
+            define_name = define.group(2)
             if not re.search("#undef\s" + define_name, file_contents):
                 located_error_tuples.append((define_name, applicable_file))
 

code/modules/unit_tests/font_awesome_icons.dm
@@ -1,0 +1,96 @@
+/**
+ * This unit test verifies that all Font Awesome icons are present in code, and that all quirk icons are valid.
+ */
+/datum/unit_test/font_awesome_icons
+	var/font_awesome_css
+	var/list/allowed_icons
+
+/datum/unit_test/font_awesome_icons/Run()
+	var/font_awesome_file = file('html/font-awesome/css/all.min.css')
+	if(isnull(font_awesome_file))
+		TEST_FAIL("Font Awesome CSS file could not be found!")
+		return
+
+	font_awesome_css = file2text(font_awesome_file)
+	if(isnull(font_awesome_css))
+		TEST_NOTICE(src, "Font Awesome CSS file could not be loaded.")
+		return
+
+	load_parse_verify()
+	verify_quirk_icons()
+	generate_helper_dm_file()
+
+/**
+ * Loads the Font Awesome CSS file, parses it into a list of icon names, and compares it to the list of icons in code.
+ * If there are any differences, note them.
+ */
+/datum/unit_test/font_awesome_icons/proc/load_parse_verify()
+	log_test("CSS Actual: [length(font_awesome_css)]")
+	allowed_icons = parse_fa_css_into_icon_list(font_awesome_css)
+
+/**
+ * Verifies that all quirk icons are valid.
+ */
+/datum/unit_test/font_awesome_icons/proc/verify_quirk_icons()
+	for(var/datum/quirk/quirk as anything in subtypesof(/datum/quirk))
+		if(quirk == initial(quirk.abstract_parent_type))
+			continue
+
+		var/quirk_icon = initial(quirk.icon)
+		if(findtext(quirk_icon, "tg-") == 1) // TODO: Validate these as well
+			continue
+
+		if(findtext(quirk_icon, " "))
+			var/list/split = splittext(quirk_icon, " ")
+			quirk_icon = split[length(split)] // respect modifier classes
+
+		if(!(quirk_icon in allowed_icons))
+			TEST_FAIL("Quirk [initial(quirk.name)]([quirk]) has invalid icon: [quirk_icon]")
+
+/// Parses the given Font Awesome CSS file into a list of icon names.
+/datum/unit_test/font_awesome_icons/proc/parse_fa_css_into_icon_list(css)
+	css = replacetext(css, "\n", "")
+	var/list/css_entries = splittext(css, "}")
+	var/list/icons = list()
+	for(var/entry in css_entries)
+		entry = replacetext(entry, "\t", "")
+		if(!length(entry))
+			continue
+
+		var/entry_contents = splittext(entry, "{")
+		var/list/entry_names = splittext(entry_contents[1], ",")
+		for(var/entry_name in entry_names)
+			entry_names -= entry_name
+
+			if(!findtext(entry_name, ":"))
+				continue
+
+			entry_name = splittext(entry_name, ":")[1]
+			if(!findtext(entry_name, ".fa-"))
+				continue
+
+			entry_name = replacetext(entry_name, ".fa-", "fa-")
+			entry_names |= entry_name
+		icons |= entry_names
+
+	return sort_list(icons)
+
+/datum/unit_test/font_awesome_icons/proc/generate_helper_dm_file()
+	var/list/output = list()
+	output += "/* This file is automatically generated by the unit test. Do not edit it manually."
+	output += " * Generating this file is done by running the unit test locally, see the fail message for more details."
+	output += " * All valid font awesome icons should be here."
+	output += " */"
+	output += ""
+
+	for(var/icon in allowed_icons)
+		var/icon_name = replacetext(icon, "fa-", "")
+		output += "#define FA_ICON_[uppertext(replacetext(icon_name, "-", "_"))] \"[icon]\""
+
+	var/output_file = "[output.Join("\n")]\n"
+	rustg_file_write(output_file, "data/font_awesome_icons.dm")
+	var/current = file2text('code/__DEFINES/font_awesome_icons.dm')
+	if(current == output_file)
+		return
+
+	TEST_FAIL("Font Awesome helper file is out of date. Run locally by enabling unit tests, (see _compile_options.dm) and copy 'data/font_awesome_icons.dm' to 'code/__DEFINES/font_awesome_icons.dm'")

code/datums/quirks/negative_quirks.dm
@@ -3,7 +3,7 @@
 /datum/quirk/badback
 	name = "Bad Back"
 	desc = "Thanks to your poor posture, backpacks and other bags never sit right on your back. More evenly weighted objects are fine, though."
-	icon = "hiking"
+	icon = FA_ICON_HIKING
 	value = -8
 	quirk_flags = QUIRK_HUMAN_ONLY|QUIRK_MOODLET_BASED
 	gain_text = span_danger("Your back REALLY hurts!")
@@ -54,7 +54,7 @@
 /datum/quirk/blooddeficiency
 	name = "Blood Deficiency"
 	desc = "Your body can't produce enough blood to sustain itself."
-	icon = "tint"
+	icon = FA_ICON_TINT
 	value = -8
 	mob_trait = TRAIT_BLOOD_DEFICIENCY
 	gain_text = span_danger("You feel your vigor slowly fading away.")
@@ -95,7 +95,7 @@
 /datum/quirk/item_quirk/blindness
 	name = "Blind"
 	desc = "You are completely blind, nothing can counteract this."
-	icon = "eye-slash"
+	icon = FA_ICON_EYE_SLASH
 	value = -16
 	gain_text = span_danger("You can't see anything.")
 	lose_text = span_notice("You miraculously gain back your vision.")
@@ -121,7 +121,7 @@
 /datum/quirk/item_quirk/brainproblems
 	name = "Brain Tumor"
 	desc = "You have a little friend in your brain that is slowly destroying it. Better bring some mannitol!"
-	icon = "brain"
+	icon = FA_ICON_BRAIN
 	value = -12
 	gain_text = span_danger("You feel smooth.")
 	lose_text = span_notice("You feel wrinkled again.")
@@ -154,7 +154,7 @@
 /datum/quirk/item_quirk/deafness
 	name = "Deaf"
 	desc = "You are incurably deaf."
-	icon = "deaf"
+	icon = FA_ICON_DEAF
 	value = -8
 	mob_trait = TRAIT_DEAF
 	gain_text = span_danger("You can't hear anything.")
@@ -169,7 +169,7 @@
 /datum/quirk/depression
 	name = "Depression"
 	desc = "You sometimes just hate life."
-	icon = "frown"
+	icon = FA_ICON_FROWN
 	mob_trait = TRAIT_DEPRESSION
 	value = -3
 	gain_text = span_danger("You start feeling depressed.")
@@ -182,7 +182,7 @@
 /datum/quirk/item_quirk/family_heirloom
 	name = "Family Heirloom"
 	desc = "You are the current owner of an heirloom, passed down for generations. You have to keep it safe!"
-	icon = "toolbox"
+	icon = FA_ICON_TOOLBOX
 	value = -2
 	medical_record_text = "Patient demonstrates an unnatural attachment to a family heirloom."
 	hardcore_value = 1
@@ -255,7 +255,7 @@
 /datum/quirk/glass_jaw
 	name = "Glass Jaw"
 	desc = "You have a very fragile jaw. Any sufficiently hard blow to your head might knock you out."
-	icon = "boxing-glove"
+	icon = FA_ICON_HAND_FIST
 	value = -4
 	gain_text = span_danger("Your jaw feels loose.")
 	lose_text = span_notice("Your jaw feels fitting again.")
@@ -306,7 +306,7 @@
 /datum/quirk/frail
 	name = "Frail"
 	desc = "You have skin of paper and bones of glass! You suffer wounds much more easily than most."
-	icon = "skull"
+	icon = FA_ICON_SKULL
 	value = -6
 	mob_trait = TRAIT_EASILY_WOUNDED
 	gain_text = span_danger("You feel frail.")
@@ -318,7 +318,7 @@
 /datum/quirk/heavy_sleeper
 	name = "Heavy Sleeper"
 	desc = "You sleep like a rock! Whenever you're put to sleep or knocked unconscious, you take a little bit longer to wake up."
-	icon = "bed"
+	icon = FA_ICON_BED
 	value = -2
 	mob_trait = TRAIT_HEAVY_SLEEPER
 	gain_text = span_danger("You feel sleepy.")
@@ -338,7 +338,7 @@
 /datum/quirk/hypersensitive
 	name = "Hypersensitive"
 	desc = "For better or worse, everything seems to affect your mood more than it should."
-	icon = "flushed"
+	icon = FA_ICON_FLUSHED
 	value = -2
 	gain_text = span_danger("You seem to make a big deal out of everything.")
 	lose_text = span_notice("You don't seem to make a big deal out of everything anymore.")
@@ -357,7 +357,7 @@
 /datum/quirk/light_drinker
 	name = "Light Drinker"
 	desc = "You just can't handle your drinks and get drunk very quickly."
-	icon = "cocktail"
+	icon = FA_ICON_COCKTAIL
 	value = -2
 	mob_trait = TRAIT_LIGHT_DRINKER
 	gain_text = span_notice("Just the thought of drinking alcohol makes your head spin.")
@@ -369,7 +369,7 @@
 /datum/quirk/item_quirk/nearsighted
 	name = "Nearsighted"
 	desc = "You are nearsighted without prescription glasses, but spawn with a pair."
-	icon = "glasses"
+	icon = FA_ICON_GLASSES
 	value = -4
 	gain_text = span_danger("Things far away from you start looking blurry.")
 	lose_text = span_notice("You start seeing faraway things normally again.")
@@ -406,7 +406,7 @@
 /datum/quirk/nyctophobia
 	name = "Nyctophobia"
 	desc = "As far as you can remember, you've always been afraid of the dark. While in the dark without a light source, you instinctively act careful, and constantly feel a sense of dread."
-	icon = "lightbulb"
+	icon = FA_ICON_LIGHTBULB
 	value = -3
 	medical_record_text = "Patient demonstrates a fear of the dark. (Seriously?)"
 	hardcore_value = 5
@@ -453,7 +453,7 @@
 /datum/quirk/nonviolent
 	name = "Pacifist"
 	desc = "The thought of violence makes you sick. So much so, in fact, that you can't hurt anyone."
-	icon = "peace"
+	icon = FA_ICON_PEACE
 	value = -8
 	mob_trait = TRAIT_PACIFISM
 	gain_text = span_danger("You feel repulsed by the thought of violence!")
@@ -465,7 +465,7 @@
 /datum/quirk/paraplegic
 	name = "Paraplegic"
 	desc = "Your legs do not function. Nothing will ever fix this. But hey, free wheelchair!"
-	icon = "wheelchair"
+	icon = FA_ICON_WHEELCHAIR
 	value = -12
 	gain_text = null // Handled by trauma.
 	lose_text = null
@@ -507,7 +507,7 @@
 /datum/quirk/poor_aim
 	name = "Stormtrooper Aim"
 	desc = "You've never hit anything you were aiming for in your life."
-	icon = "bullseye"
+	icon = FA_ICON_BULLSEYE
 	value = -4
 	mob_trait = TRAIT_POOR_AIM
 	medical_record_text = "Patient possesses a strong tremor in both hands."
@@ -517,7 +517,7 @@
 /datum/quirk/prosopagnosia
 	name = "Prosopagnosia"
 	desc = "You have a mental disorder that prevents you from being able to recognize faces at all."
-	icon = "user-secret"
+	icon = FA_ICON_USER_SECRET
 	value = -4
 	mob_trait = TRAIT_PROSOPAGNOSIA
 	medical_record_text = "Patient suffers from prosopagnosia and cannot recognize faces."
@@ -581,7 +581,7 @@
 /datum/quirk/pushover
 	name = "Pushover"
 	desc = "Your first instinct is always to let people push you around. Resisting out of grabs will take conscious effort."
-	icon = "handshake"
+	icon = FA_ICON_HANDSHAKE
 	value = -8
 	mob_trait = TRAIT_GRABWEAKNESS
 	gain_text = span_danger("You feel like a pushover.")
@@ -595,7 +595,7 @@
 	desc = "You suffer from a severe disorder that causes very vivid hallucinations. \
 		Mindbreaker toxin can suppress its effects, and you are immune to mindbreaker's hallucinogenic properties. \
 		THIS IS NOT A LICENSE TO GRIEF."
-	icon = "grin-tongue-wink"
+	icon = FA_ICON_GRIN_TONGUE_WINK
 	value = -8
 	gain_text = span_userdanger("...")
 	lose_text = span_notice("You feel in tune with the world again.")
@@ -638,7 +638,7 @@
 /datum/quirk/social_anxiety
 	name = "Social Anxiety"
 	desc = "Talking to people is very difficult for you, and you often stutter or even lock up."
-	icon = "comment-slash"
+	icon = FA_ICON_COMMENT_SLASH
 	value = -3
 	gain_text = span_danger("You start worrying about what you're saying.")
 	lose_text = span_notice("You feel easier about talking again.") //if only it were that easy!
@@ -753,7 +753,7 @@
 /datum/quirk/item_quirk/junkie
 	name = "Junkie"
 	desc = "You can't get enough of hard drugs."
-	icon = "pills"
+	icon = FA_ICON_PILLS
 	value = -6
 	gain_text = span_danger("You suddenly feel the craving for drugs.")
 	medical_record_text = "Patient has a history of hard drugs."
@@ -843,7 +843,7 @@
 /datum/quirk/item_quirk/junkie/smoker
 	name = "Smoker"
 	desc = "Sometimes you just really want a smoke. Probably not great for your lungs."
-	icon = "smoking"
+	icon = FA_ICON_SMOKING
 	value = -4
 	gain_text = span_danger("You could really go for a smoke right about now.")
 	medical_record_text = "Patient is a current smoker."
@@ -892,7 +892,7 @@
 /datum/quirk/unstable
 	name = "Unstable"
 	desc = "Due to past troubles, you are unable to recover your sanity if you lose it. Be very careful managing your mood!"
-	icon = "angry"
+	icon = FA_ICON_ANGRY
 	value = -10
 	mob_trait = TRAIT_UNSTABLE
 	gain_text = span_danger("There's a lot on your mind right now.")
@@ -904,7 +904,7 @@
 /datum/quirk/item_quirk/allergic
 	name = "Extreme Medicine Allergy"
 	desc = "Ever since you were a kid, you've been allergic to certain chemicals..."
-	icon = "prescription-bottle"
+	icon = FA_ICON_PRESCRIPTION_BOTTLE
 	value = -6
 	gain_text = span_danger("You feel your immune system shift.")
 	lose_text = span_notice("You feel your immune system phase back into perfect shape.")
@@ -999,7 +999,7 @@
 /datum/quirk/claustrophobia
 	name = "Claustrophobia"
 	desc = "You are terrified of small spaces and certain jolly figures. If you are placed inside any container, locker, or machinery, a panic attack sets in and you struggle to breathe."
-	icon = "box-open"
+	icon = FA_ICON_BOX_OPEN
 	value = -4
 	medical_record_text = "Patient demonstrates a fear of tight spaces."
 	hardcore_value = 5
@@ -1054,7 +1054,7 @@
 /datum/quirk/illiterate
 	name = "Illiterate"
 	desc = "You dropped out of school and are unable to read or write. This affects reading, writing, using computers and other electronics."
-	icon = "graduation-cap"
+	icon = FA_ICON_GRADUATION_CAP
 	value = -8
 	mob_trait = TRAIT_ILLITERATE
 	medical_record_text = "Patient is not literate."
@@ -1065,7 +1065,7 @@
 /datum/quirk/mute
 	name = "Mute"
 	desc = "For some reason you are completely unable to speak."
-	icon = "volume-xmark"
+	icon = FA_ICON_VOLUME_XMARK
 	value = -4
 	mob_trait = TRAIT_MUTE
 	gain_text = span_danger("You find yourself unable to speak!")
@@ -1076,7 +1076,7 @@
 /datum/quirk/body_purist
 	name = "Body Purist"
 	desc = "You believe your body is a temple and its natural form is an embodiment of perfection. Accordingly, you despise the idea of ever augmenting it with unnatural parts, cybernetic, prosthetic, or anything like it."
-	icon = "person-rays"
+	icon = FA_ICON_PERSON_RAYS
 	value = -2
 	quirk_flags = QUIRK_HUMAN_ONLY|QUIRK_MOODLET_BASED
 	gain_text = span_danger("You now begin to hate the idea of having cybernetic implants.")
@@ -1145,7 +1145,7 @@
 /datum/quirk/cursed
 	name = "Cursed"
 	desc = "You are cursed with bad luck. You are much more likely to suffer from accidents and mishaps. When it rains, it pours."
-	icon = "cloud-showers-heavy"
+	icon = FA_ICON_CLOUD_SHOWERS_HEAVY
 	value = -8
 	mob_trait = TRAIT_CURSED
 	gain_text = span_danger("You feel like you're going to have a bad day.")

code/datums/quirks/neutral_quirks.dm
@@ -4,7 +4,7 @@
 /datum/quirk/extrovert
 	name = "Extrovert"
 	desc = "You are energized by talking to others, and enjoy spending your free time in the bar."
-	icon = "users"
+	icon = FA_ICON_USERS
 	value = 0
 	mob_trait = TRAIT_EXTROVERT
 	gain_text = span_notice("You feel like hanging out with other people.")
@@ -15,7 +15,7 @@
 /datum/quirk/introvert
 	name = "Introvert"
 	desc = "You are energized by having time to yourself, and enjoy spending your free time in the library."
-	icon = "book-reader"
+	icon = FA_ICON_BOOK_READER
 	value = 0
 	mob_trait = TRAIT_INTROVERT
 	gain_text = span_notice("You feel like reading a good book quietly.")
@@ -26,7 +26,7 @@
 /datum/quirk/no_taste
 	name = "Ageusia"
 	desc = "You can't taste anything! Toxic food will still poison you."
-	icon = "meh-blank"
+	icon = FA_ICON_MEH_BLANK
 	value = 0
 	mob_trait = TRAIT_AGEUSIA
 	gain_text = span_notice("You can't taste anything!")
@@ -37,7 +37,7 @@
 /datum/quirk/foreigner
 	name = "Foreigner"
 	desc = "You're not from around here. You don't know Galactic Common!"
-	icon = "language"
+	icon = FA_ICON_LANGUAGE
 	value = 0
 	gain_text = span_notice("The words being spoken around you don't make any sense.")
 	lose_text = span_notice("You've developed fluency in Galactic Common.")
@@ -59,7 +59,7 @@
 /datum/quirk/vegetarian
 	name = "Vegetarian"
 	desc = "You find the idea of eating meat morally and physically repulsive."
-	icon = "carrot"
+	icon = FA_ICON_CARROT
 	value = 0
 	gain_text = span_notice("You feel repulsion at the idea of eating meat.")
 	lose_text = span_notice("You feel like eating meat isn't that bad.")
@@ -91,7 +91,7 @@
 /datum/quirk/snob
 	name = "Snob"
 	desc = "You care about the finer things, if a room doesn't look nice its just not really worth it, is it?"
-	icon = "user-tie"
+	icon = FA_ICON_USER_TIE
 	value = 0
 	gain_text = span_notice("You feel like you understand what things should look like.")
 	lose_text = span_notice("Well who cares about deco anyways?")
@@ -102,7 +102,7 @@
 /datum/quirk/pineapple_liker
 	name = "Ananas Affinity"
 	desc = "You find yourself greatly enjoying fruits of the ananas genus. You can't seem to ever get enough of their sweet goodness!"
-	icon = "thumbs-up"
+	icon = FA_ICON_THUMBS_UP
 	value = 0
 	gain_text = span_notice("You feel an intense craving for pineapple.")
 	lose_text = span_notice("Your feelings towards pineapples seem to return to a lukewarm state.")
@@ -128,7 +128,7 @@
 /datum/quirk/pineapple_hater
 	name = "Ananas Aversion"
 	desc = "You find yourself greatly detesting fruits of the ananas genus. Serious, how the hell can anyone say these things are good? And what kind of madman would even dare putting it on a pizza!?"
-	icon = "thumbs-down"
+	icon = FA_ICON_THUMBS_DOWN
 	value = 0
 	gain_text = span_notice("You find yourself pondering what kind of idiot actually enjoys pineapples...")
 	lose_text = span_notice("Your feelings towards pineapples seem to return to a lukewarm state.")
@@ -160,7 +160,7 @@
 /datum/quirk/deviant_tastes
 	name = "Deviant Tastes"
 	desc = "You dislike food that most people enjoy, and find delicious what they don't."
-	icon = "grin-tongue-squint"
+	icon = FA_ICON_GRIN_TONGUE_SQUINT
 	value = 0
 	gain_text = span_notice("You start craving something that tastes strange.")
 	lose_text = span_notice("You feel like eating normal food again.")
@@ -191,7 +191,7 @@
 /datum/quirk/heterochromatic
 	name = "Heterochromatic"
 	desc = "One of your eyes is a different color than the other!"
-	icon = "eye-low-vision" // Ignore the icon name, its actually a fairly good representation of different color eyes
+	icon = FA_ICON_EYE_LOW_VISION // Ignore the icon name, its actually a fairly good representation of different color eyes
 	quirk_flags = QUIRK_HUMAN_ONLY|QUIRK_CHANGES_APPEARANCE
 	value = 0
 	mail_goodies = list(/obj/item/clothing/glasses/eyepatch)
@@ -246,7 +246,7 @@
 /datum/quirk/monochromatic
 	name = "Monochromacy"
 	desc = "You suffer from full colorblindness, and perceive nearly the entire world in blacks and whites."
-	icon = "adjust"
+	icon = FA_ICON_ADJUST
 	value = 0
 	medical_record_text = "Patient is afflicted with almost complete color blindness."
 	mail_goodies = list( // Noir detective wannabe
@@ -270,7 +270,7 @@
 /datum/quirk/phobia
 	name = "Phobia"
 	desc = "You are irrationally afraid of something."
-	icon = "spider"
+	icon = FA_ICON_SPIDER
 	value = 0
 	medical_record_text = "Patient has an irrational fear of something."
 	mail_goodies = list(/obj/item/clothing/glasses/blindfold, /obj/item/storage/pill_bottle/psicodine)
@@ -291,7 +291,7 @@
 /datum/quirk/shifty_eyes
 	name = "Shifty Eyes"
 	desc = "Your eyes tend to wander all over the place, whether you mean to or not, causing people to sometimes think you're looking directly at them when you aren't."
-	icon = "far fa-eye"
+	icon = FA_ICON_EYE
 	value = 0
 	medical_record_text = "Fucking creep kept staring at me the whole damn checkup. I'm only diagnosing this because it's less awkward than thinking it was on purpose."
 	mob_trait = TRAIT_SHIFTY_EYES
@@ -300,7 +300,7 @@
 /datum/quirk/item_quirk/bald
 	name = "Smooth-Headed"
 	desc = "You have no hair and are quite insecure about it! Keep your wig on, or at least your head covered up."
-	icon = "egg"
+	icon = FA_ICON_EGG
 	value = 0
 	mob_trait = TRAIT_BALD
 	gain_text = span_notice("Your head is as smooth as can be, it's terrible.")
@@ -356,7 +356,7 @@
 /datum/quirk/item_quirk/photographer
 	name = "Photographer"
 	desc = "You carry your camera and personal photo album everywhere you go, and your scrapbooks are legendary among your coworkers."
-	icon = "camera"
+	icon = FA_ICON_CAMERA
 	value = 0
 	mob_trait = TRAIT_PHOTOGRAPHER
 	gain_text = span_notice("You know everything about photography.")
@@ -386,7 +386,7 @@
 /datum/quirk/item_quirk/colorist
 	name = "Colorist"
 	desc = "You like carrying around a hair dye spray to quickly apply color patterns to your hair."
-	icon = "fill-drip"
+	icon = FA_ICON_FILL_DRIP
 	value = 0
 	medical_record_text = "Patient enjoys dyeing their hair with pretty colors."
 	mail_goodies = list(/obj/item/dyespray)
@@ -398,7 +398,7 @@
 /datum/quirk/gamer
 	name = "Gamer"
 	desc = "You are a hardcore gamer, and you have a need to game. You love winning and hate losing. You only like gamer food."
-	icon = "gamepad"
+	icon = FA_ICON_GAMEPAD
 	value = 0
 	gain_text = span_notice("You feel the sudden urge to game.")
 	lose_text = span_notice("You've lost all interest in gaming.")
@@ -495,7 +495,7 @@
 /datum/quirk/item_quirk/pride_pin
 	name = "Pride Pin"
 	desc = "Show off your pride with this changing pride pin!"
-	icon = "rainbow"
+	icon = FA_ICON_RAINBOW
 	value = 0
 	gain_text = span_notice("You feel fruity.")
 	lose_text = span_danger("You feel only slightly less fruity than before.")

commit 018c10f9a04b655866667093d0672367819e6570

Fixes Define Sanity (#77845)
## About The Pull Request

Hey there,

This was broken in an update from #74573
(3902973978a78064a495bd7e9769dc3f77d25344), the RegEx was only catching
a fraction of the cases it was meant to be.

This is what we were finding on 74573 version of the RegEx:

![image](https://github.com/tgstation/tgstation/assets/34697715/a5040604-279f-4012-a5dd-3a1e9eea1b8e)

This is what we should be finding for all of the cases that
`define_sanity` will need to check for:

![image](https://github.com/tgstation/tgstation/assets/34697715/e33ad60e-fb36-448e-ae02-5494c916f450)

This is what was broken as a consequence:

![image](https://github.com/tgstation/tgstation/assets/34697715/567012db-a9c2-4118-aadf-f70996731af7)

As stated in the introductory PR #74333
(ccef887efec2cb3025228210bcb134700aac5175), it's not the end of the
world if we miss unmanaged local defines, but it's still useful to have
this as a maintainability tool to ensure that everything remains as
clean as it possibly can. I wish we could do the whole matching method
like the aforementioned PR supposed could happen, but it simply doesn't
appear to work the way we want it to.

## Changelog
Nothing player facing.

I tried to experiment with `UNLINT()` but I got absolutely ganked by
getting the regex to work, so the fix for the FA Icon file may not be
super duper great unfortunately. Let me know if you have a showstopper
idea, this is just a stopguard so this PR can get merged and I don't
have to keep talking about unmanaged local defines while reviewing PRs.

code/controllers/subsystem/tts.dm
@@ -415,3 +415,5 @@ SUBSYSTEM_DEF(tts)
 			return request.is_complete()
 	else
 		return request.is_complete() && request_blips.is_complete()
+
+#undef SHIFT_DATA_ARRAY

code/datums/station_traits/negative_traits.dm
@@ -694,3 +694,4 @@
 
 	storm_type = /datum/weather/snow_storm/forever_storm
 
+#undef GLOW_NEBULA

code/game/machinery/incident_display.dm
@@ -372,3 +372,6 @@ MAPPING_DIRECTIONAL_HELPERS(/obj/machinery/incident_display/tram, 32)
 #undef DESC_DUAL
 #undef DESC_DELAM
 #undef DESC_TRAM
+
+#undef TREND_RISING
+#undef TREND_FALLING

code/game/machinery/iv_drip.dm
@@ -460,3 +460,5 @@
 #undef IV_TRANSFER_RATE_STEP
 
 #undef ALERT_IV_CONNECTED
+
+#undef DEFAULT_IV_TRANSFER_RATE

code/game/machinery/status_display.dm
@@ -548,3 +548,5 @@ MAPPING_DIRECTIONAL_HELPERS(/obj/machinery/status_display/ai, 32)
 #undef LINE2_X
 #undef LINE2_Y
 #undef STATUS_DISPLAY_FONT_DATUM
+
+#undef SCROLL_PADDING

code/game/objects/items/handcuffs.dm
@@ -608,3 +608,6 @@
 	. = ..()
 	if(effectReference)
 		QDEL_NULL(effectReference)
+
+#undef HANDCUFFS_TYPE_WEAK
+#undef HANDCUFFS_TYPE_STRONG

tools/define_sanity/check.py
@@ -38,9 +38,11 @@ excluded_files = [
     "code/modules/tgs/**/*.dm",
 ]
 
-define_regex = re.compile(r"^(\s+)?#define\s?([A-Z0-9_]+)\(?(.+)\)?")
+define_regex = re.compile(r"(\s+)?#define\s?([A-Z0-9_]+)\(?(.+)\)?")
 
 files_to_scan = []
+
+number_of_defines = 0
 
 if not on_github:
     print(blue(f"Running define sanity check outside of Github Actions.\nFor assistance, a '{output_file_name}' file will be generated at the root of your directory if any errors are detected."))
@@ -67,9 +69,18 @@ for applicable_file in files_to_scan:
     with open(applicable_file, encoding="utf8") as file:
         file_contents = file.read()
         for define in define_regex.finditer(file_contents):
+            number_of_defines += 1
             define_name = define.group(2)
             if not re.search("#undef\s" + define_name, file_contents):
                 located_error_tuples.append((define_name, applicable_file))
+
+if number_of_defines == 0:
+    print(red("No defines found! This is likely an error."))
+    sys.exit(1)
+
+if number_of_defines <= 1000:
+    print(red(f"Only found {number_of_defines} defines! Something has likely gone wrong as the number of local defines should not be this low."))
+    sys.exit(1)
 
 if len(located_error_tuples):
 
@@ -90,4 +101,4 @@ if len(located_error_tuples):
     sys.exit(1)
 
 else:
-    print(green("No unhandled local defines found."))
+    print(green(f"No unhandled local defines found (found {number_of_defines} defines)."))

code/modules/unit_tests/font_awesome_icons.dm
@@ -85,7 +85,7 @@
 
 	for(var/icon in allowed_icons)
 		var/icon_name = replacetext(icon, "fa-", "")
-		output += "#define FA_ICON_[uppertext(replacetext(icon_name, "-", "_"))] \"[icon]\""
+		output += "#define FA_ICON_[uppertext(replacetext(icon_name, "-", "_"))] \"[icon]\"" // #undef FA_ICON_ // we have this here to avoid define_sanity throwing a fit
 
 	var/output_file = "[output.Join("\n")]\n"
 	rustg_file_write(output_file, "data/font_awesome_icons.dm")