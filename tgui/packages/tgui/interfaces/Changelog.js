a55615ba95f4af833b25c0d951392b1e9d10de62
Iamgoofball <iamgoofball@gmail.com>
2023-09-03 15:01:30 -0700

    Updates the changelog link for the pre-4407 changelog as it was moved

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -219,7 +219,7 @@ export class Changelog extends Component {
           Traditional Games Space Station 13 is thankful to the GoonStation 13
           Development Team for its work on the game up to the
           {' r4407 release. The changelog for changes up to r4407 can be seen '}
-          <a href="https://wiki.ss13.co/Changelog#April_2010">here</a>.
+          <a href="https://wiki.ss13.co/Pre-2016_Changelog#April_2010">here</a>.
         </p>
         <p>
           {'Except where otherwise noted, Goon Station 13 is licensed under a '}
