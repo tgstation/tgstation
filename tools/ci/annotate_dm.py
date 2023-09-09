287301a927331306134b946321a0c1789426e48d
distributivgesetz <distributivgesetz93@gmail.com>
2023-09-09 22:35:09 +0200

    shuffle around logging

--- tools/ci/annotate_dm.py
+++ tools/ci/annotate_dm.py
@@ -5,24 +5,6 @@ import os.path as path
 # Usage: python3 annotate_dm.py [filename]
 # If filename is not provided, stdin is checked instead
 
-raw_output = ""
-
-if len(sys.argv) > 1:
-    if not path.exists(sys.argv[1]):
-        print(f"Annotations file '{sys.argv[1]}' does not exist")
-        sys.exit(1)
-    with open(sys.argv[1], 'r') as f:
-        raw_output = f.read()
-elif not sys.stdin.isatty():
-    raw_output = sys.stdin.read()
-
-# Remove ANSI escape codes
-raw_output = re.sub(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]', '', raw_output)
-
-annotation_regex = r'(?P<filename>.*?), line (?P<line>\d+), column (?P<column>\d+):\s{1,2}(?P<type>error|warning): (?P<message>.*)'
-
-has_issues = False
-
 def red(text):
     return "\033[31m" + str(text) + "\033[0m"
 
@@ -31,6 +13,27 @@ def green(text):
 
 def yellow(text):
     return "\033[33m" + str(text) + "\033[0m"
+
+raw_output = ""
+
+if len(sys.argv) > 1:
+    if not path.exists(sys.argv[1]):
+        print(f"Error: Annotations file '{sys.argv[1]}' does not exist")
+        sys.exit(1)
+    with open(sys.argv[1], 'r') as f:
+        raw_output = f.read()
+elif not sys.stdin.isatty():
+    raw_output = sys.stdin.read()
+else:
+    print(red("Error: No input provided"))
+    sys.exit(1)
+
+# Remove ANSI escape codes
+raw_output = re.sub(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]', '', raw_output)
+
+annotation_regex = r'(?P<filename>.*?), line (?P<line>\d+), column (?P<column>\d+):\s{1,2}(?P<type>error|warning): (?P<message>.*)'
+
+has_issues = False
 
 for annotation in re.finditer(annotation_regex, raw_output):
     annotatation_type = annotation['type']
