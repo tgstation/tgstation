9e97bb0132895c065e2a653ac814353929d3ca6b
distributivgesetz <distributivgesetz93@gmail.com>
2023-09-09 22:58:48 +0200

    rewrite annotator into something a bit more usable

--- tools/ci/annotate_dm.py
+++ tools/ci/annotate_dm.py
@@ -14,37 +14,38 @@ def green(text):
 def yellow(text):
     return "\033[33m" + str(text) + "\033[0m"
 
-raw_output = ""
+def annotate(raw_output):
+    # Remove ANSI escape codes
+    raw_output = re.sub(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]', '', raw_output)
 
-if len(sys.argv) > 1:
-    if not path.exists(sys.argv[1]):
-        print(red(f"Error: Annotations file '{sys.argv[1]}' does not exist"))
+    print("::group::DreamChecker Output")
+    print(raw_output)
+    print("::endgroup::")
+
+    annotation_regex = r'(?P<filename>.*?), line (?P<line>\d+), column (?P<column>\d+):\s{1,2}(?P<type>error|warning): (?P<message>.*)'
+    has_issues = False
+
+    print("DM Code Annotations:")
+    for annotation in re.finditer(annotation_regex, raw_output):
+        print(f"::{annotation['type']} file={annotation['filename']},line={annotation['line']},col={annotation['column']}::{annotation['message']}")
+        has_issues = True
+
+    if not has_issues:
+        print(green("No DM issues found"))
+
+def main():
+    if len(sys.argv) > 1:
+        if not path.exists(sys.argv[1]):
+            print(red(f"Error: Annotations file '{sys.argv[1]}' does not exist"))
+            sys.exit(1)
+        with open(sys.argv[1], 'r') as f:
+            annotate(f.read())
+    elif not sys.stdin.isatty():
+        annotate(sys.stdin.read())
+    else:
+        print(red("Error: No input provided"))
+        print("Usage: tools/ci/annotate_dm.sh [filename]")
         sys.exit(1)
-    with open(sys.argv[1], 'r') as f:
-        raw_output = f.read()
-elif not sys.stdin.isatty():
-    raw_output = sys.stdin.read()
-else:
-    print(red("Error: No input provided"))
-    sys.exit(1)
 
-# Remove ANSI escape codes
-raw_output = re.sub(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]', '', raw_output)
-
-annotation_regex = r'(?P<filename>.*?), line (?P<line>\d+), column (?P<column>\d+):\s{1,2}(?P<type>error|warning): (?P<message>.*)'
-
-has_issues = False
-
-for annotation in re.finditer(annotation_regex, raw_output):
-    annotatation_type = annotation['type']
-
-    if(annotation['type'] == "error"):
-        annotation_type = red(annotation['type'])
-    elif(annotation['type'] == "warning"):
-        annotation_type = yellow(annotation['type'])
-
-    print(f"::{annotatation_type} file={annotation['filename']},line={annotation['line']},col={annotation['column']}::{annotation['message']}")
-    has_issues = True
-
-if not has_issues:
-    print(green("No DM issues found"))
+if __name__ == '__main__':
+    main()
