tgstation/tgstation

commit 4d654c7e2a3b918cb4d4dbb09566b5853fccb52d

Crafting recipes without an explicit name are now named after their results. (#77465)
## About The Pull Request

What it says on the tin. The code comment accompanying the "name" var
suggests this was already the case, but it was only true for the
crafting UI.
Adjusted some other code that gets recipe names to account for this
change.
## Why It's Good For The Game

Convenient, both as a fallback and for the sake of avoiding repetition
where it isn't necessary. That said, I haven't gone out of my way to
remove pre-existing names that match the result, in case that practice
is still considered desirable.
It also fixes #77379 and similar issues. 
## Changelog
:cl:
fix: crafting recipes without a name, such as the mothic pizzas, will
inherit the name of the item they make
/:cl:

code/game/objects/items/granters/crafting/_crafting_granter.dm
@@ -6,9 +6,10 @@
 	. = ..()
 	if(!user.mind)
 		return
-	for(var/datum/crafting_recipe/crafting_recipe_type as anything in crafting_recipe_types)
+	for(var/crafting_recipe_type in crafting_recipe_types)
 		user.mind.teach_crafting_recipe(crafting_recipe_type)
-		to_chat(user, span_notice("You learned how to make [initial(crafting_recipe_type.name)]."))
+		var/datum/crafting_recipe/recipe = locate(crafting_recipe_type) in GLOB.crafting_recipes
+		to_chat(user, span_notice("You learned how to make [recipe.name]."))
 
 /obj/item/book/granter/crafting_recipe/dusting
 	icon_state = "book1"

code/datums/components/crafting/_recipes.dm
@@ -43,6 +43,10 @@
 	var/delete_contents = TRUE
 
 /datum/crafting_recipe/New()
+	if(!name && result)
+		var/atom/atom_result = result
+		name = initial(atom_result.name)
+
 	if(!(result in reqs))
 		blacklist += result
 	// These should be excluded from all crafting recipies

code/datums/components/crafting/crafting.dm
@@ -462,7 +462,7 @@
 				else
 					if(!istype(result, /obj/effect/spawner))
 						result.forceMove(user.drop_location())
-				to_chat(user, span_notice("[crafting_recipe.name] constructed."))
+				to_chat(user, span_notice("Constructed [crafting_recipe.name]."))
 				user.investigate_log("crafted [crafting_recipe]", INVESTIGATE_CRAFTING)
 				crafting_recipe.on_craft_completion(user, result)
 			else
@@ -514,7 +514,7 @@
 	data["category"] = recipe.category
 
 	// Name, Description
-	data["name"] = recipe.name || initial(atom.name)
+	data["name"] = recipe.name
 
 	if(ispath(recipe.result, /datum/reagent))
 		var/datum/reagent/reagent = recipe.result

code/modules/food_and_drinks/recipes/tablecraft/recipes_guide.dm
@@ -37,7 +37,6 @@
 	non_craftable = TRUE
 
 /datum/crafting_recipe/food/reaction/New()
-	. = ..()
 	if(!reaction)
 		return
 
@@ -48,6 +47,7 @@
 		var/datum/chemical_reaction/chemical_reaction = new reaction()
 		setup_chemical_reaction_details(chemical_reaction)
 		qdel(chemical_reaction)
+	..()
 
 /**
  * Sets up information for our recipe based on the chemical reaction we have set.

commit 7cce84a33c89eefe38ef009e122e51585ada33b7

Fix crafting granters runtiming for food items (#79191)
## About The Pull Request

Caused by #77465

Two global lists, food items are only found in one. This locate failed
and caused the next line to runtime error.

## Changelog

:cl: Melbert
fix: Cooking Deserts 101 grants all intended recipes 
/:cl:

code/game/objects/items/granters/crafting/_crafting_granter.dm
@@ -8,7 +8,7 @@
 		return
 	for(var/crafting_recipe_type in crafting_recipe_types)
 		user.mind.teach_crafting_recipe(crafting_recipe_type)
-		var/datum/crafting_recipe/recipe = locate(crafting_recipe_type) in GLOB.crafting_recipes
+		var/datum/crafting_recipe/recipe = locate(crafting_recipe_type) in GLOB.crafting_recipes + GLOB.cooking_recipes
 		to_chat(user, span_notice("You learned how to make [recipe.name]."))
 
 /obj/item/book/granter/crafting_recipe/dusting