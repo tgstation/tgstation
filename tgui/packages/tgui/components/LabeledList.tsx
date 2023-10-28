eb1225721aa0897043f41f95fc9be2a521d596ce
nikothedude <59709059+nikothedude@users.noreply.github.com>
2023-10-28 17:45:41 -0400

    oops

--- tgui/packages/tgui/components/LabeledList.tsx
+++ tgui/packages/tgui/components/LabeledList.tsx
@@ -63,7 +63,7 @@ const LabeledListItem = (props: LabeledListItemProps) => {
         <Box
           as="span"
           style={{
-            'border-bottom': '2px dotted rgba(255, 255, 255, 0.1)',
+            'border-bottom': '2px dotted rgba(255, 255, 255, 0.8)',
           }}>
           {innerLabel}
         </Box>
