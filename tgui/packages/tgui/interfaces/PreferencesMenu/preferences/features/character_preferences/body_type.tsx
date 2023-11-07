60745392d392e49f7334e50930a9bf0c6a58635f
nikothedude <59709059+nikothedude@users.noreply.github.com>
2023-11-07 12:21:11 -0500

    init

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/body_type.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/body_type.tsx
@@ -3,4 +3,5 @@ import { FeatureChoiced, FeatureDropdownInput } from '../base';
 export const body_type: FeatureChoiced = {
   name: 'Body type',
   component: FeatureDropdownInput,
+  sortingPrefix: 'A_',
 };
