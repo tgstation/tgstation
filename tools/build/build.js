8891b8efa5bc52d700816c41941fafd8692c3239
LemonInTheDark <58055496+LemonInTheDark@users.noreply.github.com>
2023-11-11 01:55:54 -0800

    chmod?

--- tools/build/build.js
+++ tools/build/build.js
@@ -91,6 +91,10 @@ export const CutterTarget = new Juke.Target({
     const suffix = process.platform === 'win32' ? '.exe' : '';
     const download_from = `https://github.com/${repo}/releases/download/${ver}/hypnagogic${suffix}`
     await download_file(download_from, cutter_path);
+    await Juke.exec("chmod", [
+      '+x',
+      cutter_path,
+    ]);
   },
 });
 
