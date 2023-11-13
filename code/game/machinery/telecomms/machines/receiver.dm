tgstation/tgstation

commit ab0dce1035e557cca1562b72f0026647cd96cb80

Second telecomms receivers are now able to function if the first one isn't properly connected (#74788)
## About The Pull Request

Fixes #74786
Fixes #72613

Removes one line of useless code that fucked up the zlevels list for no
reason, causing subsequent receivers to not pick up the signal
## Why It's Good For The Game
A receiver that is perfectly functional no longer stops working because
another one in some other place is on but not functional
- Much less confusing for people trying to set up tcomms
- You can no longer build a second receiver in maint to permanently
render telecomms unfixable
## Changelog
:cl:
fix: Fixed a bug where additonal receivers wouldn't work if the first
one was on but disconnected
/:cl:
im so bad at coding i took hours to figure this out and it was such a
simple fix

![image](https://user-images.githubusercontent.com/47338680/232368379-2c4f601a-bc8e-4689-b539-d55c09c2e02f.png)

code/game/machinery/telecomms/machines/receiver.dm
@@ -21,8 +21,6 @@
 	if(!is_freq_listening(signal))
 		return
 
-	signal.levels = list()
-
 	// send the signal to the hub if possible, or a bus otherwise
 	if(!relay_information(signal, /obj/machinery/telecomms/hub))
 		relay_information(signal, /obj/machinery/telecomms/bus)

commit 8a0935fd22708d5f0c8b73bc459ef990a2956e41

[NO GBP] Fixes tcomms relays being permanent (attempt 2) (#79558)
## About The Pull Request

The problem was that the levels var of signals was being used as both a
list for the levels the receivers can receive on and the levels
broadcasters can broadcast on.

So basically I caused this problem in #74788 because I removed receivers
clearing the levels list after receiving the signal. This solved the
problem of one receiver clearing the receiving list, preventing any
other receivers from receiving the message. However, this led to the
receiving levels sticking around to the point when the list was meant to
be used for broadcasting levels.

I originally tried to solve this problem in #78812 but this fix is much
simpler.

Another solution could be to make it so that there's separate vars for
receiving levels and broadcasting levels instead of using the same var.
idk if thats better or not though
## Why It's Good For The Game

Fixes #78806
## Changelog
:cl:
fix: Fixed tcomms relays being weird, they should be sabotagable again
/:cl:

code/game/machinery/telecomms/machines/receiver.dm
@@ -20,9 +20,15 @@
 	if(!is_freq_listening(signal))
 		return
 
-	// send the signal to the hub if possible, or a bus otherwise
-	if(!relay_information(signal, /obj/machinery/telecomms/hub))
-		relay_information(signal, /obj/machinery/telecomms/bus)
+	// Make a copy of the signal so that other recievers can still recieve this signal
+	var/datum/signal/subspace/signal_copy = signal.copy()
+
+	// Signal has been recieved, so remove receiving levels. This list will be used later on to determine broadcasting levels.
+	signal_copy.levels = list()
+
+	// Send the signal to a hub if possible, or a bus otherwise.
+	if(!relay_information(signal_copy, /obj/machinery/telecomms/hub))
+		relay_information(signal_copy, /obj/machinery/telecomms/bus)
 
 	use_power(idle_power_usage)
 