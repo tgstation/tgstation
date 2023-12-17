f14416de09aa48fc26826870ffa2df3e68ce4df2
jlsnow301 <jlsnow301@pm.me>
2023-12-17 02:03:11 -0800

    adds multiline import

--- tgui/packages/tgui/interfaces/IntegratedCircuit/VariableMenu.jsx
+++ tgui/packages/tgui/interfaces/IntegratedCircuit/VariableMenu.jsx
@@ -14,6 +14,7 @@ import {
   VARIABLE_LIST,
   VARIABLE_NOT_A_LIST,
 } from './constants';
+import { multiline } from 'common/string';
 
 export class VariableMenu extends Component {
   constructor(props) {
