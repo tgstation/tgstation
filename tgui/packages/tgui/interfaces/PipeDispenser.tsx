59e28a1f7797880d3e8ed16b48d7ff5b2dbe71f6
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2023-12-24 14:25:52 -0800

    Refactors TS in Box component (#80368)
    ## About The Pull Request
    Revival of #80058, just had too many plates spinning at once.
    
    Short version: TS actually works on components now. Making this switch
    highlighted many errors, which I've solved here. The guts of this PR is
    in box.tsx.
    
    <details>
    <summary>More details</summary>
    
    Using `[key: string]: any` at the start of a type will basically tell TS
    to bug off and ignore as much as it can. This is a huge problem
    considering Box is the base type for many other components. Our TS
    implementation is made very loose by this one line.
    
    ![image](https://github.com/tgstation/tgstation/assets/42397676/f4904287-ee99-4cb6-a8d4-39dfac8c4913)
    
    Notice this simple typo - normally typescript would complain, but no!
    This fits a string key of any:
    
    ![Code_eUMhsauqoV](https://github.com/tgstation/tgstation/assets/42397676/3607e904-657c-4c07-bc23-5759b90058d5)
    
    Even props which don't fit that component at all are AOK given our
    current:
    ![Screenshot 2023-12-18
    145950](https://github.com/tgstation/tgstation/assets/42397676/9ab5318a-e2a5-4af8-bc42-b53c47897406)
    
    I removed the line and of course, errors everywhere (those are just the
    TS files!).
    ![Screenshot 2023-12-18
    150138](https://github.com/tgstation/tgstation/assets/42397676/477d9a19-69b1-4161-9984-2a0c6218a981)
    You now get much more detailed feedback in your editor. It's no longer
    passive!
    
    </details>
    
    This refactors the type to also make better use of `keyof typeof`
    meaning we don't need to keep a duplicate type containing all the
    combinations offered in `stringStyleMap` and `booleanStyleMap`.
    
    ## Why It's Good For The Game
    Bug fixes presumably, but code improvement otherwise
    ## Changelog
    
    :cl:
    fix: AI voice changer UI should show defaults properly
    fix: NTOS Software Hub should focus on the input immediately now
    fix: Ore container UI should autofocus on the search bar now
    fix: Vend-a-tray registration text should display as intended
    /:cl:

--- tgui/packages/tgui/interfaces/PipeDispenser.tsx
+++ tgui/packages/tgui/interfaces/PipeDispenser.tsx
@@ -69,7 +69,6 @@ const PipeTypeSection = (props) => {
       <Tabs>
         {categories.map((category, i) => (
           <Tabs.Tab
-            fluid
             key={category.cat_name}
             icon={ICON_BY_CATEGORY_NAME[category.cat_name]}
             selected={category.cat_name === shownCategory.cat_name}
@@ -84,7 +83,7 @@ const PipeTypeSection = (props) => {
           key={recipe.pipe_index}
           fluid
           ellipsis
-          title={recipe.pipe_name}
+          tooltip={recipe.pipe_name}
           onClick={() =>
             act('pipe_type', {
               pipe_type: recipe.pipe_index,
