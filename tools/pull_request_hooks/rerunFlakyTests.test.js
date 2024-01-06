1b78cd7ca467aa2302720e0a55fad364b792d5a7
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2024-01-05 18:07:12 -0800

    More tabs

--- tools/pull_request_hooks/rerunFlakyTests.test.js
+++ tools/pull_request_hooks/rerunFlakyTests.test.js
@@ -3,38 +3,38 @@ import fs from "node:fs";
 import { extractDetails } from "./rerunFlakyTests.js";
 
 function extractDetailsFromPayload(filename) {
-  return extractDetails(
-    fs.readFileSync(`tests/flakyTestPayloads/${filename}.txt`, {
-      encoding: "utf8",
-    })
-  );
+	return extractDetails(
+		fs.readFileSync(`tests/flakyTestPayloads/${filename}.txt`, {
+			encoding: "utf8",
+		})
+	);
 }
 
 const chatClient = extractDetailsFromPayload("chat_client");
 assert.equal(
-  chatClient.title,
-  "Flaky hard delete: /datum/computer_file/program/chatclient"
+	chatClient.title,
+	"Flaky hard delete: /datum/computer_file/program/chatclient"
 );
 assert.equal(chatClient.failures.length, 1);
 
 const monkeyBusiness = extractDetailsFromPayload("monkey_business");
 assert.equal(
-  monkeyBusiness.title,
-  "Flaky test monkey_business: Cannot execute null.resolve()."
+	monkeyBusiness.title,
+	"Flaky test monkey_business: Cannot execute null.resolve()."
 );
 assert.equal(monkeyBusiness.failures.length, 1);
 
 const shapeshift = extractDetailsFromPayload("shapeshift");
 assert.equal(
-  shapeshift.title,
-  "Multiple errors in flaky test shapeshift_spell"
+	shapeshift.title,
+	"Multiple errors in flaky test shapeshift_spell"
 );
 assert.equal(shapeshift.failures.length, 16);
 
 const multipleFailures = extractDetailsFromPayload("multiple_failures");
 assert.equal(
-  multipleFailures.title,
-  "Multiple flaky test failures in more_shapeshift_spell, shapeshift_spell"
+	multipleFailures.title,
+	"Multiple flaky test failures in more_shapeshift_spell, shapeshift_spell"
 );
 assert.equal(multipleFailures.failures.length, 2);
 
