87213bac6638ea6b9cfc78b6e8cb9af954b70919
Lucy <lucy@absolucy.moe>
2024-01-19 13:39:17 -0500

    Fix LOOC runechat prefs

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/monkestation/looc.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from '../../base';
+
+export const see_looc_on_map: FeatureToggle = {
+  name: 'Enable LOOC Runechat',
+  category: 'RUNECHAT',
+  description: 'LOOC messages will show above heads.',
+  component: CheckboxInput,
+};
