a829dfa54269912038857d9a9616dea2aa31cc75
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2024-02-06 12:42:21 -0800

    Update MedicalKiosk.jsx

--- tgui/packages/tgui/interfaces/MedicalKiosk.jsx
+++ tgui/packages/tgui/interfaces/MedicalKiosk.jsx
@@ -188,7 +188,6 @@ const MedicalKioskScanResults2 = (props) => {
     bleed_status,
     blood_levels,
     blood_status,
-    blood_alcohol,
   } = data;
   return (
     <Section title="Symptom Based Checkup">
@@ -214,19 +213,6 @@ const MedicalKioskScanResults2 = (props) => {
         </LabeledList.Item>
         <LabeledList.Item label="Blood Information">
           {blood_status}
-        </LabeledList.Item>
-        <LabeledList.Item label="Blood Alcohol">
-          <ProgressBar
-            value={blood_alcohol}
-            minValue={0}
-            maxValue={0.3}
-            ranges={{
-              blue: [-Infinity, 0.23],
-              bad: [0.23, Infinity],
-            }}
-          >
-            <AnimatedNumber value={blood_alcohol} />
-          </ProgressBar>
         </LabeledList.Item>
       </LabeledList>
     </Section>
@@ -262,6 +248,7 @@ const MedicalKioskScanResults4 = (props) => {
     overdose_list = [],
     addict_list = [],
     hallucinating_status,
+    blood_alcohol,
   } = data;
   return (
     <Section title="Chemical and Psychoactive Analysis">
@@ -294,6 +281,19 @@ const MedicalKioskScanResults4 = (props) => {
         </LabeledList.Item>
         <LabeledList.Item label="Psychoactive Status">
           {hallucinating_status}
+        </LabeledList.Item>
+        <LabeledList.Item label="Blood Alcohol Content">
+          <ProgressBar
+            value={blood_alcohol}
+            minValue={0}
+            maxValue={0.3}
+            ranges={{
+              blue: [-Infinity, 0.23],
+              bad: [0.23, Infinity],
+            }}
+          >
+            <AnimatedNumber value={blood_alcohol} />
+          </ProgressBar>
         </LabeledList.Item>
       </LabeledList>
     </Section>
