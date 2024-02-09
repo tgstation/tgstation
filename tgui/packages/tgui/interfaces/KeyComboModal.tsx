d0aeb80faba3ec04b3de0922ebec996403238848
jimmyl <70376633+mc-oofert@users.noreply.github.com>
2024-02-09 15:26:52 +0100

    Update KeyComboModal.tsx

--- tgui/packages/tgui/interfaces/KeyComboModal.tsx
+++ tgui/packages/tgui/interfaces/KeyComboModal.tsx
@@ -16,7 +16,7 @@ type KeyInputData = {
   title: string;
 };
 
-const isStandardKey = (event: KeyboardEvent): boolean => {
+const isStandardKey = (event: KeyEvent): boolean => {
   return (
     event.key !== KEY.Alt &&
     event.key !== KEY.Control &&
@@ -41,7 +41,7 @@ const KEY_CODE_TO_BYOND: Record<string, string> = {
 
 const DOM_KEY_LOCATION_NUMPAD = 3;
 
-const formatKeyboardEvent = (event: KeyboardEvent): string => {
+const formatKeyboardEvent = (event: KeyEvent): string => {
   let text = '';
 
   if (event.altKey) {
