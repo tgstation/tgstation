4a22f595d8071db7e9e44845e6c2a239d7983b27
Rhials <28870487+Rhials@users.noreply.github.com>
2024-02-10 15:19:59 -0500

    ok why did I do olive

--- tgui/packages/tgui/interfaces/Orbit/constants.ts
+++ tgui/packages/tgui/interfaces/Orbit/constants.ts
@@ -10,7 +10,7 @@ export const ANTAG2COLOR = {
   'Xenomorph Infestation': 'violet',
   'Spacetime Aberrations': 'white',
   'Deviant Crew': 'white',
-  'Invasive Overgrowth': 'olive',
+  'Invasive Overgrowth': 'green',
 } as const;
 
 export const THREAT = {
