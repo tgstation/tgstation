HolyTigerGod/Voidcrew

Lighting object oddities (#76009)
## About The Pull Request

Fire stacks status effect no longer uses a weakref for the mob light, I
am pretty sure there was no real reason to use a weakref there.
Deleted weird luminescent glow dummy, now it just uses the standard
moblight obj.
Put all /obj/effect/dummy/lighting_obj together in a single file and
added a comment explaining why they exist.

(I severely dislike the /obj/effect/dummy typepath, but I am very much
unsure if just replacing all of them with /obj/effect/abstract would
break shit)

## Why It's Good For The Game

Code organization good

code/datums/status_effects/debuffs/fire_stacks.dm
@@ -152,12 +152,12 @@
 
 	/// If we're on fire
 	var/on_fire = FALSE
-	/// A weakref to the mob light emitter
-	var/datum/weakref/firelight_ref
-	/// Type of mob light emitter we use when on fire
-	var/firelight_type = /obj/effect/dummy/lighting_obj/moblight/fire
 	/// Stores current fire overlay icon state, for optimisation purposes
 	var/last_icon_state
+	/// Reference to the mob light emitter itself
+	var/obj/effect/dummy/lighting_obj/moblight
+	/// Type of mob light emitter we use when on fire
+	var/moblight_type = /obj/effect/dummy/lighting_obj/moblight/fire
 
 /datum/status_effect/fire_handler/fire_stacks/tick(seconds_per_tick, times_fired)
 	if(stacks <= 0)
@@ -249,8 +249,10 @@
 	if(!silent)
 		owner.visible_message(span_warning("[owner] catches fire!"), span_userdanger("You're set on fire!"))
 
-	if(firelight_type)
-		firelight_ref = WEAKREF(new firelight_type(owner))
+	if(moblight_type)
+		if(moblight)
+			qdel(moblight)
+		moblight = new moblight_type(owner)
 
 	SEND_SIGNAL(owner, COMSIG_LIVING_IGNITED, owner)
 	cache_stacks()
@@ -263,9 +265,7 @@
  */
 
 /datum/status_effect/fire_handler/fire_stacks/proc/extinguish()
-	if(firelight_ref)
-		qdel(firelight_ref)
-
+	QDEL_NULL(moblight)
 	on_fire = FALSE
 	owner.clear_mood_event("on_fire")
 	SEND_SIGNAL(owner, COMSIG_LIVING_EXTINGUISHED, owner)
@@ -289,6 +289,11 @@
 /datum/status_effect/fire_handler/fire_stacks/on_apply()
 	. = ..()
 	update_overlay()
+
+/obj/effect/dummy/lighting_obj/moblight/fire
+	name = "fire"
+	light_color = LIGHT_COLOR_FIRE
+	light_range = LIGHT_RANGE_FIRE
 
 /datum/status_effect/fire_handler/wet_stacks
 	id = "wet_stacks"

code/game/objects/effects/lighting.dm
@@ -1,0 +1,42 @@
+/**
+ * Basically, a fake object that emits light.
+ *
+ * Why is this used sometimes instead of giving atoms light values directly?
+ * Because using these, you can have multiple light sources in a single object.
+ */
+/obj/effect/dummy/lighting_obj
+	name = "lighting"
+	desc = "Tell a coder if you're seeing this."
+	icon_state = "nothing"
+	light_system = MOVABLE_LIGHT
+	light_range = MINIMUM_USEFUL_LIGHT_RANGE
+	light_color = COLOR_WHITE
+	blocks_emissive = EMISSIVE_BLOCK_NONE
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+
+/obj/effect/dummy/lighting_obj/Initialize(mapload, range, power, color, duration)
+	. = ..()
+	if(!isnull(range))
+		set_light_range(range)
+	if(!isnull(power))
+		set_light_power(power)
+	if(!isnull(color))
+		set_light_color(color)
+	if(duration)
+		QDEL_IN(src, duration)
+
+/obj/effect/dummy/lighting_obj/moblight
+	name = "mob lighting"
+
+/obj/effect/dummy/lighting_obj/moblight/Initialize(mapload, range, power, color, duration)
+	. = ..()
+	if(!ismob(loc))
+		return INITIALIZE_HINT_QDEL
+
+/obj/effect/dummy/lighting_obj/moblight/fire
+	name = "mob fire lighting"
+	light_color = LIGHT_COLOR_FIRE
+	light_range = LIGHT_RANGE_FIRE
+
+/obj/effect/dummy/lighting_obj/moblight/species
+	name = "species lighting"

tgstation/tgstation

code/datums/status_effects/debuffs/fire_stacks.dm
@@ -151,12 +151,12 @@
 
 	/// If we're on fire
 	var/on_fire = FALSE
-	/// A weakref to the mob light emitter
-	var/datum/weakref/firelight_ref
-	/// Type of mob light emitter we use when on fire
-	var/firelight_type = /obj/effect/dummy/lighting_obj/moblight/fire
 	/// Stores current fire overlay icon state, for optimisation purposes
 	var/last_icon_state
+	/// Reference to the mob light emitter itself
+	var/obj/effect/dummy/lighting_obj/moblight
+	/// Type of mob light emitter we use when on fire
+	var/moblight_type = /obj/effect/dummy/lighting_obj/moblight/fire
 
 /datum/status_effect/fire_handler/fire_stacks/tick(seconds_per_tick, times_fired)
 	var/turf/source_turf = get_turf(owner)
@@ -253,8 +253,10 @@
 	if(!silent)
 		owner.visible_message(span_warning("[owner] catches fire!"), span_userdanger("You're set on fire!"))
 
-	if(firelight_type)
-		firelight_ref = WEAKREF(new firelight_type(owner))
+	if(moblight_type)
+		if(moblight)
+			qdel(moblight)
+		moblight = new moblight_type(owner)
 
 	cache_stacks()
 	update_overlay()
@@ -267,9 +269,7 @@
  */
 
 /datum/status_effect/fire_handler/fire_stacks/proc/extinguish()
-	if(firelight_ref)
-		qdel(firelight_ref)
-
+	QDEL_NULL(moblight)
 	on_fire = FALSE
 	owner.clear_mood_event("on_fire")
 	SEND_SIGNAL(owner, COMSIG_LIVING_EXTINGUISHED, owner)
@@ -296,6 +296,11 @@
 /datum/status_effect/fire_handler/fire_stacks/on_apply()
 	. = ..()
 	update_overlay()
+
+/obj/effect/dummy/lighting_obj/moblight/fire
+	name = "fire"
+	light_color = LIGHT_COLOR_FIRE
+	light_outer_range = LIGHT_RANGE_FIRE
 
 /datum/status_effect/fire_handler/wet_stacks
 	id = "wet_stacks"

code/game/objects/effects/lighting.dm
@@ -1,0 +1,42 @@
+/**
+ * Basically, a fake object that emits light.
+ *
+ * Why is this used sometimes instead of giving atoms light values directly?
+ * Because using these, you can have multiple light sources in a single object.
+ */
+/obj/effect/dummy/lighting_obj
+	name = "lighting"
+	desc = "Tell a coder if you're seeing this."
+	icon_state = "nothing"
+	light_system = MOVABLE_LIGHT
+	light_outer_range = MINIMUM_USEFUL_LIGHT_RANGE
+	light_color = COLOR_WHITE
+	blocks_emissive = EMISSIVE_BLOCK_NONE
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+
+/obj/effect/dummy/lighting_obj/Initialize(mapload, range, power, color, duration)
+	. = ..()
+	if(!isnull(range))
+		set_light_range(range)
+	if(!isnull(power))
+		set_light_power(power)
+	if(!isnull(color))
+		set_light_color(color)
+	if(duration)
+		QDEL_IN(src, duration)
+
+/obj/effect/dummy/lighting_obj/moblight
+	name = "mob lighting"
+
+/obj/effect/dummy/lighting_obj/moblight/Initialize(mapload, range, power, color, duration)
+	. = ..()
+	if(!ismob(loc))
+		return INITIALIZE_HINT_QDEL
+
+/obj/effect/dummy/lighting_obj/moblight/fire
+	name = "mob fire lighting"
+	light_color = LIGHT_COLOR_FIRE
+	light_outer_range = LIGHT_RANGE_FIRE
+
+/obj/effect/dummy/lighting_obj/moblight/species
+	name = "species lighting"