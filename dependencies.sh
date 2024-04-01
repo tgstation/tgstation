b3f1c44db32fd4a7c170bebbfd07b788d13363e1
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2024-03-31 20:41:25 -0700

    Creates a node compatibility mode (#82334)
    ## About The Pull Request
    By default this will install node v20 LTS, but if a user is detected to
    be using win 7 it's node v14
    
    This lets us run higher node versions (with presumably more stable and
    performant content) while allowing win 7 users to play
    
    I should note that this is making clean tgui builds run at ~6.7sec which
    is about a 6.9% speed increase (nice) from the previous #80310
    ## Why It's Good For The Game
    Better tools
    ## Changelog
    N/A nothing player facing

--- dependencies.sh
+++ dependencies.sh
@@ -12,7 +12,8 @@ export RUST_G_VERSION=3.1.0
 
 #node version
 export NODE_VERSION=14
-export NODE_VERSION_PRECISE=14.16.1
+export NODE_VERSION_LTS=20.12.0
+export NODE_VERSION_COMPAT=14.16.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.8
