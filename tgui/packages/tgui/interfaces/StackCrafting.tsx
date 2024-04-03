61069f4534b9da7fe742bd8ac82121aa4eb9232e
Arthri <41360489+a@users.noreply.github.com>
2024-04-03 03:40:26 +0000

    Rewrite StackCrafting's filterRecipeList

--- tgui/packages/tgui/interfaces/StackCrafting.tsx
+++ tgui/packages/tgui/interfaces/StackCrafting.tsx
@@ -1,5 +1,3 @@
-import { sortBy } from 'common/collections';
-import { flow } from 'common/fp';
 import { clamp } from 'common/math';
 import { createSearch } from 'common/string';
 import { useState } from 'react';
@@ -49,8 +47,7 @@ type RecipeBoxProps = {
 };
 
 // RecipeList converted via Object.entries() for filterRecipeList
-type RecipeListEntry = [string, RecipeList | Recipe];
-type RecipeListFilterableEntry = [string, RecipeList | Recipe | undefined];
+type RecipeListFilterableEntry = [string, RecipeList | Recipe];
 
 /**
  * Type guard for recipe vs recipe list
@@ -70,33 +67,29 @@ function isRecipeList(value: Recipe | RecipeList): value is RecipeList {
 const filterRecipeList = (
   list: RecipeList,
   keyFilter: (key: string) => boolean,
-) => {
-  const filteredList: RecipeList = flow([
-    (list) =>
-      list.map((entry: RecipeListEntry): RecipeListFilterableEntry => {
+): RecipeList | undefined => {
+  const filteredList = Object.fromEntries(
+    Object.entries(list)
+      .flatMap((entry): RecipeListFilterableEntry[] => {
         const [key, recipe] = entry;
 
-        if (isRecipeList(recipe)) {
-          // If category name matches, return the whole thing.
-          if (keyFilter(key)) {
-            return entry;
-          }
-
-          // otherwise, filter sub-entries.
-          return [key, filterRecipeList(recipe, keyFilter)];
+        // If category name matches, return the whole thing.
+        if (keyFilter(key)) {
+          return [entry];
         }
 
-        return keyFilter(key) ? entry : [key, undefined];
-      }),
-    (list) =>
-      list.filter((entry: RecipeListFilterableEntry) => entry[1] !== undefined),
-    sortBy((entry: RecipeListEntry) => entry[0].toLowerCase()),
-    (list) =>
-      list.reduce((obj: RecipeList, entry: RecipeListEntry) => {
-        obj[entry[0]] = entry[1];
-        return obj;
-      }, {}),
-  ])(Object.entries(list));
+        if (isRecipeList(recipe)) {
+          // otherwise, filter sub-entries.
+          const subEntries = filterRecipeList(recipe, keyFilter);
+          if (subEntries !== undefined) {
+            return [[key, subEntries]];
+          }
+        }
+
+        return [];
+      })
+      .sort(([a], [b]) => (a < b ? -1 : a !== b ? 1 : 0)),
+  );
 
   return Object.keys(filteredList).length ? filteredList : undefined;
 };
