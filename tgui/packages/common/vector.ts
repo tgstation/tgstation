8f5e0d79e19a664160930a0253e23f295fb05844
Arthri <41360489+a@users.noreply.github.com>
2024-04-04 02:21:50 +0000

    Add explicit return types to vectors

--- tgui/packages/common/vector.ts
+++ tgui/packages/common/vector.ts
@@ -10,42 +10,42 @@
 
 import { zip } from './collections';
 
-const ADD = (a: number, b: number) => a + b;
-const SUB = (a: number, b: number) => a - b;
-const MUL = (a: number, b: number) => a * b;
-const DIV = (a: number, b: number) => a / b;
+const ADD = (a: number, b: number): number => a + b;
+const SUB = (a: number, b: number): number => a - b;
+const MUL = (a: number, b: number): number => a * b;
+const DIV = (a: number, b: number): number => a / b;
 
 export type Vector = number[];
 
-export const vecAdd = (...vecs: Vector[]) => {
+export const vecAdd = (...vecs: Vector[]): Vector => {
   return zip(...vecs).map((x) => x.reduce(ADD));
 };
 
-export const vecSubtract = (...vecs: Vector[]) => {
+export const vecSubtract = (...vecs: Vector[]): Vector => {
   return zip(...vecs).map((x) => x.reduce(SUB));
 };
 
-export const vecMultiply = (...vecs: Vector[]) => {
+export const vecMultiply = (...vecs: Vector[]): Vector => {
   return zip(...vecs).map((x) => x.reduce(MUL));
 };
 
-export const vecDivide = (...vecs: Vector[]) => {
+export const vecDivide = (...vecs: Vector[]): Vector => {
   return zip(...vecs).map((x) => x.reduce(DIV));
 };
 
-export const vecScale = (vec: Vector, n: number) => {
+export const vecScale = (vec: Vector, n: number): Vector => {
   return vec.map((x) => x * n);
 };
 
-export const vecInverse = (vec: Vector) => {
+export const vecInverse = (vec: Vector): Vector => {
   return vec.map((x) => -x);
 };
 
-export const vecLength = (vec: Vector) => {
+export const vecLength = (vec: Vector): number => {
   return Math.sqrt(vecMultiply(vec, vec).reduce(ADD));
 };
 
-export const vecNormalize = (vec: Vector) => {
+export const vecNormalize = (vec: Vector): Vector => {
   const length = vecLength(vec);
   return vec.map((c) => c / length);
 };
