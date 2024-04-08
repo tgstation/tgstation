794806a6d7bb17ca37892f047f479dc520221579
Jerm <jlsnow301@pm.me>
2024-04-08 00:18:18 -0700

    test

--- tgui/packages/tgui/components/Image.tsx
+++ tgui/packages/tgui/components/Image.tsx
@@ -1,14 +1,11 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useRef } from 'react';
 
 import { BoxProps, computeBoxProps } from './Box';
-import { Icon } from './Icon';
 import { Tooltip } from './Tooltip';
 
 type Props = Partial<{
   /** True is default, this fixes an ie thing */
   fixBlur: boolean;
-  /** A common case if the cdn does not have the image. This will try to refetch every 2s */
-  fixErrors: boolean;
   /** Fill is default. */
   objectFit: 'contain' | 'cover'; // fill is default
   /** Creates a tooltip window using tooltip component. */
@@ -32,17 +29,8 @@ const maxAttempts = 5;
 
 /** Image component. Use this instead of Box as="img". */
 export function Image(props: Props) {
-  const {
-    fixBlur = true,
-    fixErrors = true,
-    objectFit = 'fill',
-    src,
-    tooltip,
-    ...rest
-  } = props;
-
-  const [error, setError] = useState(false);
-  const [attempts, setAttempts] = useState(0);
+  const { fixBlur = true, objectFit = 'fill', src, tooltip, ...rest } = props;
+  const attempts = useRef(0);
 
   const computedProps = computeBoxProps(rest);
   computedProps['style'] = {
@@ -51,41 +39,24 @@ export function Image(props: Props) {
     objectFit,
   };
 
-  let content;
-  if (!error && attempts < maxAttempts) {
-    content = (
-      <img
-        onError={() => fixErrors && setError(true)}
-        src={src}
-        {...computedProps}
-      />
-    );
-  } else {
-    content = <Icon name="spinner" spin size={2.4} color="gray" />;
-  }
+  let content = (
+    <img
+      onError={(event) => {
+        if (attempts.current < maxAttempts) {
+          setTimeout(() => {
+            attempts.current++;
+            event.currentTarget.src = `${src}`;
+          }, 1500);
+        }
+      }}
+      src={src}
+      {...computedProps}
+    />
+  );
 
   if (tooltip) {
     content = <Tooltip content={tooltip}>{content}</Tooltip>;
   }
-
-  useEffect(() => {
-    if (!error) return;
-
-    // Extra careful with memory leaks, possibly unnecessary
-    let isMounted = true;
-
-    const timer = setTimeout(() => {
-      if (isMounted) {
-        setAttempts((prev) => prev + 1);
-        setError(false);
-      }
-    }, 1500);
-
-    return () => {
-      isMounted = false;
-      clearTimeout(timer);
-    };
-  }, [error]);
 
   return content;
 }
