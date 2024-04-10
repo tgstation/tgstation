e12a22fd20726619b19d196bffe6f0c2dba896c6
Jerm <jlsnow301@pm.me>
2024-04-09 22:51:27 -0700

    converts

--- tgui/packages/tgui/components/Grid.tsx
+++ tgui/packages/tgui/components/Grid.tsx
@@ -4,20 +4,29 @@
  * @license MIT
  */
 
+import { PropsWithChildren } from 'react';
+
+import { BoxProps } from './Box';
 import { Table } from './Table';
 
-/** @deprecated */
-export const Grid = (props) => {
+/** @deprecated Do not use. Use stack instead. */
+export function Grid(props: PropsWithChildren<BoxProps>) {
   const { children, ...rest } = props;
   return (
     <Table {...rest}>
       <Table.Row>{children}</Table.Row>
     </Table>
   );
-};
+}
 
-/** @deprecated */
-export const GridColumn = (props) => {
+type Props = Partial<{
+  /** Width of the column in percentage. */
+  size: number;
+}> &
+  BoxProps;
+
+/** @deprecated Do not use. Use stack instead. */
+export function GridColumn(props: Props) {
   const { size = 1, style, ...rest } = props;
   return (
     <Table.Cell
@@ -28,6 +37,6 @@ export const GridColumn = (props) => {
       {...rest}
     />
   );
-};
+}
 
 Grid.Column = GridColumn;
