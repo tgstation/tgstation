aaa62283ad230159ce32f590f20662e4a398edd6
Mikhail G <furiorg@gmail.com>
2024-04-11 08:18:50 +0700

    Tranlate pr2 (#43)

--- tools/translate/add_new_translation.sh
+++ tools/translate/add_new_translation.sh
@@ -8,16 +8,18 @@ git config --local user.name "ss220bot"
 git checkout -b translate_tmp
 git reset --hard origin/master
 
+echo Moving old translation...
 git checkout translate -- ./tools/translate/ss220replace.json
 git commit -m "Move old translation"
 
-git cherry-pick translate
-
+echo Generating new translation...
+git cherry-pick -n translate
 python ./tools/translate/converter.py
 git add ./tools/translate/ss220replace.json
 git commit -m "Generate translation file"
 git reset .
 
+echo Applying result...
 ./tools/translate/ss220_replacer_linux
 git commit -m "Apply translation"
 
