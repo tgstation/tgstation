6edf0848aab3ecece7e2a932c47532faae3d06f6
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2024-05-02 23:43:22 +0100

    Fixes

--- lua/SS13_base.lua
+++ lua/SS13_base.lua
@@ -135,7 +135,7 @@ function SS13.unregister_signal(datum, signal, callback)
 		if not SS13.istype(datum, "/datum/weakref") then
 			handler_callback:call_proc("UnregisterSignal", datum, signal)
 		else
-			local actualDatum = callbackWeakRef:call_proc("hard_resolve")
+			local actualDatum = datum:call_proc("hard_resolve")
 			-- Turfs don't remove their signals on deletion.
 			if SS13.is_valid(actualDatum) or SS13.istype(actualDatum, "/turf") then
 				handler_callback:call_proc("UnregisterSignal", actualDatum, signal)
