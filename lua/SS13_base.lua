8aa72b7215c8102eb5705be7490761b9039d4e2c
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2024-05-03 00:08:47 +0100

    Better fix

--- lua/SS13_base.lua
+++ lua/SS13_base.lua
@@ -100,7 +100,8 @@ function SS13.register_signal(datum, signal, func)
 	callback:call_proc("RegisterSignal", datum, signal, "Invoke")
 	local path = { "__SS13_signal_handlers", datumWeakRef, signal, callbackWeakRef, "func" }
 	callback.vars.arguments = { path }
-	if not __SS13_signal_handlers[datumWeakRef]._cleanup then
+	-- Turfs don't remove their signals on deletion.
+	if not __SS13_signal_handlers[datumWeakRef]._cleanup and not SS13.istype(datum, "/turf") then
 		local cleanupCallback = SS13.new("/datum/callback", SS13.state, "call_function_return_first")
 		local cleanupPath = { "__SS13_signal_handlers", datumWeakRef, "_cleanup"}
 		cleanupCallback.vars.arguments = { cleanupPath }
@@ -136,8 +137,7 @@ function SS13.unregister_signal(datum, signal, callback)
 			handler_callback:call_proc("UnregisterSignal", datum, signal)
 		else
 			local actualDatum = datum:call_proc("hard_resolve")
-			-- Turfs don't remove their signals on deletion.
-			if SS13.is_valid(actualDatum) or SS13.istype(actualDatum, "/turf") then
+			if SS13.is_valid(actualDatum) then
 				handler_callback:call_proc("UnregisterSignal", actualDatum, signal)
 			end
 		end
