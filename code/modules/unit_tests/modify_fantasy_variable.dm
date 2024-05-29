tgstation/tgstation

commit 69827604c46952dd4393db8617cd494ade17bea2

Improves the RPG loot wizard event. (#77218)
## About The Pull Request
As the title says. Adds a bunch more stat changes to various different
items and a somewhat simple way of modifying them whilst minimizing
side-effects as much as possible.
Added a new negative curse of polymorph suffix that can randomly
polymorph you once you pick up the item.
Curse of hunger items won't start on items that are not on a turf.
Curse of polymorph will only activate when equipped.

Bodyparts, two-handed melees, bags, guns and grenades, to name a few,
have a bunch of type-specific stat changes depending on their quality.

Some items won't gain fantasy suffixes during the RPG loot event, like
stacks, chairs and paper, to make gamifying the stats a bit harder.
I'm sure there'll still be other ways to game the event, but it's not
that big of a deal since these are the easiest ways to game it.
High level items also have a cool unusual effect aura

## Why It's Good For The Game
Makes the RPG item event cooler. Right now, it's a bit lame since
everything only gains force value and wound bonus on attack. This makes
the statistic increases more type-based and make it interesting to use

It's okay for some items to be powerful since this is a wizard event and
a very impactful one too. By making the curse of hunger items not spawn
on people, it'll also make it a less painful event too.

## Changelog
:cl:
add: Expanded the RPG loot wizard event by giving various different
items their own statistic boost.
/:cl:

---------

Co-authored-by: Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>

code/datums/components/fantasy/_fantasy.dm
@@ -17,16 +17,18 @@
 	if(!isitem(parent))
 		return COMPONENT_INCOMPATIBLE
 
-	src.quality = quality || randomQuality()
+	src.quality = quality
+	if(isnull(src.quality))
+		src.quality = random_quality()
 	src.canFail = canFail
 	src.announce = announce
 
 	src.affixes = affixes
 	appliedComponents = list()
 	if(affixes && affixes.len)
-		setAffixes()
+		set_affixes()
 	else
-		randomAffixes()
+		random_affixes()
 
 /datum/component/fantasy/Destroy()
 	unmodify()
@@ -37,6 +39,11 @@
 	var/obj/item/master = parent
 	originalName = master.name
 	modify()
+	RegisterSignal(parent, COMSIG_STACK_CAN_MERGE, PROC_REF(try_merge_stack))
+
+/datum/component/fantasy/proc/try_merge_stack(obj/item/stack/to_merge, in_hand)
+	SIGNAL_HANDLER
+	return CANCEL_STACK_MERGE
 
 /datum/component/fantasy/UnregisterFromParent()
 	unmodify()
@@ -53,14 +60,20 @@
 		src.announce = announce || src.announce
 	modify()
 
-/datum/component/fantasy/proc/randomQuality()
+/datum/component/fantasy/proc/random_quality()
 	var/quality = pick(1;15, 2;14, 2;13, 2;12, 3;11, 3;10, 3;9, 4;8, 4;7, 4;6, 5;5, 5;4, 5;3, 6;2, 6;1, 6;0)
 	if(prob(50))
 		quality = -quality
 	return quality
 
 ///proc on creation for random affixes
-/datum/component/fantasy/proc/randomAffixes(force)
+/datum/component/fantasy/proc/random_affixes(force)
+	var/alignment
+	if(quality >= 0)
+		alignment |= AFFIX_GOOD
+	if(quality <= 0)
+		alignment |= AFFIX_EVIL
+
 	if(!affixListing)
 		affixListing = list()
 		for(var/T in subtypesof(/datum/fantasy_affix))
@@ -71,12 +84,6 @@
 		if(!force)
 			return
 		affixes = list()
-
-	var/alignment
-	if(quality >= 0)
-		alignment |= AFFIX_GOOD
-	if(quality <= 0)
-		alignment |= AFFIX_EVIL
 
 	var/usedSlots = NONE
 	for(var/i in 1 to max(1, abs(quality))) // We want at least 1 affix applied
@@ -91,7 +98,7 @@
 		usedSlots |= affix.placement
 
 ///proc on creation for specific affixes given to the fantasy component
-/datum/component/fantasy/proc/setAffixes(force)
+/datum/component/fantasy/proc/set_affixes(force)
 	var/usedSlots = NONE
 	for(var/datum/fantasy_affix/affix in affixes) // We want at least 1 affix applied
 		if((affix.placement & usedSlots) || (!affix.validate(parent)))
@@ -101,12 +108,7 @@
 
 /datum/component/fantasy/proc/modify()
 	var/obj/item/master = parent
-
-	master.force = max(0, master.force + quality)
-	master.throwforce = max(0, master.throwforce + quality)
-	master.set_armor(master.get_armor().generate_new_with_modifiers(list(ARMOR_ALL = quality)))
-	master.wound_bonus += quality
-	master.bare_wound_bonus += quality
+	master.apply_fantasy_bonuses(quality)
 
 	var/newName = originalName
 	for(var/i in affixes)
@@ -121,10 +123,10 @@
 		place.visible_message(span_danger("[parent] [span_blue("violently glows blue")] for a while, then evaporates."))
 		master.burn()
 		return
-	else if(announce)
-		announce()
 
 	master.name = newName
+	if(announce)
+		announce()
 
 /datum/component/fantasy/proc/unmodify()
 	var/obj/item/master = parent
@@ -133,12 +135,7 @@
 		var/datum/fantasy_affix/affix = i
 		affix.remove(src)
 	QDEL_LIST(appliedComponents)
-
-	master.force = max(0, master.force - quality)
-	master.throwforce = max(0, master.throwforce - quality)
-	master.set_armor(master.get_armor().generate_new_with_modifiers(list(ARMOR_ALL = -quality)))
-	master.wound_bonus -= quality
-	master.bare_wound_bonus -= quality
+	master.remove_fantasy_bonuses(quality)
 
 	master.name = originalName
 
@@ -153,4 +150,4 @@
 		span = "<span class='danger'>"
 		effect_description = span_bold("mottled black glow")
 
-	location.visible_message("[span][originalName] is covered by a [effect_description] and then transforms into [parent]!</span>")
+	location.visible_message("[span]The [originalName] is covered by a [effect_description] and then transforms into [parent]!</span>")

code/datums/components/slippery.dm
@@ -53,8 +53,28 @@
 		if(isitem(parent))
 			RegisterSignal(parent, COMSIG_ITEM_EQUIPPED, PROC_REF(on_equip))
 			RegisterSignal(parent, COMSIG_ITEM_DROPPED, PROC_REF(on_drop))
+			RegisterSignal(parent, COMSIG_ITEM_APPLY_FANTASY_BONUSES, PROC_REF(apply_fantasy_bonuses))
+			RegisterSignal(parent, COMSIG_ITEM_REMOVE_FANTASY_BONUSES, PROC_REF(remove_fantasy_bonuses))
 	else
 		RegisterSignal(parent, COMSIG_ATOM_ENTERED, PROC_REF(Slip))
+
+/datum/component/slippery/proc/apply_fantasy_bonuses(obj/item/source, bonus)
+	SIGNAL_HANDLER
+	knockdown_time = source.modify_fantasy_variable("knockdown_time", knockdown_time, bonus)
+	if(bonus >= 5)
+		paralyze_time = source.modify_fantasy_variable("paralyze_time", paralyze_time, bonus)
+		LAZYSET(source.fantasy_modifications, "lube_flags", lube_flags)
+		lube_flags |= SLIDE
+	if(bonus >= 10)
+		lube_flags |= GALOSHES_DONT_HELP|SLIP_WHEN_CRAWLING
+
+/datum/component/slippery/proc/remove_fantasy_bonuses(obj/item/source, bonus)
+	SIGNAL_HANDLER
+	knockdown_time = source.reset_fantasy_variable("knockdown_time", knockdown_time)
+	paralyze_time = source.reset_fantasy_variable("paralyze_time", paralyze_time)
+	var/previous_lube_flags = LAZYACCESS(source.fantasy_modifications, "lube_flags")
+	if(!isnull(previous_lube_flags))
+		lube_flags = previous_lube_flags
 
 /datum/component/slippery/proc/add_connect_loc_behalf_to_parent()
 	if(ismovable(parent))

code/game/objects/items.dm
@@ -219,8 +219,10 @@
 	/// Used if we want to have a custom verb text for throwing. "John Spaceman flicks the ciggerate" for example.
 	var/throw_verb
 
-/obj/item/Initialize(mapload)
+	/// A lazylist used for applying fantasy values, contains the actual modification applied to a variable.
+	var/list/fantasy_modifications
 
+/obj/item/Initialize(mapload)
 	if(attack_verb_continuous)
 		attack_verb_continuous = string_list(attack_verb_continuous)
 	if(attack_verb_simple)
@@ -1555,3 +1557,41 @@
 /obj/item/update_atom_colour()
 	. = ..()
 	update_slot_icon()
+
+/// Modifies the fantasy variable
+/obj/item/proc/modify_fantasy_variable(variable_key, value, bonus, minimum = 0)
+	if(LAZYACCESS(fantasy_modifications, variable_key) != null)
+		stack_trace("modify_fantasy_variable was called twice for the same key '[variable_key]' on type '[type]' before reset_fantasy_variable could be called!")
+	var/intended_target = value + bonus
+	value = max(minimum, intended_target)
+
+	var/difference = intended_target - value
+	var/modified_amount = bonus - difference
+	LAZYSET(fantasy_modifications, variable_key, modified_amount)
+	return value
+
+/// Returns the original fantasy variable value
+/obj/item/proc/reset_fantasy_variable(variable_key, current_value)
+	var/modification = LAZYACCESS(fantasy_modifications, variable_key)
+	LAZYREMOVE(fantasy_modifications, variable_key)
+	if(!modification)
+		return current_value
+	return current_value - modification
+
+/obj/item/proc/apply_fantasy_bonuses(bonus)
+	SHOULD_CALL_PARENT(TRUE)
+	SEND_SIGNAL(src, COMSIG_ITEM_APPLY_FANTASY_BONUSES, bonus)
+	force = modify_fantasy_variable("force", force, bonus)
+	throwforce = modify_fantasy_variable("throwforce", throwforce, bonus)
+	wound_bonus = modify_fantasy_variable("wound_bonus", wound_bonus, bonus)
+	bare_wound_bonus = modify_fantasy_variable("bare_wound_bonus", bare_wound_bonus, bonus)
+	toolspeed = modify_fantasy_variable("toolspeed", toolspeed, -bonus/10, minimum = 0.1)
+
+/obj/item/proc/remove_fantasy_bonuses(bonus)
+	SHOULD_CALL_PARENT(TRUE)
+	force = reset_fantasy_variable("force", force)
+	throwforce = reset_fantasy_variable("throwforce", throwforce)
+	wound_bonus = reset_fantasy_variable("wound_bonus", wound_bonus)
+	bare_wound_bonus = reset_fantasy_variable("bare_wound_bonus", bare_wound_bonus)
+	toolspeed = reset_fantasy_variable("toolspeed", toolspeed)
+	SEND_SIGNAL(src, COMSIG_ITEM_REMOVE_FANTASY_BONUSES, bonus)

code/game/objects/items/storage/storage.dm
@@ -10,6 +10,28 @@
 	/// What storage type to use for this item
 	var/datum/storage/storage_type = /datum/storage
 
+/obj/item/storage/apply_fantasy_bonuses(bonus)
+	. = ..()
+	atom_storage.max_slots = modify_fantasy_variable("max_slots", atom_storage.max_slots, round(bonus/2))
+	atom_storage.max_total_storage = modify_fantasy_variable("max_total_storage", atom_storage.max_total_storage, round(bonus/2))
+	LAZYSET(fantasy_modifications, "max_specific_storage", atom_storage.max_specific_storage)
+	if(bonus >= 15)
+		atom_storage.max_specific_storage = max(WEIGHT_CLASS_HUGE, atom_storage.max_specific_storage)
+	else if(bonus >= 10)
+		atom_storage.max_specific_storage = max(WEIGHT_CLASS_BULKY, atom_storage.max_specific_storage)
+	else if(bonus <= -10)
+		atom_storage.max_specific_storage = WEIGHT_CLASS_SMALL
+	else if(bonus <= -15)
+		atom_storage.max_specific_storage = WEIGHT_CLASS_TINY
+
+/obj/item/storage/remove_fantasy_bonuses(bonus)
+	atom_storage.max_slots = reset_fantasy_variable("max_slots", atom_storage.max_slots)
+	atom_storage.max_total_storage = reset_fantasy_variable("max_total_storage", atom_storage.max_total_storage)
+	var/previous_max_storage = LAZYACCESS(fantasy_modifications, "max_specific_storage")
+	if(previous_max_storage)
+		atom_storage.max_specific_storage = previous_max_storage
+	return ..()
+
 /obj/item/storage/Initialize(mapload)
 	. = ..()
 

code/modules/projectiles/guns/energy/beam_rifle.dm
@@ -74,6 +74,18 @@
 
 	var/mob/listeningTo
 
+/obj/item/gun/energy/beam_rifle/apply_fantasy_bonuses(bonus)
+	. = ..()
+	delay = modify_fantasy_variable("delay", delay, -bonus * 2)
+	aiming_time = modify_fantasy_variable("aiming_time", aiming_time, -bonus * 2)
+	recoil = modify_fantasy_variable("aiming_time", aiming_time, round(-bonus / 2))
+
+/obj/item/gun/energy/beam_rifle/remove_fantasy_bonuses(bonus)
+	delay = reset_fantasy_variable("delay", delay)
+	aiming_time = reset_fantasy_variable("aiming_time", aiming_time)
+	recoil = reset_fantasy_variable("recoil", recoil)
+	return ..()
+
 /obj/item/gun/energy/beam_rifle/debug
 	delay = 0
 	cell_type = /obj/item/stock_parts/cell/infinite

tgstation.dme
@@ -926,6 +926,7 @@
 #include "code\datums\components\creamed.dm"
 #include "code\datums\components\cult_ritual_item.dm"
 #include "code\datums\components\curse_of_hunger.dm"
+#include "code\datums\components\curse_of_polymorph.dm"
 #include "code\datums\components\customizable_reagent_holder.dm"
 #include "code\datums\components\damage_aura.dm"
 #include "code\datums\components\deadchat_control.dm"
@@ -1072,6 +1073,7 @@
 #include "code\datums\components\udder.dm"
 #include "code\datums\components\unbreakable.dm"
 #include "code\datums\components\unobserved_actor.dm"
+#include "code\datums\components\unusual_effect.dm"
 #include "code\datums\components\uplink.dm"
 #include "code\datums\components\usb_port.dm"
 #include "code\datums\components\vacuum.dm"

code/modules/unit_tests/modify_fantasy_variable.dm
@@ -1,0 +1,21 @@
+// Unit test to make sure that there are no duplicate keys when modify_fantasy_variable is called when applying fantasy bonuses.
+// Also to make sure the fantasy_modifications list is null when fantasy bonuses are removed.
+/datum/unit_test/modify_fantasy_variable/Run()
+
+	for(var/obj/item/path as anything in subtypesof(/obj/item))
+		var/obj/item/object = allocate(path)
+		// Try positive
+		object.apply_fantasy_bonuses(bonus = 5)
+		object.remove_fantasy_bonuses(bonus = 5)
+		TEST_ASSERT_NULL(object.fantasy_modifications)
+		// Then negative
+		object.apply_fantasy_bonuses(bonus = -5)
+		object.remove_fantasy_bonuses(bonus = -5)
+		TEST_ASSERT_NULL(object.fantasy_modifications)
+		// Now try the extremes of each
+		object.apply_fantasy_bonuses(bonus = 500)
+		object.remove_fantasy_bonuses(bonus = 500)
+		TEST_ASSERT_NULL(object.fantasy_modifications)
+		object.apply_fantasy_bonuses(bonus = -500)
+		object.remove_fantasy_bonuses(bonus = -500)
+		TEST_ASSERT_NULL(object.fantasy_modifications)

code/controllers/subsystem/atoms.dm
@@ -155,6 +155,7 @@ SUBSYSTEM_DEF(atoms)
 		BadInitializeCalls[the_type] |= BAD_INIT_DIDNT_INIT
 	else
 		SEND_SIGNAL(A, COMSIG_ATOM_AFTER_SUCCESSFUL_INITIALIZE)
+		SEND_GLOBAL_SIGNAL(COMSIG_GLOB_ATOM_AFTER_POST_INIT, A)
 		var/atom/location = A.loc
 		if(location)
 			/// Sends a signal that the new atom `src`, has been created at `loc`

commit da05ffa8c90ac1c251d37cf9bb73f17defdc3c75

[Port] unit test fixes (#1982)
* Fixes Ticked File Enforcement and Missing Unit Test (and makes said Unit Test Compile) (and genericizes the C&D list to the base unit test datum) (#77632)

Closes #77631

Hey there,

Ticked File Enforcement simply wasn't catching files that were missed.
That's a bit stupid, so I decided to look into what the issue might be,
and whoopsie daisies I did double periods back in #76592
(020ac2405308eab83f314282499dfe777aba5874).

![image](https://github.com/tgstation/tgstation/assets/34697715/6023afe8-313d-4550-9a60-58a8bc211b4f)

I also added some debug info and some more checks to prevent such a
break from happening again on runtime of this script. I thought it was a
weird string concatenation issue (and not the simple break I thought it
was), so I rewrote how it adds `glob`s. I think it's cleaner so I'll
keep it anyhow

This PR also corrects the oversight of the missing unit test (introduced
in #77218 (69827604c46952dd4393db8617cd494ade17bea2)) by reticking it in
the `_unit_tests.dm` file, and also makes it compile because it didn't
do that.

I also then had to do some more work to get the unit test to work.
* Genericizes the Create-and-Destroy "ignore" list to be a static list
on `/datum/unit_test` to allow it to be shared between these types of
tests that we need to test.
* Adds that list to C&D and the broken unit test regarding fantasy
bonuses
* Fixes some actually broken that the unit test was made to catch (beam
rifles, butterdogs and other slippery items, random ingredient boxes).
* Adds cases for things that the unit test and overall framework really
shouldn't be altering anyways (mythril), and was likely causing
inappropriate stack traces on master

Unit Tests WORK. Tools WORK.

![image](https://github.com/tgstation/tgstation/assets/34697715/9a59c0db-7a33-4546-918b-c73372a5b867)

:cl:
fix: Beam rifles will no longer inappropriately retain any bonuses they
may gain from wizardry.

code/__DEFINES/traits.dm
@@ -293,6 +293,8 @@ Remember to update _globalvars/traits.dm if you're adding/removing/renaming trai
 #define TRAIT_ANTIMAGIC_NO_SELFBLOCK "anti_magic_no_selfblock"
 /// The user can do things like use magic staffs without penalty
 #define TRAIT_MAGICALLY_GIFTED "magically_gifted"
+/// This object innately spawns with fantasy variables already applied (the magical component is given to it on initialize), and thus we never want to give it the component again.
+#define TRAIT_INNATELY_FANTASTICAL_ITEM "innately_fantastical_item"
 #define TRAIT_DEPRESSION "depression"
 #define TRAIT_BLOOD_DEFICIENCY "blood_deficiency"
 #define TRAIT_JOLLY "jolly"

code/__HELPERS/~monkestation-helpers/atoms.dm
@@ -1,0 +1,6 @@
+/atom/proc/effective_contents(list/typecache = null)
+	var/static/list/default_typecache
+	if(!typecache)
+		default_typecache ||= typecacheof(list(/obj/effect, /atom/movable/screen))
+		typecache = default_typecache
+	return typecache_filter_list_reverse(src.contents, typecache)

code/datums/components/fantasy/_fantasy.dm
@@ -14,7 +14,7 @@
 
 ///affixes expects an initialized list
 /datum/component/fantasy/Initialize(quality, list/affixes = list(), canFail=FALSE, announce=FALSE)
-	if(!isitem(parent))
+	if(!isitem(parent) || HAS_TRAIT(parent, TRAIT_INNATELY_FANTASTICAL_ITEM))
 		return COMPONENT_INCOMPATIBLE
 
 	src.quality = quality

code/datums/components/slippery.dm
@@ -62,6 +62,7 @@
 	knockdown_time = source.reset_fantasy_variable("knockdown_time", knockdown_time)
 	paralyze_time = source.reset_fantasy_variable("paralyze_time", paralyze_time)
 	var/previous_lube_flags = LAZYACCESS(source.fantasy_modifications, "lube_flags")
+	LAZYREMOVE(source.fantasy_modifications, "lube_flags")
 	if(!isnull(previous_lube_flags))
 		lube_flags = previous_lube_flags
 

code/datums/materials/basemats.dm
@@ -325,10 +325,12 @@ Unless you know what you're doing, only use the first three numbers. They're in 
 	. = ..()
 	if(isitem(source))
 		source.AddComponent(/datum/component/fantasy)
+		ADD_TRAIT(source, TRAIT_INNATELY_FANTASTICAL_ITEM, REF(src)) // DO THIS LAST OR WE WILL NEVER GET OUR BONUSES!!!
 
 /datum/material/mythril/on_removed_obj(atom/source, amount, material_flags)
 	. = ..()
 	if(isitem(source))
+		REMOVE_TRAIT(source, TRAIT_INNATELY_FANTASTICAL_ITEM, REF(src)) // DO THIS FIRST OR WE WILL NEVER GET OUR BONUSES DELETED!!!
 		qdel(source.GetComponent(/datum/component/fantasy))
 
 /datum/material/mythril/on_accidental_mat_consumption(mob/living/carbon/victim, obj/item/source_item)

code/game/objects/items.dm
@@ -231,7 +231,7 @@
 	var/throw_verb
 
 	/// A lazylist used for applying fantasy values, contains the actual modification applied to a variable.
-	var/list/fantasy_modifications
+	var/list/fantasy_modifications = null
 
 /obj/item/Initialize(mapload)
 	if(attack_verb_continuous)
@@ -1617,8 +1617,13 @@
 
 /// Modifies the fantasy variable
 /obj/item/proc/modify_fantasy_variable(variable_key, value, bonus, minimum = 0)
-	if(LAZYACCESS(fantasy_modifications, variable_key) != null)
+	var/result = LAZYACCESS(fantasy_modifications, variable_key)
+	if(!isnull(result))
+		if(HAS_TRAIT(src, TRAIT_INNATELY_FANTASTICAL_ITEM))
+			return result // we are immune to your foul magicks you inferior wizard, we keep our bonuses
+
 		stack_trace("modify_fantasy_variable was called twice for the same key '[variable_key]' on type '[type]' before reset_fantasy_variable could be called!")
+
 	var/intended_target = value + bonus
 	value = max(minimum, intended_target)
 
@@ -1630,9 +1635,14 @@
 /// Returns the original fantasy variable value
 /obj/item/proc/reset_fantasy_variable(variable_key, current_value)
 	var/modification = LAZYACCESS(fantasy_modifications, variable_key)
+
+	if(isnum(modification) && HAS_TRAIT(src, TRAIT_INNATELY_FANTASTICAL_ITEM))
+		return modification // we are immune to your foul magicks you inferior wizard, we keep our bonuses the way they are
+
 	LAZYREMOVE(fantasy_modifications, variable_key)
-	if(!modification)
+	if(isnull(modification))
 		return current_value
+
 	return current_value - modification
 
 /obj/item/proc/apply_fantasy_bonuses(bonus)

code/game/objects/items/storage/storage.dm
@@ -12,6 +12,9 @@
 
 /obj/item/storage/apply_fantasy_bonuses(bonus)
 	. = ..()
+	if(isnull(atom_storage)) // some abstract types of storage (yes i know) don't get a datum
+		return
+
 	atom_storage.max_slots = modify_fantasy_variable("max_slots", atom_storage.max_slots, round(bonus/2))
 	atom_storage.max_total_storage = modify_fantasy_variable("max_total_storage", atom_storage.max_total_storage, round(bonus/2))
 	LAZYSET(fantasy_modifications, "max_specific_storage", atom_storage.max_specific_storage)
@@ -25,9 +28,13 @@
 		atom_storage.max_specific_storage = WEIGHT_CLASS_TINY
 
 /obj/item/storage/remove_fantasy_bonuses(bonus)
+	if(isnull(atom_storage)) // some abstract types of storage (yes i know) don't get a datum
+		return ..()
+
 	atom_storage.max_slots = reset_fantasy_variable("max_slots", atom_storage.max_slots)
 	atom_storage.max_total_storage = reset_fantasy_variable("max_total_storage", atom_storage.max_total_storage)
 	var/previous_max_storage = LAZYACCESS(fantasy_modifications, "max_specific_storage")
+	LAZYREMOVE(fantasy_modifications, "max_specific_storage")
 	if(previous_max_storage)
 		atom_storage.max_specific_storage = previous_max_storage
 	return ..()

code/modules/projectiles/guns/energy/beam_rifle.dm
@@ -78,7 +78,7 @@
 	. = ..()
 	delay = modify_fantasy_variable("delay", delay, -bonus * 2)
 	aiming_time = modify_fantasy_variable("aiming_time", aiming_time, -bonus * 2)
-	recoil = modify_fantasy_variable("aiming_time", aiming_time, round(-bonus / 2))
+	recoil = modify_fantasy_variable("recoil", recoil, round(-bonus / 2))
 
 /obj/item/gun/energy/beam_rifle/remove_fantasy_bonuses(bonus)
 	delay = reset_fantasy_variable("delay", delay)

tgstation.dme
@@ -554,6 +554,7 @@
 #include "code\__HELPERS\sorts\TimSort.dm"
 #include "code\__HELPERS\~monkestation-helpers\announcements.dm"
 #include "code\__HELPERS\~monkestation-helpers\antags.dm"
+#include "code\__HELPERS\~monkestation-helpers\atoms.dm"
 #include "code\__HELPERS\~monkestation-helpers\icon_smoothing.dm"
 #include "code\__HELPERS\~monkestation-helpers\roundend.dm"
 #include "code\__HELPERS\~monkestation-helpers\time.dm"

tools/ticked_file_enforcement/ticked_file_enforcement.py
@@ -37,7 +37,7 @@ for excluded_file in excluded_files:
         post_error(f"Excluded file {full_file_path} does not exist, please remove it!")
         sys.exit(1)
 
-file_extensions = (".dm", ".dmf")
+file_extensions = ("dm", "dmf")
 
 reading = False
 lines = []
@@ -64,7 +64,12 @@ fail_no_include = False
 
 scannable_files = []
 for file_extension in file_extensions:
-    scannable_files += glob.glob(scannable_directory + f"**/*.{file_extension}", recursive=True)
+    compiled_directory = f"{scannable_directory}/**/*.{file_extension}"
+    scannable_files += glob.glob(compiled_directory, recursive=True)
+
+if len(scannable_files) == 0:
+    post_error(f"No files were found in {scannable_directory}. Ticked File Enforcement has failed!")
+    sys.exit(1)
 
 for code_file in scannable_files:
     dm_path = ""
@@ -148,4 +153,4 @@ for (index, line) in enumerate(lines):
         post_error(f"The include at line {index + offset} is out of order ({line}, expected {sorted_lines[index]})")
         sys.exit(1)
 
-print(green(f"Ticked File Enforcement: [{file_reference}] All includes are in order!"))
+print(green(f"Ticked File Enforcement: [{file_reference}] All includes (for {len(scannable_files)} scanned files) are in order!"))

code/modules/unit_tests/_unit_tests.dm
@@ -128,6 +128,7 @@
 #include "heretic_rituals.dm"
 #include "high_five.dm"
 #include "holidays.dm"
+#include "human_through_recycler.dm"
 #include "hunger_curse.dm"
 #include "hydroponics_extractor_storage.dm"
 #include "hydroponics_validate_genes.dm"
@@ -151,6 +152,7 @@
 #include "mob_chains.dm"
 #include "mob_faction.dm"
 #include "mob_spawn.dm"
+#include "modify_fantasy_variable.dm"
 #include "modsuit.dm"
 #include "modular_map_loader.dm"
 #include "mouse_bite_cable.dm"

code/modules/unit_tests/human_through_recycler.dm
@@ -18,7 +18,7 @@
 	TEST_ASSERT(chewer.bloody, "The emagged recycler did not become bloody after crushing the assistant!")
 
 	// Now, let's test to see if all of their clothing got properly deleted.
-	TEST_ASSERT_EQUAL(length(assistant.contents), 0, "Assistant still has items in its contents after being put through an emagged recycler!")
+	TEST_ASSERT_EQUAL(length(assistant.effective_contents()), 0, "Assistant still has items in its contents after being put through an emagged recycler!")
 	// Consistent Assistants will always have the following: ID, PDA, backpack, a uniform, a headset, and a pair of shoes. If any of these are still present, then the recycler did not properly delete the assistant's clothing.
 	// However, let's check for EVERYTHING just in case, because we don't want to miss anything.
 	// This is just what we expect to be deleted.

code/modules/unit_tests/modify_fantasy_variable.dm
@@ -1,21 +1,46 @@
 // Unit test to make sure that there are no duplicate keys when modify_fantasy_variable is called when applying fantasy bonuses.
 // Also to make sure the fantasy_modifications list is null when fantasy bonuses are removed.
-/datum/unit_test/modify_fantasy_variable/Run()
+/datum/unit_test/modify_fantasy_variable
+	priority = TEST_LONGER
 
-	for(var/obj/item/path as anything in subtypesof(/obj/item))
+/datum/unit_test/modify_fantasy_variable/Run()
+	var/list/applicable_types = subtypesof(/obj/item) - uncreatables
+
+	for(var/obj/item/path as anything in applicable_types)
 		var/obj/item/object = allocate(path)
+		// objects will have fantasy bonuses inherent to their type (like butterdogs and the slippery component), so we need to take this into account
+		var/number_of_extant_bonuses = LAZYLEN(object.fantasy_modifications)
+
+#define TEST_SUCCESS LAZYLEN(object.fantasy_modifications) == number_of_extant_bonuses
+
 		// Try positive
 		object.apply_fantasy_bonuses(bonus = 5)
 		object.remove_fantasy_bonuses(bonus = 5)
-		TEST_ASSERT_NULL(object.fantasy_modifications)
+		TEST_ASSERT(TEST_SUCCESS, generate_failure_message(object))
+
 		// Then negative
 		object.apply_fantasy_bonuses(bonus = -5)
 		object.remove_fantasy_bonuses(bonus = -5)
-		TEST_ASSERT_NULL(object.fantasy_modifications)
+		TEST_ASSERT(TEST_SUCCESS, generate_failure_message(object))
+
 		// Now try the extremes of each
 		object.apply_fantasy_bonuses(bonus = 500)
 		object.remove_fantasy_bonuses(bonus = 500)
-		TEST_ASSERT_NULL(object.fantasy_modifications)
+		TEST_ASSERT(TEST_SUCCESS, generate_failure_message(object))
+
 		object.apply_fantasy_bonuses(bonus = -500)
 		object.remove_fantasy_bonuses(bonus = -500)
-		TEST_ASSERT_NULL(object.fantasy_modifications)
+		TEST_ASSERT(TEST_SUCCESS, generate_failure_message(object))
+
+/// Returns a string that we use to describe the failure of the test.
+/datum/unit_test/modify_fantasy_variable/proc/generate_failure_message(obj/item/failed_object)
+	var/list/cached_modifications = failed_object.fantasy_modifications
+	var/length_of_modifications = LAZYLEN(cached_modifications)
+	var/list/failure_messages = list("Error found when adding+removing fantasy bonuses for [failed_object.type].")
+	failure_messages += "The length of the fantasy_modifications list was [length_of_modifications]."
+	if(length_of_modifications)
+		failure_messages += "The fantasy_modifications list was [cached_modifications.Join(", ")]."
+
+	return failure_messages.Join(" ")
+
+#undef TEST_SUCCESS