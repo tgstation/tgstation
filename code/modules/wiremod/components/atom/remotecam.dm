tgstation/tgstation

commit 326d2711525bea072d76e742d113965a871572a1

Use list for RegisterSignals

code/modules/wiremod/components/atom/remotecam.dm
@@ -283,8 +283,7 @@
 	if(istype(shell_parent, /obj/item/organ/internal/cyberimp/bci))
 		shell_camera = new /obj/machinery/camera (shell_parent)
 		init_camera()
-		RegisterSignal(shell_parent, COMSIG_ORGAN_IMPLANTED, PROC_REF(on_organ_implanted))
-		RegisterSignal(shell_parent, COMSIG_ORGAN_REMOVED, PROC_REF(on_organ_removed))
+		RegisterSignals(shell_parent, list(COMSIG_ORGAN_IMPLANTED, COMSIG_ORGAN_REMOVED), PROC_REF(on_organ_implanted))
 		var/obj/item/organ/internal/cyberimp/bci/bci = shell_parent
 		if(bci.owner) //If somehow the camera was added while shell is already installed inside a mob, assign signals
 			on_organ_implanted(null, bci.owner)

commit 072fd5ebbe85ea67012c86d45bd0155d89bfcf5f

Fix bug introduced in commit 326d2711525

code/modules/wiremod/components/atom/remotecam.dm
@@ -288,7 +288,8 @@
 		return
 	shell_camera = new /obj/machinery/camera (shell_parent)
 	init_camera()
-	RegisterSignals(shell_parent, list(COMSIG_ORGAN_IMPLANTED, COMSIG_ORGAN_REMOVED), PROC_REF(on_organ_implanted))
+	RegisterSignal(shell_parent, COMSIG_ORGAN_IMPLANTED, PROC_REF(on_organ_implanted))
+	RegisterSignal(shell_parent, COMSIG_ORGAN_REMOVED, PROC_REF(on_organ_removed))
 	var/obj/item/organ/internal/cyberimp/bci/bci = shell_parent
 	if(bci.owner) //If somehow the camera was added while shell is already installed inside a mob, assign signals
 		RegisterSignal(bci.owner, COMSIG_MOVABLE_MOVED, PROC_REF(update_camera_location))