e5b7ec6a3bba0e179248050d35e010f1f11cb013
Aylong <alexanderkitsa@gmail.com>
2024-07-30 04:33:32 +0300

    Better layout

--- tgui/packages/tgui-panel/settings/SettingsStatPanel.tsx
+++ tgui/packages/tgui-panel/settings/SettingsStatPanel.tsx
@@ -24,65 +24,61 @@ export function SettingsStatPanel(props) {
   const dispatch = useDispatch();
 
   return (
-    <Stack fill vertical>
-      <Section fill>
-        <Stack fill vertical>
-          <Stack.Item grow>
-            <LabeledList>
-              <LabeledList.Item label="Tabs" verticalAlign="middle">
-                <Stack.Item grow>
-                  {TabsViews.map((view) => (
-                    <Button
-                      key={view}
-                      color="transparent"
-                      selected={statTabsStyle === view}
-                      onClick={() =>
-                        dispatch(updateSettings({ statTabsStyle: view }))
-                      }
-                    >
-                      {capitalize(view)}
-                    </Button>
-                  ))}
-                </Stack.Item>
-              </LabeledList.Item>
-              <LabeledList.Item label="Font size">
-                <Stack.Item grow>
-                  {statLinked ? (
-                    <LinkedToChat />
-                  ) : (
-                    <Slider
-                      width="100%"
-                      step={1}
-                      stepPixelSize={20}
-                      minValue={8}
-                      maxValue={32}
-                      value={statFontSize}
-                      unit="px"
-                      format={(value) => toFixed(value)}
-                      onChange={(e, value) =>
-                        dispatch(updateSettings({ statFontSize: value }))
-                      }
-                    />
-                  )}
-                </Stack.Item>
-              </LabeledList.Item>
-            </LabeledList>
-          </Stack.Item>
-          <Stack.Divider />
-          <Stack.Item textAlign="center">
-            <Button
-              fluid
-              icon="link"
-              color={statLinked ? 'bad' : 'good'}
-              onClick={() =>
-                dispatch(updateSettings({ statLinked: !statLinked }))
-              }
-            >
-              {statLinked ? 'Unlink from chat' : 'Link to chat'}
-            </Button>
-          </Stack.Item>
-        </Stack>
-      </Section>
-    </Stack>
+    <Section fill>
+      <Stack fill vertical>
+        <Stack.Item>
+          <LabeledList>
+            <LabeledList.Item label="Tabs" verticalAlign="middle">
+              {TabsViews.map((view) => (
+                <Button
+                  key={view}
+                  color="transparent"
+                  selected={statTabsStyle === view}
+                  onClick={() =>
+                    dispatch(updateSettings({ statTabsStyle: view }))
+                  }
+                >
+                  {capitalize(view)}
+                </Button>
+              ))}
+            </LabeledList.Item>
+            <LabeledList.Item label="Font size">
+              <Stack.Item grow>
+                {statLinked ? (
+                  <LinkedToChat />
+                ) : (
+                  <Slider
+                    width="100%"
+                    step={1}
+                    stepPixelSize={20}
+                    minValue={8}
+                    maxValue={32}
+                    value={statFontSize}
+                    unit="px"
+                    format={(value) => toFixed(value)}
+                    onChange={(e, value) =>
+                      dispatch(updateSettings({ statFontSize: value }))
+                    }
+                  />
+                )}
+              </Stack.Item>
+            </LabeledList.Item>
+          </LabeledList>
+        </Stack.Item>
+        <Stack.Divider />
+        <Stack.Item textAlign="center">
+          <Button
+            fluid
+            icon="link"
+            color={statLinked ? 'bad' : 'good'}
+            onClick={() =>
+              dispatch(updateSettings({ statLinked: !statLinked }))
+            }
+          >
+            {statLinked ? 'Unlink from chat' : 'Link to chat'}
+          </Button>
+        </Stack.Item>
+      </Stack>
+    </Section>
   );
 }
