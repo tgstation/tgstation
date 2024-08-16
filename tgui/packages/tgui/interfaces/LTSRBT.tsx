85e01a497fea86ab31c2a16ab20de03b7b7720ab
Ghom <42542238+Ghommie@users.noreply.github.com>
2024-08-16 18:47:47 +0200

    You can sell stuff on the blackmarket now. (#85464)
    ## About The Pull Request
    The idea came up during the last blackmarket-related PR (#85066), when
    the original creator @TheChosenEvilOne came and said the LTSRBT was
    originally supposed to allow players to sell stuff on the blackmarket. I
    replied saying the idea has some potential, and then other github users
    followed, also saying it's a good idea.
    
    So, here we are, adding another feature to the LTSRBT, to let you
    provide other players with potentially knock-offs and fraudulent (or
    genuinely honest) items on the blackmarket.
    
    How you do it is fairly simple: left click the machine to open it, place
    the item inside, then right-click to open the UI. From there you can
    adjust the name, description and price of the market item. Finally,
    click on the "Place on Market" button twice, pay a 30 credits fee, and
    it's done. You have an item on the market, and once it's sold, 85% or
    the earnings (the price) will be deposited on your account.
    
    By the by, the icon of the item you're trying to sell will also show up
    on the market. You should get a picture of what you're getting before
    falling for an obvious scam.
    
    
    ![screenie](https://github.com/user-attachments/assets/fae70c25-ab46-4ceb-af9e-f4818d8a1c68)
    
    By the by, it's a cardboard cutout.
    
    ## Why It's Good For The Game
    This fits the design of the LTSRBT and blackmarket in general. 
    
    ## Changelog
    
    :cl:
    add: You can sell items on the blackmarket with the LTSRBT now.
    fix: Added some checks to prevent the swapper device and bluespace
    anomalies from theorically being able to send things and people to
    nullspace.
    /:cl:
    
    ---------
    
    Co-authored-by: ArcaneMusic <41715314+ArcaneMusic@users.noreply.github.com>

---
+++ tgui/packages/tgui/interfaces/LTSRBT.tsx
@@ -0,0 +1,94 @@
+import { useBackend } from '../backend';
+import {
+  Button,
+  Image,
+  Input,
+  NumberInput,
+  Section,
+  Stack,
+  TextArea,
+} from '../components';
+import { Window } from '../layouts';
+
+type Data = {
+  name: string;
+  price: number;
+  min_price: number;
+  max_price: number;
+  loaded_icon: string;
+  desc: string;
+};
+
+export const LTSRBT = (props) => {
+  const { act, data } = useBackend<Data>();
+  const { name, price, min_price, max_price, loaded_icon, desc } = data;
+
+  return (
+    <Window width={300} height={380} theme="hackerman">
+      <Window.Content>
+        <Stack vertical fill>
+          <Stack.Item>
+            <Input
+              width="80%"
+              value={name}
+              placeholder="Insert a name"
+              onChange={(e, value) =>
+                act('change_name', {
+                  value: value,
+                })
+              }
+            />
+            <NumberInput
+              step={1}
+              width="20%"
+              value={price}
+              minValue={min_price}
+              maxValue={max_price}
+              unit="cr"
+              onChange={(value) =>
+                act('change_price', {
+                  value: value,
+                })
+              }
+            />
+          </Stack.Item>
+          <Stack.Divider />
+          {!!loaded_icon && (
+            <Stack.Item>
+              <Section align="center">
+                <Image
+                  m={1}
+                  src={`data:image/jpeg;base64,${loaded_icon}`}
+                  height="96px"
+                  width="96px"
+                />
+              </Section>
+            </Stack.Item>
+          )}
+          <Stack.Divider />
+          <Stack.Item grow>
+            <TextArea
+              height="90%"
+              value={desc}
+              placeholder="Insert a description (or don't)"
+              onChange={(e, value) =>
+                act('change_desc', {
+                  value: value,
+                })
+              }
+            />
+          </Stack.Item>
+          <Stack.Divider />
+          <Stack.Item>
+            <Button.Confirm
+              fluid
+              icon="truck-arrow-right"
+              content="Place on Market"
+              onClick={() => act('place_on_market')}
+            />
+          </Stack.Item>
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
