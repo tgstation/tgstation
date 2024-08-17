8dff5c57fed6cb44da28593ee364b577cef6d3bf
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-08-17 17:18:21 -0400

    Add a helpful messages to users when InstallDeps.sh inevitably fails due to lack of perms (#85908)
    People always trip on this
    
    ---------
    
    Co-authored-by: san7890 <the@san7890.com>

--- tools/tgs_scripts/InstallDeps.sh
+++ tools/tgs_scripts/InstallDeps.sh
@@ -12,16 +12,24 @@ set -e
 set -x
 
 # apt packages, libssl needed by rust-g but not included in TGS barebones install
-if ! ( [ -x "$has_git" ] && [ -x "$has_curl" ] && [ -f "/usr/lib/i386-linux-gnu/libssl.so" ] ); then
-	echo "Installing apt dependencies..."
+if ! ( [ -x "$has_git" ] && [ -x "$has_curl" ] && [ -x "$has_pip3" ] && [ -f "/usr/lib/i386-linux-gnu/libssl.so" ] ); then
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!!! HEY YOU THERE, READING THE TGS LOGS READ THIS!!!"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "We are about to try installing native dependencies, we will use 'sudo' if possible for this, but it may fail because the tgstation-server user doesn't have passwordless sudo."
+	echo "WE DO NOT RECOMMEND GRANTING PASSWORDLESS SUDO!!! Instead, install all the dependencies yourself with the following command:"
+	echo "............................................................................................................................................"
+	echo "sudo apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl libclang-dev g++-multilib python3 python3-pip"
+	echo "............................................................................................................................................"
+	echo "Attempting to install apt dependencies..."
 	if ! [ -x "$has_sudo" ]; then
 		dpkg --add-architecture i386
 		apt-get update
-		apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl libclang-dev g++-multilib
+		apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl libclang-dev g++-multilib python3 python3-pip
 	else
 		sudo dpkg --add-architecture i386
 		sudo apt-get update
-		sudo apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl libclang-dev g++-multilib
+		sudo apt-get install -y lib32z1 git pkg-config libssl-dev:i386 libssl-dev zlib1g-dev:i386 curl libclang-dev g++-multilib python3 python3-pip
 	fi
 fi
 
@@ -36,15 +44,8 @@ fi
 # which we assume was used to install it
 if ! [ -x "$has_youtubedl" ]; then
 	echo "Installing youtube-dl with pip3..."
-	if ! [ -x "$has_sudo" ]; then
-		apt-get update
-		apt-get install -y python3 python3-pip
-	else
-		sudo apt-get update
-		sudo apt-get install -y python3 python3-pip
-	fi
 	pip3 install youtube-dl --break-system-packages
-elif [ -x "$has_pip3" ]; then
+else
 	echo "Ensuring youtube-dl is up-to-date with pip3..."
 	pip3 install youtube-dl -U --break-system-packages
 fi
