a4796986ff24b4c8bbfb5280a57d0b9ed15d6416
tgstation-ci <179393467+tgstation-ci[bot]@users.noreply.github.com>
2024-08-30 16:49:58 +0000

    Revert "You can sell stuff on the blackmarket now. (#85464)"
    This reverts commit 85e01a497fea86ab31c2a16ab20de03b7b7720ab.

--- tgui/packages/tgui/interfaces/BlackMarketUplink.tsx
+++ tgui/packages/tgui/interfaces/BlackMarketUplink.tsx
@@ -2,7 +2,6 @@ import {
   AnimatedNumber,
   Box,
   Button,
-  Image,
   Modal,
   Section,
   Stack,
@@ -37,7 +36,6 @@ type Item = {
   desc: string;
   amount: number;
   cost: number;
-  html_icon: string;
 };
 
 type DeliveryMethod = {
@@ -108,22 +106,8 @@ export const BlackMarketUplink = (props) => {
             {items.map((item) => (
               <Box key={item.name} className="candystripe" p={1} pb={2}>
                 <Stack align="baseline">
-                  <Stack.Item grow>
-                    <Stack align="horizontal">
-                      {!!item.html_icon && (
-                        <Stack.Item>
-                          <Image
-                            m={1}
-                            src={`data:image/jpeg;base64,${item.html_icon}`}
-                            height="64px"
-                            width="64px"
-                          />
-                        </Stack.Item>
-                      )}
-                      <Stack.Item grow bold>
-                        {item.name}
-                      </Stack.Item>
-                    </Stack>
+                  <Stack.Item grow bold>
+                    {item.name}
                   </Stack.Item>
                   <Stack.Item color="label">
                     {item.amount ? item.amount + ' in stock' : 'Out of stock'}
