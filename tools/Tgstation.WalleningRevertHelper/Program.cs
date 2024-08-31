b642941b3727e7f27d7a53c53b3c415660b366d7
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-08-31 17:29:35 -0400

    Link the shitter

--- tools/Tgstation.WalleningRevertHelper/Program.cs
+++ tools/Tgstation.WalleningRevertHelper/Program.cs
@@ -223,7 +223,7 @@ After that some startup commits on this branch need to be reverted then it can b
 			if (conflicted)
 			{
 				var originalPR = await client.PullRequest.Get("tgstation", "tgstation", nextPr);
-				body += $"\n\nThis PR appears to be conflicting. Please push a resolution and enable auto-merge. Pinging original author @{originalPR.User.Login} and @tgstation/commit-access for assistance.\n\nAlternatively, **execute the `Wallening Revert` action from the GitHub actions view with THIS PR's number (Not the one in the title) as input** to skip it. The original PR will be labelled `Lost to Wallening Revert`.";
+				body += $"\n\nThis PR appears to be conflicting. Please push a resolution and enable auto-merge. Pinging original author @{originalPR.User.Login} and @tgstation/commit-access for assistance.\n\nAlternatively, **execute the `Wallening Revert` action from the GitHub actions view with THIS PR's number (Not the one in the title) as input** to skip it ([Link](https://github.com/tgstation/tgstation/actions/workflows/wallening_revert.yml)). The original PR will be labelled `Lost to Wallening Revert`.";
 				title += " (CONFLICTS)";
 			}
 
