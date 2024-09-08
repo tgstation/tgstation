tgstation/tgstation

commit e0c2639faf62e41f0d54752ba0768d7753fc5799

Fixes lag in search bars (#81968)
## About The Pull Request
- Fixes #81132
- Fixes #80853

To properly reproduce the issue you only need to type slowly in the
search bar(type the word "basic") of the shared engineering techfab in
metastation.


https://github.com/tgstation/tgstation/assets/110812394/1c2a5791-66e2-468a-9827-6541966c18a9

I don't fully understand why this fixes it so i need an explanation
before it gets merged.

This was implemented back in #80614 to fix this

> Problem goes a little deeper than simply adding "selfClear" prop -
ntos messenger is looking for more of a controlled component. Whenever
messages are sent, it attempts to update the value in the input box

But upon testing the chat messenger with this removed it appears to work
fine so not sure what got fixed where but one thing i can assert is that
it definitely has nothing to do with debounce.

## Changelog
:cl:
fix: search bars don't swallow characters or lag behind your typing
speed
/:cl:

tgui/packages/tgui/components/Input.tsx
@@ -150,17 +150,6 @@ export function Input(props: Props) {
     }, 1);
   }, []);
 
-  /** Updates the initial value on props change */
-  useEffect(() => {
-    const input = inputRef.current;
-    if (!input) return;
-
-    const newValue = toInputValue(value);
-    if (input.value === newValue) return;
-
-    input.value = newValue;
-  }, [value]);
-
   return (
     <Box
       className={classes([

commit 5691449521a6e6fa9214d02114dcec953b83a5e9

Fixes certain prefs menu fields not updating when swapping character slots (#86495)
## About The Pull Request

Fixes https://github.com/NovaSector/NovaSector/issues/4268

My second time fixing this bug...was broken again by
https://github.com/tgstation/tgstation/pull/81968 very shortly after I
fixed it.

Puts the gutted code that was necessary for this was happen back where
it belongs and locks it behind a conditional so it should not cause any
lag.

## Why It's Good For The Game

Restores missing functionality required for certain tgui components and
makes preferences menu useable again.

<details><summary>Before (watch the ringtone field)</summary>


![Yd1Af0Hx5e](https://github.com/user-attachments/assets/bbc4d1ee-7021-4db3-9e18-baa97efb227f)

</details>

<details><summary>After (now it updates properly)</summary>


![OvdRvWRhFe](https://github.com/user-attachments/assets/d2d71f78-e442-4c52-8fa5-7843272c511f)

</details>


## Changelog

:cl:
fix: certain text input fields in the character setup menu will now
update properly when swapping character slots
/:cl:

Co-authored-by: Ghom <42542238+Ghommie@users.noreply.github.com>

tgui/packages/tgui/components/Input.tsx
@@ -59,6 +59,8 @@ type OptionalProps = Partial<{
   placeholder: string;
   /** Clears the input value on enter */
   selfClear: boolean;
+  /** Auto-updates the input value on props change */
+  updateOnPropsChange: boolean;
   /** The state variable of the input. */
   value: string | number;
 }>;
@@ -96,6 +98,7 @@ export function Input(props: Props) {
     placeholder,
     selfClear,
     value,
+    updateOnPropsChange,
     ...rest
   } = props;
 
@@ -154,6 +157,19 @@ export function Input(props: Props) {
       }
     }, 1);
   }, []);
+
+  if (updateOnPropsChange) {
+    /** Updates the initial value on props change */
+    useEffect(() => {
+      const input = inputRef.current;
+      if (!input) return;
+
+      const newValue = toInputValue(value);
+      if (input.value === newValue) return;
+
+      input.value = newValue;
+    }, [value]);
+  }
 
   return (
     <Box

tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/base.tsx
@@ -261,6 +261,7 @@ export const FeatureShortTextInput = (
       width="100%"
       value={props.value}
       maxLength={props.serverData.maximum_length}
+      updateOnPropsChange
       onChange={(_, value) => props.handleSetValue(value)}
     />
   );