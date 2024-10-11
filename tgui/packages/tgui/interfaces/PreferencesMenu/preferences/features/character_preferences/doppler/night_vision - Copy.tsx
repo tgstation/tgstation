9fd65f6f02604e7c3bee24ebec9abff5c0da2289
Nerevar <12636964+Nerev4r@users.noreply.github.com>
2024-10-11 16:42:27 -0600

    dummy!!

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/doppler/night_vision - Copy.tsx
@@ -0,0 +1,7 @@
+// THIS IS A DOPPLER SHIFT UI FILE
+import { FeatureChoiced, FeatureDropdownInput } from '../../base';
+
+export const photophobia_severity: FeatureChoiced = {
+  name: 'Severity',
+  component: FeatureDropdownInput,
+};
