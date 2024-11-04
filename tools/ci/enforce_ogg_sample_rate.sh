5a97c5ca218cd18e70792f7344b57692e5ae5939
grungussuss <The5thbandit@gmail.com>
2024-11-05 01:02:57 +0300

    Update enforce_ogg_sample_rate.sh

--- tools/ci/enforce_ogg_sample_rate.sh
+++ tools/ci/enforce_ogg_sample_rate.sh
@@ -1,12 +1,13 @@
 actions/checkout@v2
+$fail = false
 for file in $(find . -type f -name '*.ogg'); do
   sample_rate=$(ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate -of default=noprint_wrappers=1:nokey=1 "$file")
   if [ "$sample_rate" != "44100" ]; then
-    fail = true
+    $fail = true
     echo "Error: $file has sample rate $sample_rate Hz (expected 44100 Hz)"
     fi
     done
-if(fail); then
+if($fail); then
   echo "Files are not up to sample rate standard, see standard.dm in the sound folder."
   exit 1
 echo "All OGG files have the correct sample rate."
