c10a0e9684917bdf860b4e658e00fed4fd31dda7
grungussuss <The5thbandit@gmail.com>
2024-11-05 23:55:05 +0300

    Update enforce_ogg_sample_rate.sh

--- tools/ci/enforce_ogg_sample_rate.sh
+++ tools/ci/enforce_ogg_sample_rate.sh
@@ -8,9 +8,3 @@ else
 	echo "All OGG files have the correct sample rate."
 fi
 exit 0
-
-find ~/tgstation/sound/ -type f \( -iname "*.mp3" -o -iname "*.wav" -o -iname "*.flac" -o -iname "*.aac" -o -iname "*.ogg" -o -iname "*.m4a" \) -print0 | xargs -0 -n 1 -P "$(nproc)" sh -c 'mediainfo --Inform="Audio;%SamplingRate%" "$1" > "./$(basename "$1").txt" && echo "File: $1" >> "./$(basename "$1").txt"' _ && cat *.txt > sample_rate.log && rm *.txt
-
-real    0m8.786s
-user    0m44.484s
-sys    0m12.694s
