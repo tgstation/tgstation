1e875ad4b3b93b15b2e98473571be39a3feb6bcd
unknown <borodinov542@yandex.ru>
2024-11-11 00:28:31 +0700

    modpack tgui v2

---
+++ modular_meta/__config_modpacks.dm
@@ -0,0 +1,15 @@
+// Change define from 1 to 0, if you want to remove module from compilation prosses -> this means that code of chosen modpack would be Fully Removed (all objs and procs too)
+// If you don't see module name here = you can't turn it off. So, please remove code manualy.
+// Looks like mods in minecraft, isn't it?
+// Some mods may have dependencies, have fun buddy!
+
+// If you are ENG server - you need at first turn off all PEREVODY (= translations on russian lang)
+
+
+/// FEATURES
+#define ADDITIONAL_CIRCUITS 1
+#define CHEBUREK_CAR 1
+/// PEREVODY
+#define RU_CRAYONS 1
+#define RU_VENDORS 1
+/// REVERTS
--- tgui/packages/tgui/interfaces/Modpacks.tsx
+++ tgui/packages/tgui/interfaces/Modpacks.tsx
@@ -1,51 +1,117 @@
 // THIS IS A MASSMETA UI FILE
 
-import { useBackend } from '../backend';
-import { Box, LabeledList, NoticeBox, Section } from '../components';
+import { useBackend, useLocalState } from '../backend';
+import {
+  Box,
+  LabeledList,
+  Input,
+  NoticeBox,
+  Section,
+  Collapsible,
+  Stack,
+} from '../components';
 import { Window } from '../layouts';
-
-export const Modpacks = (props) => {
   const { act, data } = useBackend();
   const { modpacks = [] } = data;
+
+export const Modpacks = (props, context) => {
 
   return (
     <Window title="Список модификаций" width={480} height={580}>
       <Window.Content scrollable>
+	    <NoticeBox>
+            В данный момент идёт наполнение меню модпаков, в игре модицикаций больше чем вы можете тут видеть.
+        </NoticeBox>
         {modpacks.length === 0 ? (
           <NoticeBox>
             Этот сервер не использует какие-либо модификации
           </NoticeBox>
         ) : (
-          <ModpackList modpacks={modpacks} />
+          <Stack fill vertical>
+            <ModpackList modpacks={modpacks} />
+          </Stack>
         )}
       </Window.Content>
     </Window>
   );
 };
 
-const ModpackList = ({ modpacks }) => (
-  <Section>
-    <LabeledList>
-      {modpacks.map((modpack) => (
-        <Box
-          key={modpack.id}
-          style={{
-            borderBottom: '1px solid #888',
-            paddingBottom: '10px',
-            fontSize: '14px',
-            textAlign: 'center',
-          }}
+const ModpackList = ({ modpacks }) => {
+  
+  const [searchText, setSearchText] = useLocalState('searchText', '');
+
+  const searchBar = (
+    <Input
+      placeholder="Искать модпак..."
+      fluid
+      onInput={(e, value) => setSearchText(value)}
+    />
+  );
+
+  return (
+    <>
+      <Stack.Item>
+        <Section fill>{searchBar}</Section>
+      </Stack.Item>
+      <Stack.Item grow>
+        <Section
+          fill
+          scrollable
+          title={
+            searchText.length > 0 ? (
+              <span>Результаты поиска "{searchText}":</span>
+            ) : (
+              <span>Суммарно модификации &mdash; {modpacks.length}</span>
+            )
+          }
         >
-          <ModpackItem key={modpack.id} modpack={modpack} />
-        </Box>
-      ))}
-    </LabeledList>
-  </Section>
-);
+          <Stack fill vertical>
+            <Stack.Item>
+              {modpacks
+                .filter(
+                  (modpack) =>
+                    modpack.name &&
+                    (searchText.length > 0
+                      ? modpack.name
+                          .toLowerCase()
+                          .includes(searchText.toLowerCase()) ||
+                        modpack.desc
+                          .toLowerCase()
+                          .includes(searchText.toLowerCase()) ||
+                        modpack.author
+                          .toLowerCase()
+                          .includes(searchText.toLowerCase())
+                      : true),
+                )
+                .map((modpack) => (
+                  <Collapsible
+                    color="transparent"
+                    key={modpack.name}
+                    title={<span class="color-white">{modpack.name}</span>}
+                  >
+                    <Box
+                      key={modpack.id}
+                      style={{
+                        borderBottom: '1px solid #888',
+                        paddingBottom: '10px',
+                        fontSize: '14px',
+                        textAlign: 'center',
+                      }}
+                    >
+                      <ModpackItem key={modpack.id} modpack={modpack} />
+                    </Box>
+                  </Collapsible>
+                ))}
+            </Stack.Item>
+          </Stack>
+        </Section>
+      </Stack.Item>
+    </>
+  );
+};
 
 const ModpackItem = ({ modpack }) => (
   <>
-    <LabeledList.Item label="Модуль">{modpack.name}</LabeledList.Item>
     <LabeledList.Item label="Описание">{modpack.desc}</LabeledList.Item>
     <LabeledList.Item label="Автор">{modpack.author}</LabeledList.Item>
   </>
