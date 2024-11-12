a209b361b64e9e512da3f2e386a7e50e9407d781
jlsnow301 <jlsnow301@pm.me>
2024-11-11 17:24:47 -0800

    the ride never ends

--- dependencies.sh
+++ dependencies.sh
@@ -12,6 +12,8 @@ export RUST_G_VERSION=3.3.0
 
 #node version
 export NODE_VERSION_LTS=22.11.0
+# compatiblility mode MUST work with windows 7
+export NODE_VERSION_COMPAT=20.2.0
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.9
