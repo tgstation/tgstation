2903746bb52adcf0973350081538e9fd02a27a6b
MTandi <mt.forspam@gmail.com>
2025-01-17 14:58:47 +0300

    icon display revert

--- tgui/packages/tgui/interfaces/LootPanel/IconDisplay.tsx
+++ tgui/packages/tgui/interfaces/LootPanel/IconDisplay.tsx
@@ -4,6 +4,7 @@ import { SearchItem } from './types';
 
 type Props = {
   item: SearchItem;
+  size: Size;
 };
 
 type Size = {
@@ -14,28 +15,30 @@ type Size = {
 export function IconDisplay(props: Props) {
   const {
     item: { icon, icon_state },
+    size: { height, width },
   } = props;
 
-  const fallback = <Icon name="spinner" spin color="gray" p={1.5} />;
+  const fallback = <Icon name="spinner" size={2.2} spin color="gray" />;
 
   if (!icon) {
     return fallback;
   }
 
   if (icon === 'n/a') {
-    return <Icon name="dumpster-fire" color="gray" p={1.5} />;
+    return <Icon name="dumpster-fire" size={2} color="gray" />;
   }
 
   if (icon_state) {
     return (
       <DmIcon
-        height={'32px'}
         fallback={fallback}
         icon={icon}
         icon_state={icon_state}
+        height={height}
+        width={width}
       />
     );
   }
 
-  return <Image fixErrors src={icon} height={'32px'} />;
+  return <Image fixErrors src={icon} height={3} width={3} />;
 }
