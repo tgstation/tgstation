0653bd01c011f2db4898456cc2da98b1d28d3e69
MrMelbert <kmelbert4@gmail.com>
2025-01-22 22:30:30 -0600

    Error

--- tools/pull_request_hooks/auto_label.js
+++ tools/pull_request_hooks/auto_label.js
@@ -137,7 +137,7 @@ function check_body_for_labels(body) {
 	const labels_to_add = [];
 
 	// if the body contains a github "fixes #1234" line, add the Fix tag
-	const fix_regex = new RegExp(`(?i)(fix[des]*|resolve[sd]*)\s*#[0-9]+/`);
+	const fix_regex = new RegExp(`(?i)(fix[des]*|resolve[sd]*)\s*#\d+`);
 	if (fix_regex.test(body)) {
 		labels_to_add.push('Fix');
 	}
