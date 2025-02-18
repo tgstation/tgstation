0c4d292a87ebd65d88b557fcec15141353e6233e
MrMelbert <kmelbert4@gmail.com>
2025-02-17 18:00:17 -0600

    Language Menu Pt2

--- tgui/packages/tgui/interfaces/LanguageMenu.tsx
+++ tgui/packages/tgui/interfaces/LanguageMenu.tsx
@@ -1,4 +1,6 @@
+import { PropsWithChildren } from 'react';
 import {
+  Box,
   Button,
   DmIcon,
   Icon,
@@ -15,212 +17,211 @@ import { Window } from '../layouts';
 type Language = {
   name: string;
   desc: string;
-  key: string;
-  is_default: BooleanLike;
-  can_speak?: BooleanLike;
-  can_understand?: BooleanLike;
+  key: string; // the key used to speak the language
+  is_default: BooleanLike; // the user's selected default language
+  can_speak: BooleanLike; // mentally, we know how to speak it
+  could_speak: BooleanLike; // physically, we are capable of speaking it
+  can_understand: BooleanLike; // we can understand it regardless
   icon: string;
   icon_state: string;
 };
 
 type Data = {
   is_living: BooleanLike;
-  admin_mode?: BooleanLike;
-  omnitongue?: BooleanLike;
+  admin_mode: BooleanLike;
+  omnitongue: BooleanLike;
   languages: Language[];
-  unknown_languages?: Language[];
+};
+
+const LangSpeakIcon = (props: { language: Language } & PropsWithChildren) => {
+  const { language } = props;
+  return (
+    <Icon
+      name="comment"
+      color={
+        language.could_speak
+          ? language.can_speak
+            ? 'good' // could speak and can speak
+            : 'bad' // could speak but cannot speak
+          : language.can_speak
+            ? 'average' // could not speak but can speak
+            : 'grey' // could not speak and cannot speak
+      }
+      {...props}
+    />
+  );
+};
+
+const LangUnderstandIcon = (props: { language: Language }) => {
+  const { language } = props;
+  return <Icon name="brain" color={language.can_understand ? 'good' : 'bad'} />;
+};
+
+const LanguageNameAndDesc = (props: { language: Language }) => {
+  const { language } = props;
+  return language.desc ? (
+    <Tooltip content={language.desc} position="bottom-start">
+      <Box
+        inline
+        style={{
+          borderBottom: '2px dotted rgba(255, 255, 255, 0.8)',
+        }}
+      >
+        {language.name}
+      </Box>
+    </Tooltip>
+  ) : (
+    <Box>{language.name}</Box>
+  );
+};
+
+const LanguageRow = (props: { language: Language }) => {
+  const { act, data } = useBackend<Data>();
+  const { is_living, admin_mode } = data;
+  const { language } = props;
+
+  return (
+    <Table.Row className="candystripe">
+      <Table.Cell verticalAlign="middle">
+        {language.icon && language.icon_state ? (
+          <Stack>
+            <Stack.Item>
+              <DmIcon icon={language.icon} icon_state={language.icon_state} />
+            </Stack.Item>
+            <Stack.Item>
+              <LanguageNameAndDesc language={language} />
+            </Stack.Item>
+          </Stack>
+        ) : (
+          <LanguageNameAndDesc language={language} />
+        )}
+      </Table.Cell>
+      <Table.Cell>
+        {language.can_speak && !language.could_speak ? (
+          <Tooltip
+            content={`Despite knowing how to speak ${language.name},
+              you are unable to due to physical limitations
+              (usually, your tongue).`}
+          >
+            <LangSpeakIcon
+              language={language}
+              inline
+              style={{
+                borderBottom: '2px dotted rgba(255, 255, 255, 0.8)',
+              }}
+            />
+          </Tooltip>
+        ) : (
+          <LangSpeakIcon language={language} />
+        )}
+      </Table.Cell>
+      <Table.Cell>
+        <LangUnderstandIcon language={language} />
+      </Table.Cell>
+      <Table.Cell>,{language.key}</Table.Cell>
+      {!!is_living && (
+        <Table.Cell>
+          <Button.Checkbox
+            checked={language.is_default}
+            color={
+              language.is_default
+                ? 'good'
+                : language.could_speak && language.can_speak
+                  ? 'grey'
+                  : 'transparent'
+            }
+            onClick={() =>
+              act('select_default', {
+                language_name: language.name,
+              })
+            }
+          >
+            Default
+          </Button.Checkbox>
+        </Table.Cell>
+      )}
+      {!!admin_mode && (
+        <Table.Cell>
+          <Button
+            disabled={language.can_speak && language.can_understand}
+            onClick={() =>
+              act('grant_language', {
+                language_name: language.name,
+              })
+            }
+          >
+            Grant
+          </Button>
+          <Button
+            disabled={!language.can_speak && !language.can_understand}
+            onClick={() =>
+              act('remove_language', {
+                language_name: language.name,
+              })
+            }
+          >
+            Remove
+          </Button>
+        </Table.Cell>
+      )}
+    </Table.Row>
+  );
+};
+
+const OmnitongueToggle = (props) => {
+  const { act, data } = useBackend<Data>();
+  const { omnitongue } = data;
+  return (
+    <Button
+      tooltip={`If enabled, the mob's tongue will no longer prevent them
+        from speaking languages they are physically incapable of speaking.`}
+      selected={omnitongue}
+      onClick={() => act('toggle_omnitongue')}
+    >
+      {'Omnitongue ' + (omnitongue ? 'Enabled' : 'Disabled')}
+    </Button>
+  );
 };
 
 export const LanguageMenu = (props) => {
-  const {
-    act,
-    data: {
-      admin_mode,
-      is_living,
-      omnitongue,
-      languages = [],
-      unknown_languages = [],
-    },
-  } = useBackend<Data>();
+  const { data } = useBackend<Data>();
+  const { admin_mode, languages } = data;
+
+  // only show languages we can speak OR understand, UNLESS we're an admin
+  // also, push all languages we can speak to the top, then all languagse we can only understand, then alphabetize
+  const shown_languages = languages
+    .filter(
+      (language) => admin_mode || language.can_speak || language.can_understand,
+    )
+    .sort(
+      (a, b) => (a.can_speak - b.can_speak) * -2 + (a.name > b.name ? 1 : -1),
+    );
+
   return (
-    <Window title="Language Menu" width={700} height={600}>
-      <Window.Content scrollable>
-        <Section title="Known Languages">
+    <Window
+      title="Language Menu"
+      width={700}
+      height={Math.min(shown_languages.length * 30 + 75, 500)}
+    >
+      <Window.Content>
+        <Section
+          scrollable
+          title="&nbsp;"
+          buttons={admin_mode ? <OmnitongueToggle /> : null}
+          fill
+        >
           <Table>
             <Table.Row header>
               <Table.Cell>Name</Table.Cell>
-              <Table.Cell>Understand</Table.Cell>
               <Table.Cell>Speak</Table.Cell>
+              <Table.Cell>Understand</Table.Cell>
               <Table.Cell>Key</Table.Cell>
-              <Table.Cell>Description</Table.Cell>
             </Table.Row>
-            {languages.map((language) => (
-              <Table.Row key={language.name}>
-                <Table.Cell verticalAlign="middle">
-                  <Stack>
-                    <Stack.Item>
-                      <DmIcon
-                        icon={language.icon}
-                        icon_state={language.icon_state}
-                      />
-                    </Stack.Item>
-                    <Stack.Item>{language.name}</Stack.Item>
-                  </Stack>
-                </Table.Cell>
-                <Table.Cell textAlign="center">
-                  <Tooltip
-                    content={
-                      language.can_understand
-                        ? 'You can understand this language.'
-                        : 'You cannot understand this language.'
-                    }
-                  >
-                    <Icon
-                      name="brain"
-                      color={language.can_understand ? 'good' : 'bad'}
-                    />
-                  </Tooltip>
-                </Table.Cell>
-                <Table.Cell textAlign="center">
-                  <Tooltip
-                    content={
-                      language.can_speak
-                        ? 'You can speak this language.'
-                        : 'You cannot speak this language.'
-                    }
-                  >
-                    <Icon
-                      name="comment"
-                      color={language.can_speak ? 'good' : 'bad'}
-                    />
-                  </Tooltip>
-                </Table.Cell>
-                <Table.Cell textAlign="center">,{language.key}</Table.Cell>
-                <Table.Cell p={0.5}>{language.desc}</Table.Cell>
-                {!!is_living && (
-                  <Table.Cell>
-                    <Button
-                      disabled={!language.can_speak}
-                      selected={language.is_default}
-                      onClick={() =>
-                        act('select_default', {
-                          language_name: language.name,
-                        })
-                      }
-                    >
-                      {language.is_default
-                        ? 'Default Language'
-                        : 'Select as Default'}
-                    </Button>
-                  </Table.Cell>
-                )}
-                {!!admin_mode && (
-                  <Table.Cell>
-                    <>
-                      <Button
-                        onClick={() =>
-                          act('grant_language', {
-                            language_name: language.name,
-                          })
-                        }
-                      >
-                        Grant
-                      </Button>
-                      <Button
-                        onClick={() =>
-                          act('remove_language', {
-                            language_name: language.name,
-                          })
-                        }
-                      >
-                        Remove
-                      </Button>
-                    </>
-                  </Table.Cell>
-                )}
-              </Table.Row>
+            {shown_languages.map((language) => (
+              <LanguageRow key={language.name} language={language} />
             ))}
           </Table>
         </Section>
-        {!!admin_mode && (
-          <Section
-            title="Unknown Languages"
-            buttons={
-              <Button
-                selected={omnitongue}
-                onClick={() => act('toggle_omnitongue')}
-              >
-                {'Omnitongue ' + (omnitongue ? 'Enabled' : 'Disabled')}
-              </Button>
-            }
-          >
-            <Table>
-              <Table.Row header>
-                <Table.Cell>Name</Table.Cell>
-                <Table.Cell>Understand</Table.Cell>
-                <Table.Cell>Speak</Table.Cell>
-                <Table.Cell>Key</Table.Cell>
-                <Table.Cell>Description</Table.Cell>
-              </Table.Row>
-              {unknown_languages.map((language) => (
-                <Table.Row key={language.name}>
-                  <Table.Cell verticalAlign="middle">
-                    <Stack>
-                      <Stack.Item>
-                        <DmIcon
-                          icon={language.icon}
-                          icon_state={language.icon_state}
-                        />
-                      </Stack.Item>
-                      <Stack.Item>{language.name}</Stack.Item>
-                    </Stack>
-                  </Table.Cell>
-                  <Table.Cell textAlign="center">
-                    <Tooltip
-                      content={
-                        language.can_understand
-                          ? 'You can understand this language.'
-                          : 'You cannot understand this language.'
-                      }
-                    >
-                      <Icon
-                        name="brain"
-                        color={language.can_understand ? 'good' : 'bad'}
-                      />
-                    </Tooltip>
-                  </Table.Cell>
-                  <Table.Cell textAlign="center">
-                    <Tooltip
-                      content={
-                        language.can_speak
-                          ? 'You can speak this language.'
-                          : 'You cannot speak this language.'
-                      }
-                    >
-                      <Icon
-                        name="comment"
-                        color={language.can_speak ? 'good' : 'bad'}
-                      />
-                    </Tooltip>
-                  </Table.Cell>
-                  <Table.Cell textAlign="center">,{language.key}</Table.Cell>
-                  <Table.Cell p={0.5}>{language.desc}</Table.Cell>
-                  <Table.Cell verticalAlign="middle">
-                    <Button
-                      onClick={() =>
-                        act('grant_language', {
-                          language_name: language.name,
-                        })
-                      }
-                    >
-                      Grant
-                    </Button>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-            </Table>
-          </Section>
-        )}
       </Window.Content>
     </Window>
   );
