09045fb1a5d2a79f51fcddfd2936e65a441ee0cc
OrionTheFox <76465278+OrionTheFox@users.noreply.github.com>
2025-03-07 00:37:43 -0700

    Adds new tile sprays yippee

--- tgui/packages/tgui/interfaces/DecalPainter.tsx
+++ tgui/packages/tgui/interfaces/DecalPainter.tsx
@@ -1,4 +1,4 @@
-import { Button, ColorBox, Flex, Section } from 'tgui-core/components';
+import { Button, ColorBox, Flex, Section, Stack } from 'tgui-core/components';
 
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
@@ -52,82 +52,88 @@ export const DecalPainter = (props) => {
   const preview_color = custom_color_selected ? 'custom' : data.current_color;
 
   return (
-    <Window width={550} height={400}>
+    <Window width={525} height={400}>
       <Window.Content>
-        <Section title="Decal Color">
-          {data.color_list.map((color) => {
-            return (
-              <Button
-                key={color.color}
-                selected={color.color === data.current_color}
-                onClick={() =>
-                  act('select color', {
-                    color: color.color,
-                  })
-                }
-              >
-                <ColorBox color={filterBoxColor(color.color)} mr={0.5} />
-                {color.name}
-              </Button>
-            );
-          })}
-          {supports_custom_color && (
-            <Button
-              selected={custom_color_selected}
-              onClick={() => act('pick custom color')}
-            >
-              <ColorBox color={data.current_custom_color} mr={0.5} />
-              Custom
-            </Button>
-          )}
-        </Section>
-        <Section title="Decal Style">
-          <Flex direction="row" wrap="nowrap" align="fill" justify="fill">
-            {data.decal_list.map((decal) => {
-              const nondirectional = data.nondirectional_decals.includes(
-                decal.decal,
-              );
-
-              return nondirectional ? (
-                // Tallll button for nondirectional
-                <IconButton
-                  key={decal.decal}
-                  decal={decal.decal}
-                  dir={2}
-                  color={preview_color}
-                  label={decal.name}
-                  selected={decal.decal === data.current_decal}
-                />
-              ) : (
-                // 4 buttons for directional
-                <Flex
-                  key={decal.decal}
-                  direction="column"
-                  wrap="nowrap"
-                  align="fill"
-                  justify="fill"
+        <Stack fill vertical>
+          <Stack.Item>
+            <Section title="Decal Color">
+              {data.color_list.map((color) => {
+                return (
+                  <Button
+                    key={color.color}
+                    selected={color.color === data.current_color}
+                    onClick={() =>
+                      act('select color', {
+                        color: color.color,
+                      })
+                    }
+                  >
+                    <ColorBox color={filterBoxColor(color.color)} mr={0.5} />
+                    {color.name}
+                  </Button>
+                );
+              })}
+              {supports_custom_color && (
+                <Button
+                  selected={custom_color_selected}
+                  onClick={() => act('pick custom color')}
                 >
-                  {data.dir_list.map((dir) => {
-                    const selected =
-                      decal.decal === data.current_decal &&
-                      dir.dir === data.current_dir;
+                  <ColorBox color={data.current_custom_color} mr={0.5} />
+                  Custom
+                </Button>
+              )}
+            </Section>
+          </Stack.Item>
+          <Stack.Item grow style={{ overflowY: 'auto', overflowX: 'hidden' }}>
+            <Section title="Decal Style" scrollable fill>
+              <Flex direction="row" wrap="wrap" align="fill" justify="fill">
+                {data.decal_list.map((decal) => {
+                  const nondirectional = data.nondirectional_decals.includes(
+                    decal.decal,
+                  );
 
-                    return (
-                      <IconButton
-                        key={dir.dir}
-                        decal={decal.decal}
-                        dir={dir.dir}
-                        color={preview_color}
-                        label={`${dir.name} ${decal.name}`}
-                        selected={selected}
-                      />
-                    );
-                  })}
-                </Flex>
-              );
-            })}
-          </Flex>
-        </Section>
+                  return nondirectional ? (
+                    // Tallll button for nondirectional
+                    <IconButton
+                      key={decal.decal}
+                      decal={decal.decal}
+                      dir={2}
+                      color={preview_color}
+                      label={decal.name}
+                      selected={decal.decal === data.current_decal}
+                    />
+                  ) : (
+                    // 4 buttons for directional
+                    <Flex
+                      key={decal.decal}
+                      direction="column"
+                      wrap="nowrap"
+                      align="fill"
+                      justify="fill"
+                    >
+                      {data.dir_list.map((dir) => {
+                        const selected =
+                          decal.decal === data.current_decal &&
+                          dir.dir === data.current_dir;
+
+                        return (
+                          <IconButton
+                            key={dir.dir}
+                            decal={decal.decal}
+                            dir={dir.dir}
+                            color={preview_color}
+                            label={`${dir.name} ${decal.name}`}
+                            selected={selected}
+                          />
+                        );
+                      })}
+                    </Flex>
+                  );
+                })}
+              </Flex>
+            </Section>
+          </Stack.Item>
+        </Stack>
       </Window.Content>
     </Window>
   );
