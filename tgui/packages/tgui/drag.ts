84b509775a686a6cbda615a7f594ac1f43df7b90
Aylong <alexanderkitsa@gmail.com>
2025-03-27 23:53:35 +0200

    STOP IT

--- tgui/packages/tgui/drag.ts
+++ tgui/packages/tgui/drag.ts
@@ -6,7 +6,7 @@
 
 import { storage } from 'common/storage';
 import { vecAdd, vecMultiply, vecScale, vecSubtract } from 'common/vector';
-import { BooleanLike } from 'tgui-core/byond';
+import { BooleanLike } from 'tgui-core/react';
 
 import { createLogger } from './logging';
 
