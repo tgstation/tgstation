e5533d46064c108b4271e559090b104a4de08caf
Y0SH1M4S73R <legoboyo@earthlink.net>
2025-03-28 23:57:21 -0400

    Update backend.ts

--- tgui/packages/tgui/backend.ts
+++ tgui/packages/tgui/backend.ts
@@ -305,27 +305,54 @@ export const backendMiddleware = (store) => {
   };
 };
 
+const encodedLengthBinarySearch = (haystack: string[], length: number) => {
+  const haystackLength = haystack.length;
+  let high = haystackLength - 1;
+  let low = 0;
+  let mid = 0;
+  while (low < high) {
+    mid = Math.round((low + high) / 2);
+    const substringLength = encodeURIComponent(
+      haystack.slice(0, mid).join(''),
+    ).length;
+    if (substringLength === length) {
+      break;
+    }
+    if (substringLength < length) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return mid;
+};
+
 const chunkSplitter = {
   [Symbol.split]: (string: string) => {
+    const charSeq = string[Symbol.iterator]().toArray();
+    const length = charSeq.length;
     let chunks: string[] = [];
-    const uriEncoded = encodeURIComponent(string);
     let startIndex = 0;
     let endIndex = 1024;
-    while (startIndex < uriEncoded.length) {
-      const lastFoundPercent = uriEncoded.lastIndexOf('%', endIndex - 1);
-      if (lastFoundPercent > endIndex - 3) {
-        endIndex = lastFoundPercent + 3;
-      }
-      chunks.push(
-        decodeURIComponent(
-          uriEncoded.substring(
-            startIndex,
-            endIndex < uriEncoded.length ? endIndex : undefined,
-          ),
-        ),
+    while (startIndex < length) {
+      const cut = charSeq.slice(
+        startIndex,
+        endIndex < length ? endIndex : undefined,
       );
-      startIndex = endIndex;
-      endIndex += 1024;
+      const cutString = cut.join('');
+      if (encodeURIComponent(cutString).length > 1024) {
+        const splitIndex = startIndex + encodedLengthBinarySearch(cut, 1024);
+        chunks.push(
+          charSeq
+            .slice(startIndex, splitIndex < length ? splitIndex : undefined)
+            .join(''),
+        );
+        startIndex = splitIndex;
+      } else {
+        chunks.push(cutString);
+        startIndex = endIndex;
+      }
+      endIndex = startIndex + 1024;
     }
     return chunks;
   },
