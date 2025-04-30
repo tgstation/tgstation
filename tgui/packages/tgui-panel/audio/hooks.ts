7e07bb903bcd581ff125b69e1c36498357408eb9
jlsnow301 <jlsnow301@pm.me>
2025-04-29 18:26:50 -0700

    removed

--- tgui/packages/tgui-panel/audio/hooks.ts
+++ tgui/packages/tgui-panel/audio/hooks.ts
@@ -5,14 +5,12 @@ import { useAudioStore } from '../events/stores/audio';
 export function useAudio() {
   const meta = useAudioStore(useShallow((state) => state.meta));
   const playing = useAudioStore((state) => state.playing);
-  const stopMusic = useAudioStore((state) => state.stopMusic);
   const toggle = useAudioStore((state) => state.toggle);
   const visible = useAudioStore((state) => state.visible);
 
   return {
     meta,
     playing,
-    stopMusic,
     toggle,
     visible,
   };
