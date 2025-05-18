6afc4f80c8b32eca420cc7c6f357b7c899b0c2cf
Joshua Kidder <anticitizenthree@gmail.com>
2025-05-18 18:00:41 -0400

    use onEnter instead of onChange to prevent early lastname nulling

--- tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/names.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/names.tsx
@@ -154,7 +154,7 @@ export function NameInput(props: NameInputProps) {
             <Input
               autoSelect
               expensive
-              onChange={updateName}
+              onEnter={updateName}
               onEscape={() => {
                 setLastNameBeforeEdit(null);
               }}
