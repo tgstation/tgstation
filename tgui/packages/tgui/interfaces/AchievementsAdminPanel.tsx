539d1de9a659f942594e46fbf41831449d0792b6
AnturK <AnturK@users.noreply.github.com>
2025-07-27 22:37:26 +0200

    can biome just work

--- tgui/packages/tgui/interfaces/AchievementsAdminPanel.tsx
+++ tgui/packages/tgui/interfaces/AchievementsAdminPanel.tsx
@@ -1,16 +1,10 @@
+import { Button, LabeledList, NoticeBox, Section } from 'tgui-core/components';
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
-import {
-  LabeledList,
-  Button,
-  Section,
-  NoticeBox
-} from 'tgui-core/components';
 
 type Data = {
   orphaned_keys: string[];
 };
-
 
 export const AchievementsAdminPanel = (props) => {
   const { act, data } = useBackend<Data>();
@@ -20,14 +14,28 @@ export const AchievementsAdminPanel = (props) => {
       <Window.Content scrollable>
         <Section title="Orphaned achievements">
           <NoticeBox>
-            These achievements are present in the database but are missing definitions in code. Most likely these were removed and can be cleaned up safely.
-            If you're sharing the same database on multiple servers it's possible these come from a server with later version of the code than this one.
+            These achievements are present in the database but are missing
+            definitions in code. Most likely these were removed and can be
+            cleaned up safely. If you're sharing the same database on multiple
+            servers it's possible these come from a server with later version of
+            the code than this one.
           </NoticeBox>
           <LabeledList>
-            {orphaned_keys.map(key => (
-              <LabeledList.Item label="" buttons={<Button.Confirm onClick={() => act('cleanup_orphan', { 'key': key })}>Cleanup</Button.Confirm>}>
+            {orphaned_keys.map((key) => (
+              <LabeledList.Item
+                key={key}
+                label=""
+                buttons={
+                  <Button.Confirm
+                    onClick={() => act('cleanup_orphan', { key: key })}
+                  >
+                    Cleanup
+                  </Button.Confirm>
+                }
+              >
                 {key}
-              </LabeledList.Item>))}
+              </LabeledList.Item>
+            ))}
           </LabeledList>
         </Section>
       </Window.Content>
