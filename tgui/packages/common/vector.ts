42d52586f76bf090c9af2060134dc9cf3d7d9f4e
jlsnow301 <jlsnow301@pm.me>
2026-01-05 17:05:29 -0800

    refactor vectors

--- tgui/packages/common/vector.ts
+++ tgui/packages/common/vector.ts
@@ -9,47 +9,38 @@
  */
 
 import { zip } from 'es-toolkit';
-import { map, reduce } from 'es-toolkit/compat';
-
-const ADD = (a: number, b: number): number => a + b;
-const SUB = (a: number, b: number): number => a - b;
-const MUL = (a: number, b: number): number => a * b;
-const DIV = (a: number, b: number): number => a / b;
 
 export type Vector = number[];
 
-// It's really not ideal to bypass the type system and use `as Vector`
-// however, there isn't a more eloquent way to type these
+export function vecAdd(...vecs: Vector[]): Vector {
+  return zip(...vecs).map((xs) => xs.reduce((a, b) => a + b));
+}
 
-export const vecAdd = (...vecs: Vector[]): Vector => {
-  return map(zip<number>(...vecs) as Vector[], (x) => reduce(x, ADD)) as Vector;
-};
+export function vecSubtract(...vecs: Vector[]): Vector {
+  return zip(...vecs).map((xs) => xs.reduce((a, b) => a - b));
+}
 
-export const vecSubtract = (...vecs: Vector[]): Vector => {
-  return map(zip<number>(...vecs) as Vector[], (x) => reduce(x, SUB)) as Vector;
-};
+export function vecMultiply(...vecs: Vector[]): Vector {
+  return zip(...vecs).map((xs) => xs.reduce((a, b) => a * b));
+}
 
-export const vecMultiply = (...vecs: Vector[]): Vector => {
-  return map(zip<number>(...vecs) as Vector[], (x) => reduce(x, MUL)) as Vector;
-};
+export function vecDivide(...vecs: Vector[]): Vector {
+  return zip(...vecs).map((xs) => xs.reduce((a, b) => a / b));
+}
 
-export const vecDivide = (...vecs: Vector[]): Vector => {
-  return map(zip<number>(...vecs) as Vector[], (x) => reduce(x, DIV)) as Vector;
-};
+export function vecScale(vec: Vector, n: number): Vector {
+  return vec.map((x) => x * n);
+}
 
-export const vecScale = (vec: Vector, n: number): Vector => {
-  return map(vec, (x) => x * n);
-};
+export function vecInverse(vec: Vector): Vector {
+  return vec.map((x) => -x);
+}
 
-export const vecInverse = (vec: Vector): Vector => {
-  return map(vec, (x) => -x);
-};
+export function vecLength(vec: Vector): number {
+  return Math.sqrt(vec.reduce((sum, x) => sum + x * x, 0));
+}
 
-export const vecLength = (vec: Vector): number => {
-  return Math.sqrt(reduce(vecMultiply(vec, vec), ADD) as number);
-};
-
-export const vecNormalize = (vec: Vector): Vector => {
+export function vecNormalize(vec: Vector): Vector {
   const length = vecLength(vec);
-  return map(vec, (c) => c / length);
-};
+  return vec.map((c) => c / length);
+}
