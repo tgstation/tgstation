8335e8df3540629009761b8311aae4569a8760b5
Tad Hardesty <tad@platymuus.com>
2026-01-22 14:01:14 -0800

    Rewrite CreditsTool in Python (#94716)
    Follows #94254. Why have an in-repo .exe (yikes!) _and_ a Python script,
    glued together by a .bat, when we can do everything in one Python
    script?
    
    Run like:
    ```sh
    tools/bootstrap/python -m CreditsTool tgstation tgstation
    ```
    Full usage:
    ```
    usage: __main__.py [-h] [--output OUTPUT] [--remappings REMAPPINGS]
                       [--icon-size ICON_SIZE]
                       repo_owner repo [auth_token]
    ```

--- tools/CreditsTool/UpdateCreditsDMI.bat
+++ tools/CreditsTool/UpdateCreditsDMI.bat
@@ -1,8 +1,3 @@
 @echo off
-REM If you hit github's rate limit, add a 3rd parameter here that is a github personal access token
-CreditsTool.exe tgstation tgstation
-
-del "%~dp0\..\..\config\contributors.dmi"
-call "%~dp0\..\bootstrap\python" credits.py %*
-rmdir /s /q credit_pngs
+call "%~dp0\..\bootstrap\python" -m CreditsTool %*
 pause
