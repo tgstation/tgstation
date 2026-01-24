tgstation/tgstation

commit c74f104c899b026d830ffcb0120a372fc66896ef

Rewrites plasma rigging code to be much more flexible and dynamic, and cover more cases (#94861)
## About The Pull Request
Instead of being snowflake interactions on objects themselves, rigging
things with plasma or welding fuel is now handled on reagents' side via
``on_spark_act``, allowing each reagent to decide how they want to
handle exposure to electric currents or violent heating.
- Obviously plasma and welding fuel cause explosions, but former gains
additional power from electric current being passed through it (same
math as powercells), while latter doesn't explode unless in an enclosed
space, or a high enough current/temperature has been reached, and merely
creates a hotspot instead.
- Napalm and phlogiston create hotspots, or (try to) damage the holder
if they're enclosed
- Sorium, liquid dark matter, TATP, nitroglycerin and flash/smoke/sonic
powders trigger their usual detonation effects, albeit slightly weaker.
- Teslium creates a ZAP, with additional power for a BIG ZAP if it has
been triggered by electric current.
- RDX becomes spicier with temperature and current, similarly to how it
becomes more dangerous when mixed with LE or teslium.
- Gunpowder produces a delayed explosion effect (unless its hot or the
current is high enough, in which case its instant) and causes sparks as
the message claims it does (normal gunpowder explosions also do that)

Plasma and gunpowder rigged items can now be controlled using
stabilizing agent - in its presence, plasma will not explode unless the
used charge is higher than 0.2% of a standard cell's per unit of agent,
while gunpowder will instead spend 0.1 + (charge / 10% of a standard
cell's charge) units of agent to delay its detonation.

Smoke (powder) now produces a visible message and slightly damages lungs
of whoever it reacted inside of, if the container was a mob.
The numbers on plasma/welding fuel rigs should be ***roughly*** the
same. Also lighters can now be rigged in a fashion similar to welding
tools.

code/modules/power/lighting/light.dm
@@ -231,6 +231,7 @@
 		if(LIGHT_BROKEN,LIGHT_BURNED,LIGHT_EMPTY)
 			on = FALSE
 	low_power_mode = FALSE
+
 	if(on)
 		var/brightness_set = brightness
 		var/power_set = bulb_power
@@ -239,6 +240,10 @@
 			color_set = color
 		if(reagents || !is_full_charge())
 			START_PROCESSING(SSmachines, src)
+			if (reagents.spark_act(active_power_usage, TRUE) & SPARK_ACT_DESTRUCTIVE)
+				message_admins("A rigged lightbulb at [AREACOORD(src)] has exploded.")
+				qdel(src)
+				return
 		var/area/local_area = get_room_area()
 		if (flickering)
 			brightness_set = brightness * bulb_low_power_brightness_mul
@@ -274,6 +279,10 @@
 	else if(has_emergency_power(LIGHT_EMERGENCY_POWER_USE * SSMACHINES_DT) && !turned_off())
 		use_power = IDLE_POWER_USE
 		low_power_mode = TRUE
+		if (reagents?.spark_act(idle_power_usage, TRUE) & SPARK_ACT_DESTRUCTIVE)
+			message_admins("A rigged lightbulb at [AREACOORD(src)] has exploded.")
+			qdel(src)
+			return
 		START_PROCESSING(SSmachines, src)
 	else
 		use_power = IDLE_POWER_USE

_maps/map_files/wawastation/wawastation.dmm
@@ -62053,7 +62053,7 @@
 /obj/item/stock_parts/power_store/cell/bluespace{
 	pixel_x = -5;
 	pixel_y = -8;
-	rigged = 1
+	corrupted = 1
 	},
 /turf/open/misc/asteroid,
 /area/station/asteroid)

code/__DEFINES/reagents.dm
@@ -296,3 +296,17 @@
 
 /// Cooldown between patch reagent messages
 #define PATCH_MESSAGE_COOLDOWN 10 SECONDS
+
+// on_spark_act returns
+/// We caused a non-destructive reaction
+#define SPARK_ACT_NON_DESTRUCTIVE (1 << 0)
+/// We caused a destructive action or an explosion, the holder should probably delete itself
+#define SPARK_ACT_DESTRUCTIVE (1 << 1)
+/// Don't remove this reagent, even if it reacted to spark_act
+#define SPARK_ACT_KEEP_REAGENT (1 << 2)
+/// Remove all reagents even if they didn't react and stop the processing
+/// Usually means an explosion or a destroyed parent
+#define SPARK_ACT_CLEAR_ALL (1 << 3)
+
+/// Valid values to be returned from reagent holder's spark_act
+#define SPARK_ACT_RETURNS (SPARK_ACT_NON_DESTRUCTIVE | SPARK_ACT_DESTRUCTIVE | SPARK_ACT_CLEAR_ALL)

code/controllers/subsystem/explosions.dm
@@ -169,6 +169,8 @@ ADMIN_VERB(check_bomb_impacts, R_DEBUG, "Check Bomb Impact", "See what the effec
  * 5 explosion power is a (0, 1, 3) explosion.
  * 1 explosion power is a (0, 0, 1) explosion.
  *
+ * The easy formula for defaults is sqrt(power * 2) * (0.25 | 0.5 | 1)
+ *
  * Arguments:
  * * epicenter: Turf the explosion is centered at.
  * * power - Dyn explosion power. See reference above.
@@ -176,7 +178,6 @@ ADMIN_VERB(check_bomb_impacts, R_DEBUG, "Check Bomb Impact", "See what the effec
  * * flash_range: The range at which the explosion flashes people. Equal to the equivalent of the light impact range multiplied by this value.
  * * adminlog: Whether to log the explosion/report it to the administration.
  * * ignorecap: Whether to ignore the relevant bombcap. Defaults to FALSE.
- * * flame_range: The range at which the explosion should produce hotspots.
  * * silent: Whether to generate/execute sound effects.
  * * smoke: Whether to generate a smoke cloud provided the explosion is powerful enough to warrant it.
  * * explosion_cause: [Optional] The atom that caused the explosion, when different to the origin. Used for logging.
@@ -186,7 +187,7 @@ ADMIN_VERB(check_bomb_impacts, R_DEBUG, "Check Bomb Impact", "See what the effec
 		return
 	var/range = 0
 	range = round((2 * power)**GLOB.DYN_EX_SCALE)
-	explosion(epicenter, devastation_range = round(range * 0.25), heavy_impact_range = round(range * 0.5), light_impact_range = round(range), flame_range = flame_range*range, flash_range = flash_range*range, adminlog = adminlog, ignorecap = ignorecap, silent = silent, smoke = smoke, explosion_cause = explosion_cause)
+	explosion(epicenter, devastation_range = round(range * 0.25), heavy_impact_range = round(range * 0.5), light_impact_range = round(range), flame_range = isnull(flame_range) ? null : flame_range * range, flash_range = isnull(flash_range) ? null : flash_range * range, adminlog = adminlog, ignorecap = ignorecap, silent = silent, smoke = smoke, explosion_cause = explosion_cause)
 
 
 

code/game/machinery/_machinery.dm
@@ -1092,9 +1092,7 @@
 			if(istype(secondary_part, /obj/item/stock_parts/power_store/cell) && works_from_distance)
 				var/obj/item/stock_parts/power_store/cell/checked_cell = secondary_part
 				// If it's rigged or corrupted, max the charge. Then explode it.
-				if(checked_cell.rigged || checked_cell.corrupted)
-					checked_cell.charge = checked_cell.maxcharge
-					checked_cell.explode()
+				if(checked_cell.try_explode(max_charge = TRUE))
 					break
 			if(secondary_part.get_part_rating() > current_rating)
 				//store name of part incase we qdel it below

code/game/objects/effects/effect_system/effects_other.dm
@@ -88,20 +88,19 @@
 
 /datum/effect_system/reagents_explosion
 	var/amount // TNT equivalent
-	var/flashing = FALSE // does explosion creates flash effect?
-	var/flashing_factor = 0 // factor of how powerful the flash effect relatively to the explosion
+	var/flashing_factor = null // factor of how powerful the flash effect relatively to the explosion
+	var/flaming_factor = null // factor of how powerful the flame effect is relatively to explosion
 	var/explosion_message = 1 //whether we show a message to mobs.
 
-/datum/effect_system/reagents_explosion/set_up(amt, loca, flash = FALSE, flash_fact = 0, message = TRUE)
+/datum/effect_system/reagents_explosion/set_up(amt, loca, flash_fact = null, flame_fact = null, message = TRUE)
 	amount = amt
 	explosion_message = message
 	if(isturf(loca))
 		location = loca
 	else
 		location = get_turf(loca)
-
-	flashing = flash
 	flashing_factor = flash_fact
+	flaming_factor = flame_fact
 
 /// Starts the explosion. The explosion_source is as part of logging and identifying the source of the explosion for logs.
 /datum/effect_system/reagents_explosion/start(atom/explosion_source = null)
@@ -111,4 +110,4 @@
 	if(explosion_message)
 		location.visible_message(span_danger("The solution violently explodes!"), span_hear("You hear an explosion!"))
 
-	dyn_explosion(location, amount, flash_range = flashing_factor, explosion_cause = explosion_source)
+	dyn_explosion(location, amount, flash_range = flashing_factor, flame_range = flaming_factor, explosion_cause = explosion_source)

code/game/objects/items/cigarettes.dm
@@ -426,6 +426,12 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 	damtype = BURN
 	force = 4
 
+	if(reagents?.spark_act(0, FALSE, banned_reagents = /datum/reagent/flash_powder) & SPARK_ACT_DESTRUCTIVE)
+		usr?.log_message("lit a rigged cigarette", LOG_VICTIM)
+		qdel(src)
+		return
+
+	// Custom handling for the hallucination effect
 	if(reagents?.has_reagent(/datum/reagent/flash_powder))
 		if(!isliving(loc))
 			loc.visible_message(span_hear("\The [src] burns up!"))
@@ -442,18 +448,6 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 	if(reagents?.has_reagent(/datum/reagent/drug/methamphetamine))
 		reagents.flags |= NO_REACT
 
-	if(reagents?.get_reagent_amount(/datum/reagent/toxin/plasma)) // the plasma explodes when exposed to fire
-		var/datum/effect_system/reagents_explosion/e = new()
-		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/toxin/plasma) / 2.5, 1), get_turf(src), 0, 0)
-		e.start(src)
-		qdel(src)
-		return
-	if(reagents?.get_reagent_amount(/datum/reagent/fuel)) // the fuel explodes, too, but much less violently
-		var/datum/effect_system/reagents_explosion/e = new()
-		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/fuel) / 5, 1), get_turf(src), 0, 0)
-		e.start(src)
-		qdel(src)
-		return
 	// allowing reagents to react after being lit
 	update_appearance(UPDATE_ICON)
 	if(flavor_text)
@@ -1178,7 +1172,7 @@ CIGARETTE PACKETS ARE IN FANCY.DM
 
 	if(reagents.get_reagent_amount(/datum/reagent/toxin/plasma)) // the plasma explodes when exposed to fire
 		var/datum/effect_system/reagents_explosion/e = new()
-		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/toxin/plasma) / 2.5, 1), get_turf(src), 0, 0)
+		e.set_up(round(reagents.get_reagent_amount(/datum/reagent/toxin/plasma) / 2.5, 1), get_turf(src), 0)
 		e.start(src)
 		qdel(src)
 

code/game/objects/items/lighter.dm
@@ -245,6 +245,10 @@
 	if(!lit)
 		return FALSE
 
+	if (reagents.spark_act(0, TRUE, banned_reagents = /datum/reagent/fuel) & SPARK_ACT_DESTRUCTIVE)
+		qdel(src)
+		return FALSE
+
 	if(used > 0)
 		burned_fuel_for = 0
 
@@ -257,7 +261,7 @@
 
 ///Returns the amount of fuel
 /obj/item/lighter/proc/get_fuel()
-	return reagents.get_reagent_amount(/datum/reagent/fuel)
+	return reagents.get_reagent_amount(/datum/reagent/fuel) + reagents.get_reagent_amount(/datum/reagent/toxin/plasma)
 
 /obj/item/lighter/greyscale
 	name = "cheap lighter"

commit 2b02cc7099850493d77358c6a399eddf6b5effc9

Fixes lights refusing to turn back on (#94968)
## About The Pull Request

Fixes https://github.com/tgstation/tgstation/issues/94966 introduced
with https://github.com/tgstation/tgstation/pull/94861, lights runtiming
and not turning on if they don't have reagents inside.

## Why It's Good For The Game

Fixes the lights

## Changelog

:cl: LT3
fix: Fixed lights breaking forever if they lose power
/:cl:

code/modules/power/lighting/light.dm
@@ -240,7 +240,7 @@
 			color_set = color
 		if(reagents || !is_full_charge())
 			START_PROCESSING(SSmachines, src)
-			if (reagents.spark_act(active_power_usage, TRUE) & SPARK_ACT_DESTRUCTIVE)
+			if (reagents?.spark_act(active_power_usage, TRUE) & SPARK_ACT_DESTRUCTIVE)
 				message_admins("A rigged lightbulb at [AREACOORD(src)] has exploded.")
 				qdel(src)
 				return