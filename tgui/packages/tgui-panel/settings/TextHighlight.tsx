2ecaf28bf9b91c42ddab7104745241eb43b5b158
Lucy <lucy@absolucy.moe>
2026-03-30 18:45:07 -0400

    invalid regex now cause the highlight textbox to have a red border

--- tgui/packages/tgui-panel/settings/TextHighlight.tsx
+++ tgui/packages/tgui-panel/settings/TextHighlight.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import {
   Box,
   Button,
@@ -61,6 +62,22 @@ export function TextHighlightSettings(props) {
   );
 }
 
+const oneCharacterRegex = /^(\[.*\]|\\.|.)$/;
+
+function extractRegex(highlight: string): string | null {
+  if (
+    highlight.charAt(0) !== '/' ||
+    highlight.charAt(highlight.length - 1) !== '/'
+  ) {
+    return null;
+  }
+  const expr = highlight.substring(1, highlight.length - 1);
+  if (oneCharacterRegex.test(expr)) {
+    return null;
+  }
+  return expr;
+}
+
 function TextHighlightSetting(props) {
   const { id, ...rest } = props;
   const {
@@ -76,6 +93,21 @@ function TextHighlightSetting(props) {
     matchWord,
     matchCase,
   } = highlightSettingById[id];
+
+  const highlightRegex = useMemo(
+    () => extractRegex(highlightText),
+    [highlightText],
+  );
+
+  const isRegexValid = useMemo(() => {
+    if (!highlightRegex) return true;
+    try {
+      new RegExp(highlightRegex, 'g');
+      return true;
+    } catch {
+      return false;
+    }
+  }, [highlightRegex]);
 
   return (
     <Stack.Item {...rest}>
@@ -120,6 +152,7 @@ function TextHighlightSetting(props) {
             checked={matchWord}
             tooltipPosition="bottom-start"
             tooltip="If this option is selected, only exact matches (no extra letters before or after) will trigger. Not compatible with punctuation. Overriden if regex is used."
+            disabled={!!highlightRegex}
             onClick={() =>
               updateHighlight({
                 id,
@@ -165,6 +198,7 @@ function TextHighlightSetting(props) {
         height="3em"
         value={highlightText}
         placeholder="Put words to highlight here. Separate terms with commas, i.e. (term1, term2, term3)"
+        style={{ border: isRegexValid ? '' : '1px solid red' }}
         onBlur={(value) =>
           updateHighlight({
             id: id,
