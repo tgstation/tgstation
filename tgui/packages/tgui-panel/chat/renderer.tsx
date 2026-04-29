b548cb2112ba520829c82ea4be520d966c307324
Lucy <lucy@absolucy.moe>
2026-04-29 10:59:19 -0400

    makes chat highlights look WAY better

--- tgui/packages/tgui-panel/chat/renderer.tsx
+++ tgui/packages/tgui-panel/chat/renderer.tsx
@@ -48,7 +48,7 @@ export const TGUI_CHAT_ATTRIBUTES_TO_PROPS = {
 function createHighlightNode(text, color) {
   const node = document.createElement('span');
   node.className = 'Chat__highlight';
-  node.setAttribute('style', `background-color:${color}`);
+  node.setAttribute('style', `--h-color:${color}`);
   node.textContent = text;
   return node;
 }
@@ -449,6 +449,7 @@ class ChatRenderer {
               );
               if (highlighted && parser.highlightWholeMessage) {
                 node.className += ' ChatMessage--highlighted';
+                node.style.setProperty('--h-color', parser.highlightColor);
               }
             });
         }
