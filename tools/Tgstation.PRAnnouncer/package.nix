c87139f7081b127955791ac09474b589b6391546
nicoo <nicoo@realraum.at>
2026-07-23 19:21:32 +0000

    Tgstation.PRAnnouncer/package.nix: avoid import-from-derivation
    This is likely a cause of eval slowdown for `tgstation-operations`,
    and the version number has never changed anyhow.

--- tools/Tgstation.PRAnnouncer/package.nix
+++ tools/Tgstation.PRAnnouncer/package.nix
@@ -3,36 +3,9 @@
   ...
 }:
 
-let
-  inherit (pkgs) stdenv lib;
-
-  versionParse = stdenv.mkDerivation {
-    pname = "tgstation-pr-announcer-version-parse";
-    version = "1.0.0";
-
-    meta = with pkgs.lib; {
-      description = "Version parser for tgstation-pr-announcer";
-      homepage = "https://github.com/tgstation/tgstation";
-      license = licenses.agpl3Plus;
-      platforms = platforms.x86_64;
-    };
-
-    nativeBuildInputs = with pkgs; [
-      xmlstarlet
-    ];
-
-    src = ./.;
-
-    installPhase = ''
-      mkdir -p $out
-      xmlstarlet sel --template --value-of /Project/PropertyGroup/Version ./Tgstation.PRAnnouncer.csproj > $out/tgstation-pr-announcer-version.txt
-    '';
-  };
-  version = (builtins.readFile "${versionParse}/tgstation-pr-announcer-version.txt");
-in
 pkgs.buildDotnetModule  {
   pname = "tgstation-pr-announcer";
-  version = (builtins.readFile "${versionParse}/tgstation-pr-announcer-version.txt");
+  version = "1.0.0";
 
   meta = with pkgs.lib; {
     description = "Tool for forwarding GitHub webhooks for PRs to DM game servers";
