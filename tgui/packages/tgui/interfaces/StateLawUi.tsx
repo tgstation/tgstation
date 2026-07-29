3317413b297144bdd46cdca0c7bddad57ac75eb7
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-07-29 18:06:06 -0400

    type import

--- tgui/packages/tgui/interfaces/StateLawUi.tsx
+++ tgui/packages/tgui/interfaces/StateLawUi.tsx
@@ -1,5 +1,5 @@
 import { BlockQuote, Button, Flex, Section, Stack } from 'tgui-core/components';
-import { BooleanLike } from 'tgui-core/react';
+import type { BooleanLike } from 'tgui-core/react';
 
 import { useBackend } from '../backend';
 import { Window } from '../layouts';
