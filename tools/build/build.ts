45163b29b717fdae072f5bbf293e7ee5f433d579
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-07-30 01:11:58 -0400

    Refactor BT compiler to support multiple code directories (#97240)
    ## About The Pull Request
    
    Changes the behavior tree compiler to take a list of code directories
    instead of just using hard-coded `code/`, this makes it possible for
    downstreams to have modular behavior trees properly compiled
    
    ## Why It's Good For The Game
    
    Nearly all downstream forks active today have code in places other than
    `code/`, this makes the BT compiler account for that
    
    ## Changelog
    
    N/A

--- tools/build/build.ts
+++ tools/build/build.ts
@@ -160,10 +160,14 @@ export const DmMapsIncludeTarget = new Juke.Target({
 });
 
 export const BehaviorTreeCompilerTarget = new Juke.Target({
-  inputs: ['code/**/*.bt.json', 'code/__DEFINES/**/*.dm'],
+  inputs: [
+    'code/**/*.bt.json',
+    'code/__DEFINES/**/*.dm',
+    'tools/build_bt.py',
+  ],
   outputs: () => {
     return Juke.glob('code/**/*.bt.json').map((file) => {
-      const rel = file.replace(/^code\//, '').replace(/\.bt\.json$/, '');
+      const rel = file.replace(/\.bt\.json$/, '');
       return `build/behavior_trees/${rel}.bt.compiled.json`;
     });
   },
