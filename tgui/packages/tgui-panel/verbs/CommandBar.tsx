d096307f7a2d5f69b60feacb7fd292e3e9150704
harryob <55142896+harryob@users.noreply.github.com>
2026-08-08 10:16:53 +0100

    special case for typepath works

--- tgui/packages/tgui-panel/verbs/CommandBar.tsx
+++ tgui/packages/tgui-panel/verbs/CommandBar.tsx
@@ -498,7 +498,7 @@ export function CommandBar() {
           } else {
             selectVerb(verb);
           }
-        } else if (selectedVerb && hasSuggestions && !currentToken) {
+        } else if (selectedVerb && hasSuggestions && !isCurrentArgTypepath) {
           selectCurrentSuggestion();
         } else if (selectedVerb) {
           invokeVerb();
